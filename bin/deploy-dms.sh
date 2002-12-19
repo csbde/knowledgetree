a3a1c374212b2cf2d04df14ce7adf159c1dc0a52
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2002-12-19 10:26:24 +0000

    initial revisions of backup and test deploy scripts
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@34 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ bin/deploy-dms.sh
@@ -0,0 +1,13 @@
+#!/usr/local/bin/bash
+#
+# deploy-dms.sh - cvs update the current dev build and update the version/builddate
+
+wwwroot=/usr/local/www/owl/intranet
+version=/usr/local/www/owl/intranet/config/owl.php
+tmp=/tmp/owl.php
+
+cd $wwwroot
+cvs update
+# sed the version with today's date
+cat $version | sed 's#@build-date@#`date +%Y%m%d`g' > $tmp
+mv $tmp $version
