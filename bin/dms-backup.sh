86f64f18318fdd13c6023222d1c58be7df9fb34a
Michael Joseph <michael@knowledgetree.com>
2002-12-19 10:26:24 +0000

    initial revisions of backup and test deploy scripts
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@34 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ bin/dms-backup.sh
@@ -0,0 +1,42 @@
+#!/usr/local/bin/bash
+#
+# backup.sh - archives and copies project cvs root to windoze file server
+# todo: return code checking, mail status
+
+# define paths
+BACKUP_USER=guest
+BACKUP_SERVER="//$BACKUP_USER@Jam001/JAM Backups"
+LOCAL_MOUNT=/tmp/backup
+BACKUP_FROM=/usr/local/cvsroot
+BACKUP_TO=/tmp/backup/Backups\ to\ tape/
+
+# check that we're not mounted already
+umount $LOCAL_MOUNT
+
+# clear the backup mount point
+rm -rf $LOCAL_MOUNT
+mkdir -p $LOCAL_MOUNT
+
+# mount the server
+mount_smbfs "$BACKUP_SERVER" $LOCAL_MOUNT
+
+# tar onto the local mount point
+archive=mrc_dms_`date +%Y-%m-%d`.tgz
+tar czvf /tmp/$archive $BACKUP_FROM
+
+# copy to backup server
+cp /tmp/$archive "$BACKUP_TO"
+
+# check that its there
+ls -al "$BACKUP_TO"
+
+# sleep for a bit
+sleep 6 
+
+pwd
+ps auxww | grep -i smb
+
+# disconnect
+umount $LOCAL_MOUNT
+
+exit
