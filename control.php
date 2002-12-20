afc703b0aff8495c0e0c77b80de8dbd9c94db558
Michael Joseph <michael@knowledgetree.com>
2002-12-20 11:32:21 +0000

    initial revisions of new authentication and controller implementation
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@38 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ control.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * control.php -- Controller page
+ *  
+ * This page controls the web application by responding to a set of
+ * defined actions.  The controller performs session handling, page-level
+ * authentication and forwards the request to the appropriate handling
+ * page.  
+ *
+  *
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * @version $Id$
+ * @Copyright (c) 1999-2002 The Owl Project Team
+ * @author michael
+ * @package dmsWebApplication
+ */
+
+// main library routines and defaults
+require_once("./config/owl.php");
+require_once("./lib/owl.lib.php");
+require_once("./config/html.php");
+require_once("./lib/Authenticator.inc");
+require_once("./lib/control.inc");
+require_once("./lib/SiteMap.inc");
+
+// -------------------------------
+// page start
+// -------------------------------
+
+if (!checkSession($sessionID)) {
+    // no session, redirect to login
+    $action = "loginForm";
+}
+
+$page = $default->siteMap->getPage($action, getUserClass($userID))
+
+if (isset($page)) {
+    redirect($page);
+} else {
+    // TODO: build no permission page
+    print "you do not have access to view this page!  please go away, and come back when you do.";
+}
+
+?>
