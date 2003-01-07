f8e4ea20374230a97bb4277f132b0df06c3dbe0d
Michael Joseph <michael@knowledgetree.com>
2003-01-07 13:10:39 +0000

    initial revision
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@51 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ logout.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * logout.php -- Logout page
+ *  
+ * This page controls the web application by responding to a set of
+ * defined actions.  The controller performs session handling, page-level
+ * authentication and forwards the request to the appropriate handling
+ * page.  
+ *
+  *
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * @version $Id$
+ * @Copyright (c) 1999-2002 The Owl Project Team
+ * @author michael
+ * @package dms
+ */
+ 
+// main library routines and defaults
+require_once("./config/dmsDefaults.php");
+require_once("./lib/owl.lib.php");
+require_once("./lib/control.inc");
+require_once("./config/html.php");
+require_once("./lib/Session.inc");
+
+// destroy the session
+Session::destroy();
+// redirect to login
+redirect("control.php");
+?>
