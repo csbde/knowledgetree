21ea88a983c9e487fc1c2df06eb4c9daeb0fce84
Michael Joseph <michael@knowledgetree.com>
2003-01-07 15:33:05 +0000

    fixed session handling and request forwarding
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@54 c91229c3-7414-0410-bfa2-8a42b809f60b

--- control.php
+++ control.php
@@ -14,11 +14,10 @@
  * @version $Id$
  * @Copyright (c) 1999-2002 The Owl Project Team
  * @author michael
- * @package dmsWebApplication
+ * @package dms
  */
 
 // main library routines and defaults
-require_once("./config/owl.php");
 require_once("./config/dmsDefaults.php");
 require_once("./lib/owl.lib.php");
 require_once("./config/html.php");
@@ -30,19 +29,32 @@ require_once("./lib/SiteMap.inc");
 // page start
 // -------------------------------
 
-if (!checkSession($sessionID)) {
+if (!checkSession()) {
     // no session, redirect to login
     $action = "LOGIN_FORM";
+    // redirect to login page with redirect
+    $originalRequest = urlencode($_SERVER[REQUEST_URI]);// . $_SERVER[QUERY_STRING];
 }
 
 // retrieve the login page to redirect to
-$page = $default->siteMap->getPage($action, getUserClass($userID))
+// FIXME: defaulting to A access
+$page = $default->siteMap->getPage($action, "A");
+//$page = $default->siteMap->getPage($action, getUserClass($userID));
 
 // getPage returns false for no permisssion
 if (!$page) {
     // TODO: build no permission page
     print "you do not have access to view this page!  please go away, and come back when you do.";    
 } else {
+    //echo "about to redirect to $page<br>";
     redirect($page);
+    // FIXME: append original request if necessary
+    /*
+    if (isset($originalRequest)) {
+        redirect($page . "?fRedirect=$originalRequest");
+    } else {
+        redirect($page);        
+    }
+    */
 }
 ?>
