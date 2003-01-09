fd75b5d0618b21d56ac24f2000d34601a3e484ca
Michael Joseph <michael@knowledgetree.com>
2003-01-09 13:12:06 +0000

    initial revision
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@84 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ tests/administration/UnitManager.php
@@ -0,0 +1,44 @@
+<?php
+
+// main library routines and defaults
+require_once("../config/dmsDefaults.php");
+require_once("$default->owl_fs_root/lib/owl.lib.php");
+require_once("$default->owl_fs_root/lib/administration/UnitManager.inc");
+
+echo "<pre>";
+// unit tests for UnitManager methods
+
+$um = new UnitManager();
+/*
+$userArray = $um->listLdapUsers($userNameSearch);
+if (!'userArray') {
+    echo "ldap user lookup failed!<br>";
+} else {
+    print_r($userArray);
+}
+*/
+// do some transformation of the first entry in the array?
+// think maybe just set username = uid
+
+// setup user details array
+$unitID = 1;
+//($userDetails['username'], $userDetails['name'], '', $userDetails['email'], $userDetails['mobile'], $userDetails['ldap_dn'])";
+$userDetails = array("username" => "michael",
+                     "name" => "michael joseph",
+                     "email" => "michael@jamwarehouse.com",
+                     "mobile" => "0731418818",
+                     "ldap_dn" => "uid=michael,ou=Alcohol and Drug Abuse,ou=Environment and Development,o=Medical Research Council"
+                    );
+
+global $default;
+
+$result = $um->addUser($unitID, $userDetails);
+if (!$result) {
+    echo "add user failed!<br>";
+    echo "error message=$default->errorMessage";
+} else {
+    echo "added user successfully<br>";
+}
+
+echo "</pre>";
+?>
