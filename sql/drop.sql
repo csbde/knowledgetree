af3e36cbfa246df8f88320c03755e7f854b5ab04
Michael Joseph <michael@knowledgetree.com>
2003-01-14 14:32:38 +0000

    updated to reflect new/renamed tables
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@180 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/drop.sql
+++ sql/drop.sql
@@ -1,53 +1,28 @@
-DROP TABLE active_sessions; 
-
+DROP TABLE active_sessions;
 DROP TABLE document_fields;
-
 DROP TABLE document_fields_link;
-
-DROP TABLE document_transaction_types_lu;
-
-DROP TABLE document_transactions; 
-
+DROP TABLE document_transaction_types_lookup;
+DROP TABLE document_transactions;
 DROP TABLE document_type_fields_link;
-
-DROP TABLE document_types_lu;
-
+DROP TABLE document_types_lookup;
 DROP TABLE document_words_link;
-
-DROP TABLE documents; 
-
+DROP TABLE documents;
 DROP TABLE folders;
-
-DROP TABLE folders_user_roles_link; 
-
+DROP TABLE folders_user_roles_link;
 DROP TABLE groups_folders_approval_link;
-
 DROP TABLE groups_folders_link;
-
-DROP TABLE groups_lu;
-
-DROP TABLE groups_users_link; 
-
-DROP TABLE links; 
-
+DROP TABLE groups_lookup;
+DROP TABLE groups_users_link;
+DROP TABLE links;
 DROP TABLE mime_types;
-
-DROP TABLE organisations_lu;
-
+DROP TABLE organisations_lookup;
 DROP TABLE roles;
-
-DROP TABLE subscriptions; 
-
-DROP TABLE system_settings; 
-
-DROP TABLE units; 
-
-DROP TABLE users; 
-
-DROP TABLE web_documents; 
-
-DROP TABLE web_documents_status_lu; 
-
-DROP TABLE web_sites; 
-
-DROP TABLE words_lu;
+DROP TABLE subscriptions;
+DROP TABLE system_settings;
+DROP TABLE units;
+DROP TABLE users;
+DROP TABLE user_group_link;
+DROP TABLE web_documents;
+DROP TABLE web_documents_status_lookup;
+DROP TABLE web_sites;
+DROP TABLE words_lookup;
