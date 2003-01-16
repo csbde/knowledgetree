d434392b9d91dd0f25827e8966655a0e0c5984bb
mukhtar <mukhtar@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-16 09:13:00 +0000

    changed one of the tables -> users_groups_link
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@246 c91229c3-7414-0410-bfa2-8a42b809f60b

--- config/dmsDefaults.php
+++ config/dmsDefaults.php
@@ -94,6 +94,36 @@ $default->owl_system_settings_table = "system_settings";
 $default->owl_unit_table = "units";
 // Table with user info
 $default->owl_users_table	= "users";
+// User-unit mapping table
+$default->owl_user_unit_table = "users_unit";
+// Table with group membership for users 
+$default->owl_users_grpmem_table= "membergroup";
+/// Table with session information
+$default->owl_sessions_table = "active_sessions";
+// Table with file info
+$default->owl_files_table	= "files";
+// Table with folders info
+$default->owl_folders_table	= "folders";
+// Table with group info
+$default->owl_groups_table	= "groups_lookup";
+//Table with user group linkage
+$default->owl_users_groups_table	= "users_groups_link";
+// Table with mime info
+$default->owl_mime_table	= "mime_types";
+// Table with html attributes
+$default->owl_html_table	= "intranet.html";
+// Table with html attributes
+$default->owl_prefs_table	= "intranet.prefs";
+// Table with file data info
+$default->owl_files_data_table  = "filedata";
+//Table with document type info
+$default->owl_document_types_table = "document_types_lookup";
+//Table that links document types to document type fields
+$default->owl_document_type_fields_table = "document_type_fields";
+//Table with document type field info
+$default->owl_fields_table = "document_fields_link";
+// Table with document transactions info
+$default->owl_document_transactions_table = "document_transactions";
 // Table with web documents info for web publishing
 $default->owl_web_documents_table = "web_documents";
  // Table with web documents info for web publishing
@@ -110,8 +140,8 @@ require_once("$default->owl_fs_root/phplib/db_mysql.inc");
 // Change this to reflect the authentication method you are using
 //require_once("$default->owl_fs_root/lib/LDAPAuthenticator.inc");
 //require_once("$default->owl_fs_root/lib/Authenticator.inc");
-$default->authentication_class = "DBAuthenticator";
-require_once("$default->owl_fs_root/lib/authentication/$default->authentication_class.inc");
+//$default->authentication_class = "DBAuthenticator";
+//require_once("$default->owl_fs_root/lib/authentication/$default->authentication_class.inc");
 
 // logo file that must reside inside lang/graphics directory
 $default->logo = "kt.jpg";
