8154ec35dccf13f89cac3d7ae02aeba5ad730e8c
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-16 07:34:44 +0000

    updated comments more debug logging
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@235 c91229c3-7414-0410-bfa2-8a42b809f60b

--- control.php
+++ control.php
@@ -1,20 +1,19 @@
 <?php
 
 /**
- * control.php -- Controller page
+ * $Id$ 
  *  
- * This page controls the web application by responding to a set of
+ * Controller page -- controls the web application by responding to a set of
  * defined actions.  The controller performs session handling, page-level
  * authentication and forwards the request to the appropriate handling
  * page.  
  *
-  *
+ *
  * Licensed under the GNU GPL. For full terms see the file COPYING.
  *
- * @version $Id$
- * @Copyright (c) 1999-2002 The Owl Project Team
+ * @version $Revision$
  * @author <a href="mailto:michael@jamwarehouse.com">Michael Joseph</a>, Jam Warehouse (Pty) Ltd, South Africa
- * @package dms
+ * @package dmslib
  */
 
 // main library routines and defaults
@@ -22,7 +21,6 @@ require_once("./config/dmsDefaults.php");
 require_once("$default->owl_fs_root/lib/db.inc");
 require_once("$default->owl_fs_root/lib/dms.inc");
 require_once("$default->owl_fs_root/lib/lookup.inc");
-//require_once("$default->owl_fs_root/config/html.php");
 require_once("$default->owl_fs_root/lib/control.inc");
 require_once("$default->owl_fs_root/lib/Session.inc");
 require_once("$default->owl_fs_root/lib/SiteMap.inc");
@@ -32,14 +30,12 @@ require_once("$default->owl_fs_root/lib/SiteMap.inc");
 // -------------------------------
 
 if (checkSession()) {
-    // session check succeeds, so default action should be the dashboard 
-    // (if no action specified
+    // session check succeeds, so default action should be the dashboard if no action was specified
     if (!isset($action)) {
         $action = "dashboard";
     }
 } else {
-    // session check fails, so default action should be the login form
-    // (if no action specified)
+    // session check fails, so default action should be the login form if no action was specified
     if (!isset($action)) {
         $action = "loginForm";
     }
@@ -48,11 +44,10 @@ if (checkSession()) {
 // reset authorisation flag before checking access
 $_SESSION["authorised"] = false;
 
-$default->log->debug("control.php about to retrieve page from sitemap");
 // check whether the users group has access to the requested page
 $page = $default->siteMap->getPage($action, $_SESSION["groupID"]);
 
-$default->log->debug("control.php retrieved page=$page");
+$default->log->debug("control.php retrieved page=$page from SiteMap");
 if (!$page) {
     // this group doesn't have permission to access the page
     // or there is no page mapping for the requested action
