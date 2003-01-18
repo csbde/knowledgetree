6c028c64116a428e6f1b0242f1bd7ced9525c09b
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-18 14:11:26 +0000

    Initial revision.  Unit tests for document transaction type
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@339 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ tests/documentmanagement/documentTransactions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Unit test for class DocumentTransaction in /lib/documentmanagement/DocumentTransaction.inc
+*
+* @author Rob Cherry, Jam Warehouse (Pty) Ltd, South Africa
+* @date 18 January 2003
+*
+*/
+require_once("../../config/dmsDefaults.php");
+
+if (checkSession()) {
+	require_once("$default->owl_fs_root/lib/documentmanagement/Document.inc");
+	require_once("$default->owl_fs_root/lib/documentmanagement/DocumentTransaction.inc");
+	require_once("$default->owl_fs_root/lib/foldermanagement/FolderLib.inc");
+	require_once("$default->owl_fs_root/lib/foldermanagement/FolderManager.inc");
+
+	$oDocTransaction = & new DocumentTransaction(11, 'Test transaction', 1);
+	echo "DB create successful? " . ($oDocTransaction->create() ? "Yes" : "No");
+}
+?>
