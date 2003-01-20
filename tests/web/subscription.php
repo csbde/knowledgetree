40e8a1584ef6aefb47230fee5fa013845e4a074f
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-20 11:47:53 +0000

    Initial revision.  Unit test for Subscription class in /lib/web/Subscription.inc
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@386 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ tests/web/subscription.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* @package tests.web
+*
+* Unit tests for class Subscription found in /lib/web/Subscription.inc
+*
+* @author Rob Cherry, Jam Warehouse (Pty) Ltd, South Africa
+* @date 20 January 2003
+*/
+
+require_once("../../config/dmsDefaults.php");
+
+if (checkSession) {
+	require_once("$default->owl_fs_root/lib/web/Subscription.inc");
+	
+	$oSubscription = & new Subscription(1,1);
+	echo "Create ? " . ($oSubscription->create() ? "Yes" : "No") . "<br>";
+	$oSubscription = & new Subscription(1,1);
+	$oSubscription->create();
+	$oSubscription = & new Subscription(1,1);
+	$oSubscription->create();
+	$oSubscription = & new Subscription(2,1);
+	$oSubscription->create();
+	$oSubscription = & new Subscription(1,1);
+	$oSubscription->create();
+	echo "Update ? " . ($oSubscription->update() ? "Yes" : "No") . "<br>";
+	echo "Delete ? " . ($oSubscription->delete() ? "Yes" : "No") . "<br>";
+	$oNewSubscription = Subscription::get(1);
+	echo "Get ? <pre>" . print_r($oNewSubscription) . "</pre>";
+	$oNewSubscription = Subscription::getList();
+	echo "GetList ? <pre>" . print_r($oNewSubscription) . "</pre>";
+	$oNewSubscription = Subscription::getList("WHERE user_id = 2");
+	echo "GetList ? <pre>" . print_r($oNewSubscription) . "</pre>";
+	
+}
+?>
