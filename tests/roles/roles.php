39861e49668de4a09ffb9f5dc1c2e381d5e53d2c
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-20 11:47:07 +0000

    Initial revision.  Unit test for Role class in /lib/roles/Role.inc
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@385 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ tests/roles/roles.php
@@ -0,0 +1,37 @@
+<?php
+/**
+* @package tests.roles
+*
+* Unit tests for Role class found in /lib/roles/Role.inc
+*
+* @author Rob Cherry, Jam Warehouse (Pty) Ltd, South Africa
+* @date 20 January 2003
+*/
+
+require_once("../../config/dmsDefaults.php");
+
+if (checkSession) {
+	require_once("$default->owl_fs_root/lib/roles/Role.inc");
+	
+	$oRole = & new Role("test role",true, true);
+	echo "Create ? " . ($oRole->create() ? "Yes" : "No") . "<br>";
+	$oRole = & new Role("test role",true, false);
+	$oRole->create();
+	$oRole = & new Role("test role",false, true);
+	$oRole->create();
+	$oRole = & new Role("test role",false, true);
+	$oRole->create();
+	$oRole = & new Role("test role",true, true);
+	$oRole->create();
+	echo "Update ? " . ($oRole->update() ? "Yes" : "No") . "<br>";
+	echo "Delete ? " . ($oRole->delete() ? "Yes" : "No") . "<br>";
+	$oNewRole = Role::get(1);
+	echo "Get ? <pre>" . print_r($oNewRole) . "</pre>";
+	$oNewRole = Role::getList();
+	echo "GetList ? <pre>" . print_r($oNewRole) . "</pre>";
+	$oNewRole = Role::getList("WHERE can_read = 1");
+	echo "GetList ? <pre>" . print_r($oNewRole) . "</pre>";
+	
+}
+
+?>
