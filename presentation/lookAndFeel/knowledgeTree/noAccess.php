2829233f8a85655f8a7e032ac0b5040d9faf6249
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-21 08:53:34 +0000

    Initial revision.  Default noAccess page to which a user is redirected if he/she does not have permission to view a page
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@419 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/noAccess.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* No access page
+*
+*
+*
+*/
+
+echo "<center><b>GO AWAY you bloody idiot.  You do not have rights to access this page.<br>  Your harddrive is now being formated<br>Have a nice day :-)</b></center>";
+?>
