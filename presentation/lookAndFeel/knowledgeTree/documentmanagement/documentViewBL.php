e414ebe915259d2859076c73cfe281432a81674d
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-22 11:04:34 +0000

    Initial revision.  Document view page
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@451 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/documentViewBL.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* documentViewUI.php
+* Contains the business logic required to build the document view page.
+* Will use documentViewUI.php for HTML
+*
+* Variables expected:
+*			o $fDocumentID		Primary key of document to view
+*
+*
+* @author Rob Cherry, Jam Warehouse (Pty) Ltd, South Africa
+* @date 21 January 2003
+* @package presentation.lookAndFeel.knowledgeTree.documentManager
+*/
+
+
+require_once("../../../../config/dmsDefaults.php");
+require_once("$default->owl_fs_root/lib/security/permission.inc");
+require_once("$default->owl_fs_root/lib/documentmanagement/Document.inc");
+require_once("$default->owl_fs_root/lib/foldermanagement/Folder.inc");
+require_once("$default->owl_fs_root/lib/visualpatterns/PatternListFromQuery.inc");
+require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/documentmanagement/documentViewUI.php");
+
+//if (checkSession()) {
+	$oDocument = & Document::get(12);	
+	renderDocumentMetaData($oDocument);
+/*} else {
+	echo "You do not have permission for this page";
+}*/
+
+?>
