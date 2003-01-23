8dbbc980a228558a9194911e9114d11755443240
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-23 15:37:50 +0000

    Added security functionality
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@515 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/documentmanagement/viewBL.php
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/viewBL.php
@@ -18,26 +18,31 @@ require_once("../../../../config/dmsDefaults.php");
 
 if (checkSession()) {
 	
-	//MUST PUT IN DOCUMENT PERMISSION CHECK
-	
 	require_once("$default->owl_fs_root/lib/security/permission.inc");
 	require_once("$default->owl_fs_root/lib/documentmanagement/Document.inc");
 	require_once("$default->owl_fs_root/lib/foldermanagement/Folder.inc");
-	require_once("$default->owl_fs_root/lib/visualpatterns/PatternListFromQuery.inc");	
-	require_once("$default->owl_fs_root/lib/visualpatterns/PatternTableSqlQuery.inc");
-	require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");
-	require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/documentmanagement/viewUI.inc");
-	require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-	require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");
-	require_once("$default->owl_fs_root/presentation/Html.inc");
-
-	$oDocument = & Document::get(12);	
-	$oPatternCustom = & new PatternCustom();
-	$oPatternCustom->setHtml(getPage($oDocument));
-	$main->setCentralPayload($oPatternCustom);
-	$main->render();
-} else {
-	echo "You do not have permission for this page";
+	
+	if (Permission::userHasDocumentReadPermission($fDocumentID)) {
+	
+		require_once("$default->owl_fs_root/lib/security/permission.inc");
+		require_once("$default->owl_fs_root/lib/documentmanagement/Document.inc");
+		require_once("$default->owl_fs_root/lib/foldermanagement/Folder.inc");
+		require_once("$default->owl_fs_root/lib/visualpatterns/PatternListFromQuery.inc");	
+		require_once("$default->owl_fs_root/lib/visualpatterns/PatternTableSqlQuery.inc");
+		require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");
+		require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/documentmanagement/viewUI.inc");
+		require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+		require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");
+		require_once("$default->owl_fs_root/presentation/Html.inc");
+	
+		$oDocument = & Document::get($fDocumentID);	
+		$oPatternCustom = & new PatternCustom();
+		$oPatternCustom->setHtml(getPage($oDocument));
+		$main->setCentralPayload($oPatternCustom);
+		$main->render();
+	} else {
+		echo "You do not have permission to view this document";
+	}
 }
 
 ?>
