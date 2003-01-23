c7622a888b2de70ae0f584ede90df78f21107b6b
Michael Joseph <michael@knowledgetree.com>
2003-01-23 08:21:52 +0000

    updated to use webpage template
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@497 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/dashboardBL.php
+++ presentation/dashboardBL.php
@@ -3,11 +3,6 @@
 // main library routines and defaults
 require_once("../config/dmsDefaults.php");
 require_once("$default->owl_ui_directory/dashboardUI.inc");
-require_once("$default->owl_fs_root/lib/visualpatterns/PatternMainPage.inc");
-require_once("$default->owl_fs_root/lib/visualpatterns/PatternImage.inc");
-require_once("$default->owl_fs_root/lib/visualpatterns/PatternTableLinks.inc");
-require_once("$default->owl_fs_root/lib/visualpatterns/PatternTableSqlQuery.inc");
-require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");
 
 /**
  * $Id$
@@ -28,25 +23,9 @@ require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");
 // -------------------------------
 
 if (checkSession()) {
-    // create a page  
+    // include the page template (with navbar)
+    require_once("webpageTemplate.inc");
 
-    // logo
-    $img = new PatternImage("$default->owl_root_url/locale/$default->owl_lang/graphics/$default->logo");
-    $img->setImgSize(238, 178);
-    
-    // build the top menu of links
-    // TODO: this is a function of the sitemap
-    // get list of sections
-    $aTopMenuLinks = array(generateControllerUrl("dashboard"), generateControllerUrl("browse"), generateControllerUrl("subscriptions"),
-                           generateControllerUrl("search"), generateControllerUrl("administration"), generateControllerUrl("preferences"),
-                           generateControllerUrl("documentBrowserTest"), generateControllerUrl("logout"));
-    $aTopMenuText = array("Dashboard", "Browse Documents", "Subscriptions", "Advanced Search", "Administration", "Preferences", "Help", "Logout");
-    $aTopMenuImages = array("$default->owl_graphics_url/dashboard.jpg", "$default->owl_graphics_url/browse.jpg",
-                            "$default->owl_graphics_url/subscriptions.jpg", "$default->owl_graphics_url/search.jpg",
-                            "$default->owl_graphics_url/administration.jpg", "$default->owl_graphics_url/preferences.jpg", 
-                            "$default->owl_graphics_url/help.jpg", "$default->owl_graphics_url/logout.jpg");
-    
-    $oPatternTableLinks = new PatternTableLinks($aTopMenuLinks, null, 1, 8, 2, $aTopMenuImages);
     
     $sHtml = startTable("0", "100%") .
                  // pending documents
@@ -109,15 +88,8 @@ if (checkSession()) {
     $oContent = new PatternCustom();
     $oContent->setHtml($sHtml);
     
-    /* get a page */
-    $tmp = new PatternMainPage();
-    
-    /* put the page together */
-    $tmp->setNorthWestPayload($img);
-    $tmp->setNorthPayload($oPatternTableLinks);
-    $tmp->setCentralPayload($oContent);
-    $tmp->setFormAction("dashboard.php");
-    $tmp->render();
+    $main->setCentralPayload($oContent);
+    $main->render();
         
 } else {
     // redirect to no permission page
