d93eb2fb68324d2d4735c322196235c7479c9e2b
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-23 08:06:48 +0000

    Replaced documentViewUI/BL for consistent file naming
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@494 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/viewBL.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* documentViewUI.php
+* Contains the business logic required to build the document view page.
+* Will use documentViewUI.php for HTML
+*
+* Variables expected:
+*			o $fDocumentID		Primary key of document to view
+*
+*
+* @author Rob Cherry, Jam Warehouse (Pty) Ltd, South Africa
+* @date 21 January 2003
+* @package presentation.lookAndFeel.knowledgeTree.documentManager
+*/
+
+
+require_once("../../../../config/dmsDefaults.php");
+
+if (checkSession()) {
+	
+	//MUST PUT IN DOCUMENT PERMISSION CHECK
+	
+	require_once("$default->owl_fs_root/lib/security/permission.inc");
+	require_once("$default->owl_fs_root/lib/documentmanagement/Document.inc");
+	require_once("$default->owl_fs_root/lib/foldermanagement/Folder.inc");
+	require_once("$default->owl_fs_root/lib/visualpatterns/PatternListFromQuery.inc");	
+	require_once("$default->owl_fs_root/lib/visualpatterns/PatternTableSqlQuery.inc");
+	require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");
+	require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/documentmanagement/documentViewUI.php");
+	require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");
+
+	$oDocument = & Document::get(12);	
+	$oPatternCustom = & new PatternCustom();
+	$oPatternCustom->setHtml(getPage($oDocument));
+	$main->setCentralPayload($oPatternCustom);
+	$main->render();
+} else {
+	echo "You do not have permission for this page";
+}
+
+?>
