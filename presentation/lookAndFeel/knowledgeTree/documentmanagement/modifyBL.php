c7175f2a8514161513d56641217681d9b64a3441
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-24 15:09:45 +0000

    Initial revision.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@553 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/modifyBL.php
@@ -0,0 +1,77 @@
+<?php
+/**
+* Business logic data used to modify documents (will use modifyUI.inc)
+*
+* @author Rob Cherry, Jam Warehouse (Pty) Ltd, South Africa
+* @date 24 January 2003
+* @package presentation.lookAndFeel.knowledgeTree.documentmanagement
+*
+*/
+
+require_once("../../../../config/dmsDefaults.php");
+
+if (checkSession()) {
+	
+	require_once("$default->owl_fs_root/lib/security/permission.inc");
+	require_once("$default->owl_fs_root/lib/documentmanagement/Document.inc");
+	require_once("$default->owl_fs_root/lib/foldermanagement/Folder.inc");						
+	require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");
+	require_once("$default->owl_fs_root/lib/visualpatterns/PatternEditableTableSqlQuery.inc");
+	require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/documentmanagement/modifyUI.inc");
+	require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+	require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");			
+	require_once("$default->owl_fs_root/presentation/Html.inc");
+	
+	$aDocumentDataArray;
+	settype($aDocumentDataArray, "array");
+		
+	$oDocument = & Document::get($fDocumentID);
+	echo $sToRender = "<html><head></head><body><form method=\"POST\" action=\"../store.php\" >\n";
+	echo renderEditableGenericMetaData($oDocument);
+	echo "<input type=submit value=\"Submit\" /></form></body></html>";
+	/*if (isset($fDocumentID)) {	
+		require_once("$default->owl_fs_root/lib/security/permission.inc");
+		require_once("$default->owl_fs_root/lib/documentmanagement/Document.inc");
+		require_once("$default->owl_fs_root/lib/foldermanagement/Folder.inc");
+		
+		if (Permission::userHasDocumentReadPermission($fDocumentID)) {		
+			require_once("$default->owl_fs_root/lib/security/permission.inc");
+			require_once("$default->owl_fs_root/lib/documentmanagement/Document.inc");
+			require_once("$default->owl_fs_root/lib/foldermanagement/Folder.inc");						
+			require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");
+			require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/documentmanagement/modifyUI.inc");
+			require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+			require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");			
+			require_once("$default->owl_fs_root/presentation/Html.inc");
+		
+			$oDocument = & Document::get($fDocumentID);
+			
+			$aDocumentDataArray;
+			settype($aDocumentDataArray, "array");
+			
+			$oPatternCustom = & new PatternCustom();
+			$oPatternCustom->setHtml(getPage($oDocument));
+			$main->setCentralPayload($oPatternCustom);
+			$main->render();
+		} else {
+		require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");
+		require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");
+		
+		$oPatternCustom = & new PatternCustom();
+		$oPatternCustom->setHtml("<p class=\"errorText\">Either you do not have permission to view this document,<br>" .
+								"or the document you have chosen no longer exists on the file sytem.</p>\n");
+		$main->setCentralPayload($oPatternCustom);
+		$main->render();
+		}
+	} else {
+		require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");
+		require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");
+		
+		$oPatternCustom = & new PatternCustom();
+		$oPatternCustom->setHtml("<p class=\"errorText\">You have not chosen a document to view</p>\n");
+		$main->setCentralPayload($oPatternCustom);
+		$main->render();			
+	}*/
+}
+
+?>
