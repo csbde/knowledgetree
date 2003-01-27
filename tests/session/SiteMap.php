045978ac5fca82d57f8dd5c7eab4d121c6677984
Michael Joseph <michael@knowledgetree.com>
2003-01-27 10:09:59 +0000

    updated sitemap tests
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@577 c91229c3-7414-0410-bfa2-8a42b809f60b

--- tests/session/SiteMap.php
+++ tests/session/SiteMap.php
@@ -15,14 +15,62 @@ require_once("../../config/dmsDefaults.php");
 
 if (checkSession()) {
 
-        echo "<pre>";
-        echo "SiteMap::getPage(addUser) = " . $default->siteMap->getPage("addUser") . "<br>";
-        echo "SiteMap::getSectionName(" . $_SERVER['PHP_SELF'] . ") = " . $default->siteMap->getSectionName($_SERVER['PHP_SELF']) . "<br>";
-        echo "SiteMap::getSectionLinks(Administration) = ";
-              print_r($default->siteMap->getSectionLinks("Administration"));
-        echo "SiteMap::getDefaultAction(Manage Documents) = " . $default->siteMap->getDefaultAction("Manage Documents") . "<br>";
-        echo "SiteMap::getActionFromPage(/presentation/documentmanagement/browseBL.php) = " . $default->siteMap->getActionFromPage("/presentation/documentmanagement/browseBL.php") . "<br>";
-        echo "</pre>";
+    echo "<pre>";
+    
+    // array
+    echo "SiteMap:: using the array<br>";
+    
+    // get section links
+    echo "SiteMap::getSectionLinks(Administration) = ";
+          print_r($default->siteMap->getSectionLinks("Administration"));
+    echo "SiteMap::getSectionLinks(Manage Documents) = ";
+          print_r($default->siteMap->getSectionLinks("Manage Documents"));              
+    
+    // get page
+    echo "SiteMap::getPage(viewDocument) = " . $default->siteMap->getPage("viewDocument") . "<br>";
+    echo "SiteMap::getPage(unitAdministration) = " . $default->siteMap->getPage("unitAdministration") . "<br>";
+    echo "SiteMap::getPage(systemAdministration) = " . $default->siteMap->getPage("systemAdministration") . "<br>";
+    
+    // get section name
+    echo "SiteMap::getSectionName(" . $_SERVER['PHP_SELF'] . ") = " . $default->siteMap->getSectionName($_SERVER['PHP_SELF']) . "<br>";
+    echo "SiteMap::getSectionName(/presentation/lookAndFeel/knowledgeTree/documentmanagement/browseBL.php) = " . $default->siteMap->getSectionName("/presentation/lookAndFeel/knowledgeTree/documentmanagement/browseBL.php") . "<br>";
+    
+    // get default action
+    echo "SiteMap::getDefaultAction(Manage Documents) = " . $default->siteMap->getDefaultAction("Manage Documents") . "<br>";
+    echo "SiteMap::getDefaultAction(Administration) = " . $default->siteMap->getDefaultAction("Administration") . "<br>";
+    
+    // get action from page
+    echo "SiteMap::getActionFromPage(/presentation/lookAndFeel/knowledgeTree/documentmanagement/browseBL.php) = " . $default->siteMap->getActionFromPage("/presentation/lookAndFeel/knowledgeTree/documentmanagement/browseBL.php") . "<br>";
+    echo "SiteMap::getActionFromPage(/presentation/documentmanagement/moveFolder.php) = " . $default->siteMap->getActionFromPage("/presentation/documentmanagement/moveFolder.php") . "<br>";    
+
+    // DB
+    $default->siteMap->setUseDB(true);
+    echo "<br>---------------------------<br>";
+    echo "SiteMap:: using the database<br>";
+    
+    // get section links    
+    echo "SiteMap::getSectionLinks(Administration) = ";
+          print_r($default->siteMap->getSectionLinks("Administration"));
+    echo "SiteMap::getSectionLinks(Manage Documents) = ";
+          print_r($default->siteMap->getSectionLinks("Manage Documents"));              
+
+    // get page
+    echo "SiteMap::getPage(viewDocument) = " . $default->siteMap->getPage("viewDocument") . "<br>";
+    echo "SiteMap::getPage(unitAdministration) = " . $default->siteMap->getPage("unitAdministration") . "<br>";
+    echo "SiteMap::getPage(systemAdministration) = " . $default->siteMap->getPage("systemAdministration") . "<br>";
+    
+    // get section name
+    echo "SiteMap::getSectionName(" . $_SERVER['PHP_SELF'] . ") = " . $default->siteMap->getSectionName($_SERVER['PHP_SELF']) . "<br>";
+    echo "SiteMap::getSectionName(/presentation/lookAndFeel/knowledgeTree/documentmanagement/browseBL.php) = " . $default->siteMap->getSectionName("/presentation/lookAndFeel/knowledgeTree/documentmanagement/browseBL.php") . "<br>";
+
+    // get default action
+    echo "SiteMap::getDefaultAction(Manage Documents) = " . $default->siteMap->getDefaultAction("Manage Documents") . "<br>";
+    echo "SiteMap::getDefaultAction(Administration) = " . $default->siteMap->getDefaultAction("Administration") . "<br>";
+    
+    // get action from page
+    echo "SiteMap::getActionFromPage(/presentation/lookAndFeel/knowledgeTree/documentmanagement/browseBL.php) = " . $default->siteMap->getActionFromPage("/presentation/lookAndFeel/knowledgeTree/documentmanagement/browseBL.php") . "<br>";
+    echo "SiteMap::getActionFromPage(/presentation/documentmanagement/moveFolder.php) = " . $default->siteMap->getActionFromPage("/presentation/documentmanagement/moveFolder.php") . "<br>";    
+    echo "</pre>";
         
 } else {
     // FIXME: redirect to no permission page
@@ -31,4 +79,3 @@ if (checkSession()) {
 }
 
 ?>
-
