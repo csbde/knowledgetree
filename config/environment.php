77ad7239b9c1636aeddf13d9b474b63560137756
Michael Joseph <michael@knowledgetree.com>
2003-01-28 15:07:47 +0000

    updating environment with live details
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@652 c91229c3-7414-0410-bfa2-8a42b809f60b

--- config/environment.php
+++ config/environment.php
@@ -13,22 +13,26 @@
 $default->ldapServer = "192.168.1.9";
 $default->ldapRootDn = "o=Medical Research Council";
 // Database info
-$default->owl_db_user           = "rob";
-$default->owl_db_pass           = "rob";
+$default->owl_db_user           = "dms";
+$default->owl_db_pass           = "djw9281js";
 $default->owl_db_host           = "localhost";
-$default->owl_db_name           = "owl";
+$default->owl_db_name           = "dms";
+
 // email settings
 $default->owl_email_server = "mail.jamwarehouse.com";
-$default->owl_email_from = "owl@rob.jamwarehouse.com";
-$default->owl_email_fromname = "owl";
-$default->owl_root_url		= "C:/Projects/MRC/Devel/owl";
+$default->owl_email_from = "dms@jamwarehouse.com";
+$default->owl_email_fromname = "the knowledge tree";
+
+// url relative to website root
+$default->owl_root_url		= "/dms";
 // Directory where owl is located
-$default->owl_fs_root		= "C:/Projects/MRC/Devel/owl";
+$default->owl_fs_root		= "/usr/local/www/owl/dms";
 // Directory where The Documents Directory is On Disc
-$default->owl_FileDir           =  "C:/Projects/MRC/Devel/owl";
+$default->owl_FileDir           =  "/usr/local/www/owl/dms/Documents";
 
-require_once($default->owl_fs_root . "/lib/Log.inc");
+// url of graphics dir
+$default->owl_graphics_url	= $default->owl_root_url . "/graphics";
 
-$default->log = new Log($default->owl_fs_root . "/log.txt", INFO);
-
+require_once("$default->owl_fs_root/lib/Log.inc");
+$default->log = new Log($default->owl_fs_root . "/log.txt", DEBUG);
 ?>
