67787e6043ce6c2be15c26f34335743a0beea503
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-28 12:20:02 +0000

    Added download and document transaction functionality
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@636 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/documentmanagement/viewBL.php
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/viewBL.php
@@ -19,9 +19,30 @@ if (checkSession()) {
 	if (isset($fDocumentID)) {	
 		require_once("$default->owl_fs_root/lib/security/permission.inc");
 		require_once("$default->owl_fs_root/lib/documentmanagement/Document.inc");
+		require_once("$default->owl_fs_root/lib/documentmanagement/PhysicalDocumentManager.inc");
+		require_once("$default->owl_fs_root/lib/documentmanagement/DocumentTransaction.inc");
 		require_once("$default->owl_fs_root/lib/foldermanagement/Folder.inc");
 		
-		if (Permission::userHasDocumentReadPermission($fDocumentID)) {		
+		if (isset($fForDownload)) {
+			if (Permission::userHasDocumentReadPermission($fDocumentID)) {
+				$oDocumentTransaction = & new DocumentTransaction($fDocumentID, "Document downloaded", DOWNLOAD);
+				$oDocumentTransaction->create();
+				PhysicalDocumentManager::downloadPhysicalDocument($fDocumentID);
+				//redirect($_SERVER["PHP_SELF"] . "?fDocumentID=" . $fDocumentID);
+			}
+			else {
+				require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");
+				require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");
+			
+				$oPatternCustom = & new PatternCustom();
+				$oPatternCustom->setHtml("<p class=\"errorText\">Either you do not have permission to view this document,<br>" .
+									"or the document you have chosen no longer exists on the file sytem.</p>\n");
+				$main->setCentralPayload($oPatternCustom);
+				$main->render();
+			}
+		}
+		
+		if (Permission::userHasDocumentWritePermission($fDocumentID)) {		
 			require_once("$default->owl_fs_root/lib/security/permission.inc");
 			require_once("$default->owl_fs_root/lib/documentmanagement/Document.inc");
 			require_once("$default->owl_fs_root/lib/foldermanagement/Folder.inc");
@@ -35,19 +56,37 @@ if (checkSession()) {
 		
 			$oDocument = & Document::get($fDocumentID);	
 			$oPatternCustom = & new PatternCustom();
-			$oPatternCustom->setHtml(getPage($oDocument));
+			$oPatternCustom->setHtml(getEditPage($oDocument));
 			$main->setCentralPayload($oPatternCustom);
 			$main->setFormAction("$default->owl_root_url/control.php?action=modifyDocument&fDocumentID=" . $oDocument->getID());
 			$main->render();
+		} else if (Permission::userHasDocumentReadPermission($fDocumentID)) {
+			require_once("$default->owl_fs_root/lib/security/permission.inc");
+			require_once("$default->owl_fs_root/lib/documentmanagement/Document.inc");
+			require_once("$default->owl_fs_root/lib/foldermanagement/Folder.inc");
+			require_once("$default->owl_fs_root/lib/visualpatterns/PatternListFromQuery.inc");	
+			require_once("$default->owl_fs_root/lib/visualpatterns/PatternTableSqlQuery.inc");
+			require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");
+			require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/documentmanagement/viewUI.inc");
+			require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+			require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");
+			require_once("$default->owl_fs_root/presentation/Html.inc");			
+			
+			$oDocument = & Document::get($fDocumentID);	
+			$oPatternCustom = & new PatternCustom();
+			$oPatternCustom->setHtml(getEditPage($oDocument));
+			$main->setCentralPayload($oPatternCustom);
+			$main->setFormAction("$default->owl_root_url/control.php?action=modifyDocument&fDocumentID=" . $oDocument->getID());
+			$main->render();			
 		} else {
-		require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");
-		require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");
-		
-		$oPatternCustom = & new PatternCustom();
-		$oPatternCustom->setHtml("<p class=\"errorText\">Either you do not have permission to view this document,<br>" .
-								"or the document you have chosen no longer exists on the file sytem.</p>\n");
-		$main->setCentralPayload($oPatternCustom);
-		$main->render();
+			require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");
+			require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");
+			
+			$oPatternCustom = & new PatternCustom();
+			$oPatternCustom->setHtml("<p class=\"errorText\">Either you do not have permission to view this document,<br>" .
+									"or the document you have chosen no longer exists on the file sytem.</p>\n");
+			$main->setCentralPayload($oPatternCustom);
+			$main->render();
 		}
 	} else {
 		require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");
