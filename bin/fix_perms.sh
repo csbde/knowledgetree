e3f3d5a4e99b84ca9f356e9a5937340a3278ac22
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-29 13:42:30 +0000

    updates
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@674 c91229c3-7414-0410-bfa2-8a42b809f60b

--- bin/fix_perms.sh
+++ bin/fix_perms.sh
@@ -2,7 +2,6 @@
 
 wwwroot=/usr/local/www/owl/dms
 
-chown -R www:wheel $wwwroot
 touch $wwwroot/log.txt
-#chown -R www:www $wwwroot/log.txt $wwwroot/Documents
+chown -R www:wheel $wwwroot
 chmod -R 750 $wwwroot/Documents/*
