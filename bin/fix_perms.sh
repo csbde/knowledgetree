a08eeb9478dcf36b6a05dab9bfc3c62d25452fe5
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-29 13:36:36 +0000

    removed fix_perms.sh so it can be recreated with execute permission
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@671 c91229c3-7414-0410-bfa2-8a42b809f60b

--- bin/fix_perms.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-wwwroot=/usr/local/www/owl/dms
-
-chown -R www:wheel $wwwroot
-touch $wwwroot/log.txt
-#chown -R www:www $wwwroot/log.txt $wwwroot/Documents
-chmod -R 750 $wwwroot/Documents/*
