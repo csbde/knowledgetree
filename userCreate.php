7c73fce64b9256c6f5c1e5cf060ee7b9adaa4511
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-31 12:57:45 +0000

    Adds users to the db.  For testing purposes only
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@734 c91229c3-7414-0410-bfa2-8a42b809f60b

--- userCreate.php
+++ userCreate.php
@@ -1,16 +1,12 @@
 <?php
 
-$filename = "c:\script.txt";
-
-$fp = fopen ($filename, "w");
-
 require_once("/config/dmsDefaults.php");
 
 // write to file
 for ($i = 0; $i < 1000; $i++) {
 	$sql = $default->db;
 	db->query("INSERT INTO users (username, name, password, quota_max, quota_current, email, mobile, email_notification, sms_notification, ldap_dn, max_sessions, language_id) " .
-            "VALUES ('admin" . $i . "', 'Administrator', '21232f297a57a5a743894a0e4a801fc3', 0, 0, '', '', 1, 1, '', 10000, 1)");
+            "VALUES ('admin'" . $i . "', 'Administrator', '21232f297a57a5a743894a0e4a801fc3', 0, 0, '', '', 1, 1, '', 10000, 1)");
 	db->query("INSERT INTO users_groups_link (group_id, user_id) SELECT 1, ID FROM users where username = 'admin" . $i . "'");
 }
 
