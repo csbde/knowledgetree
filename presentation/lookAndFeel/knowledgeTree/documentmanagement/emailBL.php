e86e5d6ed36d21de5cb03abaf1b52e1de9d1d98c
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-01-31 11:55:05 +0000

    Fixed syntax error
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@730 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/documentmanagement/emailBL.php
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/emailBL.php
@@ -39,7 +39,7 @@ if (checkSession()) {
 						} else {
 							$sMessage = "Your colleague, " . $oUser->getName() . ", wishes you to view the document entitled '" . $oDocument->getName() . "'.\n  Click on the hyperlink below to view it";
 						}
-						$sLink = "https://" . $_SERVER["SERVER_NAME"] . $default->owl_root_url . "/control.php?action=viewDocument&fDocumentID=" . $fDocumentID;
+						$sLink = "https://" . $_SERVER["SERVER_NAME"] . $default->owl_root_url . "/presentation/lookAndFeel/knowledgeTree/documentmanagement/viewBL.php?fDocumentID=" . $fDocumentID;
 						
 						$sHyperlink = "<a href = " . $sLink .">" . $oDocument->getName() . "</a>";
 						//email the hyperlink						
