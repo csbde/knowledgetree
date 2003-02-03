9094aad7b9e57baf9df3d03bbe7575c32857ba11
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-02-03 13:43:13 +0000

    updated tests for subscription manager
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@786 c91229c3-7414-0410-bfa2-8a42b809f60b

--- tests/subscriptions/subscriptionManager.php
+++ tests/subscriptions/subscriptionManager.php
@@ -35,7 +35,51 @@ if (checkSession()) {
             echo "Failed removing document subscription(" . $_SESSION["errorMessage"] . ")</li>";
         }
     } else {
-        echo "Failed creating document subscription(" . $_SESSION["errorMessage"] . "), skipped document subscription removal</li>";
-    }    
+        echo "Failed creating document subscription(" . $_SESSION["errorMessage"] . ")</li><li>Skipped document subscription removal</li></ul>";
+    }
+    
+    echo "</ul><b>Testing subscription retrieval</b><br>";
+    // first create some subscriptions
+    $iUserID = 10;
+    echo "<ul><li>Creating folder subscriptions<ul>";
+    for ($i = 1; $i<=5; $i++) {
+        
+        if ($oSubscriptionManager->createFolderSubscription($i, $iUserID)) {
+            echo "<li>created folder subscription folderID=$i for userID=$iUserID</li>";
+        } else {
+            echo "<li>folder subscription creation failed(" . $_SESSION["errorMessage"] . "): folderID=$i for userID=$iUserID</li>";
+        }
+    }
+    echo "</li></ul>";    
+    // now try to retrieve them
+    echo "<li>folder subscriptions for userID=$iUserID:";
+    $aFolders = $oSubscriptionManager->retrieveFolderSubscriptions($iUserID);
+    echo "<pre>" . arrayToString($aFolders) . "</pre></li>";
+    
+    echo "<li>Creating document subscriptions<ul>";
+    for ($i = 6; $i>0; $i--) {
+        if ($oSubscriptionManager->createDocumentSubscription($i, $iUserID)) {
+            echo "<li>created document subscription documentID=$i for userID=$iUserID</li>";
+        } else {
+            echo "<li>folder document creation failed(" . $_SESSION["errorMessage"] . "): documentID=$i for userID=$iUserID</li>";
+        }
+    }
+    echo "</li></ul>";    
+    // now try to retrieve them
+    echo "<li>folder subscriptions for userID=$iUserID:";    
+    $aDocuments = $oSubscriptionManager->retrieveDocumentSubscriptions($iUserID);
+    echo "<pre>" . arrayToString($aDocuments) . "</pre></li>";
+
+    // now try retrieving both
+    echo "<li>all subscriptions for userID=$iUserID:";
+    $oResults = $oSubscriptionManager->retrieveSubscriptions($iUserID);
+    echo "<pre>" . arrayToString($oResults) . "</pre></li>";
+    echo "</ul>";
+
+    // cleanup silently
+    for ($i = 1; $i<=5; $i++) {       
+        $oSubscriptionManager->removeFolderSubscription($i, $iUserID);
+        $oSubscriptionManager->removeDocumentSubscription($i, $iUserID);
+    } 
 }
 ?>
