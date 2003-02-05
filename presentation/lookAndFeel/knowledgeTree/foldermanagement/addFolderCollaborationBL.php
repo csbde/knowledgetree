ea86637bc3b78d6e6aff112149d109f90ff3f50a
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-02-05 15:39:02 +0000

    Initial revisions
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@818 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/foldermanagement/addFolderCollaborationBL.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("../../../../config/dmsDefaults.php");
+
+if (checkSession()) {
+	require_once("$default->owl_fs_root/lib/visualpatterns/PatternListBox.inc");
+	require_once("$default->owl_fs_root/lib/visualpatterns/PatternCreate.inc");
+	require_once("addFolderCollaborationUI.inc");
+	require_once("$default->owl_fs_root/lib/security/permission.inc");
+	require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");
+	require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");	
+	require_once("$default->owl_fs_root/lib/foldermanagement/Folder.inc");
+	require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+	require_once("$default->owl_fs_root/presentation/Html.inc");
+			
+	$oPatternCustom = & new PatternCustom();
+	$oPatternCustom->setHtml(getPage());
+	$main->setCentralPayload($oPatternCustom);
+	$main->setFormAction("$default->owl_root_url/presentation/lookAndFeel/knowledgeTree/create.php");
+	$main->render();
+}
+?>
