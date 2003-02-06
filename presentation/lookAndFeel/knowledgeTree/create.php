9f4f3e1a4b6bc96ca9399e72ca278c4292775f82
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-02-06 10:17:08 +0000

    Initial revision.  Handles creation of new objects.  Any page that uses pattern create must POST to this page
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@834 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/create.php
@@ -0,0 +1,74 @@
+<?php
+/**
+* Page used by PatternCreate.  Creates the actual object and stores it
+*
+* @author Rob Cherry, Jam Warehouse (Pty) Ltd, South Africa
+* @date 5 February 2003
+* @package presentation.lookAndFeel.knowledgeTree
+*
+*/
+
+require_once("../../../config/dmsDefaults.php");
+
+$aKeys = array_keys($_POST);
+$aParameterValues = array();
+$sObjectName;
+$sObjectFolderName;
+//parse the information in the html page
+for ($i = 0; $i < count($aKeys); $i++) {	
+	$sRowStart = $aKeys[$i];		
+	$pos = strncasecmp("unique_start", $sRowStart, 12);
+	if ($pos == 0) {		
+		$i++;
+		//get the object to create
+		$sObjectName = $_POST[$aKeys[$i]];
+		$i++;
+		//get the object folder name
+		$sObjectFolderName = $_POST[$aKeys[$i]];		
+		
+		while ((strncasecmp("unique_end", $sRowStart, 10) != 0)  && ($i < count($aKeys))) {
+			$i++;
+			//get the paramater number
+			$iParameterNumber = $_POST[$aKeys[$i]];			
+			$i++;
+			$iType = $_POST[$aKeys[$i]];			
+			$value;
+			switch ($iType) {
+				case 1:					
+					$i++;					
+					$value = $_POST[$aKeys[$i]];
+					break;
+				case 2:			
+					//check boxes don't post back any values if they are unchecked
+					//so we have to do a special check
+					if (strpos($aKeys[$i + 1], "parnum") != 0) {
+						//if the next key is one of type parnum, then the checkbox
+						//didn't post anything back and is obviously unchecked
+						$value = 0;
+					} else {
+						//the checkbox did post back a value and was therefore checked
+						//the checkbox posts back a value of "on" so don't actually use the postback
+						//value, rather just set value to true
+						$i++;
+						$value = 1;					
+					}
+					break;
+				case 3:				
+					$i++;
+					$value = $_POST[$aKeys[$i]];
+					break;
+			}			
+			$aParameterValues[$iParameterNumber] = $value;		
+		}		
+	}
+}
+
+//include the correct file for the object
+include_once("$default->owl_fs_root/lib/$sObjectFolderName");
+
+$oObject = call_user_func("createFromArray",$aParameterValues);
+$oObject->create();
+
+
+
+?>
