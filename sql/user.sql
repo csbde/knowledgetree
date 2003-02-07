2c71e3004ffac43f35bd2b9fc1e62e35c432cec6
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-02-07 08:28:04 +0000

    added restricted login for indexing process
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@864 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/user.sql
+++ sql/user.sql
@@ -1,1 +1,7 @@
+-- default dms user
 GRANT SELECT, INSERT, UPDATE, DELETE ON dms.* TO dms@localhost IDENTIFIED BY 'djw9281js';
+-- restricted indexer user
+GRANT SELECT ON dms.documents TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT ON dms.folders TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT, INSERT, UPDATE, DELETE ON dms.document_words_link TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT, INSERT, UPDATE, DELETE ON dms.words_lookup TO indexer@localhost IDENTIFIED BY 'idx158pqg';
