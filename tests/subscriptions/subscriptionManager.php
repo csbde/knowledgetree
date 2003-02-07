12360146091af599a3df66ce0f075c99735c40d6
Michael Joseph <michael@knowledgetree.com>
2003-02-07 14:21:06 +0000

    updated tests
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@883 c91229c3-7414-0410-bfa2-8a42b809f60b

--- tests/subscriptions/subscriptionManager.php
+++ tests/subscriptions/subscriptionManager.php
@@ -8,11 +8,11 @@ if (checkSession()) {
     $iFolderID = 1;
     $iUserID = 1;
     echo "<ul><li>Testing folder subscription creation with folderID=$iFolderID, userID=$iUserID :";
-    if ($oSubscriptionManager->createFolderSubscription($iFolderID, $iUserID)) {
+    if ($oSubscriptionManager->createSubscription($iUserID, $iFolderID, SubscriptionConstants::subscriptionType("FolderSubscription"))) {
         echo "Passed creating folder subscription</li>";
 
         echo "<li>Testing folder subscription removal with folderID=$iFolderID, userID=$iUserID :";
-        if ($oSubscriptionManager->removeFolderSubscription($iFolderID, $iUserID)) {
+        if ($oSubscriptionManager->removeSubscription($iUserID, $iFolderID, SubscriptionConstants::subscriptionType("FolderSubscription"))) {
             echo "Passed removing folder subscription</li>";
         } else {
             echo "Failed removing folder subscription(" . $_SESSION["errorMessage"] . ")</li>";
@@ -22,14 +22,14 @@ if (checkSession()) {
     }
 
     echo "</ul><b>Testing document subscriptions</b><br>";
-    $iDocumentID = 2;
+    $iDocumentID = 6;
     $iUserID = 1;
     echo "<ul><li>Testing document subscription creation with documentID=$iDocumentID, userID=$iUserID :";
-    if ($oSubscriptionManager->createDocumentSubscription($iDocumentID, $iUserID)) {
+    if ($oSubscriptionManager->createSubscription($iUserID, $iDocumentID, SubscriptionConstants::subscriptionType("DocumentSubscription"))) {
         echo "Passed creating document subscription</li>";
 
         echo "<li>Testing document subscription removal with documentID=$iDocumentID, userID=$iUserID :";
-        if ($oSubscriptionManager->removeDocumentSubscription($iDocumentID, $iUserID)) {
+        if ($oSubscriptionManager->removeSubscription($iUserID, $iDocumentID, SubscriptionConstants::subscriptionType("DocumentSubscription"))) {
             echo "Passed removing document subscription</li>";
         } else {
             echo "Failed removing document subscription(" . $_SESSION["errorMessage"] . ")</li>";
@@ -42,110 +42,47 @@ if (checkSession()) {
     // first create some subscriptions
     $iUserID = 10;
     echo "<ul><li>Creating folder subscriptions<ul>";
-    for ($i = 1; $i<=5; $i++) {
+    for ($tmpFolderID = 1; $tmpFolderID<=5; $tmpFolderID++) {
 
-        if ($oSubscriptionManager->createFolderSubscription($i, $iUserID)) {
-            echo "<li>created folder subscription folderID=$i for userID=$iUserID</li>";
+        if ($oSubscriptionManager->createSubscription($iUserID, $tmpFolderID, SubscriptionConstants::subscriptionType("FolderSubscription"))) {
+            echo "<li>created folder subscription folderID=$tmpFolderID for userID=$iUserID</li>";
         } else {
-            echo "<li>folder subscription creation failed(" . $_SESSION["errorMessage"] . "): folderID=$i for userID=$iUserID</li>";
+            echo "<li>folder subscription creation failed(" . $_SESSION["errorMessage"] . "): folderID=$tmpFolderID for userID=$iUserID</li>";
         }
     }
     echo "</li></ul>";
     echo "<li>Creating document subscriptions<ul>";
-    for ($i = 5; $i>0; $i--) {
-        if ($oSubscriptionManager->createDocumentSubscription($i, $iUserID)) {
-            echo "<li>created document subscription documentID=$i for userID=$iUserID</li>";
+    for ($tmpDocumentID = 5; $tmpDocumentID>0; $tmpDocumentID--) {
+        if ($oSubscriptionManager->createSubscription($iUserID, $tmpDocumentID, SubscriptionConstants::subscriptionType("DocumentSubscription"))) {
+            echo "<li>created document subscription documentID=$tmpDocumentID for userID=$iUserID</li>";
         } else {
-            echo "<li>document subscription creation failed(" . $_SESSION["errorMessage"] . "): documentID=$i for userID=$iUserID</li>";
+            echo "<li>document subscription creation failed(" . $_SESSION["errorMessage"] . "): documentID=$tmpDocumentID for userID=$iUserID</li>";
         }
     }
     echo "</li></ul>";
 
     // now try to retrieve them
     echo "<li>folder subscriptions for userID=$iUserID:";
-    $aFolders = $oSubscriptionManager->retrieveFolderSubscriptions($iUserID);
+    $aFolders = $oSubscriptionManager->retrieveSubscriptions($iUserID, SubscriptionConstants::subscriptionType("FolderSubscription"));
     echo "<pre>" . arrayToString($aFolders) . "</pre></li>";
 
     echo "<li>document subscriptions for userID=$iUserID:";
-    $aDocuments = $oSubscriptionManager->retrieveDocumentSubscriptions($iUserID);
+    $aDocuments = $oSubscriptionManager->retrieveSubscriptions($iUserID, SubscriptionConstants::subscriptionType("DocumentSubscription"));
     echo "<pre>" . arrayToString($aDocuments) . "</pre></li>";
 
     // now try retrieving both
     echo "<li>all subscriptions for userID=$iUserID:";
-    $oResults = $oSubscriptionManager->retrieveSubscriptions($iUserID);
+    $oResults = $oSubscriptionManager->listSubscriptions($iUserID);
     echo "<pre>" . arrayToString($oResults) . "</pre></li>";
     echo "</ul>";
 
-    // test subscribers retrieval methods
-    echo "<b>Testing subscriber retrieval</b><br>";
-    // add some subscriptions
-    $iDocumentID = 4;
-    $iFolderID = 20;
-    for ($i = 5; $i>0; $i--) {
-        if ($oSubscriptionManager->createFolderSubscription($iFolderID, $i)) {
-            echo "<li>created folder subscription folderID=$iFolderID for userID=$i</li>";
-        } else {
-            echo "<li>folder creation failed(" . $_SESSION["errorMessage"] . "): folderID=$iFolderID for userID=$i</li>";
-        }
-        if ($oSubscriptionManager->createDocumentSubscription($iDocumentID, $i)) {
-            echo "<li>created document subscription documentID=$iDocumentID for userID=$i</li>";
-        } else {
-            echo "<li>document creation failed(" . $_SESSION["errorMessage"] . "): documentID=$iDocumentID for userID=$i</li>";
-        }
-    }
-
-    $aFolderSubscribers = $oSubscriptionManager->retrieveFolderSubscribers($iFolderID);
-    $aDocumentSubscribers = $oSubscriptionManager->retrieveDocumentSubscribers($iDocumentID);
-    echo "Subscribers for folderID=$iFolderID:";
-    echo "<pre>" . arrayToString($aFolderSubscribers) . "</pre>";
-    echo "Subscribers for documentID=$iDocumentID:";
-    echo "<pre>" . arrayToString($aDocumentSubscribers) . "</pre>";
-
-    echo "<pre>";
-
-    echo "<b>Testing subscription firing</b>";
-    // test subscription firing
-    require_once("../../phpSniff/phpTimer.class.php");
-    $timer = new phpTimer();
-    $timer->start('subscriptionFiring');
-
-    $timer->start('onAddFolder');
-    $oSubscriptionManager->onAddFolder($iFolderID, "newFolder-onAddFolder");
-    $timer->stop('onAddFolder');
-    echo "<ul><li>onAddFolder: " . $timer->get_current('onAddFolder') . "</li>";
-
-    $timer->start('onRemoveFolder');
-    $oSubscriptionManager->onRemoveFolder($iFolderID, "removeFolder");
-    $timer->stop('onRemoveFolder');
-    echo "<li>onRemoveFolder: " . $timer->get_current('onRemoveFolder') . "</li>";
-
-    $timer->start('onAddDocument');
-    $oSubscriptionManager->onAddDocument($iFolderID, "addDocumentName");
-    $timer->stop('onAddDocument');
-    echo "<li>onAddDocument: " . $timer->get_current('onAddDocument') . "</li>";
-
-    $timer->start('onRemoveDocument');
-    $oSubscriptionManager->onRemoveDocument($iFolderID, "removeDocumentName");
-    $timer->stop('onRemoveDocument');
-    echo "<li>onRemoveDocument: " . $timer->get_current('onRemoveDocument') . "</li>";
-
-    $timer->start('onModifyDocument');
-    $oSubscriptionManager->onModifyDocument($iDocumentID);
-    $timer->stop('onModifyDocument');
-    echo "<li>onModifyDocument: " . $timer->get_current('onModifyDocument') . "</li>";
-
-    $timer->stop('subscriptionFiring');
-
-    echo "</ul>all subscription firing: " . $timer->get_current('subscriptionFiring');
-    echo "</pre>";
-
     // cleanup silently
     for ($i = 1; $i<=5; $i++) {
-        $oSubscriptionManager->removeFolderSubscription($i, $iUserID);
-        $oSubscriptionManager->removeDocumentSubscription($i, $iUserID);
+        $oSubscriptionManager->removeSubscription($iUserID, $i, SubscriptionConstants::subscriptionType("FolderSubscription"));
+        $oSubscriptionManager->removeSubscription($iUserID, $i, SubscriptionConstants::subscriptionType("DocumentSubscription"));
 
-        $oSubscriptionManager->removeFolderSubscription($iFolderID, $i);
-        $oSubscriptionManager->removeDocumentSubscription($iDocumentID, $i);
+        $oSubscriptionManager->removeSubscription($i, $iFolderID,SubscriptionConstants::subscriptionType("FolderSubscription") );
+        $oSubscriptionManager->removeSubscription($i, $iDocumentID, SubscriptionConstants::subscriptionType("DocumentSubscription"));
     }
 }
 ?>
