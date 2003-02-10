80c9746086f1a6423249fa12bb5fa21aa2185ad1
Michael Joseph <michael@knowledgetree.com>
2003-02-10 11:45:38 +0000

    added parameter and usage
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@898 c91229c3-7414-0410-bfa2-8a42b809f60b

--- bin/fix_perms.sh
+++ bin/fix_perms.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+# displays the script usage message 
+#
+usage() {
+    echo "usage: `basename $0` user:group"
+    echo "       eg. `basename $0` www:wheel"
+    exit 1
+}
+
+# check the command line options
+if [ $# -lt 1 ]; then
+    usage
+fi
+
 wwwroot=../`dirname $0`
 
 touch $wwwroot/log.txt
-chown -R www:wheel $wwwroot
+chown -R $1 $wwwroot
 chmod -R 750 $wwwroot/Documents/*
