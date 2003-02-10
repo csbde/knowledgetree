7c4ed24185adac4b8abc9eea345b47ba09c61ec0
mukhtar <mukhtar@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-02-10 15:34:48 +0000

    no message
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@905 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/administration/groupmanagement/removeGroupFail.php
@@ -0,0 +1,47 @@
+<?php
+/**
+* Presentation information when updating froup properties fail
+*
+* @author Mukhtar Dharsey
+* @date 5 February 2003
+* @package presentation.lookAndFeel.knowledgeTree.
+*
+*/
+
+require_once("../../../../../config/dmsDefaults.php");
+
+global $default;
+	
+if(checkSession())
+{
+
+// include the page template (with navbar)
+require_once("$default->owl_fs_root/presentation/webPageTemplate.inc");  	
+
+
+$Center = "<br></br>\n" ;
+$Center .= "<TABLE BORDER=\"0\" CELLSPACING=\"2\" CELLPADDING=\"2\">\n";
+$Center .= "<tr>\n";
+$Center .= "<td>Deletion Failed!</td>\n";
+$Center .= "</tr>\n";	
+$Center .= "<tr></tr>\n";
+$Center .= "<tr><td>Please Ensure that The Group has been Removed from any Unit it belongs to</td></tr>\n";
+$Center .= "<tr></tr>\n";
+$Center .= "<tr></tr>\n";
+$Center .= "<tr>\n";
+$Center .= "<td align = right><a href=\"$default_owl_root_url/control.php?action=removeGroup\"><img src =\"$default->owl_graphics_url/widgets/back.gif\" border = \"0\" /></a></td>\n";
+$Center .= "</tr>\n";
+$Center .= "</table>\n";
+		
+
+$oPatternCustom = & new PatternCustom();
+$oPatternCustom->setHtml($Center); 
+$main->setCentralPayload($oPatternCustom);
+$main->render();
+
+
+
+
+}
+
+?>
