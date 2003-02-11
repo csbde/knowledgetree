3093f017b0e66a63ece064761f2942f217fca50e
Michael Joseph <michael@knowledgetree.com>
2003-02-11 08:29:19 +0000

    initial revision of alert viewing page (clears alert and redirects)
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@912 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/subscriptions/viewAlertBL.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once("../../../../config/dmsDefaults.php");
+require_once("$default->owl_fs_root/lib/subscriptions/SubscriptionManager.inc");
+
+/**
+ * $Id$
+ *  
+ * Clears the subscription alert, and forwards to the content that
+ * triggered the alert.
+ *
+ * Licensed under the GNU GPL. For full terms see the file DOCS/COPYING.
+ *
+ * @version $Revision$
+ * @author Michael Joseph <michael@jamwarehouse.com>, Jam Warehouse (Pty) Ltd, South Africa
+ * @package presentation.lookAndFeel.knowledgeTree.subscriptions
+ */
+
+/*
+ * Querystring variables
+ * ---------------------
+ * fSubscriptionID - the subscription to view
+ * fSubscriptionType - the subscription type (folder,document) to view
+ */
+
+// -------------------------------
+// page start
+// -------------------------------
+
+// only if we have a valid session
+if (checkSession()) {
+
+    $default->log->debug("subID=$fSubscriptionID, type=$fSubscriptionType");
+    // retrieve variables
+    if ((!$fSubscriptionID) || (!$fSubscriptionType)) {
+        require_once("../../../webpageTemplate.inc");
+        $main->setErrorMessage("You have not selected a subscription alert");
+        $oPatternCustom = & new PatternCustom();
+        $main->setCentralPayload($oPatternCustom);
+        $main->setFormAction($_SERVER["PHP_SELF"]);
+        $main->render();
+    } else {
+
+        // instantiate the subscription manager
+        $oSubscriptionManager = new SubscriptionManager();
+        // clear the subscription alert and return the url to redirect to
+        $sContentUrl = SubscriptionManager::viewSubscription($fSubscriptionID, $fSubscriptionType);
+        if ($sContentUrl) {
+            $default->log->debug("retrieved $sContentUrl from viewSubscription");
+            // now redirect
+            redirect($sContentUrl);
+        } else {
+            // viewSubscription called failed
+            require_once("../../../webpageTemplate.inc");
+            $main->setErrorMessage("This subscription alert does not exist (" . $_SESSION["errorMessage"] . ")");
+            $oPatternCustom = & new PatternCustom();
+            $main->setCentralPayload($oPatternCustom);
+            $main->setFormAction($_SERVER["PHP_SELF"]);
+            $main->render();
+        }
+    }
+} else {
+    // redirect to no permission page
+    redirect("$default->owl_ui_url/noAccess.php");
+}
+?>
