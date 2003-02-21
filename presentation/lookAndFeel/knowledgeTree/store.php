88586679105589f3fb7fa66a9c78307cee6d2315
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-02-21 14:55:03 +0000

    Fixed check box update error
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1051 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/store.php
+++ presentation/lookAndFeel/knowledgeTree/store.php
@@ -55,7 +55,7 @@ for ($i = 0; $i < count($aKeys); $i++) {
 					$i++;					
 					$aValues[$iColumnCount] = $_POST[$aKeys[$i]];					
 					break;
-				case 2:
+				case 2:					
 					//uncheck checkboxes don't generate any name/value pairs
 					//so if the next key doesn't contain the word "value" and it's type
 					//is checkbox, then we have an unchecked check box					
@@ -80,8 +80,7 @@ for ($i = 0; $i < count($aKeys); $i++) {
 			$sRowStart = $aKeys[$i];			
 			
 			$iColumnCount++;
-		}		
-		
+		}
 		
 		if ($iPrimaryKey < 0) {
 			//perform an insert
@@ -100,7 +99,7 @@ for ($i = 0; $i < count($aKeys); $i++) {
 						//text
 						$sQuery .= "'" . addslashes($aValues[$j]) . "', ";
 						break;
-					case 2:
+					case 2:						
 						$sQuery .= $aValues[$j] . ", ";
 						//boolean
 						break;
@@ -121,9 +120,9 @@ for ($i = 0; $i < count($aKeys); $i++) {
 						//text
 						$sQuery .= "'" . addslashes($aValues[count($aColumns) - 1]) . "') ";
 						break;
-					case 2:
+					case 2:						
 						//boolean
-						$sQuery .= $aValues[count($aColumns) - 1] . ") ";
+						$sQuery .= ($aValues[count($aColumns) - 1] ? 1 : 0) . ") ";
 						break;
 					case 3:					
 						//drop down list
@@ -149,14 +148,15 @@ for ($i = 0; $i < count($aKeys); $i++) {
 						$sQuery .= "'" . addslashes($aValues[$j]) . "', ";
 						break;
 					case 2:						
-						$sQuery .= ($aValues[count($aTypes) -1] ? 1 : 0) . ", ";
+						$sQuery .= ($aValues[$j] ? 1 : 0) . ", ";
 						break;
 					case 3:
 						$sQuery .= $aValues[$j] . ", ";
 						break;
 					default:
 						break;
-				}				
+				}			
+				
 			}
 			$sQuery .= $aColumns[count($aTypes) -1] . " = ";
 			switch ($aTypes[count($aTypes) -1]) {
@@ -167,7 +167,7 @@ for ($i = 0; $i < count($aKeys); $i++) {
 					case 1:
 						$sQuery .= "'" . addslashes($aValues[count($aTypes) -1]) . "' ";
 						break;
-					case 2:					
+					case 2:											
 						$sQuery .= ($aValues[count($aTypes) -1] ? 1 : 0) . " ";
 						break;
 					case 3:						
@@ -176,7 +176,7 @@ for ($i = 0; $i < count($aKeys); $i++) {
 					default:
 						break;
 				}			
-			$sQuery .= "WHERE id = $iPrimaryKey";
+			$sQuery .= "WHERE id = $iPrimaryKey";			
 			//execute the query
 			$sql = $default->db;
 			$sql->query($sQuery);
