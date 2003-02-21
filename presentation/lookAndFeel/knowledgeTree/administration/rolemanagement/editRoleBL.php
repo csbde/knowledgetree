47ef39c0df69611f67357d370153fe640e144e89
mukhtar <mukhtar@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-02-21 15:06:29 +0000

    preliminary pages for role management
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1054 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/administration/rolemanagement/editRoleBL.php
@@ -0,0 +1,79 @@
+<?php
+/**
+* BL information for adding a group
+*
+* @author Mukhtar Dharsey
+* @date 5 February 2003
+* @package presentation.lookAndFeel.knowledgeTree.
+*
+*/
+require_once("../../../../../config/dmsDefaults.php");
+
+if (checkSession()) {
+	require_once("$default->owl_fs_root/lib/visualpatterns/PatternListBox.inc");
+	require_once("$default->owl_fs_root/lib/visualpatterns/PatternEditableListFromQuery.inc");
+	require_once("editRoleUI.inc");
+	require_once("$default->owl_fs_root/lib/security/permission.inc");
+	require_once("$default->owl_fs_root/lib/roles/Role.inc");
+	require_once("$default->owl_fs_root/presentation/webpageTemplate.inc");
+	require_once("$default->owl_fs_root/lib/visualpatterns/PatternCustom.inc");	
+	require_once("$default->owl_fs_root/lib/foldermanagement/Folder.inc");
+	require_once("$default->owl_fs_root/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+	require_once("$default->owl_fs_root/presentation/Html.inc");
+	
+	
+	$oPatternCustom = & new PatternCustom();		
+	
+	// if a new group has been added
+	if (isset($fFromCreate)) {
+		
+		if($fRoleID == -1){
+			$oPatternCustom->setHtml(getAddFailPage());
+		}else{
+			$oPatternCustom->setHtml(getCreatePage($fRoleID));
+		}
+		
+		$main->setFormAction("$default->owl_root_url/presentation/lookAndFeel/knowledgeTree/store.php?fReturnURL=" . urlencode("$default->owl_root_url/control.php?action=editRoleSuccess"));		
+	}/*	
+	// coming from manual edit page	
+	} else if (isset($fForStore)) {
+		$oGroup = Group::get($fGroupID);
+		$oGroup->setName($fGroupName);
+		
+		//check if checkbox checked
+		if (isset($fGroupUnitAdmin)) {
+			$oGroup->setUnitAdmin(true);
+		} else {
+			$oGroup->setUnitAdmin(false);
+		}
+		//check if checkbox checked
+		if (isset($fGroupSysAdmin)) {
+			$oGroup->setSysAdmin(true);
+		} else {
+			$oGroup->setSysAdmin(false);
+		}
+		if ($oGroup->update()) {
+				// if successfull print out success message
+				$oPatternCustom->setHtml(getEditPageSuccess());
+				
+		} else {
+				// if fail print out fail message
+				$oPatternCustom->setHtml(getEditPageFail());
+		}
+	} else if (isset($fGroupID)){		
+		// post back on group select from manual edit page	
+		$oPatternCustom->setHtml(getEditPage($fGroupID));
+		$main->setFormAction($_SERVER["PHP_SELF"] . "?fForStore=1");
+		
+		
+	} else {
+		// if nothing happens...just reload edit page
+		$oPatternCustom->setHtml(getEditPage(null));
+		$main->setFormAction($_SERVER["PHP_SELF"]);
+			
+	}*/
+	//render the page
+	$main->setCentralPayload($oPatternCustom);
+	$main->render();	
+}
+?>
