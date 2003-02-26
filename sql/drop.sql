a894a40fa57900906b43021be9aa0351cbf2df9d
mukhtar <mukhtar@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-02-26 09:02:52 +0000

    removed document_fields_lookup table
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1117 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/drop.sql
+++ sql/drop.sql
@@ -1,7 +1,6 @@
 DROP TABLE active_sessions;
 DROP TABLE document_fields;
 DROP TABLE document_fields_link;
-DROP TABLE document_fields_lookup;
 DROP TABLE document_transaction_types_lookup;
 DROP TABLE document_transactions;
 DROP TABLE document_type_fields_link;
