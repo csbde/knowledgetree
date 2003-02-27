6b17d260abe6aa5bbeb0bb509e75141576ce3269
mukhtar <mukhtar@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-02-27 13:04:04 +0000

    added addDocField Success
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1169 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/addDocFieldSuccess.php
@@ -0,0 +1,59 @@
+<?php
+/**
+* Presentation information when adding a Org is successful
+*
+* @author Mukhtar Dharsey
+* @date 5 February 2003
+* @package presentation.lookAndFeel.knowledgeTree.
+*
+*/
+
+require_once("../../../../../config/dmsDefaults.php");
+
+global $default;
+	
+if(checkSession())
+{
+
+// include the page template (with navbar)
+require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");  	
+
+$Center = "<br></br>\n" ;
+$Center .= "<TABLE BORDER=\"0\" CELLSPACING=\"2\" CELLPADDING=\"2\">\n";
+$Center .= "<tr>\n";
+if($fDocTypeID == -1){
+	$Center .= "<td><b>Document Field addition Unsuccessful!</b></td>\n";
+	$Center .= "</tr>\n";
+	$Center .= "<tr></tr>\n";	
+	$Center .= "<tr>\n";	
+	$Center .= "<td>Document Type already exists</td>\n";
+	$Center .= "</tr>\n";	
+		
+}else{
+	$Center .= "<td><b>Document Field added Successfully!</b></td>\n";
+	$Center .= "</tr>\n";
+	
+}
+
+
+$Center .= "<tr></tr>\n";
+$Center .= "<tr></tr>\n";
+$Center .= "<tr></tr>\n";
+$Center .= "<tr></tr>\n";
+$Center .= "<tr>\n";
+$Center .= "<td align = right><a href=\"$default->rootUrl/control.php?action=addDocField\"><img src =\"$default->graphicsUrl/widgets/back.gif\" border = \"0\" /></a></td>\n";
+$Center .= "</tr>\n";
+$Center .= "</table>\n";
+		
+
+$oPatternCustom = & new PatternCustom();
+$oPatternCustom->setHtml($Center); 
+$main->setCentralPayload($oPatternCustom);
+$main->render();
+
+
+
+
+}
+
+?>
