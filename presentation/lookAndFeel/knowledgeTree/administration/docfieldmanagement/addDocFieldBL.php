7086b80faffd6daa2de3eaba5262d41d247f5f72
mukhtar <mukhtar@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-02-27 12:24:54 +0000

    added document field management
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1166 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/addDocFieldBL.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* BL information for adding a DocField
+*
+* @author Mukhtar Dharsey
+* @date 5 February 2003
+* @package presentation.lookAndFeel.knowledgeTree.
+*
+*/
+require_once("../../../../../config/dmsDefaults.php");
+
+if (checkSession()) {
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");
+	require_once("addDocFieldUI.inc");
+	//require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentField.inc");
+	require_once("$default->fileSystemRoot/lib/security/permission.inc");
+	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
+	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+	require_once("$default->fileSystemRoot/presentation/Html.inc");
+			
+	$oPatternCustom = & new PatternCustom();
+	$oPatternCustom->setHtml(getPage());
+	$main->setCentralPayload($oPatternCustom);
+	$main->setFormAction("$default->rootUrl/presentation/lookAndFeel/knowledgeTree/create.php?fRedirectURL=".urlencode("$default->rootUrl/control.php?action=editDocField&fFromCreate=1&fDocFieldID="));
+	$main->render();
+}
+?>
