2f1e1152a726991845a7ca3a836d053adfe38084
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-02-28 10:44:39 +0000

    added multiple unsubscribe functionality
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1198 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/subscriptions/manageSubscriptionsBL.php
+++ presentation/lookAndFeel/knowledgeTree/subscriptions/manageSubscriptionsBL.php
@@ -25,11 +25,56 @@ require_once("$default->fileSystemRoot/lib/subscriptions/SubscriptionManager.inc
 if (checkSession()) {
 
     require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+    require_once("$default->fileSystemRoot/lib/subscriptions/Subscription.inc");
     require_once("subscriptionUI.inc");
+    $oPatternCustom = & new PatternCustom();
     
+    if (isset($fFolderSubscriptionIDs) || isset($fDocumentSubscriptionIDs)) {
+        // we've got subscriptions to remove,
+        $aFolderSubscriptions = array();
+        $aDocumentSubscriptions = array();
+        
+        for ($i = 0; $i < count($fFolderSubscriptionIDs); $i++) {
+            $aFolderSubscriptions[] = & Subscription::get($fFolderSubscriptionIDs[$i], SubscriptionConstants::subscriptionType("FolderSubscription"));
+        }
+        for ($i = 0; $i < count($fDocumentSubscriptionIDs); $i++) {
+            $aDocumentSubscriptions[] = & Subscription::get($fDocumentSubscriptionIDs[$i], SubscriptionConstants::subscriptionType("DocumentSubscription"));
+        }        
+        
+        if (isset($fConfirmed)) {
+            // remove subscriptions
+            $oSubscriptions = array_merge($aFolderSubscriptions, $aDocumentSubscriptions);
+            
+            $sErrorMessage = "";
+            for ($i = 0; $i < count($oSubscriptions); $i++) {
+                if ($oSubscriptions[$i]->delete()) {
+                    $default->log->info("manageSubscriptionBL.php removed subscription for userID=$iUserID, subType=$iSubscriptionType, id=$iExternalID");
+                } else {
+                    // error removing subscription                    
+                    $default->log->error("manageSubscriptionBL.php error removing subscription=" . $oSubscriptions[$i]);
+                    // add to error message
+                    if (strlen($sErrorMessage) > 0) {
+                        $sErrorMessage .= ", ";
+                    }
+                    $sErrorMessage .= $oSubscriptions[$i]->getContentDisplayPath();
+                }
+            }
+            if (strlen($sErrorMessage) > 0) {
+                $oPatternCustom->setHtml(renderErrorPage("There were errors removing the following subscriptions: $sErrorMessage"));
+            } else {
+                // display the manage subscriptions page
+                $oPatternCustom->setHtml(renderManagePage());
+            }
+        } else {
+            // display confirmation page
+            $oPatternCustom->setHtml(renderMultipleRemoveConfirmationPage($aFolderSubscriptions, $aDocumentSubscriptions));
+        }
+    } else {
+        // display the manage subscriptions page
+        $oPatternCustom->setHtml(renderManagePage());
+    }
+
     require_once("../../../webpageTemplate.inc");
-    $oPatternCustom = & new PatternCustom();    
-    $oPatternCustom->setHtml(renderManageSubscriptions());            
     $main->setCentralPayload($oPatternCustom);
     $main->setFormAction($_SERVER["PHP_SELF"]);
     $main->render();
