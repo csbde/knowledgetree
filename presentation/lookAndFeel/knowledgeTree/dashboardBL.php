fa9352ea03c34fa0cdb984caec6b239416a29cba
Michael Joseph <michael@knowledgetree.com>
2003-02-28 14:29:44 +0000

    completed dashboard integration
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1218 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/dashboardBL.php
+++ presentation/lookAndFeel/knowledgeTree/dashboardBL.php
@@ -4,6 +4,7 @@
 require_once("../../../config/dmsDefaults.php");
 require_once("$default->fileSystemRoot/lib/subscriptions/SubscriptionManager.inc");
 require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+require_once("$default->fileSystemRoot/lib/links/link.inc");
 require_once("$default->uiDirectory/dashboardUI.inc");
 
 /**
@@ -24,6 +25,24 @@ require_once("$default->uiDirectory/dashboardUI.inc");
 // page start
 // -------------------------------
 
+/**
+ * Retrieves the collaboration documents that the current user has pending
+ *
+ * @param integer the user to retrieve pending collaboration documents for
+ */
+function getPendingCollaborationDocuments($iUserID) {
+    // TODO: move this to a more logical class/file
+    global $default;
+    $sQuery = "SELECT document_id FROM $default->owl_folders_user_roles_table WHERE active=1 AND user_id=" . $_SESSION["userID"];
+    $aDocumentList = array();
+    if ($default->db->query($sQuery)) {
+        while ($default->db->next_record()) {
+            $aDocumentList[] = & Document::get($default->db->f("id"));
+        }
+    }
+    return $aDocumentList;
+}
+
 if (checkSession()) {
     // include the page template (with navbar)
     require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
@@ -32,14 +51,18 @@ if (checkSession()) {
     $oContent = new PatternCustom();
     
     // retrieve collaboration pending documents for this user
-    $aPendingDocumentList = getPendingDocuments($_SESSION["userID"]);
+    $aPendingDocumentList = getPendingCollaborationDocuments($_SESSION["userID"]);
     // retrieve checked out documents for this user                         
-    $aCheckedOutDocumentList = getCheckedoutDocuments($_SESSION["userID"]);
+    $aCheckedOutDocumentList = Document::getList("checked_out_user_id=" . $_SESSION["userID"]);
+
     // retrieve subscription alerts for this user
     $aSubscriptionAlertList = SubscriptionManager::listSubscriptionAlerts($_SESSION["userID"]);
     
+    // retrieve quicklinks
+    $aQuickLinks = Link::getList();
+    
     // generate the html
-    $oContent->setHtml(renderPage($aPendingDocumentList, $aCheckedOutDocumentList, $aSubscriptionAlertList));
+    $oContent->setHtml(renderPage($aPendingDocumentList, $aCheckedOutDocumentList, $aSubscriptionAlertList, $aQuickLinks));
     
     // display
     $main->setCentralPayload($oContent);
