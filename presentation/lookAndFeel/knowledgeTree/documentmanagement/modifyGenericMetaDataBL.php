9b12336df82c3ea678769b6673857447ce3a0eda
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-02-28 14:15:53 +0000

    Initial revision.  Modify generic/specific meta data
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1210 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/modifyGenericMetaDataBL.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once("../../../../config/dmsDefaults.php");
+
+if (checkSession()) {
+	
+	require_once("$default->fileSystemRoot/lib/security/permission.inc");
+	require_once("$default->fileSystemRoot/lib/documentmanagement/Document.inc");
+	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableTableSqlQuery.inc");					
+	require_once("$default->fileSystemRoot/presentation/Html.inc");
+	require_once("modifyGenericMetaDataUI.inc");
+	
+	if (Permission::userHasDocumentWritePermission($fDocumentID)) {
+		$oDocument = Document::get($fDocumentID);
+		require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+		$oPatternCustom = & new PatternCustom();
+		$oPatternCustom->setHtml(getPage($fDocumentID, $oDocument->getDocumentTypeID()));
+		$main->setCentralPayload($oPatternCustom);			
+		$main->setFormAction("../store.php?fReturnURL=" . urlencode("$default->rootUrl/control.php?action=viewDocument&fDocumentID=$fDocumentID"));
+		$main->setHasRequiredFields(true);
+		$main->render();
+	}
+	
+}
+
+?>
---
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/modifySpecificMetaDataBL.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* Business logic to modify type specific meta data for a document
+* 
+* @author Rob Cherry, Jam Warehouse South Africa (Pty) Ltd
+* @date 28 February 2003
+* @package presentation.lookAndFeel.knowledgeTree.documentmanagement
+*/
+
+require_once("../../../../config/dmsDefaults.php");
+
+if (checkSession()) {
+	
+	require_once("$default->fileSystemRoot/lib/security/permission.inc");
+	require_once("$default->fileSystemRoot/lib/documentmanagement/Document.inc");
+	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableTableSqlQuery.inc");					
+	require_once("$default->fileSystemRoot/presentation/Html.inc");
+	require_once("modifySpecificMetaDataUI.inc");
+	
+	if (Permission::userHasDocumentWritePermission($fDocumentID)) {
+		$oDocument = Document::get($fDocumentID);
+		require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+		$oPatternCustom = & new PatternCustom();
+		$oPatternCustom->setHtml(getPage($fDocumentID, $oDocument->getDocumentTypeID()));
+		$main->setCentralPayload($oPatternCustom);			
+		$main->setFormAction("../store.php?fReturnURL=" . urlencode("$default->rootUrl/control.php?action=viewDocument&fDocumentID=$fDocumentID"));		
+		$main->render();
+	}
+	
+}
+
+?>
