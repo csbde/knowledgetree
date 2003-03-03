ef065c72619196f5d3a6b5bc5edc1a55769eb719
mukhtar <mukhtar@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-03-03 15:57:04 +0000

    metadatamanagement for document fields
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1284 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/metadatamanagement/removeMetaDataBL.php
@@ -0,0 +1,71 @@
+<?php
+/**
+* BL information for adding a DocField
+*
+* @author Mukhtar Dharsey
+* @date 5 February 2003
+* @package presentation.lookAndFeel.knowledgeTree.
+*
+*/
+require_once("../../../../../../config/dmsDefaults.php");
+
+if (checkSession()) {
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
+	require_once("removeMetaDataUI.inc");
+	require_once("$default->fileSystemRoot/lib/security/permission.inc");
+	require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentField.inc");
+	require_once("$default->fileSystemRoot/lib/documentmanagement/MetaData.inc");
+	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
+	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+	require_once("$default->fileSystemRoot/presentation/Html.inc");
+	
+	
+	$oPatternCustom = & new PatternCustom();		
+	
+	 
+	if(isset($fDocFieldID)){		
+		// post back on DocField select from manual edit page	
+		$oPatternCustom->setHtml(getSelectMetaDataPage($fDocFieldID,$fMetaDataID));
+		$main->setFormAction($_SERVER["PHP_SELF"] . "?fSelected=1");
+		
+		
+	} else {
+		// if nothing happens...just reload edit page
+		$oPatternCustom->setHtml(getSelectFieldPage(null));
+		$main->setFormAction($_SERVER["PHP_SELF"]);
+			
+	}
+	
+	if(isset($fSelected)){
+		$oPatternCustom->setHtml(getDeleteConfirmedPage($fDocFieldID,$fMetaDataID));
+		
+		$main->setFormAction($_SERVER["PHP_SELF"] . "?fForDelete=1");
+		
+	}
+	
+	
+	if(isset($fForDelete)){
+					
+			$oMetaData = new MetaData($fDocFieldID,$fMetaDataName);
+			$oMetaData->setMetaDataID($fDocFieldID,$fMetaDataName);
+			
+			if($oMetaData->delete())
+			{
+				$oPatternCustom->setHtml(getSuccessPage());
+			}else{
+				$oPatternCustom->setHtml(getFailPage());
+			}
+		
+			
+	}
+	
+	
+	
+	//render the page
+	$main->setCentralPayload($oPatternCustom);
+	$main->render();	
+}
+?>
