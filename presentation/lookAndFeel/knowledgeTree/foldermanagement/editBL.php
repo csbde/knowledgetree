ff27e9f558a976b6195fe9eb4079f04b1b4add86
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-03-03 08:56:44 +0000

    Changed handling of updating of folder
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1263 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/foldermanagement/editBL.php
+++ presentation/lookAndFeel/knowledgeTree/foldermanagement/editBL.php
@@ -27,7 +27,50 @@ if (checkSession()) {
     if (isset($fFolderID)) {
         //if the user can edit the folder
         if (Permission::userHasFolderWritePermission($fFolderID)) {
-            if (isset($fCollaborationEdit)) {
+			if (isset($fForUpdate)) {
+				//user is updating folder data
+				$oFolder = Folder::get($fFolderID);
+				$oFolder->setDescription($fDescription);
+				if (isset($fIsPublic)) {
+					$oFolder->setIsPublic(true);
+				} else {
+					$oFolder->setIsPublic(false);
+				}
+				$bSuccessfulUpdate = false;
+				if (strcmp($oFolder->getName(), $fFolderName) != 0) {					
+					//folder name has changed, update the full_path
+					$sOldName = $oFolder->getName();
+					$sOldPath = $default->documentRoot . "/" . $oFolder->getFullPath() . "/" . $oFolder->getName();
+					$oFolder->setName($fFolderName);					
+					if ($oFolder->update(true)) {
+						$bSuccessfulUpdate = true;
+						if (!PhysicalFolderManagement::renameFolder($sOldPath, $default->documentRoot . "/" . $oFolder->getFullPath() . "/" . $oFolder->getName())) {
+							//reverse the database changes if the physical rename failed
+							$oFolder->setName($sOldName);
+							$oFolder->update(true);
+							$bSuccessfulUpdate = false;
+						}						
+					}
+				} else {
+					$bSuccessfulUpdate = $oFolder->update();
+				}				
+				if ($bSuccessfulUpdate) {
+					$oPatternCustom = & new PatternCustom();
+					$oPatternCustom->setHtml(getPage($fFolderID));					
+					$main->setCentralPayload($oPatternCustom);
+					$main->setHasRequiredFields(true);
+					$main->setFormAction("../store.php?fReturnURL=" . urlencode("$default->rootUrl/control.php?action=browse&fFolderID=$fFolderID"));
+					$main->render();
+				} else {
+					$oPatternCustom = & new PatternCustom();
+					$oPatternCustom->setHtml(getPage($fFolderID));
+					$main->setErrorMessage("An error occured while updating this folder");
+					$main->setCentralPayload($oPatternCustom);
+					$main->setHasRequiredFields(true);
+					$main->setFormAction("../store.php?fReturnURL=" . urlencode("$default->rootUrl/control.php?action=browse&fFolderID=$fFolderID"));
+					$main->render();					
+				}
+			} else if (isset($fCollaborationEdit)) {
                 //user attempted to edit the folder collaboration process but could not because there is
                 //a document currently in this process
                 $oPatternCustom = & new PatternCustom();
