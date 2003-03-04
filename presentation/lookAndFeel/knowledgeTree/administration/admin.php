4912a89a0fec9f82ea458c25ba2857fb77dd9fbf
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-03-04 16:03:03 +0000

    added colour-coded heading
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1358 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/admin.php
+++ presentation/lookAndFeel/knowledgeTree/administration/admin.php
@@ -1,25 +1,27 @@
 <?php
 require_once("../../../../config/dmsDefaults.php");
-		
-	
-if(checkSession()) {
-	
 
-	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-	
-$Center = "
-	<br></br>
-	<b> Welcome to the Administration Section:</b>
-	<br></br>
-	<p>Please make a selection from the sidemenu.</ul></p>
-	";
-	
-	$oPatternCustom = & new PatternCustom();
-	$oPatternCustom->setHtml($Center); 
-	$main->setCentralPayload($oPatternCustom);
-	$main->render();
-
+function renderHeading() {
+    global $default;
+    $sColor = $default->siteMap->getSectionColour("Administration", "td");
+    $sToRender .= "<tr align=\"left\"><th class=\"sectionHeading\" bgcolor=\"$sColor\">Administration</th></tr>\n";
+    $sToRender .= "<tr/>\n";
+    $sToRender .= "<tr/>\n";
+    return $sToRender;
 }
 
-?>
+if(checkSession()) {
+	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
 
+    $sCenter  = "<table width=\"600\">\n";
+    $sCenter .= renderHeading();
+    $sCenter .=	"<tr/><tr/><tr><td><b> Welcome to the Administration Section</b></td></tr>\n";
+    $sCenter .=	"<br></br>\n";
+    $sCenter .=	"<tr><td>Please make a selection from the sidemenu.</td></tr>\n";
+
+	$oPatternCustom = & new PatternCustom();
+	$oPatternCustom->setHtml($sCenter);
+	$main->setCentralPayload($oPatternCustom);
+	$main->render();
+}
+?>
