052a36e4c67d4e2cbea7eb96f054c112071fa47b
mukhtar <mukhtar@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-03-04 10:42:32 +0000

    added edit website management
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1337 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/administration/websitemanagement/editWebsiteBL.php
@@ -0,0 +1,75 @@
+<?php
+/**
+* BL information for adding a User
+*
+* @author Mukhtar Dharsey
+* @date 5 February 2003
+* @package presentation.lookAndFeel.knowledgeTree.
+*
+*/
+require_once("../../../../../config/dmsDefaults.php");
+
+if (checkSession()) {
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
+	require_once("editWebSiteUI.inc");
+	require_once("$default->fileSystemRoot/lib/security/permission.inc");
+	require_once("$default->fileSystemRoot/lib/users/User.inc");
+	require_once("$default->fileSystemRoot/lib/web/WebSite.inc");
+	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
+	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+	require_once("$default->fileSystemRoot/presentation/Html.inc");
+	
+	
+	$oPatternCustom = & new PatternCustom();		
+	
+	 
+	if(isset($fUserID)){		
+		// post back on User select from manual edit page	
+		$oPatternCustom->setHtml(getSelectWebSitePage($fUserID,$fWebSiteID));
+		$main->setFormAction($_SERVER["PHP_SELF"] . "?fSelected=1");
+		
+		
+	} else {
+		// if nothing happens...just reload edit page
+		$oPatternCustom->setHtml(getSelectWebMasterPage(null));
+		$main->setFormAction($_SERVER["PHP_SELF"]);
+			
+	}
+	
+	if(isset($fSelected)){
+		$oPatternCustom->setHtml(getEditWebSitePage($fUserID,$fWebSiteID));
+		
+		$main->setFormAction($_SERVER["PHP_SELF"] . "?fForStore=1");
+		
+	}
+	
+	
+	if(isset($fForStore)){
+		
+		if($fWebSiteName != "")
+		{			
+			$oWebSite = new WebSite($fWebSiteName,$fWebSiteURL, $fUserID);
+			$oWebSite->setWebSiteID($fUserID,$fOldWebSiteName);
+						
+			if($oWebSite->update())
+			{
+				$oPatternCustom->setHtml(getSuccessPage());
+			}else{
+				$oPatternCustom->setHtml(getFailPage());
+			}
+		}
+		else
+		{
+			$oPatternCustom->setHtml(getTextPage());
+		}
+			
+	}
+	
+	//render the page
+	$main->setCentralPayload($oPatternCustom);
+	$main->render();	
+}
+?>
