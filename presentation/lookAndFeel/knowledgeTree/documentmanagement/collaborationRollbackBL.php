8ddbb154e5597dda38e9f3b0b5085b3471dbc0c7
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-03-04 07:22:55 +0000

    Initial revision.  Roll back a document collaboration step
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1301 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/collaborationRollbackBL.php
@@ -0,0 +1,68 @@
+<?php	
+
+/**
+* Business logic used for the rollback of a collaboration step
+* 
+* 
+*/
+
+require_once("../../../../config/dmsDefaults.php");
+
+require_once("$default->fileSystemRoot/lib/security/permission.inc");
+
+require_once("$default->fileSystemRoot/lib/email/Email.inc");
+
+require_once("$default->fileSystemRoot/lib/users/User.inc");
+
+require_once("$default->fileSystemRoot/lib/documentmanagement/PhysicalDocumentManager.inc");
+require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentTransaction.inc");
+require_once("$default->fileSystemRoot/lib/documentmanagement/Document.inc");
+
+require_once("$default->fileSystemRoot/lib/foldermanagement/FolderCollaboration.inc");
+require_once("$default->fileSystemRoot/lib/foldermanagement/FolderUserRole.inc");
+require_once("$default->fileSystemRoot/lib/roles/Role.inc");
+require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+
+require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/documentmanagement/documentUI.inc");
+require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/documentmanagement/collaborationRollbackUI.inc");
+require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+
+if (checkSession()) {
+    if (isset($fDocumentID)) {
+		if (Document::userIsPerformingCurrentCollaborationStep($fDocumentID)) {	
+			if (isset($fForStore)) {
+				//user has entered a comment
+				//create the transaction and rollback the step
+				$oDocumentTransaction = & new DocumentTransaction($fDocumentID, $fComment, COLLAB_ROLLBACK);
+				if ($oDocumentTransaction->create()) {					
+					Document::rollbackCollaborationStep($fDocumentID, $fComment);					
+					redirect("$default->rootUrl/control.php?action=viewDocument&fDocumentID=$fDocumentID");
+				} else {
+					$oDocument = Document::get($fDocumentID);
+					require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+					$oPatternCustom = & new PatternCustom();
+					$oPatternCustom->setHtml(getPage($oDocument->getFolderID(), $oDocument->getID(), $oDocument->getName()));
+					$main->setCentralPayload($oPatternCustom);
+					$main->setHasRequiredFields(true);
+					$main->setErrorMessage("An error occured while creating the document transaction");
+					$main->setFormAction($_SERVER["PHP_SELF"] . "?fDocumentID=$fDocumentID&fForStore=1");
+					$main->render();
+				}				
+			} else {
+				$oDocument = Document::get($fDocumentID);
+				require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+				$oPatternCustom = & new PatternCustom();
+				$oPatternCustom->setHtml(getPage($oDocument->getFolderID(), $oDocument->getID(), $oDocument->getName()));
+				$main->setCentralPayload($oPatternCustom);
+				$main->setHasRequiredFields(true);
+				$main->setFormAction($_SERVER["PHP_SELF"] . "?fDocumentID=$fDocumentID&fForStore=1");
+				$main->render();
+			}		
+		} else { 
+			redirect("$default->rootUrl/control.php?action=viewDocument&fDocumentID=$fDocumentID");
+		}
+	}
+}
+
+
+?>
