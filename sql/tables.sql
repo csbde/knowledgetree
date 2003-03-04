b4a551c1845195168b0fa7a14a38ecf60fb6ece6
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-03-04 07:30:56 +0000

    Added new document type lookup
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1303 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/tables.sql
+++ sql/tables.sql
@@ -662,6 +662,7 @@ INSERT INTO document_transaction_types_lookup (name) VALUES ("Move");
 INSERT INTO document_transaction_types_lookup (name) VALUES ("Download");
 INSERT INTO document_transaction_types_lookup (name) VALUES ("Check In");
 INSERT INTO document_transaction_types_lookup (name) VALUES ("Check Out");
+INSERT INTO document_transaction_types_lookup (name) VALUES ("Collaboration step rollback");
 
 -- roles
 INSERT INTO roles (name, active, can_read, can_write) VALUES ('Editor', 1, 1, 1);
