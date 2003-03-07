422a4f7f898e948c72f0fdcfe14ad666db803411
nadim <nadim@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-03-07 15:38:11 +0000

    new help entries
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1457 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/tables.sql
+++ sql/tables.sql
@@ -996,3 +996,7 @@ INSERT INTO help VALUES (54,'addWebsiteSuccess','addWebsiteHelp.html');
 INSERT INTO help VALUES (55,'editWebsite','editWebsiteHelp.html');
 INSERT INTO help VALUES (56,'removeWebSite','removeWebSiteHelp.html');
 INSERT INTO help VALUES (57,'standardSearch','standardSearchHelp.html');
+INSERT INTO help VALUES (58,'modifyDocumentTypeMetaData','modifyDocumentTypeMetaDataHelp.html');
+INSERT INTO help VALUES (59,'addDocField','addDocFieldHelp.html');
+
+
