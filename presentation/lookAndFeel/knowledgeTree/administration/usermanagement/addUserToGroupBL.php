395d15d2c77809ac48630e4476ecf04872c3dc10
Michael Joseph <michael@knowledgetree.com>
2003-03-11 15:15:20 +0000

    reformatted
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1503 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/usermanagement/addUserToGroupBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/usermanagement/addUserToGroupBL.php
@@ -10,63 +10,47 @@
 require_once("../../../../../config/dmsDefaults.php");
 
 if (checkSession()) {
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");
-	require_once("addUserToGroupUI.inc");
-	require_once("$default->fileSystemRoot/lib/groups/Group.inc");
-	require_once("$default->fileSystemRoot/lib/users/User.inc");
-	require_once("$default->fileSystemRoot/lib/groups/GroupUserLink.inc");
-	require_once("$default->fileSystemRoot/lib/security/permission.inc");
-	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
-	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-	require_once("$default->fileSystemRoot/presentation/Html.inc");
-	
-	
-	$oPatternCustom = & new PatternCustom();
-		
-	if(!isset($fUserSet)){
-		// build first page
-		
-		$oPatternCustom->setHtml(getPage(null,null));
-		$main->setFormAction($_SERVER["PHP_SELF"] . "?fUserSet=1");
-	
-	}else{
-		
-		// do a check to see both drop downs selected
-		if($fUserID == -1 Or $fGroupID ==-1){
-	
-			$oPatternCustom->setHtml(getPageNotSelected());
-			
-					
-		}else{
-				
-				$oPatternCustom->setHtml(getPage($fUserID,$fGroupID));
-				$main->setFormAction($_SERVER["PHP_SELF"] . "?fUserSet=1&fUserAssign=1");
-						
-		}
-	}
-	
-	if (isset($fUserAssign)){
-		
-		// else add to db and then goto page succes
-		$oUserGroup = new GroupUserLink($fGroupID,$fUserID);
-		
-		if($oUserGroup->create()){
-		
-			$oPatternCustom->setHtml(getPageSuccess());
-		}else{
-			$oPatternCustom->setHtml(getPageFail());
-		}
-		
-		
-		
-	}
-	
-	// render page
-	$main->setCentralPayload($oPatternCustom);
-	$main->render();
-	
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");
+    require_once("addUserToGroupUI.inc");
+    require_once("$default->fileSystemRoot/lib/groups/Group.inc");
+    require_once("$default->fileSystemRoot/lib/users/User.inc");
+    require_once("$default->fileSystemRoot/lib/groups/GroupUserLink.inc");
+    require_once("$default->fileSystemRoot/lib/security/permission.inc");
+    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+    require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+    require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+    require_once("$default->fileSystemRoot/presentation/Html.inc");
+
+
+    $oPatternCustom = & new PatternCustom();
+
+    if(!isset($fUserSet)) {
+        // build first page
+        $oPatternCustom->setHtml(getPage(null,null));
+        $main->setFormAction($_SERVER["PHP_SELF"] . "?fUserSet=1");
+    } else {
+        // do a check to see both drop downs selected
+        if($fUserID == -1 Or $fGroupID ==-1) {
+            $oPatternCustom->setHtml(getPageNotSelected());
+        } else {
+            $oPatternCustom->setHtml(getPage($fUserID,$fGroupID));
+            $main->setFormAction($_SERVER["PHP_SELF"] . "?fUserSet=1&fUserAssign=1");
+        }
+    }
+
+    if (isset($fUserAssign)) {
+        // else add to db and then goto page succes
+        $oUserGroup = new GroupUserLink($fGroupID,$fUserID);
+        if($oUserGroup->create()) {
+            $oPatternCustom->setHtml(getPageSuccess());
+        } else {
+            $oPatternCustom->setHtml(getPageFail());
+        }
+    }
+    // render page
+    $main->setCentralPayload($oPatternCustom);
+    $main->render();
 }
 ?>
--- presentation/lookAndFeel/knowledgeTree/administration/usermanagement/removeUserFromGroupBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/usermanagement/removeUserFromGroupBL.php
@@ -10,69 +10,54 @@
 require_once("../../../../../config/dmsDefaults.php");
 
 if (checkSession()) {
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");
-	require_once("removeUserFromGroupUI.inc");
-	require_once("$default->fileSystemRoot/lib/groups/Group.inc");
-	require_once("$default->fileSystemRoot/lib/users/User.inc");
-	require_once("$default->fileSystemRoot/lib/groups/GroupUserLink.inc");
-	require_once("$default->fileSystemRoot/lib/security/permission.inc");
-	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
-	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-	require_once("$default->fileSystemRoot/presentation/Html.inc");
-	
-	
-	$oPatternCustom = & new PatternCustom();
-	
-		
-	if(isset($fUserSet)){
-		
-		// do a check to see both drop downs selected
-		if($fUserID == -1){
-			$oPatternCustom->setHtml(getPageNotSelected());
-					
-		}else{ 		$faGroupID = GroupUserLink::getGroups($fUserID);	
-							
-				$oPatternCustom->setHtml(getGroupPage($fUserID,$faGroupID));
-				$main->setFormAction($_SERVER["PHP_SELF"] . "?fUserSet=1&fGroupSet=1");
-		}
-	
-	}else{
-		
-		// build first page
-		
-		$oPatternCustom->setHtml(getPage(null,null));
-		$main->setFormAction($_SERVER["PHP_SELF"] . "?fUserSet=1");
-		
-	}
-	
-	if(isset($fGroupSet))
-	{	
-		$oPatternCustom->setHtml(getDeleteConfirmedPage($fUserID,$fGroupID));
-		$main->setFormAction($_SERVER["PHP_SELF"] . "?fDeleteConfirmed=1&fGroupID=$fGroupID");
-	}
-		
-	if (isset($fDeleteConfirmed)){
-		
-		// else add to db and then goto page succes
-		$oUserGroup = new GroupUserLink($fGroupID, $fUserID);
-				
-		$oUserGroup->setUserGroupID($fGroupID,$fUserID);
-				
-		if($oUserGroup->delete()){
-			$oPatternCustom->setHtml(getPageSuccess());
-		}else{
-			$oPatternCustom->setHtml(getPageFail());
-		}
-		
-		
-	}
-	
-	// render page
-	$main->setCentralPayload($oPatternCustom);
-	$main->render();
-	
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");
+    require_once("removeUserFromGroupUI.inc");
+    require_once("$default->fileSystemRoot/lib/groups/Group.inc");
+    require_once("$default->fileSystemRoot/lib/users/User.inc");
+    require_once("$default->fileSystemRoot/lib/groups/GroupUserLink.inc");
+    require_once("$default->fileSystemRoot/lib/security/permission.inc");
+    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+    require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+    require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+    require_once("$default->fileSystemRoot/presentation/Html.inc");
+
+    $oPatternCustom = & new PatternCustom();
+
+    if(isset($fUserSet)) {
+        // do a check to see both drop downs selected
+        if($fUserID == -1) {
+            $oPatternCustom->setHtml(getPageNotSelected());
+        } else {
+            $faGroupID = GroupUserLink::getGroups($fUserID);
+            $oPatternCustom->setHtml(getGroupPage($fUserID,$faGroupID));
+            $main->setFormAction($_SERVER["PHP_SELF"] . "?fUserSet=1&fGroupSet=1");
+        }
+    } else {
+        // build first page
+        $oPatternCustom->setHtml(getPage(null,null));
+        $main->setFormAction($_SERVER["PHP_SELF"] . "?fUserSet=1");
+    }
+
+    if(isset($fGroupSet)) {
+        $oPatternCustom->setHtml(getDeleteConfirmedPage($fUserID,$fGroupID));
+        $main->setFormAction($_SERVER["PHP_SELF"] . "?fDeleteConfirmed=1&fGroupID=$fGroupID");
+    }
+
+    if (isset($fDeleteConfirmed)) {
+        // else add to db and then goto page succes
+        $oUserGroup = new GroupUserLink($fGroupID, $fUserID);
+        $oUserGroup->setUserGroupID($fGroupID,$fUserID);
+        if($oUserGroup->delete()) {
+            $oPatternCustom->setHtml(getPageSuccess());
+        } else {
+            $oPatternCustom->setHtml(getPageFail());
+        }
+    }
+
+    // render page
+    $main->setCentralPayload($oPatternCustom);
+    $main->render();
 }
 ?>
