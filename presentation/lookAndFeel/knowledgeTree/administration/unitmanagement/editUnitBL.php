22375a0278188a28c3ffcdefa5468897ca36b9d4
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-03-14 15:22:17 +0000

    updated look and feel and added logic to rename unit root folders on unit rename
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1587 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/unitmanagement/editUnitBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/unitmanagement/editUnitBL.php
@@ -10,51 +10,46 @@
 require_once("../../../../../config/dmsDefaults.php");
 
 if (checkSession()) {
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
-	require_once("editUnitUI.inc");
-	require_once("$default->fileSystemRoot/lib/security/permission.inc");
-	require_once("$default->fileSystemRoot/lib/unitmanagement/Unit.inc");
-	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
-	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-	require_once("$default->fileSystemRoot/presentation/Html.inc");
-	
-	
-	$oPatternCustom = & new PatternCustom();		
-	
-	// if a new unit has been added
-	// coming from manual edit page	
-	if (isset($fForStore)) {
-		$oUnit = Unit::get($fUnitID);
-		$oUnit->setName($fUnitName);
-		
-		if($fUnitName== "")
-		{
-			$oPatternCustom->setHtml(getEditPageFail());
-		}elseif ($oUnit->update()) {
-				// if successfull print out success message
-				$oPatternCustom->setHtml(getEditPageSuccess());
-				
-		} else {
-				// if fail print out fail message
-				$oPatternCustom->setHtml(getEditPageFail());
-		}
-	} else if (isset($fUnitID)){		
-		// post back on Unit select from manual edit page	
-		$oPatternCustom->setHtml(getEditPage($fUnitID));
-		$main->setFormAction($_SERVER["PHP_SELF"] . "?fForStore=1");
-		
-		
-	} else {
-		// if nothing happens...just reload edit page
-		$oPatternCustom->setHtml(getEditPage(null));
-		$main->setFormAction($_SERVER["PHP_SELF"]);
-			
-	}
-	//render the page
-	$main->setCentralPayload($oPatternCustom);
-	$main->render();	
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
+    require_once("editUnitUI.inc");
+    require_once("../adminUI.inc");
+    require_once("$default->fileSystemRoot/lib/security/permission.inc");
+    require_once("$default->fileSystemRoot/lib/unitmanagement/Unit.inc");
+    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+    require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+    require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+    require_once("$default->fileSystemRoot/presentation/Html.inc");
+
+
+    $oPatternCustom = & new PatternCustom();
+
+    // if a new unit has been added
+    // coming from manual edit page
+    if (isset($fForStore)) {
+        $oUnit = Unit::get($fUnitID);
+        $oUnit->setName($fUnitName);
+        if ($fUnitName== "") {
+            $oPatternCustom->setHtml(getEditPageFail());
+        } elseif ($oUnit->update()) {
+            // if successfull print out success message
+            $oPatternCustom->setHtml(getEditPageSuccess());
+        } else {
+            // if fail print out fail message
+            $oPatternCustom->setHtml(getEditPageFail());
+        }
+    } else if (isset($fUnitID)) {
+        // post back on Unit select from manual edit page
+        $oPatternCustom->setHtml(getEditPage($fUnitID));
+        $main->setFormAction($_SERVER["PHP_SELF"] . "?fForStore=1");
+    } else {
+        // if nothing happens...just reload edit page
+        $oPatternCustom->setHtml(getEditPage(null));
+        $main->setFormAction($_SERVER["PHP_SELF"]);
+    }
+    //render the page
+    $main->setCentralPayload($oPatternCustom);
+    $main->render();
 }
 ?>
