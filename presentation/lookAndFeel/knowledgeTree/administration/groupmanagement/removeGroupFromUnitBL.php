b151e31b9148d5140b28d08994b4bb0e7daf4ac0
Michael Joseph <michael@knowledgetree.com>
2003-03-14 09:58:08 +0000

    reformatted
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1568 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/groupmanagement/removeGroupFromUnitBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/groupmanagement/removeGroupFromUnitBL.php
@@ -10,54 +10,47 @@
 require_once("../../../../../config/dmsDefaults.php");
 
 if (checkSession()) {
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");
-	require_once("removeGroupFromUnitUI.inc");
-	require_once("$default->fileSystemRoot/lib/unitmanagement/Unit.inc");
-	require_once("$default->fileSystemRoot/lib/groups/Group.inc");
-	require_once("$default->fileSystemRoot/lib/groups/GroupUnitLink.inc");
-	require_once("$default->fileSystemRoot/lib/security/permission.inc");
-	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
-	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-	require_once("$default->fileSystemRoot/presentation/Html.inc");
-	
-	
-	$oPatternCustom = & new PatternCustom();
-		
-	if(!isset($fGroupSet)){
-		// build first page
-		
-		$oPatternCustom->setHtml(getPage(null,null));
-		$main->setFormAction($_SERVER["PHP_SELF"] . "?fGroupSet=1");
-	
-	}else{
-		// do a check to see both drop downs selected
-		if($fGroupID == -1){
-			$oPatternCustom->setHtml(getPageNotSelected());
-					
-		}else{ 		$fUnitID = GroupUnitLink::groupBelongsToUnit($fGroupID);	
-				$oPatternCustom->setHtml(getPage($fGroupID,$fUnitID));
-				$main->setFormAction($_SERVER["PHP_SELF"] . "?fGroupSet=1&fDeleteConfirmed=1");
-		}
-		
-	}
-	
-		
-	if (isset($fDeleteConfirmed)){
-				
-		// else add to db and then goto page succes
-		$oGroupUnit = new GroupUnitLink($fGroupID,$fUnitID);
-		$oGroupUnit->setGroupUnitID($fGroupID);
-		$oGroupUnit->delete();
-		$oPatternCustom->setHtml(getPageSuccess());
-		
-	}
-	
-	// render page
-	$main->setCentralPayload($oPatternCustom);
-	$main->render();
-	
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");
+    require_once("removeGroupFromUnitUI.inc");
+    require_once("$default->fileSystemRoot/lib/unitmanagement/Unit.inc");
+    require_once("$default->fileSystemRoot/lib/groups/Group.inc");
+    require_once("$default->fileSystemRoot/lib/groups/GroupUnitLink.inc");
+    require_once("$default->fileSystemRoot/lib/security/permission.inc");
+    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+    require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+    require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+    require_once("$default->fileSystemRoot/presentation/Html.inc");
+
+
+    $oPatternCustom = & new PatternCustom();
+
+    if(!isset($fGroupSet)) {
+        // build first page
+        $oPatternCustom->setHtml(getPage(null,null));
+        $main->setFormAction($_SERVER["PHP_SELF"] . "?fGroupSet=1");
+    } else {
+        // do a check to see both drop downs selected
+        if($fGroupID == -1) {
+            $oPatternCustom->setHtml(getPageNotSelected());
+        } else {
+            $fUnitID = GroupUnitLink::groupBelongsToUnit($fGroupID);
+            $oPatternCustom->setHtml(getPage($fGroupID,$fUnitID));
+            $main->setFormAction($_SERVER["PHP_SELF"] . "?fGroupSet=1&fDeleteConfirmed=1");
+        }
+    }
+
+    if (isset($fDeleteConfirmed)) {
+        // else add to db and then goto page succes
+        $oGroupUnit = new GroupUnitLink($fGroupID,$fUnitID);
+        $oGroupUnit->setGroupUnitID($fGroupID);
+        $oGroupUnit->delete();
+        $oPatternCustom->setHtml(getPageSuccess());
+    }
+
+    // render page
+    $main->setCentralPayload($oPatternCustom);
+    $main->render();
 }
 ?>
