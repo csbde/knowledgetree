f83ec2187bd78104e430e2f0f3c79f57eb3b26f5
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-03-14 15:27:26 +0000

    updated look and feel and formatted
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1588 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/unitmanagement/removeUnitBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/unitmanagement/removeUnitBL.php
@@ -10,63 +10,53 @@
 require_once("../../../../../config/dmsDefaults.php");
 
 if (checkSession()) {
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
-	require_once("removeUnitUI.inc");
-	require_once("$default->fileSystemRoot/lib/security/permission.inc");
-	require_once("$default->fileSystemRoot/lib/unitmanagement/Unit.inc");
-	require_once("$default->fileSystemRoot/lib/orgmanagement/Organisation.inc");
-	require_once("$default->fileSystemRoot/lib/unitmanagement/UnitOrganisationLink.inc");
-	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
-	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-	require_once("$default->fileSystemRoot/presentation/Html.inc");
-	
-	$oPatternCustom = & new PatternCustom();	
-	
-	// get main page
-	if (isset($fUnitID)) {
-			
-			$fOrgID = UnitOrganisationLink::unitBelongsToOrg($fUnitID);
-			
-			
-			$oPatternCustom->setHtml(getDeleteConfirmedPage($fUnitID,$fOrgID));
-			$main->setFormAction($_SERVER["PHP_SELF"] . "?fForDeleteConfirmed=1");
-	}
-	else {
-		$oPatternCustom->setHtml(getDeletePage(null));
-		$main->setFormAction($_SERVER["PHP_SELF"]);
-	}
-		
-	if (isset($fForDeleteConfirmed)) {
-	
-			// get unitorg object
-			$oUnitOrg = new UnitOrganisationLink($fUnitID,$fOrgID);
-			$oUnitOrg->setUnitOrgID($fUnitID);
-			
-			//get unit object
-			$oUnit = Unit::get($fUnitID);
-			$oUnit->setName($fUnitName);
-					
-			//delete unitorgobject
-			$oUnitOrg->delete();	
-			
-			//delet unit object
-			if ($oUnit->delete()) {
-				
-				$oPatternCustom->setHtml(getDeleteSuccessPage());
-			
-			} else {
-				$oPatternCustom->setHtml(getDeleteFailPage());
-			}
-		
-	}
-	
-	
-	
-	
-	$main->setCentralPayload($oPatternCustom);				
-	$main->render();		
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
+    require_once("removeUnitUI.inc");
+    require_once("../adminUI.inc");
+    require_once("$default->fileSystemRoot/lib/security/permission.inc");
+    require_once("$default->fileSystemRoot/lib/unitmanagement/Unit.inc");
+    require_once("$default->fileSystemRoot/lib/orgmanagement/Organisation.inc");
+    require_once("$default->fileSystemRoot/lib/unitmanagement/UnitOrganisationLink.inc");
+    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+    require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+    require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+    require_once("$default->fileSystemRoot/presentation/Html.inc");
+
+    $oPatternCustom = & new PatternCustom();
+
+    // get main page
+    if (isset($fUnitID)) {
+        $fOrgID = UnitOrganisationLink::unitBelongsToOrg($fUnitID);
+        $oPatternCustom->setHtml(getDeleteConfirmedPage($fUnitID,$fOrgID));
+        $main->setFormAction($_SERVER["PHP_SELF"] . "?fForDeleteConfirmed=1");
+    } else {
+        $oPatternCustom->setHtml(getDeletePage(null));
+        $main->setFormAction($_SERVER["PHP_SELF"]);
+    }
+
+    if (isset($fForDeleteConfirmed)) {
+        // get unitorg object
+        $oUnitOrg = new UnitOrganisationLink($fUnitID,$fOrgID);
+        $oUnitOrg->setUnitOrgID($fUnitID);
+
+        //get unit object
+        $oUnit = Unit::get($fUnitID);
+        $oUnit->setName($fUnitName);
+
+        //delete unitorgobject
+        $oUnitOrg->delete();
+
+        //delete unit object
+        if ($oUnit->delete()) {
+            $oPatternCustom->setHtml(getDeleteSuccessPage());
+        } else {
+            $oPatternCustom->setHtml(getDeleteFailPage());
+        }
+    }
+
+    $main->setCentralPayload($oPatternCustom);
+    $main->render();
 }
 ?>
