753be12858217180b65136635a745acad4da847b
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-03-14 10:06:50 +0000

    added heading
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1572 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/admin.php
+++ presentation/lookAndFeel/knowledgeTree/administration/admin.php
@@ -1,22 +1,24 @@
 <?php
 require_once("../../../../config/dmsDefaults.php");
+require_once("adminUI.inc");
 
-function renderHeading() {
-    global $default;
-    
-    $sSectionName = $default->siteMap->getSectionName(substr($_SERVER["PHP_SELF"], strlen($default->rootUrl), strlen($_SERVER["PHP_SELF"])));
-    $sColor = $default->siteMap->getSectionColour($sSectionName, "th");
-    $sToRender .= "<tr align=\"left\"><th class=\"sectionHeading\" bgcolor=\"$sColor\"><font color=\"ffffff\">Administration</font></th></tr>\n";
-    $sToRender .= "<tr/>\n";
-    $sToRender .= "<tr/>\n";
-    return $sToRender;
-}
+/**
+ * $Id$
+ *
+ * Displays the administration page.
+ *
+ * Licensed under the GNU GPL. For full terms see the file DOCS/COPYING.
+ *
+ * @version $Revision$
+ * @author Michael Joseph <michael@jamwarehouse.com>, Jam Warehouse (Pty) Ltd, South Africa
+ * @package presentation.lookAndFeel.knowledgeTree.administration
+ */
 
 if(checkSession()) {
 	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
 
     $sCenter  = "<table width=\"600\">\n";
-    $sCenter .= renderHeading();
+    $sCenter .= renderHeading("Administration");
     $sCenter .=	"<tr/><tr/><tr><td><b> Welcome to the Administration Section</b></td></tr>\n";
     $sCenter .=	"<tr><td>Please make a selection from the sidemenu.</td></tr>\n";
     $sCenter .=	"</table>";
