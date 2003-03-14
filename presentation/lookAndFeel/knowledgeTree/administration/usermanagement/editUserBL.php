a6299e6ef8e27943655a36dd590ec6a239e5a284
Michael Joseph <michael@knowledgetree.com>
2003-03-14 14:06:12 +0000

    added headings; removed non-standard success page
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1581 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/usermanagement/editUserBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/usermanagement/editUserBL.php
@@ -13,6 +13,7 @@ if (checkSession()) {
 	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
 	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
 	require_once("editUserUI.inc");
+    require_once("../adminUI.inc");
 	require_once("$default->fileSystemRoot/lib/security/permission.inc");
 	require_once("$default->fileSystemRoot/lib/users/User.inc");
 	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
@@ -21,16 +22,15 @@ if (checkSession()) {
 	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
 	require_once("$default->fileSystemRoot/presentation/Html.inc");
 	
-	
 	$oPatternCustom = & new PatternCustom();		
 	
 	 if (isset($fUserID)){		
 		// post back on User select from manual edit page	
 		$oPatternCustom->setHtml(getUserDetailsPage($fUserID));
-		$main->setFormAction("$default->rootUrl/presentation/lookAndFeel/knowledgeTree/store.php?fReturnURL=" . urlencode("$default->rootUrl/control.php?action=editUserSuccess"));		
-		
-		
-	} else {
+		$main->setFormAction("$default->rootUrl/presentation/lookAndFeel/knowledgeTree/store.php?fReturnURL=" . urlencode("$default->rootUrl/control.php?action=editUser&fSuccess=1"));		
+	} else if ($fSuccess) {
+        $oPatternCustom->setHtml(getEditPageSuccess());
+    } else {
 		// if nothing happens...just reload edit page
 		$oPatternCustom->setHtml(getEditPage(null));
 		$main->setFormAction($_SERVER["PHP_SELF"]);
