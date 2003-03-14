78e74b8738fc2059d6f4acf4935dcf0d3a284061
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-03-14 14:31:28 +0000

    added headings; removed non-standard success page
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1584 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/unitmanagement/addUnitBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/unitmanagement/addUnitBL.php
@@ -10,66 +10,56 @@
 require_once("../../../../../config/dmsDefaults.php");
 
 if (checkSession()) {
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");
-	require_once("addUnitUI.inc");
-	require_once("$default->fileSystemRoot/lib/unitmanagement/Unit.inc");
-	require_once("$default->fileSystemRoot/lib/unitmanagement/UnitOrganisationLink.inc");
-	require_once("$default->fileSystemRoot/lib/security/permission.inc");
-	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
-	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-	require_once("$default->fileSystemRoot/presentation/Html.inc");
-	
-	$oPatternCustom = & new PatternCustom();
-	
-	if (isset($fForStore)) {
-		
-		if($fUnitName != "" and $fOrgID !=-1)
-		{
-			$oUnit = new Unit($fUnitName);
-								
-			//$oOrg = Organisation($
-			// if creation is successfull..get the unit id
-			if ($oUnit->create()) {
-				$unitID = $oUnit->getID();
-				$oUnitOrg = new UnitOrganisationLink($unitID,$fOrgID);
-				
-				if($oUnitOrg->create()){
-					// if successfull print out success message
-					$oPatternCustom->setHtml(getAddPageSuccess());
-				}else{
-				
-					// if fail print out fail message
-					$oPatternCustom->setHtml(getAddToOrgFail());
-				}
-				
-							
-			} else {
-					// if fail print out fail message
-					$oPatternCustom->setHtml(getAddPageFail());
-			}
-		}else{
-		
-					$oPatternCustom->setHtml(getPageFail());
-		}
-		
-	} else if (isset($fUnitID)){		
-			// post back on Unit select from manual edit page	
-			$oPatternCustom->setHtml(getAddPage($fUnitID));
-			$main->setFormAction($_SERVER["PHP_SELF"] . "?fForStore=1");
-	}else {
-		// if nothing happens...just reload edit page
-		$oPatternCustom->setHtml(getAddPage(null));
-		$main->setFormAction($_SERVER["PHP_SELF"]. "?fForStore=1");
-			
-	}		
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");
+    require_once("addUnitUI.inc");
+    require_once("../adminUI.inc");
+    require_once("$default->fileSystemRoot/lib/unitmanagement/Unit.inc");
+    require_once("$default->fileSystemRoot/lib/unitmanagement/UnitOrganisationLink.inc");
+    require_once("$default->fileSystemRoot/lib/security/permission.inc");
+    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+    require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+    require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+    require_once("$default->fileSystemRoot/presentation/Html.inc");
 
-	//$oPatternCustom->setHtml(getPage());
-	
-	//$main->setFormAction("$default->rootUrl/presentation/lookAndFeel/knowledgeTree/create.php?fRedirectURL=".urlencode("$default->rootUrl/control.php?action=addUnitSuccess&fUnit"));
-	$main->setCentralPayload($oPatternCustom);
-	$main->render();
+    $oPatternCustom = & new PatternCustom();
+
+    if (isset($fForStore)) {
+        if($fUnitName != "" and $fOrgID != "") {
+            $oUnit = new Unit($fUnitName);
+
+            // if creation is successfull..get the unit id
+            if ($oUnit->create()) {
+                $unitID = $oUnit->getID();
+                $oUnitOrg = new UnitOrganisationLink($unitID,$fOrgID);
+
+                if($oUnitOrg->create()) {
+                    // if successfull print out success message
+                    $oPatternCustom->setHtml(getAddPageSuccess());
+                } else {
+                    // if fail print out fail message
+                    $oPatternCustom->setHtml(getAddToOrgFail());
+                }
+            } else {
+                // if fail print out fail message
+                $oPatternCustom->setHtml(getAddPageFail());
+            }
+        } else {
+            $oPatternCustom->setHtml(getPageFail());
+        }
+
+    } else if (isset($fUnitID)) {
+        // post back on Unit select from manual edit page
+        $oPatternCustom->setHtml(getAddPage($fUnitID));
+        $main->setFormAction($_SERVER["PHP_SELF"] . "?fForStore=1");
+    } else {
+        // if nothing happens...just reload edit page
+        $oPatternCustom->setHtml(getAddPage(null));
+        $main->setFormAction($_SERVER["PHP_SELF"]. "?fForStore=1");
+
+    }
+    $main->setCentralPayload($oPatternCustom);
+    $main->render();
 }
 ?>
