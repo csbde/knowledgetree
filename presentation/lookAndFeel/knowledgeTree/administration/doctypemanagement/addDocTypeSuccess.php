fcf4abdeef7b19ef1db345990c4b73fef71d6a12
Michael Joseph <michael@knowledgetree.com>
2003-03-17 09:14:25 +0000

    updated look and feel and formatted only allow editing and removal of document types that are not mapped to a document
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1593 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/doctypemanagement/addDocTypeSuccess.php
+++ presentation/lookAndFeel/knowledgeTree/administration/doctypemanagement/addDocTypeSuccess.php
@@ -9,51 +9,42 @@
 */
 
 require_once("../../../../../config/dmsDefaults.php");
+require_once("../adminUI.inc");
 
 global $default;
-	
-if(checkSession())
-{
 
-// include the page template (with navbar)
-require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");  	
+if(checkSession()) {
 
-$Center = "<br></br>\n" ;
-$Center .= "<TABLE BORDER=\"0\" CELLSPACING=\"2\" CELLPADDING=\"2\">\n";
-$Center .= "<tr>\n";
-if($fDocTypeID == -1){
-	$Center .= "<td><b>Document Type addition Unsuccessful!</b></td>\n";
-	$Center .= "</tr>\n";
-	$Center .= "<tr></tr>\n";	
-	$Center .= "<tr>\n";	
-	$Center .= "<td>Document Type already exists</td>\n";
-	$Center .= "</tr>\n";	
-		
-}else{
-	$Center .= "<td><b>Document Type added Successfully!</b></td>\n";
-	$Center .= "</tr>\n";
-	
+    // include the page template (with navbar)
+    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+
+    $Center .= "<table width=\"600\">" . renderHeading("Add Document Type") . "</table>";
+    $Center .= "<TABLE BORDER=\"0\" CELLSPACING=\"2\" CELLPADDING=\"2\">\n";
+    $Center .= "<tr>\n";
+    if($fDocTypeID == -1) {
+        $Center .= "<td><b>Document Type addition Unsuccessful!</b></td>\n";
+        $Center .= "</tr>\n";
+        $Center .= "<tr></tr>\n";
+        $Center .= "<tr>\n";
+        $Center .= "<td>Document Type already exists</td>\n";
+        $Center .= "</tr>\n";
+    } else {
+        $Center .= "<td><b>Document Type added Successfully!</b></td>\n";
+        $Center .= "</tr>\n";
+    }
+
+    $Center .= "<tr></tr>\n";
+    $Center .= "<tr></tr>\n";
+    $Center .= "<tr></tr>\n";
+    $Center .= "<tr></tr>\n";
+    $Center .= "<tr>\n";
+    $Center .= "<td align = right><a href=\"$default->rootUrl/control.php?action=addDocType\"><img src =\"$default->graphicsUrl/widgets/back.gif\" border = \"0\" /></a></td>\n";
+    $Center .= "</tr>\n";
+    $Center .= "</table>\n";
+
+    $oPatternCustom = & new PatternCustom();
+    $oPatternCustom->setHtml($Center);
+    $main->setCentralPayload($oPatternCustom);
+    $main->render();
 }
-
-
-$Center .= "<tr></tr>\n";
-$Center .= "<tr></tr>\n";
-$Center .= "<tr></tr>\n";
-$Center .= "<tr></tr>\n";
-$Center .= "<tr>\n";
-$Center .= "<td align = right><a href=\"$default->rootUrl/control.php?action=addDocType\"><img src =\"$default->graphicsUrl/widgets/back.gif\" border = \"0\" /></a></td>\n";
-$Center .= "</tr>\n";
-$Center .= "</table>\n";
-		
-
-$oPatternCustom = & new PatternCustom();
-$oPatternCustom->setHtml($Center); 
-$main->setCentralPayload($oPatternCustom);
-$main->render();
-
-
-
-
-}
-
 ?>
