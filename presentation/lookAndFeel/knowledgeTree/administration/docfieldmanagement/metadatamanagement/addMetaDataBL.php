17ea544ecada718a2b738cedcf8764abe6a3ca29
Michael Joseph <michael@knowledgetree.com>
2003-03-17 09:57:45 +0000

    updated look and feel and formatted
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1595 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/metadatamanagement/addMetaDataBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/metadatamanagement/addMetaDataBL.php
@@ -10,57 +10,46 @@
 require_once("../../../../../../config/dmsDefaults.php");
 
 if (checkSession()) {
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
-	require_once("addMetaDataUI.inc");
-	require_once("$default->fileSystemRoot/lib/security/permission.inc");
-	require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentField.inc");
-	require_once("$default->fileSystemRoot/lib/documentmanagement/MetaData.inc");
-	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
-	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-	require_once("$default->fileSystemRoot/presentation/Html.inc");
-	
-	
-	$oPatternCustom = & new PatternCustom();		
-	
-	 
-	if(isset($fDocFieldID)){		
-		// post back on DocField select from manual edit page	
-		$oPatternCustom->setHtml(getAddMetaDataPage($fDocFieldID));
-		$main->setFormAction($_SERVER["PHP_SELF"] . "?fForStore=1");
-		
-		
-	} else {
-		// if nothing happens...just reload edit page
-		$oPatternCustom->setHtml(getSelectFieldPage(null));
-		$main->setFormAction($_SERVER["PHP_SELF"]);
-			
-	}
-	
-	if(isset($fForStore))
-	{
-		if($fMetaDataName != "")
-		{			
-			$oMetaData = new MetaData($fDocFieldID,$fMetaDataName);
-			
-			if($oMetaData->create())
-			{
-				$oPatternCustom->setHtml(getSuccessPage());
-			}else{
-				$oPatternCustom->setHtml(getFailPage());
-			}
-		}
-		else
-		{
-			$oPatternCustom->setHtml(getTextPage());
-		}
-			
-	}
-	
-	//render the page
-	$main->setCentralPayload($oPatternCustom);
-	$main->render();	
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
+    require_once("addMetaDataUI.inc");
+    require_once("../../adminUI.inc");
+    require_once("$default->fileSystemRoot/lib/security/permission.inc");
+    require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentField.inc");
+    require_once("$default->fileSystemRoot/lib/documentmanagement/MetaData.inc");
+    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+    require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+    require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+    require_once("$default->fileSystemRoot/presentation/Html.inc");
+
+    $oPatternCustom = & new PatternCustom();
+
+    if(isset($fDocFieldID)) {
+        // post back on DocField select from manual edit page
+        $oPatternCustom->setHtml(getAddMetaDataPage($fDocFieldID));
+        $main->setFormAction($_SERVER["PHP_SELF"] . "?fForStore=1");
+    } else {
+        // if nothing happens...just reload edit page
+        $oPatternCustom->setHtml(getSelectFieldPage(null));
+        $main->setFormAction($_SERVER["PHP_SELF"]);
+    }
+
+    if(isset($fForStore)) {
+        if($fMetaDataName != "") {
+            $oMetaData = new MetaData($fDocFieldID,$fMetaDataName);
+            if($oMetaData->create()) {
+                $oPatternCustom->setHtml(getSuccessPage());
+            } else {
+                $oPatternCustom->setHtml(getFailPage());
+            }
+        } else {
+            $oPatternCustom->setHtml(getTextPage());
+        }
+    }
+
+    //render the page
+    $main->setCentralPayload($oPatternCustom);
+    $main->render();
 }
 ?>
