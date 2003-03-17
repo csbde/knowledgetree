1cdea1a04d3663f50cd02b696b05f39e5ff669ef
Michael Joseph <michael@knowledgetree.com>
2003-03-17 08:42:46 +0000

    updated look and feel and formatted
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1591 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/orgmanagement/removeOrgBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/orgmanagement/removeOrgBL.php
@@ -10,45 +10,44 @@
 require_once("../../../../../config/dmsDefaults.php");
 
 if (checkSession()) {
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
-	require_once("removeOrgUI.inc");
-	require_once("$default->fileSystemRoot/lib/security/permission.inc");
-	require_once("$default->fileSystemRoot/lib/orgmanagement/Organisation.inc");
-	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
-	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-	require_once("$default->fileSystemRoot/presentation/Html.inc");
-	
-	$oPatternCustom = & new PatternCustom();	
-	
-	// get main page
-	if (isset($fOrgID)) {
-			
-		$oPatternCustom->setHtml(getDeletePage($fOrgID));
-		$main->setFormAction($_SERVER["PHP_SELF"] . "?fForDelete=1");
-		
-	// get delete page
-	} else {
-		$oPatternCustom->setHtml(getDeletePage(null));
-		$main->setFormAction($_SERVER["PHP_SELF"]);
-	}
-	
-		// if delete entry
-		if (isset($fForDelete)) {
-			$oOrg = Organisation::get($fOrgID);
-			$oOrg->setName($fOrgName);
-			
-		if ($oOrg->delete()) {
-			$oPatternCustom->setHtml(getDeleteSuccessPage());
-			
-		} else {
-			$oPatternCustom->setHtml(getDeleteFailPage());
-		}
-	}
-	
-	$main->setCentralPayload($oPatternCustom);				
-	$main->render();		
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
+    require_once("removeOrgUI.inc");
+    require_once("../adminUI.inc");
+    require_once("$default->fileSystemRoot/lib/security/permission.inc");
+    require_once("$default->fileSystemRoot/lib/orgmanagement/Organisation.inc");
+    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+    require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+    require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+    require_once("$default->fileSystemRoot/presentation/Html.inc");
+
+    $oPatternCustom = & new PatternCustom();
+
+    // get main page
+    if (isset($fOrgID)) {
+        $oPatternCustom->setHtml(getDeletePage($fOrgID));
+        $main->setFormAction($_SERVER["PHP_SELF"] . "?fForDelete=1");
+        // get delete page
+    }
+    else {
+        $oPatternCustom->setHtml(getDeletePage(null));
+        $main->setFormAction($_SERVER["PHP_SELF"]);
+    }
+
+    // if delete entry
+    if (isset($fForDelete)) {
+        $oOrg = Organisation::get($fOrgID);
+        $oOrg->setName($fOrgName);
+
+        if ($oOrg->delete()) {
+            $oPatternCustom->setHtml(getDeleteSuccessPage());
+        } else {
+            $oPatternCustom->setHtml(getDeleteFailPage());
+        }
+    }
+
+    $main->setCentralPayload($oPatternCustom);
+    $main->render();
 }
 ?>
