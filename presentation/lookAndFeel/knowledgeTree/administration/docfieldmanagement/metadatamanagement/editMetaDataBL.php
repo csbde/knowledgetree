eb3a80ef780db355fa8eb4682e4cf9798974078d
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-03-17 09:57:45 +0000

    updated look and feel and formatted
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1595 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/metadatamanagement/editMetaDataBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/metadatamanagement/editMetaDataBL.php
@@ -10,68 +10,51 @@
 require_once("../../../../../../config/dmsDefaults.php");
 
 if (checkSession()) {
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
-	require_once("editMetaDataUI.inc");
-	require_once("$default->fileSystemRoot/lib/security/permission.inc");
-	require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentField.inc");
-	require_once("$default->fileSystemRoot/lib/documentmanagement/MetaData.inc");
-	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
-	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-	require_once("$default->fileSystemRoot/presentation/Html.inc");
-	
-	
-	$oPatternCustom = & new PatternCustom();		
-	
-	 
-	if(isset($fDocFieldID)){		
-		// post back on DocField select from manual edit page	
-		$oPatternCustom->setHtml(getSelectMetaDataPage($fDocFieldID,$fMetaDataID));
-		$main->setFormAction($_SERVER["PHP_SELF"] . "?fSelected=1");
-		
-		
-	} else {
-		// if nothing happens...just reload edit page
-		$oPatternCustom->setHtml(getSelectFieldPage(null));
-		$main->setFormAction($_SERVER["PHP_SELF"]);
-			
-	}
-	
-	if(isset($fSelected)){
-		$oPatternCustom->setHtml(getEditMetaDataPage($fDocFieldID,$fMetaDataID));
-		
-		$main->setFormAction($_SERVER["PHP_SELF"] . "?fForStore=1");
-		
-	}
-	
-	
-	if(isset($fForStore)){
-		
-		if($fMetaDataName != "")
-		{			
-			$oMetaData = new MetaData($fDocFieldID,$fMetaDataName);
-			$oMetaData->setMetaDataID($fDocFieldID,$fOldMetaDataName);
-			
-			
-			
-			if($oMetaData->update())
-			{
-				$oPatternCustom->setHtml(getSuccessPage());
-			}else{
-				$oPatternCustom->setHtml(getFailPage());
-			}
-		}
-		else
-		{
-			$oPatternCustom->setHtml(getTextPage());
-		}
-			
-	}
-	
-	//render the page
-	$main->setCentralPayload($oPatternCustom);
-	$main->render();	
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
+    require_once("editMetaDataUI.inc");
+    require_once("../../adminUI.inc");
+    require_once("$default->fileSystemRoot/lib/security/permission.inc");
+    require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentField.inc");
+    require_once("$default->fileSystemRoot/lib/documentmanagement/MetaData.inc");
+    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+    require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+    require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+    require_once("$default->fileSystemRoot/presentation/Html.inc");
+
+    $oPatternCustom = & new PatternCustom();
+    if(isset($fDocFieldID)) {
+        // post back on DocField select from manual edit page
+        $oPatternCustom->setHtml(getSelectMetaDataPage($fDocFieldID,$fMetaDataID));
+        $main->setFormAction($_SERVER["PHP_SELF"] . "?fSelected=1");
+    } else {
+        // if nothing happens...just reload edit page
+        $oPatternCustom->setHtml(getSelectFieldPage(null));
+        $main->setFormAction($_SERVER["PHP_SELF"]);
+    }
+
+    if(isset($fSelected)) {
+        $oPatternCustom->setHtml(getEditMetaDataPage($fDocFieldID,$fMetaDataID));
+        $main->setFormAction($_SERVER["PHP_SELF"] . "?fForStore=1");
+    }
+
+    if(isset($fForStore)) {
+        if($fMetaDataName != "") {
+            $oMetaData = new MetaData($fDocFieldID,$fMetaDataName);
+            $oMetaData->setMetaDataID($fDocFieldID,$fOldMetaDataName);
+            if($oMetaData->update()) {
+                $oPatternCustom->setHtml(getSuccessPage());
+            } else {
+                $oPatternCustom->setHtml(getFailPage());
+            }
+        } else {
+            $oPatternCustom->setHtml(getTextPage());
+        }
+    }
+
+    //render the page
+    $main->setCentralPayload($oPatternCustom);
+    $main->render();
 }
 ?>
--- presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/metadatamanagement/removeMetaDataBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/metadatamanagement/removeMetaDataBL.php
@@ -10,62 +10,48 @@
 require_once("../../../../../../config/dmsDefaults.php");
 
 if (checkSession()) {
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
-	require_once("removeMetaDataUI.inc");
-	require_once("$default->fileSystemRoot/lib/security/permission.inc");
-	require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentField.inc");
-	require_once("$default->fileSystemRoot/lib/documentmanagement/MetaData.inc");
-	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
-	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-	require_once("$default->fileSystemRoot/presentation/Html.inc");
-	
-	
-	$oPatternCustom = & new PatternCustom();		
-	
-	 
-	if(isset($fDocFieldID)){		
-		// post back on DocField select from manual edit page	
-		$oPatternCustom->setHtml(getSelectMetaDataPage($fDocFieldID,$fMetaDataID));
-		$main->setFormAction($_SERVER["PHP_SELF"] . "?fSelected=1");
-		
-		
-	} else {
-		// if nothing happens...just reload edit page
-		$oPatternCustom->setHtml(getSelectFieldPage(null));
-		$main->setFormAction($_SERVER["PHP_SELF"]);
-			
-	}
-	
-	if(isset($fSelected)){
-		$oPatternCustom->setHtml(getDeleteConfirmedPage($fDocFieldID,$fMetaDataID));
-		
-		$main->setFormAction($_SERVER["PHP_SELF"] . "?fForDelete=1");
-		
-	}
-	
-	
-	if(isset($fForDelete)){
-					
-			$oMetaData = new MetaData($fDocFieldID,$fMetaDataName);
-			$oMetaData->setMetaDataID($fDocFieldID,$fMetaDataName);
-			
-			if($oMetaData->delete())
-			{
-				$oPatternCustom->setHtml(getSuccessPage());
-			}else{
-				$oPatternCustom->setHtml(getFailPage());
-			}
-		
-			
-	}
-	
-	
-	
-	//render the page
-	$main->setCentralPayload($oPatternCustom);
-	$main->render();	
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
+    require_once("removeMetaDataUI.inc");
+    require_once("../../adminUI.inc");
+    require_once("$default->fileSystemRoot/lib/security/permission.inc");
+    require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentField.inc");
+    require_once("$default->fileSystemRoot/lib/documentmanagement/MetaData.inc");
+    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+    require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+    require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+    require_once("$default->fileSystemRoot/presentation/Html.inc");
+
+    $oPatternCustom = & new PatternCustom();
+
+    if(isset($fDocFieldID)) {
+        // post back on DocField select from manual edit page
+        $oPatternCustom->setHtml(getSelectMetaDataPage($fDocFieldID,$fMetaDataID));
+        $main->setFormAction($_SERVER["PHP_SELF"] . "?fSelected=1");
+    } else {
+        // if nothing happens...just reload edit page
+        $oPatternCustom->setHtml(getSelectFieldPage(null));
+        $main->setFormAction($_SERVER["PHP_SELF"]);
+    }
+
+    if(isset($fSelected)) {
+        $oPatternCustom->setHtml(getDeleteConfirmedPage($fDocFieldID,$fMetaDataID));
+        $main->setFormAction($_SERVER["PHP_SELF"] . "?fForDelete=1");
+    }
+
+    if(isset($fForDelete)) {
+        $oMetaData = new MetaData($fDocFieldID,$fMetaDataName);
+        $oMetaData->setMetaDataID($fDocFieldID,$fMetaDataName);
+        if($oMetaData->delete()) {
+            $oPatternCustom->setHtml(getSuccessPage());
+        } else {
+            $oPatternCustom->setHtml(getFailPage());
+        }
+    }
+
+    //render the page
+    $main->setCentralPayload($oPatternCustom);
+    $main->render();
 }
 ?>
