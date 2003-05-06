e6aa3e01a5a31405e8aa58a5411ad629568465ed
omar <omar@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-05-06 11:31:59 +0000

    *** empty log message ***
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1748 c91229c3-7414-0410-bfa2-8a42b809f60b

--- config/dmsDefaults.php
+++ config/dmsDefaults.php
@@ -325,6 +325,11 @@ $default->siteMap->addDefaultPage("help", "/presentation/lookAndFeel/knowledgeTr
 // pages for logout section section
 $default->siteMap->addDefaultPage("logout", "/presentation/logout.php", "Logout", Guest, "Logout");
 
+// pages for discussion threads
+$default->siteMap->addDefaultPage("viewDiscussion", "/presentation/lookAndFeel/knowledgeTree/discussions/viewDiscussionBL.php", "Manage Documents", User, "viewDiscussion");
+//$default->siteMap->addDefaultPage("editPrefsSuccess", "/presentation/lookAndFeel/knowledgeTree/preferences/editPrefsSuccess.php", "Preferences", User, "Preferences",false);
+
+
 // test pages
 $default->siteMap->addPage("scratchPad", "/tests/scratchPad.php", "Tests", Guest, "scratch", false);
 $default->siteMap->addPage("sitemap", "/tests/session/SiteMap.php", "Tests", Guest, "sitemap", false);
