683f7bdded8f853318d9b0489085aaac01424c01
omar <omar@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-05-08 15:26:32 +0000

    *** empty log message ***
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1770 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/discussions/addCommentBL.php
+++ presentation/lookAndFeel/knowledgeTree/discussions/addCommentBL.php
@@ -36,14 +36,17 @@ if (checkSession()) {
     $oPatternCustom = & new PatternCustom();
           
 	if(checksession) {	
-		if (isset($fAddComment)) {		
+		if (isset($fAddComment)) {			
 			if ($fDocumentID > 0) { 	
+				//if (isset($fNewThread))			
+				
 				$main->setFormAction($_SERVER['PHP_SELF'] . "?fAddCommentSubmit=1&iDocumentID=$fDocumentID");
 				$oPatternCustom->setHtml(getAddComment($fDocumentID,$sSubject,$sBody));
 			}
 			else {
 			  //nothing	
 			}
+			
 		} else if (isset($fViewComment)){		
 			if (isset($iCommentID)) {
 				$oComment = DiscussionComment::get($iCommentID);
@@ -51,12 +54,19 @@ if (checkSession()) {
 			  	
 			  	$oPatternCustom->setHtml(getCommentBody($oComment->getBody(), $oComment->getSubject(),$oComment->getDate(),$iDocumentID,$oUser->getUserName())) ;			  	
 			}		
-		} else if (isset($fAddCommentSubmit)) {								
-				$iThreadID = DiscussionThread::getThreadIDforDoc($iDocumentID);
+		} else if (isset($fAddCommentSubmit)) {					
+			if ($_POST["NewComment"] != "" and $_POST["NewCommentSubject"] != "")						{
+				if (isset($fNewThread)){ // Create a New Thread for this document as it doesn't exist 
+					// Create the thread Object
+					$oThread = & new DiscussionThread(-1,$iDocumentID, $_SESSION["userID"]);
+					$oThread->create();					
+					$iThreadID = $oThread->getID();					
+				}		
+				else { // Update the existing thread				
+					$iThreadID = DiscussionThread::getThreadIDforDoc($iDocumentID);
+				} 			
 				
-				if ($iThreadID > 0){
-					if ($_POST["NewComment"] != "" and $_POST["NewCommentSubject"] != "")
-					{
+				if ($iThreadID > 0){											
 						// Create the new comment					
 						$oComment = & new DiscussionComment(urlencode($_POST["NewComment"]),$_POST["NewCommentSubject"],$_SESSION["userID"],$iDocumentID);			
 						$oComment->setThreadID($iThreadID);
@@ -65,6 +75,11 @@ if (checkSession()) {
 						if($oComment->getID() > 0) {
 							$oThread = DiscussionThread::get($iThreadID);
 							$oThread->setLastCommentID($oComment->getID());
+							if ($oThread->getFirstComment == -1){ // if it is a new Thread
+								$oThread->setFirstCommentID($oComment->getID());
+								$oThread->setNumberOfViews();
+								$oThread->setNumberOfReplies();
+							}
 							$oThread->setNumberOfReplies();
 							if ($oThread->Update()) {						
 								$oPatternCustom->addHtml(getSubmitSuccessPage($iDocumentID));
@@ -74,21 +89,29 @@ if (checkSession()) {
 						}else {
 							$oPatternCustom->addHtml(getViewFailPage("Comment Object failed in creation.") );
 						}
-					}else {
-						$oPatternCustom->addHtml(getViewFailPage("The subject line and/or body may be empty.") );
-						$main->setFormAction($_SERVER['PHP_SELF'] . "?fAddCommentSubmit=1&iDocumentID=$fDocumentID");
-						$oPatternCustom->addHtml(getAddComment($fDocumentID,$_POST["NewCommentSubject"],$_POST["NewComment"]));
-					}
-				}else{
-					$oPatternCustom->addHtml(getViewFailPage("No threadID($iThreadID) exists for this document"));	
-				}
 				
+				}else{
+					$oPatternCustom->addHtml(getViewFailPage("No threadID($iThreadID) exists for this document" . $astr ));	
+				}// End Of if for THREAD ID test					
+			}else {
+				$oPatternCustom->addHtml(getViewFailPage("The subject line and/or body may be empty.") );
+				$main->setFormAction($_SERVER['PHP_SELF'] . "?fAddCommentSubmit=1&iDocumentID=$fDocumentID");
+				$oPatternCustom->addHtml(getAddComment($fDocumentID,$_POST["NewCommentSubject"],$_POST["NewComment"]));
+			} // end of IF for Subject and Body test				
+		
 		} else if (isset($fReplyComment)){  // if user is replying to existing comment
 			$main->setFormAction($_SERVER['PHP_SELF'] . "?fAddCommentSubmit=1&iDocumentID=$fDocumentID");
 			$oPatternCustom->addHtml(getAddComment($fDocumentID,"Re: " . $CommentSubject , "\n\n\n[Start Text Body]\n\n" . $Comment  . "\n\n[End Text Body]"));			
-				
-		}else { // If no discussion exists		
-			$oPatternCustom->setHtml(getViewFailPage("No discussions exist"));
+			
+		}else if (isset($fNewThread)){ // Start adding a new Thread 
+			$main->setFormAction($_SERVER['PHP_SELF'] . "?fAddCommentSubmit=1&iDocumentID=$fDocumentID&fNewThread=1");
+			$oPatternCustom->addHtml(getAddComment($fDocumentID, $CommentSubject , $Comment ));			
+			//$oPatternCustom->addHtml(getAddComment($fDocumentID,$CommentSubject,$Comment));			
+		
+		}	
+		
+		else { // If no discussion exists		
+			$oPatternCustom->setHtml(getViewFailPage("Comment"));
 		}
 	
 	} // end of if checksession
