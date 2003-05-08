ba6bb61d9442603d7333bd985e0da78ae698c1aa
omar <omar@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-05-08 15:26:32 +0000

    *** empty log message ***
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1770 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/discussions/viewDiscussionBL.php
+++ presentation/lookAndFeel/knowledgeTree/discussions/viewDiscussionBL.php
@@ -3,34 +3,25 @@
 * BL information for viewing a Discussion	
 *
 * @author Omar Rahbeeni
-* @date 5 May 2003
+* @date 8 May 2003
 * @package presentation.lookAndFeel.knowledgeTree.
 *
 */
 
 	require_once("../../../../config/dmsDefaults.php");
 
-if (checkSession()) {
-    //require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
-    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");
-    //require_once("addUnitUI.inc");
-    //require_once("../adminUI.inc");
-    require_once("viewDiscussionUI.inc"); //###
-    
+if (checkSession()) {    
+    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");    
+    require_once("viewDiscussionUI.inc");     
 	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
     require_once("$default->fileSystemRoot/lib/documentmanagement/Document.inc");
     require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-    require_once("$default->fileSystemRoot/lib/users/User.inc");
-    //require_once("../viewDiscussionUI.inc"); //###
-    //require_once("$default->fileSystemRoot/lib/unitmanagement/Unit.inc");
-    //require_once("$default->fileSystemRoot/lib/unitmanagement/UnitOrganisationLink.inc");
+    require_once("$default->fileSystemRoot/lib/users/User.inc");    
     require_once("$default->fileSystemRoot/lib/security/permission.inc");
     require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
     require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");    
-    require_once("$default->fileSystemRoot/lib/discussions/DiscussionThread.inc");  //###
-    require_once("$default->fileSystemRoot/lib/discussions/DiscussionComment.inc");  //###
-    //require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-    //require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+    require_once("$default->fileSystemRoot/lib/discussions/DiscussionThread.inc");  
+    require_once("$default->fileSystemRoot/lib/discussions/DiscussionComment.inc");   
     require_once("$default->fileSystemRoot/presentation/Html.inc");
 
     $oPatternCustom = & new PatternCustom();
@@ -59,20 +50,25 @@ if(checksession) {
 							$_SESSION['Discussion'][0]->bViews = true;
 						} 																
 					} else { $oPatternCustom->setHtml(getViewFailPage("")) ;}						
-				} else { $oPatternCustom->addHtml(getViewFailPage("No threadID for this document.")); }	
+				} else { // No current thread, option to create one		
+					
+					$oPatternCustom->addHtml(getNewThreadOption($fDocumentID)); 
+					
+					
+					}	
 			} else { }
 		} else if (isset($fViewComment)){		
 			if (isset($iCommentID)) {
 				$oComment = DiscussionComment::get($iCommentID);
-			  	$oUser =  User::get($oComment->getUserID());	  	
-			  	
-			  	//$main->setFormAction(generateControllerLink("addComment", "fDocumentID=$iDocumentID&fCommentID=$iCommentID&fReplyComment=1"));
-			  	$main->setFormAction("/presentation/lookAndFeel/knowledgeTree/discussions/addCommentBL.php?fDocumentID=$iDocumentID&fCommentID=$iCommentID&fReplyComment=1");
-			  	//$main->setFormAction($_SERVER['PHP_SELF'] . "?fReplyComment=1");
+			  	$oUser = User::get($oComment->getUserID());	  	
+		
+			  	$main->setFormAction("/presentation/lookAndFeel/knowledgeTree/discussions/addCommentBL.php?fDocumentID=$iDocumentID&fCommentID=$iCommentID&fReplyComment=1");		
 			  	$oPatternCustom->setHtml(getCommentBody($oComment,$iDocumentID,$oUser)) ;	  	
 			}
-		} else if (isset($fReplyComment)){ 
-			$oPatternCustom->setHtml("###" . count($_POST) . "###" . $Comment . "###" . $CommentSubject);
+		} else if (isset($fNewThread)){
+			
+			
+						
 		} else { // If no discussion exists 
 		}	
 
