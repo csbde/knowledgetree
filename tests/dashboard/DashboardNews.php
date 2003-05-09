60031d39c261229471b3df9ebc75dd04f84e390b
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-05-09 08:55:42 +0000

    completed new image handling functionality and tests
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1772 c91229c3-7414-0410-bfa2-8a42b809f60b

--- tests/dashboard/DashboardNews.php
+++ tests/dashboard/DashboardNews.php
@@ -20,18 +20,22 @@ require_once("$default->fileSystemRoot/lib/dashboard/DashboardNews.inc");
  * @package tests.dashboard
  */
 
-//if (checkSession()) {
-	
+$sTestImage = "c:/test.gif";
+
+if (isset($fViewImage)) {
+	$oNews = & new DashboardNews("test synopsis", "test body", 1, $sTestImage);
+	$oNews->displayImage();
+} else {
 	// test creation of a dashboard news item
 	echo "<b>Testing dashboard news creation</b><br>";
-	// convert the image to a string
-	$sTestImage = "c:\test.jpg";
-	// TODO: implement
-	//$sImgStr = DashboardNews::imageToString($sTestImage);
-	$sImgStr = $sTestImage;
-	$oNews = & new DashboardNews("test synopsis", "test body", 1, $sImgStr);
+	
+	$oNews = & new DashboardNews("test synopsis", "test body", 1, $sTestImage);
+
 	if (isset($oNews)) {
 		echo "Passed dashboard news creation test<br><br>";
+		
+		echo "<b>Testing image retrieval</b><br>";
+		echo "<img src=\"" . $_SERVER['PHP_SELF'] . "?fViewImage\">";
 		
 		echo "<b>Testing setting and getting of dashboard news values</b><br>";		
 		echo "Current dashboard synopsis: " . $oNews->getSynopsis() . "<br>";		
@@ -43,40 +47,45 @@ require_once("$default->fileSystemRoot/lib/dashboard/DashboardNews.inc");
 		echo "Setting news body to: 'blurg'<br>";
 		$oNews->setBody("blurg");
 		echo "New news body: " . $oNews->getBody() . "<br><br>";
-	
-		echo "Current news img: " . $oNews->getImage() . "<br>";
-		echo "Setting news img to 'foo'<br>";
-		$oNews->setImage('foo');
-		echo "New news img: " . $oNews->getImage() . "<br><br>";
 		
-		echo "Current new rank: " . $oNews->getRank() . "<br>";
+		echo "Current news rank: " . $oNews->getRank() . "<br>";
 		echo "Set new rank to 5<br>";
 		$oNews->setRank(5);
-		echo "New new rank: " . $oNews->getRank() . "<br><br>";
+		echo "New new rank: " . $oNews->getRank() . "<br><br>";		
+		
+		echo "Current image size: " . $oNews->getImageSize() . "<br>";
+		
+		echo "Current image mime type id: " . $oNews->getImageMimeTypeID() . "<br>";
 				
 		echo "<b>Testing dashboard news storage</b><br>";
 		if ($oNews->create()) {
 			echo "Passed dashboard news storage test<br><br>";
-
+	
 			echo "<b>Testing dashboard news update</b><br>";
-			echo "setting attributes to ('a','b','c',10)";
+			echo "setting attributes to ('a','b','c:\sample.jpg',10)";
 			$oNews->setSynopsis("a");
 			$oNews->setBody("b");
-			$oNews->setImage("c");
+			//$oNews->setImageFile("c:/test.gif");
 			$oNews->setRank(10);
 			if ($oNews->update()) {
 				echo "Passed dashboard news update test<br>";
+				
+				echo "<b>Testing dashboard news retrieval</b><br>";
+				$oRetrievedNews = DashboardNews::get($oNews->getID());
+				if ($oRetrievedNews) {
+					echo "Passed dashboard news retrieval test<br><br>";
+				}				
 			} else {
 				echo "Failed dashboard news update test: " . $_SESSION["errorMessage"];
 			}
-
+	        /*
 			echo "<b>Testing dashboard news deletion</b><br>";
 			if ($oNews->delete()) {
 				echo "Passed dashboard news deletion test<br>";
 			} else {
 				echo "Failed dashboard news deletion test: " . $_SESSION["errorMessage"];
 			}
-
+			*/
 		} else {
 			echo "Failed dashboard news storage test: " . $_SESSION["errorMessage"] . "<br>";
 			echo "Tests NOT run: (a)dashboard news deletion (b) dashboard news update<br>";
@@ -85,5 +94,5 @@ require_once("$default->fileSystemRoot/lib/dashboard/DashboardNews.inc");
 		echo "Failed dashboard news creation test<br>";
 		echo "Tests NOT run: (a)getting and setting (b)dashboard news storage (c)dashboard news deletion<br>";
 	}
-//}
+}
 ?>
