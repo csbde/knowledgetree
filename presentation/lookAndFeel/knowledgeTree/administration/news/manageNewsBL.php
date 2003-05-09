50ca97089d0a5d064519f0a3b2b7ddd27ba1b9fb
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-05-09 15:24:43 +0000

    initial presentation files for dashboard news
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1782 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/administration/news/manageNewsBL.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once("../../../../../config/dmsDefaults.php");
+require_once("$default->fileSystemRoot/lib/dashboard/DashboardNews.inc");
+require_once("$default->fileSystemRoot/lib/visualpatterns/PatternMainPage.inc");
+require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+require_once("$default->fileSystemRoot/lib/visualpatterns/PatternTableSqlQuery.inc");
+require_once("$default->uiDirectory/administration/news/newsUI.inc");
+
+/**
+ * $Id$
+ *  
+ * This page handles the administration of dashboard news items
+ *
+ * Licensed under the GNU GPL. For full terms see the file DOCS/COPYING.
+ *
+ * @version $Revision$
+ * @author Michael Joseph <michael@jamwarehouse.com>, Jam Warehouse (Pty) Ltd, South Africa
+ * @package presentation.lookAndFeel.knowledgeTree.administration.news
+ */
+
+if (checkSession()) {	
+		
+    // instantiate my content pattern
+    $oContent = new PatternCustom();
+    
+    if (isset($fNewsID)) {
+    	// we have an id, so we're either deleting, editing or previewing a current item
+    } else {
+    	// no news id, so list the current news items
+    	$oContent->setHtml(renderViewNewsPage());
+    }        
+
+	//$main->setFormAction("$default->rootUrl/presentation/lookAndFeel/knowledgeTree/create.php?fRedirectURL=".urlencode("$default->rootUrl/control.php?action=addLinkSuccess&fLinkID="));
+    //$main->setHasRequiredFields(true);
+
+	// build the page
+	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");    
+	$main->setCentralPayload($oContent);
+	$main->render();	
+} 
+?>
