c2a4e2947b891479b3185b1e025aa9320f95fe3a
Michael Joseph <michael@knowledgetree.com>
2003-05-09 15:52:12 +0000

    initial version
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1783 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/dashboard/news/displayNewsImage.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once("../../../../../config/dmsDefaults.php");
+require_once("$default->fileSystemRoot/lib/dashboard/DashboardNews.inc");
+
+/**
+ * $Id$
+ *  
+ * Displays a news item image
+ *
+ * Licensed under the GNU GPL. For full terms see the file DOCS/COPYING.
+ *
+ * @version $Revision$
+ * @author Michael Joseph <michael@jamwarehouse.com>, Jam Warehouse (Pty) Ltd, South Africa
+ * @package presentation.lookAndFeel.knowledgeTree.administration.news
+ */
+if (isset($fNewsID)) {
+	$oNews = DashboardNews::get($fNewsID);
+	$oNews->displayImage();
+}
