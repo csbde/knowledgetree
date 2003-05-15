be79881b174ed5750c294c47a017238d6eb41399
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-05-15 11:44:19 +0000

    *** empty log message ***
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1852 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/foldermanagement/viewDependantDocumentsBL.php
+++ presentation/lookAndFeel/knowledgeTree/foldermanagement/viewDependantDocumentsBL.php
@@ -16,6 +16,7 @@ require_once("../../../../config/dmsDefaults.php");
 
 if (checkSession()) {	
 	if (isset($fFolderID) && isset($fFolderCollaborationID)) {
+	echo $fUserID;
 	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
 	require_once("$default->fileSystemRoot/lib/foldermanagement/FolderCollaboration.inc");
 	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
@@ -29,8 +30,7 @@ if (checkSession()) {
 		$oFolderCollaboration = FolderCollaboration::get($fFolderCollaborationID);
 		if ($oFolderCollaboration->hasDocumentInProcess()) {
 			//can't add document links if a document is currently undergoing the
-			//collaboration process
-			echo "in here";
+			//collaboration process			
 			
 		} if (isset($fForAdd)) {
 			//we are adding a new dependant document			
@@ -38,7 +38,8 @@ if (checkSession()) {
 			$oPatternCustom = & new PatternCustom();
 			$oPatternCustom->setHtml(getAddPage($fFolderCollaborationID, $fFolderID, (isset($fUnitID) ? $fUnitID : -1), (isset($fDocumentTitle) ? $fDocumentTitle : "")));
     		$main->setCentralPayload($oPatternCustom);
-    	    $main->setFormAction("document.MainForm.action = " . $_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fForAdd=1;document.MainForm.submit();");    		
+    	    $main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fForStore=1");
+    	    $main->setHasRequiredFields(true);    		
     		$main->render();
 		} else {
 			include_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
