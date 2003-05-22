9e95d2f38b2eb04213bfe34e8dac4d30085d53c6
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-05-22 11:12:36 +0000

    Initial revision.  Functionality to remove the link between a parent and a child document
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1893 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/removeDocumentLinkBL.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+* Business logic for unlinking a parent document from a child documenbt
+*
+* Expected form variables:
+*	$fDocumentLinkID - primary key of document link to delete
+*	$fChildDocumentID - primary key of child document to which parent document is linked
+*	$fParentDocumentID - primary key of parent document
+*
+* @author Rob Cherry, Jam Warehouse (Pty) Ltd, South Africa
+* @date 22 May 2003
+*
+*/
+
+
+require_once("../../../../config/dmsDefaults.php");
+
+if (checkSession()) {
+	require_once("$default->fileSystemRoot/lib/security/permission.inc");
+	require_once("$default->fileSystemRoot/lib/documentmanagement/Document.inc");	
+	require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentLink.inc");
+	require_once("$default->fileSystemRoot/presentation/Html.inc");
+	require_once("removeDocumentLinkUI.inc");
+	
+	
+	if (Permission::userHasDocumentWritePermission($fParentDocumentID)) {
+		if (isset($fForDelete)) {
+			//deleting a document link
+			$oDocumentLink = DocumentLink::get($fDocumentLinkID);
+			if ($oDocumentLink->delete()) {
+				redirect("$default->rootUrl/control.php?action=viewDocument&fDocumentID=$fParentDocumentID");				
+			} else {
+				//an error occured whilst trying to delete the document link
+				require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+										
+				$oParentDocument = Document::get($fParentDocumentID);
+				$oChildDocument = Document::get($fChildDocumentID);
+			
+				$oPatternCustom = & new PatternCustom();
+				$oPatternCustom->setHtml(getPage($oParentDocument->getName(), $oChildDocument->getName(), $fParentDocumentID));
+				$main->setCentralPayload($oPatternCustom);
+				$main->setFormAction($_SERVER["PHP_SELF"] . "?fDocumentLinkID=$fDocumentLinkID&fParentDocumentID=$fParentDocumentID&fChildDocumentID=$fChildDocumentID&fForDelete=1");
+				$mail->setErrorMessage("An error occured whilst attempting to delete the link between the two documents");	
+				$main->render();				
+			}			
+		} else {
+			//user has document write permission and can therefore remove the
+			//link between the two documents		
+			require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+										
+			$oParentDocument = Document::get($fParentDocumentID);
+			$oChildDocument = Document::get($fChildDocumentID);
+			
+			$oPatternCustom = & new PatternCustom();
+			$oPatternCustom->setHtml(getPage($oParentDocument->getName(), $oChildDocument->getName(), $fParentDocumentID));
+			$main->setCentralPayload($oPatternCustom);
+			$main->setFormAction($_SERVER["PHP_SELF"] . "?fDocumentLinkID=$fDocumentLinkID&fParentDocumentID=$fParentDocumentID&fChildDocumentID=$fChildDocumentID&fForDelete=1");	
+			$main->render();	
+		}
+	} else {
+		//user does not have permission to be here
+		require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+										
+		$oPatternCustom = & new PatternCustom();
+		$oPatternCustom->setHtml("");
+		$main->setCentralPayload($oPatternCustom);
+		$main->setErrorMessage("You do not have permission to delete links between documents");			
+		$main->render();
+	}
+}
+
+
+?>
