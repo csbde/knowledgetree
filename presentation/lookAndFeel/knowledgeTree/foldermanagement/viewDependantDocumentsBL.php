cca95445d86ab847465439956166f9289e418a5c
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-05-22 08:19:12 +0000

    Added delete button to depedant documents
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1884 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/foldermanagement/viewDependantDocumentsBL.php
+++ presentation/lookAndFeel/knowledgeTree/foldermanagement/viewDependantDocumentsBL.php
@@ -15,110 +15,119 @@
 require_once("../../../../config/dmsDefaults.php");
 
 if (checkSession()) {	
-	if (isset($fFolderID) && isset($fFolderCollaborationID)) {	
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-	require_once("$default->fileSystemRoot/lib/foldermanagement/FolderCollaboration.inc");
-	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+	if (isset($fFolderID) && isset($fFolderCollaborationID)) {
+		
+		//folder and collaboration are selected	
+		require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+		require_once("$default->fileSystemRoot/lib/foldermanagement/FolderCollaboration.inc");
+		require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
 		require_once("$default->fileSystemRoot/lib/documentmanagement/Document.inc");
-	require_once("$default->fileSystemRoot/lib/users/User.inc");	
-	require_once("$default->fileSystemRoot/lib/documentmanagement/DependantDocumentTemplate.inc");
-	require_once("$default->fileSystemRoot/presentation/Html.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternTableSqlQuery.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
-	require_once("viewDependantDocumentsUI.inc");	
+		require_once("$default->fileSystemRoot/lib/users/User.inc");	
+		require_once("$default->fileSystemRoot/lib/documentmanagement/DependantDocumentTemplate.inc");
+		require_once("$default->fileSystemRoot/presentation/Html.inc");
+		require_once("$default->fileSystemRoot/lib/visualpatterns/PatternTableSqlQuery.inc");
+		require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+		require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+		require_once("viewDependantDocumentsUI.inc");	
 	
-	//folder and collaboration are selected
-		if (isset($fForStore)) {
-			$oDependantDocumentTemplate;			
-			if ($fTemplateDocumentID == "-1") {				
-				$oDependantDocumentTemplate = & new DependantDocumentTemplate($fDocumentTitle, $fUserID, $fFolderCollaborationID);
-			} else {
-				$oDependantDocumentTemplate = & new DependantDocumentTemplate($fDocumentTitle, $fUserID, $fFolderCollaborationID, $fTemplateDocumentID);
-			}
-			if (!($oDependantDocumentTemplate->create())) {
-				include_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-			
-				$oPatternCustom = & new PatternCustom();
-				$oPatternCustom->setHtml(getViewPage($fFolderCollaborationID, $fFolderID));
-    			$main->setCentralPayload($oPatternCustom);
-    	    	$main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fForAdd=1");
-    	    	$main->setErrorMessage("An error occured attempting to store the depedant document");    		
-    			$main->render();								 
-			} else {				
+	
+		if (Permission::userHasFolderWritePermission($fFolderID)) {
+			//user has folder write permission
+			if (isset($fForStore)) {
+				$oDependantDocumentTemplate;			
+				if ($fTemplateDocumentID == "-1") {				
+					$oDependantDocumentTemplate = & new DependantDocumentTemplate($fDocumentTitle, $fUserID, $fFolderCollaborationID);
+				} else {
+					$oDependantDocumentTemplate = & new DependantDocumentTemplate($fDocumentTitle, $fUserID, $fFolderCollaborationID, $fTemplateDocumentID);
+				}
+				if (!($oDependantDocumentTemplate->create())) {
+					include_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+				
+					$oPatternCustom = & new PatternCustom();
+					$oPatternCustom->setHtml(getViewPage($fFolderCollaborationID, $fFolderID));
+	    			$main->setCentralPayload($oPatternCustom);
+	    	    	$main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fForAdd=1");
+	    	    	$main->setErrorMessage("An error occured attempting to store the depedant document");    		
+	    			$main->render();								 
+				} else {				
+					redirect("$default->rootUrl/control.php?action=viewDependantDocument&fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID");
+				}			
+			} else if (isset($fForAdd)) {
+				//we are adding a new dependant document
+				$oFolderCollaboration = FolderCollaboration::get($fFolderCollaborationID);
+				if ($oFolderCollaboration->hasDocumentInProcess()) {
+					include_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+				
+					$oPatternCustom = & new PatternCustom();
+					$oPatternCustom->setHtml(getViewPage($fFolderCollaborationID, $fFolderID));
+	    			$main->setCentralPayload($oPatternCustom);
+		    	    $main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fForAdd=1");
+		    	    $main->setErrorMessage("You cannot add a new depedant document as there is currently a document in this folder undergoing collaboration");    		
+	    			$main->render();
+					
+				} else {						
+					include_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");		
+					
+					$oPatternCustom = & new PatternCustom();
+					$oPatternCustom->setHtml(getAddPage($fFolderCollaborationID, $fFolderID, (isset($fUnitID) ? $fUnitID : -1), (isset($fDocumentTitle) ? $fDocumentTitle : ""), (isset($fTemplateDocument) ? $fTemplateDocument : ""), (isset($fDocumentID) ? $fDocumentID : "") ));
+		    		$main->setCentralPayload($oPatternCustom);
+		    	    $main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fForStore=1");
+		    	    $main->setHasRequiredFields(true);    		
+		    		$main->render();
+				}
+			} else if (isset($fForEdit)) {
+				$oFolderCollaboration = FolderCollaboration::get($fFolderCollaborationID);
+				if ($oFolderCollaboration->hasDocumentInProcess()) {
+					//can't edit if there is a document currently undergoing collaboration
+					include_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+				
+					$oPatternCustom = & new PatternCustom();
+					$oPatternCustom->setHtml(getViewPage($fFolderCollaborationID, $fFolderID));
+	    			$main->setCentralPayload($oPatternCustom);
+		    	    $main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fForUpdate=1");
+		    	    $main->setErrorMessage("You cannot add a new depedant document as there is currently a document in this folder undergoing collaboration");    		
+	    			$main->render();
+					
+				} else {						
+					include_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+					
+					$oDependantDocumentTemplate = DependantDocumentTemplate::get($fDependantDocumentTemplateID);				
+					if ($oDependantDocumentTemplate->getTemplateDocumentID() >= 1) {
+						$oDocument = Document::get($oDependantDocumentTemplate->getTemplateDocumentID());
+					}							
+					
+					$oPatternCustom = & new PatternCustom();				
+					$oPatternCustom->setHtml(getEditPage($fFolderID, $fDependantDocumentTemplateID, $fFolderCollaborationID, $oDependantDocumentTemplate->getDocumentTitle(), (isset($oDocument) ? $oDocument->getName() : ""), (isset($oDocument) ? $oDependantDocumentTemplate->getTemplateDocumentID() : null), $oDependantDocumentTemplate->getDefaultUserID()));
+		    		$main->setCentralPayload($oPatternCustom);
+		    	    $main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fDependantDocumentTemplateID=$fDependantDocumentTemplateID&fForUpdate=1");
+		    	    $main->setHasRequiredFields(true);    		
+		    		$main->render();
+				}
+			} else if (isset($fForUpdate)) {
+				$oDependantDocumentTemplate = DependantDocumentTemplate::get($fDependantDocumentTemplateID);
+				$oDependantDocumentTemplate->setDefaultUserID($fUserID);
+				$oDependantDocumentTemplate->setDocumentTitle($fDocumentTitle);
+				$oDependantDocumentTemplate->setTemplateDocumentID((isset($fTemplateDocumentID) ? $fTemplateDocumentID : null));
+				$oDependantDocumentTemplate->update();			
+				
 				redirect("$default->rootUrl/control.php?action=viewDependantDocument&fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID");
-			}			
-		} else if (isset($fForAdd)) {
-			//we are adding a new dependant document
-			$oFolderCollaboration = FolderCollaboration::get($fFolderCollaborationID);
-			if ($oFolderCollaboration->hasDocumentInProcess()) {
+				
+			} else {
 				include_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-			
+				
 				$oPatternCustom = & new PatternCustom();
 				$oPatternCustom->setHtml(getViewPage($fFolderCollaborationID, $fFolderID));
-    			$main->setCentralPayload($oPatternCustom);
-	    	    $main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fForAdd=1");
-	    	    $main->setErrorMessage("You cannot add a new depedant document as there is currently a document in this folder undergoing collaboration");    		
-    			$main->render();
-				
-			} else {						
-				include_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");		
-				
-				$oPatternCustom = & new PatternCustom();
-				$oPatternCustom->setHtml(getAddPage($fFolderCollaborationID, $fFolderID, (isset($fUnitID) ? $fUnitID : -1), (isset($fDocumentTitle) ? $fDocumentTitle : ""), (isset($fTemplateDocument) ? $fTemplateDocument : ""), (isset($fDocumentID) ? $fDocumentID : "") ));
 	    		$main->setCentralPayload($oPatternCustom);
-	    	    $main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fForStore=1");
-	    	    $main->setHasRequiredFields(true);    		
-	    		$main->render();
+	    	    $main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fForAdd=1");    		
+	    		$main->render();   			
 			}
-		} else if (isset($fForEdit)) {
-			$oFolderCollaboration = FolderCollaboration::get($fFolderCollaborationID);
-			if ($oFolderCollaboration->hasDocumentInProcess()) {
-				//can't edit if there is a document currently undergoing collaboration
-				include_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-			
-				$oPatternCustom = & new PatternCustom();
-				$oPatternCustom->setHtml(getViewPage($fFolderCollaborationID, $fFolderID));
-    			$main->setCentralPayload($oPatternCustom);
-	    	    $main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fForUpdate=1");
-	    	    $main->setErrorMessage("You cannot add a new depedant document as there is currently a document in this folder undergoing collaboration");    		
-    			$main->render();
-				
-			} else {						
-				include_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-				
-				$oDependantDocumentTemplate = DependantDocumentTemplate::get($fDependantDocumentTemplateID);				
-				if ($oDependantDocumentTemplate->getTemplateDocumentID() >= 1) {
-					$oDocument = Document::get($oDependantDocumentTemplate->getTemplateDocumentID());
-				}							
-				
-				$oPatternCustom = & new PatternCustom();				
-				$oPatternCustom->setHtml(getEditPage($fFolderID, $fDependantDocumentTemplateID, $fFolderCollaborationID, $oDependantDocumentTemplate->getDocumentTitle(), (isset($oDocument) ? $oDocument->getName() : ""), (isset($oDocument) ? $oDependantDocumentTemplate->getTemplateDocumentID() : null), $oDependantDocumentTemplate->getDefaultUserID()));
-	    		$main->setCentralPayload($oPatternCustom);
-	    	    $main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fDependantDocumentTemplateID=$fDependantDocumentTemplateID&fForUpdate=1");
-	    	    $main->setHasRequiredFields(true);    		
-	    		$main->render();
-			}
-		} else if (isset($fForUpdate)) {
-			$oDependantDocumentTemplate = DependantDocumentTemplate::get($fDependantDocumentTemplateID);
-			$oDependantDocumentTemplate->setDefaultUserID($fUserID);
-			$oDependantDocumentTemplate->setDocumentTitle($fDocumentTitle);
-			$oDependantDocumentTemplate->setTemplateDocumentID((isset($fTemplateDocumentID) ? $fTemplateDocumentID : null));
-			$oDependantDocumentTemplate->update();			
-			
-			redirect("$default->rootUrl/control.php?action=viewDependantDocument&fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID");
-			
 		} else {
-			include_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-			
-			$oPatternCustom = & new PatternCustom();
-			$oPatternCustom->setHtml(getViewPage($fFolderCollaborationID, $fFolderID));
-    		$main->setCentralPayload($oPatternCustom);
-    	    $main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fForAdd=1");    		
-    		$main->render();   			
-		} 
-	
+			//redirect the user back to their start page if they somehow
+			//got here without the relevant permission
+			redirect($default->root_url . "/control.php");			
+		}
 	}
+	
 
 }
 
