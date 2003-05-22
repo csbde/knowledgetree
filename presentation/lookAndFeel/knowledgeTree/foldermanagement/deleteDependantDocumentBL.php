dbf1d88e1dc8f70b88e4afa8332bddc269581dc5
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-05-22 08:18:54 +0000

    Initial revision.  Functionality for deleting a depedant document
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1883 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/foldermanagement/deleteDependantDocumentBL.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+* Business logic for deleting a dependant document
+*
+* Expected variables:
+*	$fFolderCollaborationID: primary key of folder collaboration to check
+*	$fFolderID: folder we are currently editing
+*	$fDependantDocumentTemplateID: primary key of dependant document to be deleted
+*
+* @author Rob Cherry, Jam Warehouse (Pty) Ltd, South Africa
+* @date 14 May 2003
+* @package presentation.lookAndFeel.knowledgeTree.foldermanagement
+*/
+
+require_once("../../../../config/dmsDefaults.php");
+
+if (checkSession()) {	
+	if (isset($fFolderID) && isset($fFolderCollaborationID)) {				
+		require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+		require_once("$default->fileSystemRoot/lib/users/User.inc");
+		require_once("$default->fileSystemRoot/lib/documentmanagement/DependantDocumentTemplate.inc");
+		require_once("$default->fileSystemRoot/presentation/Html.inc");		
+		require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+		require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+		require_once("deleteDependantDocumentUI.inc");	
+	
+	
+		if (Permission::userHasFolderWritePermission($fFolderID)) {
+			//user has permission to alter folder contents
+			if (isset($fForDelete)) {
+				$oDependantDocumentTemplate = DependantDocumentTemplate::get($fDependantDocumentTemplateID);
+				if ($oDependantDocumentTemplate->delete()) {
+					redirect($default->rootUrl . "/control.php?action=viewDependantDocument&fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID");				
+				} else {
+					$oDependantDocumentTemplate = DependantDocumentTemplate::get($fDependantDocumentTemplateID);
+					$oUser = User::get($oDependantDocumentTemplate->getDefaultUserId());
+				
+					$oPatternCustom = & new PatternCustom();				
+					$oPatternCustom->setHtml(getPage($fFolderID, $fFolderCollaborationID, $oDependantDocumentTemplate->getDocumentTitle(), $oUser->getName(), 'sTemplateDocument'));
+	    			$main->setCentralPayload($oPatternCustom);
+	    	    	$main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fDependantDocumentTemplateID=$fDependantDocumentTemplateID&fForDelete=1");
+	    	    	$main->setErrorMessage("An error occured while attempting to delete the depedant document");	    	        		
+	    			$main->render();						
+				}
+				
+			} else {				
+				include_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+				
+				$oDependantDocumentTemplate = DependantDocumentTemplate::get($fDependantDocumentTemplateID);
+				$oUser = User::get($oDependantDocumentTemplate->getDefaultUserId());
+				
+				$oPatternCustom = & new PatternCustom();				
+				$oPatternCustom->setHtml(getPage($fFolderID, $fFolderCollaborationID, $oDependantDocumentTemplate->getDocumentTitle(), $oUser->getName(), 'sTemplateDocument'));
+	    		$main->setCentralPayload($oPatternCustom);
+	    	    $main->setFormAction($_SERVER["PHP_SELF"] . "?fFolderID=$fFolderID&fFolderCollaborationID=$fFolderCollaborationID&fDependantDocumentTemplateID=$fDependantDocumentTemplateID&fForDelete=1");	    	        		
+	    		$main->render();	
+				
+			}
+
+		
+		}
+		
+	}
+}
+
+?>
+		
