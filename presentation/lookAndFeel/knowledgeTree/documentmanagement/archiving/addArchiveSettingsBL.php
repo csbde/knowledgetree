bc81e443542242e0d4a955452ba1aa7ab9f484b1
Michael Joseph <michael@knowledgetree.com>
2003-05-26 14:09:49 +0000

    completed archive settings pages
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1918 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/documentmanagement/archiving/addArchiveSettingsBL.php
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/archiving/addArchiveSettingsBL.php
@@ -3,10 +3,7 @@
 require_once("../../../../../config/dmsDefaults.php");
 require_once("$default->fileSystemRoot/lib/documentmanagement/Document.inc");
 
-require_once("$default->fileSystemRoot/lib/archiving/DocumentArchiving.inc");
-require_once("$default->fileSystemRoot/lib/archiving/ArchivingUtilisationSettings.inc");
-require_once("$default->fileSystemRoot/lib/archiving/ArchivingDateSettings.inc");
-require_once("$default->fileSystemRoot/lib/archiving/TimePeriod.inc");
+require_once("$default->fileSystemRoot/lib/archiving/DocumentArchiveSettingsFactory.inc");
 
 require_once("$default->fileSystemRoot/lib/visualpatterns/PatternMainPage.inc");
 require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
@@ -27,70 +24,16 @@ require_once("$default->uiDirectory/documentmanagement/archiving/archiveSettings
  * @package presentation.lookAndFeel.knowledgeTree.documentmanagement
  */
 
-class DocumentArchiveSettingsFactory {
-	function create($iDocumentID, $iArchivingTypeID, $dExpirationDate, $iDocumentTransactionID, $iTimeUnitID, $iUnits) {
-		global $default;		
-		$sArchivingType = lookupName($default->owl_archiving_type_lookup_table, $iArchivingTypeID);
-				
-		// search for an existing time period id		
-		$aTimePeriod = TimePeriod::getList("time_unit_id=$iTimeUnitID AND units=$iUnits");
-		if (count($aTimePeriod) > 0) {
-			$iTimePeriodID = $aTimePeriod[0]->getID();
-		} else {
-			// create it
-			$oTimePeriod = new TimePeriod($iTimeUnitID, $iUnits);
-			if ($oTimePeriod->create()) {
-				$iTimePeriodID = $oTimePeriod->getID();
-			} else {
-				$default->log->error("couldn't create time period- " . arrayToString($oTimePeriod));
-				return false;
-			}
-		}
-		
-		// construction strings
-		switch ($sArchivingType) {
-			case "Date" : 			$sSearchConstruction = "\$aArchiveSettings = ArchivingDateSettings::getList(\"expiration_date='$dExpirationDate' AND time_period_id=$iTimePeriodID\");";
-									$sConstruction  = "\$oArchiveSettings = new ArchivingDateSettings($dExpirationDate, $iTimePeriodID);";
-									break;
-			case "Utilisation" : 	$sSearchConstruction = "\$aArchiveSettings = ArchivingUtilisationSettings::getList(\"document_transaction_id=$iDocumentTransactionID AND time_period_id=$iTimePeriodID\");";
-									$sConstruction  = "\$oArchiveSettings = new ArchivingUtilisationSettings($iDocumentTransactionID, $iTimePeriodID);";
-									break;
-		}
-		
-		// search for the settings first
-		eval($sSearchConstruction);
-		if (count($aArchiveSettings) > 0) {
-			$iArchiveSettingsID = $aArchiveSettings[0]->getID();
-		} else {
-			// create them			
-			eval($sConstruction);				
-			if ($oArchiveSettings->create()) {
-				$iArchiveSettingsID = $oArchiveSettings->getID();
-			} else {
-				$default->log->error("couldn't create archive settings- " . arrayToString($oArchiveSettings));
-				return false;
-			}
-			
-			// now link to the documents
-			$oDocumentArchiving = new DocumentArchiving($iDocumentID, $iArchivingTypeID, $iArchiveSettingsID);
-			if ($oDocumentArchiving->create()) {
-				return true;
-			} else {
-				$default->log->error("couldn't create document archiving - " . arrayToString($oDocumentArchiving));
-				return false;
-			}				
-		}
-	}
-}
-
 if (checkSession()) {	
 	global $default;
 			
     // instantiate my content pattern
     $oContent = new PatternCustom();
-	$default->log->info(arrayToString($_REQUEST));
+
     if ($fStore) {
-    	if (DocumentArchiveSettingsFactory::create($fDocumentID, $fArchivingTypeID, $fExpirationDate, $fDocumentTransactionID, $fTimeUnitID, $fUnits)) {
+    	$oDASFactory = new DocumentArchiveSettingsFactory($fArchivingTypeID);
+    	
+    	if ($oDASFactory->create($fDocumentID, $fExpirationDate, $fDocumentTransactionID, $fTimeUnitID, $fUnits)) {
 			// created, redirect to view page
 			redirect("$default->rootUrl/control.php?action=viewDocument&fDocumentID=$fDocumentID");
     	} else {
@@ -102,10 +45,9 @@ if (checkSession()) {
   		    	
     } elseif (isset($fArchivingTypeID)) {
     	// the archiving type has been chosen, so display the correct form   	
-		// display the edit/add page
 		$oContent->setHtml(renderAddArchiveSettingsPage($fArchivingTypeID));    	
     } else {
-		// display the choose archiving type page
+		// display the select archiving type page
 		$oContent->setHtml(renderAddArchiveSettingsPage(null));    	
     }    	
              
