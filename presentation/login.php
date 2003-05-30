d22673310ebdb0bb308fc35032320f47ac39d66d
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-05-30 10:37:18 +0000

    reformatted javascript
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@1985 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/login.php
+++ presentation/login.php
@@ -31,20 +31,22 @@ if ($loginAction == "loginForm") {
 
 	<SCRIPT TYPE=\"text/javascript\">
 	<!--
-	function submitenter(myfield,e)
-	{
-	var keycode;
-	if (window.event) keycode = window.event.keyCode;
-	else if (e) keycode = e.which;
-	else return true;
-	
-	if (keycode == 13)
-	   {
-	   myfield.form.submit();
-	   return false;
-	   }
-	else
-	   return true;
+	function submitenter(myfield,e)	{
+		var keycode;
+		if (window.event) { 
+			keycode = window.event.keyCode;
+		} else if (e) {
+			keycode = e.which;
+		} else {
+			return true;
+		}
+		
+		if (keycode == 13) {
+		   myfield.form.submit();
+		   return false;
+		} else {
+		   return true;
+		}
 	}
 	//-->
 	</SCRIPT
