847795ffb0fd1c1b544cf5590d010021c19e392c
omar <omar@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-06-02 10:20:15 +0000

    Removed finding file error
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2019 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/groupmanagement/listGroupsBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/groupmanagement/listGroupsBL.php
@@ -12,8 +12,7 @@
 
 if (checkSession()) {    
     require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");    
-    require_once("listGroupsUI.inc"); 
-    require_once("../adminUI.inc");    
+    require_once("listGroupsUI.inc");        
 	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
     require_once("$default->fileSystemRoot/lib/documentmanagement/Document.inc");
     require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
