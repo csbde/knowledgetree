141a9a6e6c51cf505ee44a73e7202ab01d318c7a
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-07-07 15:15:15 +0000

    refactored, tidied and added confirmation page for removing document field from document type (fixes #2639)
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2313 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/doctypemanagement/editDocTypeFieldsBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/doctypemanagement/editDocTypeFieldsBL.php
@@ -23,171 +23,48 @@ if (checkSession()) {
 	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
 	require_once("$default->fileSystemRoot/presentation/Html.inc");
 	
-	
-	$oPatternCustom = & new PatternCustom();		
-	
-	// if a new DocType has been added
-	// coming from manual edit page	
-	if (isset($fUpdate)) {
-		
-		//$oDocType = DocumentType::get($fDocTypeID);
-		//$oDocType->setName($fDocTypeName);
-		
-		// store type specific field id's in an array
-		$aGenericDocFields  = DocumentField::getGenericFields();
-		$aSpecificDocFields = DocumentTypeFieldLink::getSpecificFields($fDocTypeID);
-		
-		 echo "<li><pre>" . arrayToString($aSpecificDocFields ) . "</pre></li></ul>";
-		
-		/**
-		if ($oDocType->update()) {
-				// if successfull print out success message
-				$oPatternCustom->setHtml(getEditPageSuccess());
-				
-		} else {
-				// if fail print out fail message
-				$oPatternCustom->setHtml(getEditPageFail());
-		}
-		**/
-	} else if (isset($fDocTypeSelected)){		
-		// post back on DocType select from manual edit page	
-		// store type generic field id's in an array
-		$aGenericFields  = DocumentField::getGenericFields();
-		
-		// get all specific fields
-		$aAllSpecificFields  = DocumentField::getAllSpecificFields();
-		
-		
-		// store type specific field id's and names in an array for specifc doctype
-		$aSpecificFields = DocumentTypeFieldLink::getSpecificFields($fDocTypeID);
-		
-		
-		$oPatternCustom->setHtml(getDetailsPage($fDocTypeID, $aGenericFields, $aSpecificFields,  $aAllSpecificFields));
-		$main->setFormAction($_SERVER["PHP_SELF"] . "?fShowDetails=1");
-		
-		
-	}else if(isset($fAdd))
-		{	
-			if($fIsMandatory == 1){
-				$iMandatory = 1;
-			}else{
-				$iMandatory = 0;
+	$oPatternCustom = & new PatternCustom();
+	if ($fDocTypeID) {
+		if ($fDocFieldID) {
+			$oDocTypeFieldLink = DocumentTypeFieldLink::getByFieldAndTypeIDs($fDocTypeID, $fDocFieldID);			
+			if (isset($fRemove)) {
+				if ($fConfirm) {
+					$oDocTypeFieldLink = DocumentTypeFieldLink::getByFieldAndTypeIDs($fDocTypeID, $fDocFieldID);
+					if ($oDocTypeFieldLink->delete()) {
+						// success
+						$oPatternCustom->setHtml(getSuccessPage("Document field successfully deleted.", $fDocTypeID));
+					} else {
+						// failure
+						$oPatternCustom->setHtml(getSuccessPage("Error deleting document field.", $fDocTypeID));
+					}
+				} else {
+					// ask for confirmation
+					$oPatternCustom->setHtml(getDeleteConfirmationPage($fDocTypeID, $fDocFieldID));
+				}
+			} else if(isset($fUpdateMandatory)) {
+				if ($fConfirm) {
+					if (isset($fIsMandatory)) {
+						$oDocTypeFieldLink->setIsMandatory(true);
+					} else {
+						$oDocTypeFieldLink->setIsMandatory(false);
+					}
+					$default->log->info("dfl=" . arrayToString($oDocTypeFieldLink));
+					$oDocTypeFieldLink->update();
+					$oPatternCustom->setHtml(getDetailsPage($fDocTypeID));
+				} else {
+					// display edit form
+					$oPatternCustom->setHtml(getEditDocumentFieldLinkPage($oDocTypeFieldLink));
+					$main->setFormAction($_SERVER["PHP_SELF"] . "?fUpdateMandatory=1");
+				}
 			}
-	
-			$oDocTypeField = new DocumentTypeFieldLink($fDocTypeID, $fDocFieldTypeID, $iMandatory);
-			
-			$oDocTypeField->create();
-			
-							
-			// post back on DocType select from manual edit page	
-			// store type generic field id's in an array
-			//$oDocFieldType = new DocumentTypeFieldLink(
-			$aGenericFields  = DocumentField::getGenericFields();
-		
-			// get all specific fields
-			$aAllSpecificFields  = DocumentField::getAllSpecificFields();
-		
-		
-			// store type specific field id's and names in an array for specifc doctype
-			$aSpecificFields = DocumentTypeFieldLink::getSpecificFields($fDocTypeID);
-			
-			$oPatternCustom->setHtml(getDetailsPage($fDocTypeID, $aGenericFields, $aSpecificFields,  $aAllSpecificFields));
-
-			$main->setFormAction($_SERVER["PHP_SELF"] . "?fDocTypeSelected=1");					
-			//$oPatternCustom->setHtml(getEditPageSuccess());
-			
-		
-	}else if(isset($fRemove)){
-			
-			$fFieldID = $fRemove;
-			
-			// does'nt matter wot ismandatory is..not checking for it when deleting
-			$iMandatory = 0;
-			//create new object
-			$oDocTypeField = new DocumentTypeFieldLink($fDocTypeID, $fFieldID, $iMandatory);
-			
-			//delete it by first getting hte corresponding id
-			$oDocTypeField ->setDocTypeFieldID($fDocTypeID, $fFieldID);
-			$oDocTypeField->delete();
-			
-			// post back on DocType select from manual edit page	
-			// store type generic field id's in an array
-			//$oDocFieldType = new DocumentTypeFieldLink(
-			$aGenericFields  = DocumentField::getGenericFields();
-		
-			// get all specific fields
-			$aAllSpecificFields  = DocumentField::getAllSpecificFields();
-		
-		
-			// store type specific field id's and names in an array for specifc doctype
-			$aSpecificFields = DocumentTypeFieldLink::getSpecificFields($fDocTypeID);
-			
-			$oPatternCustom->setHtml(getDetailsPage($fDocTypeID, $aGenericFields, $aSpecificFields,  $aAllSpecificFields));
-			$main->setFormAction($_SERVER["PHP_SELF"] . "?fDocTypeSelected=1");
-	
-	}else if(isset($fEdit)){ 
-		$fFieldID = $fEdit;
-		
-		$iMandatory = 0;
-		 
-		$oDocTypeField = new DocumentTypeFieldLink($fDocTypeID, $fFieldID, $iMandatory);
-			
-		//delete it by first getting hte corresponding id
-		$oDocTypeField ->setDocTypeFieldID($fDocTypeID, $fFieldID);
-
-		$iDocTypeFieldID = $oDocTypeField->getID();
-						
-		$oPatternCustom->setHtml(getMandatoryPage($fFieldID,$iDocTypeFieldID));
-		$main->setFormAction($_SERVER["PHP_SELF"] . "?fUpdateMandatory=1");
-			
-	
-	} else if(isset($fUpdateMandatory)){ 
-						 
-		$oDocTypeField = new DocumentTypeFieldLink($fDocTypeID, $fFieldID, $fMandatory);
-			
-			//delete it by first getting hte corresponding id
-		$oDocTypeField->setDocTypeFieldID($fDocTypeID, $fFieldID);
-		
-		if(isset($fIsMandatory)){
-			$oDocTypeField->setIsMandatory(true);
-		}else{
-			$oDocTypeField->setIsMandatory(false);
+		} else {
+			$oPatternCustom->setHtml(getDetailsPage($fDocTypeID));
+			$main->setFormAction($_SERVER["PHP_SELF"] . "?fUpdateMandatory=1");
 		}
-		
-		$oDocTypeField->update();
-		
-		
-						
-			// post back on DocType select from manual edit page	
-			// store type generic field id's in an array
-			//$oDocFieldType = new DocumentTypeFieldLink(
-			$aGenericFields  = DocumentField::getGenericFields();
-		
-			// get all specific fields
-			$aAllSpecificFields  = DocumentField::getAllSpecificFields();
-		
-		
-			// store type specific field id's and names in an array for specifc doctype
-			$aSpecificFields = DocumentTypeFieldLink::getSpecificFields($fDocTypeID);
-			
-			$oPatternCustom->setHtml(getDetailsPage($fDocTypeID, $aGenericFields, $aSpecificFields,  $aAllSpecificFields));
-			$main->setFormAction($_SERVER["PHP_SELF"] . "?fDocTypeSelected=1");
-		
-		
-	
-	
-	}else {
-		// if nothing happens...just reload edit page
-		$oPatternCustom->setHtml(getEditPage(null));
-		$main->setFormAction($_SERVER["PHP_SELF"] ."?fDocTypeSelected=1");
-			
-	
+	} else {
+		// no document type selected to edit
+		// FIXME
 	}
-	
-
-		
-		
-	
 	
 	//render the page
 	$main->setCentralPayload($oPatternCustom);
