b69ebd60fa1b51e27e96e97384f58f0685aebffb
Michael Joseph <michael@knowledgetree.com>
2003-07-09 13:17:24 +0000

    fixed #2673- if a new document field has a lookup, then redirect to add lookup page
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2331 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/addDocFieldSuccess.php
+++ presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/addDocFieldSuccess.php
@@ -10,28 +10,35 @@
 
 require_once("../../../../../config/dmsDefaults.php");
 
-global $default;
-
 if(checkSession()) {
+	require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentField.inc");
+	
+	$oDocField = DocumentField::get($fDocFieldID);
+	if ($oDocField) {
+		// if we're setting lookup to be true, then prompt for an initial lookup value??
+		if ($oDocField->getHasLookup()) {
+			// and there are no metadata values for this lookup
+			// there shouldn't be since this has just been added- but lets be paranoid shall we?
+			if (DocumentField::getLookupCount($fDocFieldID) == 0) {
+				// then redirect to the edit metadata page
+				controllerRedirect("addMetaDataForField", "fDocFieldID=$fDocFieldID");
+			}
+		}
+	}
 
     // include the page template (with navbar)
     require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-
+	global $default;
     $Center .= renderHeading("Add Document Field");
     $Center .= "<TABLE BORDER=\"0\" CELLSPACING=\"2\" CELLPADDING=\"2\">\n";
     $Center .= "<tr>\n";
-    if ($fDocTypeID == -1) {
+    if ($fDocFieldID == -1) {
         $Center .= "<td><b>Document Field addition Unsuccessful!</b></td>\n";
         $Center .= "</tr>\n";
         $Center .= "<tr></tr>\n";
-        $Center .= "<tr>\n";
-        $Center .= "<td>Document Type already exists</td>\n";
-        $Center .= "</tr>\n";
-
     } else {
         $Center .= "<td><b>Document Field added Successfully!</b></td>\n";
         $Center .= "</tr>\n";
-
     }
     $Center .= "<tr></tr>\n";
     $Center .= "<tr></tr>\n";
