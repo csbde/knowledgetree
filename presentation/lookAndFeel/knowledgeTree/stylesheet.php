e368c370203d6d47e0bc3de4762995f95b095840
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-07-10 13:37:12 +0000

    moved browser positioning code
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2351 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/stylesheet.php
+++ presentation/lookAndFeel/knowledgeTree/stylesheet.php
@@ -1,77 +1,7 @@
 <?php
-header('mime_type', 'text/css');
 require("../../../config/dmsDefaults.php");
-
-$scroll = array();
-
-$browser = $default->browser;
-$version = $default->version;
-echo "$browser-$version";
-if ( ($browser == "moz") && ($version <= 4.79)) {
-    $scroll["upArrowLeft"] = "780";
-    $scroll["upArrowTop"] = "190";
-    $scroll["downArrowLeft"] = "780";
-    $scroll["downArrowTop"] = "585";
-    
-    $scroll["textBoxLeft"] = "145";
-    $scroll["textBoxTop"] = "190";
-    $scroll["textBoxWidth"] = "620";
-    $scroll["textBoxHeight"] = "395";  
-} elseif ( ($browser == "moz") && ($version == "5.0")) {
-    $scroll["upArrowLeft"] = "742";
-    $scroll["upArrowTop"] = "107";
-    $scroll["downArrowLeft"] = "742";
-    $scroll["downArrowTop"] = "588";
-    
-    $scroll["textBoxLeft"] = "135";
-    $scroll["textBoxTop"] = "105";
-    $scroll["textBoxWidth"] = "600";
-    $scroll["textBoxHeight"] = "490";
-} elseif ($browser == "ie") {
-    // MSIE
-    $scroll["upArrowLeft"] = "745";
-    $scroll["upArrowTop"] = "122";
-    $scroll["downArrowLeft"] = "745";
-    $scroll["downArrowTop"] = "604";
-    
-    $scroll["textBoxLeft"] = "134";
-    $scroll["textBoxTop"] = "118";
-    $scroll["textBoxWidth"] = "608";
-    $scroll["textBoxHeight"] = "493";
-} elseif ( ($browser == "ns") && ($version == "6.2.3") ) {
-    $scroll["upArrowLeft"] = "745";
-    $scroll["upArrowTop"] = "110";
-    $scroll["downArrowLeft"] = "745";
-    $scroll["downArrowTop"] = "697";
-    
-    $scroll["textBoxLeft"] = "132";
-    $scroll["textBoxTop"] = "104";
-    $scroll["textBoxWidth"] = "610";
-    $scroll["textBoxHeight"] = "595";
-} elseif ( ($browser == "ns") && ($version == "7.0") ) {
-    $scroll["upArrowLeft"] = "742";
-    $scroll["upArrowTop"] = "107";
-    $scroll["downArrowLeft"] = "742";
-    $scroll["downArrowTop"] = "588";
-    
-    $scroll["textBoxLeft"] = "132";
-    $scroll["textBoxTop"] = "103";
-    $scroll["textBoxWidth"] = "608";
-    $scroll["textBoxHeight"] = "497";	    
-} else {
-    $scroll["upArrowLeft"] = "742";
-    $scroll["upArrowTop"] = "107";
-    $scroll["downArrowLeft"] = "742";
-    $scroll["downArrowTop"] = "588";
-    
-    $scroll["textBoxLeft"] = "132";
-    $scroll["textBoxTop"] = "103";
-    $scroll["textBoxWidth"] = "608";
-    $scroll["textBoxHeight"] = "497";	
-}
+header('mime_type', 'text/css');
 ?>
-<pre>
-
 P {
 font-size : 9pt;
 font-family : Verdana, sans-serif;
@@ -191,21 +121,21 @@ SELECT {
 
 #divUp   {
     position:absolute; 
-    left: <?php echo $scroll["upArrowLeft"] ?>px; 
-    top: <?php echo $scroll["upArrowTop"] ?>px;}
+    left: <?php echo $default->scroll["upArrowLeft"] ?>px; 
+    top: <?php echo $default->scroll["upArrowTop"] ?>px;}
 
 #divDown {
     position:absolute; 
-    left: <?php echo $scroll["downArrowLeft"] ?>px; 
-    top: <?php echo $scroll["downArrowTop"] ?>px;}
+    left: <?php echo $default->scroll["downArrowLeft"] ?>px; 
+    top: <?php echo $default->scroll["downArrowTop"] ?>px;}
 
 #divScrollTextCont {
     position:absolute; 
-    left: <?php echo $scroll["textBoxLeft"] ?>px; 
-    top: <?php echo $scroll["textBoxTop"] ?>px; 
-    width: <?php echo $scroll["textBoxWidth"] ?>px; 
-    height: <?php echo $scroll["textBoxHeight"] ?>px; 
-    clip:rect(0px <?php echo $scroll["textBoxWidth"] ?>px <?php echo $scroll["textBoxHeight"] ?>px 0px); 
+    left: <?php echo $default->scroll["textBoxLeft"] ?>px; 
+    top: <?php echo $default->scroll["textBoxTop"] ?>px; 
+    width: <?php echo $default->scroll["textBoxWidth"] ?>px; 
+    height: <?php echo $default->scroll["textBoxHeight"] ?>px; 
+    clip:rect(0px <?php echo $default->scroll["textBoxWidth"] ?>px <?php echo $default->scroll["textBoxHeight"] ?>px 0px); 
     overflow:hidden; 
     visibility:hidden;
 }
