1ebb7d25264f64c4ecd090b96ee5175b278dd82c
Michael Joseph <michael@knowledgetree.com>
2003-07-11 13:18:06 +0000

    redirect to show document data on first edit
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2365 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/documentmanagement/modifySpecificMetaDataBL.php
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/modifySpecificMetaDataBL.php
@@ -26,8 +26,12 @@ if (checkSession()) {
 		require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
 		$oPatternCustom = & new PatternCustom();
 		$oPatternCustom->setHtml(getPage($fDocumentID, $oDocument->getDocumentTypeID(), $fFirstEdit));
-		$main->setCentralPayload($oPatternCustom);			
-		$main->setFormAction("../store.php?fReturnURL=" . urlencode("$default->rootUrl/control.php?action=viewDocument&fDocumentID=$fDocumentID&fShowSection=typeSpecificMetaData"));
+		$main->setCentralPayload($oPatternCustom);
+        if (isset($fFirstEdit)) {
+            $main->setFormAction("../store.php?fReturnURL=" . urlencode("$default->rootUrl/control.php?action=viewDocument&fDocumentID=$fDocumentID"));            
+        } else {
+			$main->setFormAction("../store.php?fReturnURL=" . urlencode("$default->rootUrl/control.php?action=viewDocument&fDocumentID=$fDocumentID&fShowSection=typeSpecificMetaData"));
+        }					
         $main->setHasRequiredFields(true);		
 		$main->render();
 	}
