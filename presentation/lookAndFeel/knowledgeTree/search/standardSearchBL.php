50d24b23f7bd25777df3538882ac0e0dbe9d62fd
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-07-29 14:53:22 +0000

    removed comments and formatted
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2433 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/search/standardSearchBL.php
+++ presentation/lookAndFeel/knowledgeTree/search/standardSearchBL.php
@@ -21,90 +21,82 @@ if (checkSession()) {
 	require_once("$default->fileSystemRoot/presentation/Html.inc");	
 	require_once("standardSearchUI.inc");
 	
-	//echo $fSearchText . "<br>" . $fFolderID;		
-	
 	if (!isset($fStartIndex)) {
 		$fStartIndex = 0;
 	}
-	//escape the search string
-	//$fStandardSearchString = addslashes($fStandardSearchString);
-	
-	
-		if (strlen($fBrowseType) > 0) {			
-			//the user was browsing by a specific type
-			switch ($fBrowseType) {
-			case "folder" :
-							 //echo "searching by folder browsetype";
-							//user was browsing a specific folder - search that folder							
-							if (!$fFolderID) {
-								//start at the root folder
-								$fFolderID = 1;
-							}								
-							require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-							$oPatternCustom = & new PatternCustom();                                
-                            $oPatternCustom->setHtml(getSeachResultsByFolder($fFolderID, $fStartIndex, $fSearchText));
-                            $main->setCentralPayload($oPatternCustom);
-                            $main->render();
-							break;
-			case "category" :
-							//echo "searching by category browse type";
-							//user was browsing by category - search all documents in that category
-							if (!$fCategoryName) {
-								//no category name specified, so just start at the root folder								
-								$fFolderID = 1;
-							}								
-							require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-							$oPatternCustom = & new PatternCustom();                                
-                            $oPatternCustom->setHtml(getSearchResultsByCategory($fFolderID, $fSearchText, $fStartIndex, $fCategoryName));
-                            $main->setCentralPayload($oPatternCustom);
-                            $main->render();
-							break;							
-			case "documentType" :
-							//echo "searching by documentType browseType";
-							//user was browsing by document type - search all documents in that doc type
-							if (!$fDocTypeID) {
-								//no document type specified, so just start at the root folder
-								$fFolderID = 1;
-							}							
-							require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-							$oPatternCustom = & new PatternCustom();                                
-                            $oPatternCustom->setHtml(getSearchResultsByDocumentType($fFolderID, $fSearchText, $fStartIndex, $fDocTypeID));
-                            $main->setCentralPayload($oPatternCustom);
-                            $main->render();
-							break;
-			default:
-				//search from the root folder down i.e. all documents
-				//echo "unknown search type";
-				break;
-			}
-		} else if (strlen($fFolderID) > 0) {
-			//the user was browsing a folder, search that folder
-			//echo "searching by folder id";			
-			require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-			$oPatternCustom = & new PatternCustom();            
-            $oPatternCustom->setHtml(getSeachResultsByFolder($fFolderID, $fStartIndex, $fSearchText));
-            $main->setCentralPayload($oPatternCustom);
-            $main->render();
-			
-		} else  if (strlen($fDocumentID) > 0) {
-			//echo "searching by document id";
-			//the user was viewing a document, search in that document's folder
-			$oDocument = Document::get($fDocumentID);
-			require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-			$oPatternCustom = & new PatternCustom();			
-			$oPatternCustom->setHtml(getSeachResultsByFolder($oDocument->getFolderID(), $fStartIndex, $fSearchText));
-			$main->setCentralPayload($oPatternCustom);
-			$main->render();												
-		} else {
-			//echo "searching by folder";
-			//search from the root folder down i.e. all documents			
-			$fFolderID = 1;
-			require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-			$oPatternCustom = & new PatternCustom();
-			$oPatternCustom->setHtml(getSeachResultsByFolder($fFolderID, $fStartIndex, $fSearchText));
-			$main->setCentralPayload($oPatternCustom);
-			$main->render();							
-		}		
+
+	if (strlen($fBrowseType) > 0) {			
+		//the user was browsing by a specific type
+		switch ($fBrowseType) {
+		case "folder" :
+						//user was browsing a specific folder - search that folder							
+						if (!$fFolderID) {
+							//start at the root folder
+							$fFolderID = 1;
+						}								
+						require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+						$oPatternCustom = & new PatternCustom();                                
+                        $oPatternCustom->setHtml(getSeachResultsByFolder($fFolderID, $fStartIndex, $fSearchText));
+                        $main->setCentralPayload($oPatternCustom);
+                        $main->render();
+						break;
+		case "category" :
+						//user was browsing by category - search all documents in that category
+						if (!$fCategoryName) {
+							//no category name specified, so just start at the root folder								
+							$fFolderID = 1;
+						}
+						require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+						$oPatternCustom = & new PatternCustom();                                
+                        $oPatternCustom->setHtml(getSearchResultsByCategory($fFolderID, $fSearchText, $fStartIndex, $fCategoryName));
+                        $main->setCentralPayload($oPatternCustom);
+                        $main->render();
+						break;							
+		case "documentType" :
+						//echo "searching by documentType browseType";
+						//user was browsing by document type - search all documents in that doc type
+						if (!$fDocTypeID) {
+							//no document type specified, so just start at the root folder
+							$fFolderID = 1;
+						}							
+						require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+						$oPatternCustom = & new PatternCustom();                                
+                        $oPatternCustom->setHtml(getSearchResultsByDocumentType($fFolderID, $fSearchText, $fStartIndex, $fDocTypeID));
+                        $main->setCentralPayload($oPatternCustom);
+                        $main->render();
+						break;
+		default:
+			//search from the root folder down i.e. all documents
+			break;
+		}
+	} else if (strlen($fFolderID) > 0) {
+		//the user was browsing a folder, search that folder
+		//echo "searching by folder id";			
+		require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+		$oPatternCustom = & new PatternCustom();            
+          $oPatternCustom->setHtml(getSeachResultsByFolder($fFolderID, $fStartIndex, $fSearchText));
+          $main->setCentralPayload($oPatternCustom);
+          $main->render();
+		
+	} else  if (strlen($fDocumentID) > 0) {
+		//echo "searching by document id";
+		//the user was viewing a document, search in that document's folder
+		$oDocument = Document::get($fDocumentID);
+		require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+		$oPatternCustom = & new PatternCustom();			
+		$oPatternCustom->setHtml(getSeachResultsByFolder($oDocument->getFolderID(), $fStartIndex, $fSearchText));
+		$main->setCentralPayload($oPatternCustom);
+		$main->render();												
+	} else {
+		//echo "searching by folder";
+		//search from the root folder down i.e. all documents			
+		$fFolderID = 1;
+		require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+		$oPatternCustom = & new PatternCustom();
+		$oPatternCustom->setHtml(getSeachResultsByFolder($fFolderID, $fStartIndex, $fSearchText));
+		$main->setCentralPayload($oPatternCustom);
+		$main->render();							
+	}
 }
 //echo "not searching"
 ?>
