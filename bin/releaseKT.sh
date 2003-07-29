5b7415dff9408e80b77ce6f0932febcb39c29003
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-07-29 08:32:39 +0000

    added release script
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2426 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ bin/releaseKT.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+#
+# script to checkout the latest tagged build from cvs and upload
+# to the remote server of your choice
+
+## functions
+
+# displays the script usage message 
+#
+usage() {
+    echo "usage: `basename $0` -b branch -v version -v version"
+    echo "       eg. `basename $0` -b BRANCH_1_0_1_20030728 -v 1.1.2"
+    exit 1
+}
+
+deploy() {
+    # cleanup
+    rm -rf $tmp 2> /dev/null
+    mkdir $tmp
+
+    # export kt
+    cd $tmp
+    cvs -d $cvsroot export -r $branch knowledgeTree
+
+    # tar it up
+    rm /tmp/knowledgeTree-$version.tgz 2> /dev/null
+    tar -czvf /tmp/knowledgeTree-$version.tgz knowledgeTree
+
+    # convert src to windoze line-endings
+    find $tmp/knowledgeTree -name \*\.php -exec unix2dos {} \; 2> /dev/null
+    find $tmp/knowledgeTree -name \*\.inc -exec unix2dos {} \; 2> /dev/null
+    find $tmp/knowledgeTree -name \*\.txt -exec unix2dos {} \; 2> /dev/null
+    
+    # zip it up
+    rm /tmp/knowledgeTree-$version.zip 2> /dev/null
+    zip -r /tmp/knowledgeTree-$version.zip knowledgeTree
+ 
+    # move them to this dir
+    cd -
+    mv /tmp/knowledgeTree-$version.* .
+
+    # clean up
+    rm -rf $tmp 2> /dev/null
+}
+
+# check the command line options
+if [ $# -lt 2 ]; then
+    usage
+fi
+
+# process the params
+while getopts ":b:v:" Option
+do
+  case $Option in
+    b   ) branch=$OPTARG;;
+    v   ) version=$OPTARG;;
+    *   ) usage;;
+  esac
+done
+
+# check that everything we want is set
+if [ -z $branch -o -z $version ]; then
+    usage
+fi 
+
+# setup up some paths and stuff
+cvsroot=/usr/local/cvsroot
+tmp=/tmp/dms
+
+# now just do it
+deploy
