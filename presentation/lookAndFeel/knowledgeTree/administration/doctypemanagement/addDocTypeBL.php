80ebb234d2357d2e56efb6c610d56ffb2e7dd4ca
Michael Joseph <michael@knowledgetree.com>
2003-08-03 13:52:57 +0000

    used refactored cancel button function
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2507 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/doctypemanagement/addDocTypeBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/doctypemanagement/addDocTypeBL.php
@@ -8,19 +8,19 @@
 *
 */
 require_once("../../../../../config/dmsDefaults.php");
+require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
+require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");
+require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentTypeFieldLink.inc");
+require_once("$default->fileSystemRoot/lib/security/permission.inc");
+require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
+require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+require_once("$default->fileSystemRoot/presentation/Html.inc");
+require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/administration/adminUI.inc");
+require_once("addDocTypeUI.inc");
 
 if (checkSession()) {
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");
-	require_once("addDocTypeUI.inc");
-	require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentTypeFieldLink.inc");
-	require_once("$default->fileSystemRoot/lib/security/permission.inc");
 	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");	
-	require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-	require_once("$default->fileSystemRoot/presentation/Html.inc");
-			
 	$oPatternCustom = & new PatternCustom();
 	$oPatternCustom->setHtml(getPage());
 	$main->setCentralPayload($oPatternCustom);
