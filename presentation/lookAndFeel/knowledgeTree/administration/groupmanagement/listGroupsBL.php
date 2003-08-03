ede712dec9b29cb7f94109b4273af541b29839d3
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-08-03 13:34:47 +0000

    used refactored getAddLink method
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2500 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/groupmanagement/listGroupsBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/groupmanagement/listGroupsBL.php
@@ -16,6 +16,7 @@ require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
 require_once("$default->fileSystemRoot/lib/visualpatterns/PatternTableSqlQuery.inc");    
 require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");    
 require_once("$default->fileSystemRoot/presentation/Html.inc");
+require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/administration/adminUI.inc");
 require_once("listGroupsUI.inc");
 
 if (checkSession()) {	
