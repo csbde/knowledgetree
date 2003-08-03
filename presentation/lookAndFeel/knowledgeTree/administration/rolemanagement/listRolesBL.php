afa154d2adce9d52f7793245736284099da1a9a3
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-08-03 14:12:50 +0000

    (#2813) refactored and tidied incorporated addRole link
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2514 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/rolemanagement/listRolesBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/rolemanagement/listRolesBL.php
@@ -8,59 +8,22 @@
 *
 */
 
-	require_once("../../../../../config/dmsDefaults.php");
+require_once("../../../../../config/dmsDefaults.php");
+require_once("$default->fileSystemRoot/lib/users/User.inc");    
+require_once("$default->fileSystemRoot/lib/security/permission.inc");
+require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");    
+require_once("$default->fileSystemRoot/lib/visualpatterns/PatternTableSqlQuery.inc");    
+require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");    
+require_once("$default->fileSystemRoot/presentation/Html.inc");
+require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/administration/adminUI.inc");
+require_once("listRolesUI.inc");
 
-if (checkSession()) {    
-    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");    
-    require_once("listRolesUI.inc"); 
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-    require_once("$default->fileSystemRoot/lib/documentmanagement/Document.inc");
-    require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-    require_once("$default->fileSystemRoot/lib/users/User.inc");    
-    require_once("$default->fileSystemRoot/lib/security/permission.inc");
-    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");    
-    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternTableSqlQuery.inc");    
-    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternBrowsableSearchResults.inc");
-    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");    
-    require_once("$default->fileSystemRoot/lib/discussions/DiscussionThread.inc");  
-    require_once("$default->fileSystemRoot/lib/discussions/DiscussionComment.inc");   
-    require_once("$default->fileSystemRoot/presentation/Html.inc");
-	
-	
+if (checkSession()) {	
     $oPatternCustom = & new PatternCustom();
-
-if(checkSession()) {	
-		global $default;
-		
-		$oPatternCustom->addHtml(renderHeading("Current System Roles"));		// Create the Heading				
-		 
-		
-		$main->setFormAction($_SERVER['PHP_SELF']);
-				
-		$sQuery = 	"SELECT id as roleID, name as name, active, can_read as reader, can_write as writer, " . 
-					"'Edit', 'Delete' " .
-					"FROM roles " .
-					"ORDER BY name";				
-		
-	    $aColumns = array("name", 	  "active", "reader", "writer", "Edit", "Delete");
-	    $aColumnNames = array("Name", "Active", "Read", "Write", "Edit", "Delete");
-	    $aColumnTypes = array(1,2,2,2,3,3);
-	    $aDBColumnArray = array("roleID");
-	    $aQueryStringVariableNames = array("fRoleID");
-	    	    
-	    $aHyperLinkURL = array(	4=> "$default->rootUrl/control.php?action=editRole",                       			
-                       			5=> "$default->rootUrl/control.php?action=removeRole"); //"$default->rootUrl/control.php?action=removeUserFromGroup");
-	    	    
-	    $oSearchResults = & new PatternTableSqlQuery($sQuery, $aColumns, $aColumnTypes, $aColumnNames, "100%", $aHyperLinkURL,$aDBColumnArray,$aQueryStringVariableNames);	    
-		$oSearchResults->setDisplayColumnHeadings(true);
-	    $htmlTables = $oSearchResults->render() ;
-	
-	    $oPatternCustom->addHtml($htmlTables);	    
-	
-	} // end of if checksession
-	
+    $oPatternCustom->setHtml(getPage($fGroupID));
+	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
 	$main->setCentralPayload($oPatternCustom);
-    $main->render();
+	$main->setFormAction($_SERVER['PHP_SELF']);	
+    $main->render();	
 }
 ?>
