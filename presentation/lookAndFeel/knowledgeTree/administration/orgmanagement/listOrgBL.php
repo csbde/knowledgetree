c3fbff796d29af8e356b8dae0305b0efb7ea0b0c
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-08-03 13:37:03 +0000

    (#2813) refactored and tidied incorporated addOrganisation link
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2503 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/orgmanagement/listOrgBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/orgmanagement/listOrgBL.php
@@ -8,60 +8,22 @@
 *
 */
 
-	require_once("../../../../../config/dmsDefaults.php");
+require_once("../../../../../config/dmsDefaults.php");
+require_once("$default->fileSystemRoot/lib/users/User.inc");    
+require_once("$default->fileSystemRoot/lib/security/permission.inc");
+require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");    
+require_once("$default->fileSystemRoot/lib/visualpatterns/PatternTableSqlQuery.inc");    
+require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");    
+require_once("$default->fileSystemRoot/presentation/Html.inc");
+require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/administration/adminUI.inc");
+require_once("listOrgUI.inc");
 
-if (checkSession()) {    
-    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCreate.inc");    
-    require_once("listOrgUI.inc"); 
-	require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-    require_once("$default->fileSystemRoot/lib/documentmanagement/Document.inc");
-    require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-    require_once("$default->fileSystemRoot/lib/users/User.inc");    
-    require_once("$default->fileSystemRoot/lib/security/permission.inc");
-    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");    
-    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternTableSqlQuery.inc");    
-    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternBrowsableSearchResults.inc");
-    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");    
-    require_once("$default->fileSystemRoot/lib/discussions/DiscussionThread.inc");  
-    require_once("$default->fileSystemRoot/lib/discussions/DiscussionComment.inc");   
-    require_once("$default->fileSystemRoot/presentation/Html.inc");
-	
-	
+if (checkSession()) {	
     $oPatternCustom = & new PatternCustom();
-
-if(checkSession()) {	
-
-		$oPatternCustom->addHtml(renderHeading("Current System Organisations"));		// Create the Heading				
-		 
-		
-		$main->setFormAction($_SERVER['PHP_SELF']);
-				
-		$sQuery = 	"SELECT org.id as orgID, org.name as name, " . 
-					"'Edit', 'Delete' " .
-					"FROM organisations_lookup as org " .
-					"ORDER BY org.name";
-				
-		
-	    $aColumns = array("name", "Edit", "Delete");
-	    $aColumnNames = array("Name", "Edit", "Delete");
-	    $aColumnTypes = array(1,3,3);
-	    $aDBColumnArray = array("orgID");
-	    $aQueryStringVariableNames = array("fOrgID");
-	    	    
-	    $aHyperLinkURL = array(	1=> "$default->rootUrl/control.php?action=editOrg",                       			
-                       			2=> "$default->rootUrl/control.php?action=removeOrg"); //"$default->rootUrl/control.php?action=removeUserFromGroup");
-	    	    
-	    $oSearchResults = & new PatternTableSqlQuery($sQuery, $aColumns, $aColumnTypes, $aColumnNames, "100%", $aHyperLinkURL,$aDBColumnArray,$aQueryStringVariableNames);
-	    
-		$oSearchResults->setDisplayColumnHeadings(true);
-	    $htmlTables = $oSearchResults->render() ;
-	
-	    $oPatternCustom->addHtml($htmlTables);	    
-	
-	} // end of if checksession
-	
+    $oPatternCustom->setHtml(getPage($fGroupID));
+	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
 	$main->setCentralPayload($oPatternCustom);
-    $main->render();
+	$main->setFormAction($_SERVER['PHP_SELF']);	
+    $main->render();	
 }
 ?>
