2061fe19e4b75c79bcc4a11879f14e854f415885
Michael Joseph <michael@knowledgetree.com>
2003-08-06 14:42:31 +0000

    added help inserts, additional document_text index and version number to system settings
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2549 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/tables.sql
+++ sql/tables.sql
@@ -127,8 +127,10 @@ CREATE TABLE document_text (
   id INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
   document_id integer,
   document_text MEDIUMTEXT,
-  FULLTEXT (document_text)
+  FULLTEXT (document_text),
+  KEY document_text_document_id_indx (document_id)
 ) Type = MyISAM;
+
 
 CREATE TABLE document_transactions ( 
 id INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
@@ -534,6 +536,7 @@ INSERT INTO language_lookup (name) VALUES ("Spanish");
 
 -- system settings
 INSERT INTO system_settings (name, value) values ("lastIndexUpdate", "0");
+INSERT INTO system_settings (name, value) values ("knowledgeTreeVersion", "1.2.0");
 
 -- document statuses
 INSERT INTO web_documents_status_lookup (name) VALUES ("Pending");
@@ -571,39 +574,102 @@ INSERT INTO time_unit_lookup (name) VALUES ("Years");
 INSERT INTO time_unit_lookup (name) VALUES ("Months");
 INSERT INTO time_unit_lookup (name) VALUES ("Days");
 
--- organisation
-INSERT INTO organisations_lookup (name) VALUES ("Default Organisation");
-
--- units
-INSERT INTO units_lookup (name) VALUES ("Default Unit");
-
-INSERT INTO units_organisations_link (unit_id, organisation_id) VALUES (1, 1);
-
--- setup groups
-INSERT INTO groups_lookup (name, is_sys_admin, is_unit_admin) VALUES ("System Administrators", 1, 0); -- id=1
-INSERT INTO groups_lookup (name, is_sys_admin, is_unit_admin) VALUES ("Unit Administrators", 0, 1); -- id=2
-INSERT INTO groups_lookup (name, is_sys_admin, is_unit_admin) VALUES ("Anonymous", 0, 0); -- id=3
-
--- unit administrators
-INSERT INTO groups_units_link (group_id, unit_id) VALUES (2, 1);
-
--- system administrator
-INSERT INTO users (username, name, password, quota_max, quota_current, email, mobile, email_notification, sms_notification, ldap_dn, max_sessions, language_id)
-            VALUES ("admin", "Administrator", "21232f297a57a5a743894a0e4a801fc3", "0", "0", "", "", 1, 1, "", 1, 1);
-INSERT INTO users_groups_link (group_id, user_id) VALUES (1, 1);
-
--- unit administrator
-INSERT INTO users (username, name, password, quota_max, quota_current, email, mobile, email_notification, sms_notification, ldap_dn, max_sessions, language_id)
-            VALUES ("unitAdmin", "Unit Administrator", "21232f297a57a5a743894a0e4a801fc3", "0", "0", "", "", 1, 1, "", 1, 1);
-INSERT INTO users_groups_link (group_id, user_id) VALUES (2, 2);
-                        
--- guest user
-INSERT INTO users (username, name, password, quota_max, quota_current, email, mobile, email_notification, sms_notification, ldap_dn, max_sessions, language_id)
-            VALUES ("guest", "Anonymous", "084e0343a0486ff05530df6c705c8bb4", "0", "0", "", "", 0, 0, "", 19, 1);
-INSERT INTO users_groups_link (group_id, user_id) VALUES (3, 3);
-            
--- define folder structure
-INSERT INTO folders (name, description, parent_id, creator_id, unit_id, is_public)
-             VALUES ("Root Folder", "Root Document Folder", 0, 1, 0, 0);
-INSERT INTO folders (name, description, parent_id, creator_id, unit_id, is_public)
-             VALUES ("Default Unit", "Default Unit Root Folder", 1, 1, 1, 0);
+-- help
+INSERT INTO help VALUES (1,'browse','dochelp.html');
+INSERT INTO help VALUES (2,'dashboard','dashboardHelp.html');
+INSERT INTO help VALUES (3,'addFolder','addFolderHelp.html');
+INSERT INTO help VALUES (4,'editFolder','editFolderHelp.html');
+INSERT INTO help VALUES (5,'addFolderCollaboration','addFolderCollaborationHelp.html');
+INSERT INTO help VALUES (6,'modifyFolderCollaboration','addFolderCollaborationHelp.html');
+INSERT INTO help VALUES (7,'addDocument','addDocumentHelp.html');
+INSERT INTO help VALUES (8,'viewDocument','viewDocumentHelp.html');
+INSERT INTO help VALUES (9,'modifyDocument','modifyDocumentHelp.html');
+INSERT INTO help VALUES (10,'modifyDocumentRouting','modifyDocumentRoutingHelp.html');
+INSERT INTO help VALUES (11,'emailDocument','emailDocumentHelp.html');
+INSERT INTO help VALUES (12,'deleteDocument','deleteDocumentHelp.html');
+INSERT INTO help VALUES (13,'administration','administrationHelp.html');
+INSERT INTO help VALUES (14,'addGroup','addGroupHelp.html');
+INSERT INTO help VALUES (15,'editGroup','editGroupHelp.html');
+INSERT INTO help VALUES (16,'removeGroup','removeGroupHelp.html');
+INSERT INTO help VALUES (17,'assignGroupToUnit','assignGroupToUnitHelp.html');
+INSERT INTO help VALUES (18,'removeGroupFromUnit','removeGroupFromUnitHelp.html');
+INSERT INTO help VALUES (19,'addUnit','addUnitHelp.html');
+INSERT INTO help VALUES (20,'editUnit','editUnitHelp.html');
+INSERT INTO help VALUES (21,'removeUnit','removeUnitHelp.html');
+INSERT INTO help VALUES (22,'addOrg','addOrgHelp.html');
+INSERT INTO help VALUES (23,'editOrg','editOrgHelp.html');
+INSERT INTO help VALUES (24,'removeOrg','removeOrgHelp.html');
+INSERT INTO help VALUES (25,'addRole','addRoleHelp.html');
+INSERT INTO help VALUES (26,'editRole','editRoleHelp.html');
+INSERT INTO help VALUES (27,'removeRole','removeRoleHelp.html');
+INSERT INTO help VALUES (28,'addLink','addLinkHelp.html');
+INSERT INTO help VALUES (29,'addLinkSuccess','addLinkHelp.html');
+INSERT INTO help VALUES (30,'editLink','editLinkHelp.html');
+INSERT INTO help VALUES (31,'removeLink','removeLinkHelp.html');
+INSERT INTO help VALUES (32,'systemAdministration','systemAdministrationHelp.html');
+INSERT INTO help VALUES (33,'deleteFolder','deleteFolderHelp.html');
+INSERT INTO help VALUES (34,'editDocType','editDocTypeHelp.html');
+INSERT INTO help VALUES (35,'removeDocType','removeDocTypeHelp.html');
+INSERT INTO help VALUES (36,'addDocType','addDocTypeHelp.html');
+INSERT INTO help VALUES (37,'addDocTypeSuccess','addDocTypeHelp.html');
+INSERT INTO help VALUES (38,'manageSubscriptions','manageSubscriptionsHelp.html');
+INSERT INTO help VALUES (39,'addSubscription','addSubscriptionHelp.html');
+INSERT INTO help VALUES (40,'removeSubscription','removeSubscriptionHelp.html');
+INSERT INTO help VALUES (41,'preferences','preferencesHelp.html');
+INSERT INTO help VALUES (42,'editPrefsSuccess','preferencesHelp.html');
+INSERT INTO help VALUES (43,'modifyDocumentGenericMetaData','modifyDocumentGenericMetaDataHelp.html');
+INSERT INTO help VALUES (44,'viewHistory','viewHistoryHelp.html');
+INSERT INTO help VALUES (45,'checkInDocument','checkInDocumentHelp.html');
+INSERT INTO help VALUES (46,'checkOutDocument','checkOutDocumentHelp.html');
+INSERT INTO help VALUES (47,'advancedSearch','advancedSearchHelp.html');
+INSERT INTO help VALUES (48,'deleteFolderCollaboration','deleteFolderCollaborationHelp.html');
+INSERT INTO help VALUES (49,'addFolderDocType','addFolderDocTypeHelp.html');
+INSERT INTO help VALUES (50,'deleteFolderDocType','deleteFolderDocTypeHelp.html');
+INSERT INTO help VALUES (51,'addGroupFolderLink','addGroupFolderLinkHelp.html');
+INSERT INTO help VALUES (52,'deleteGroupFolderLink','deleteGroupFolderLinkHelp.html');
+INSERT INTO help VALUES (53,'addWebsite','addWebsiteHelp.html');
+INSERT INTO help VALUES (54,'addWebsiteSuccess','addWebsiteHelp.html');
+INSERT INTO help VALUES (55,'editWebsite','editWebsiteHelp.html');
+INSERT INTO help VALUES (56,'removeWebSite','removeWebSiteHelp.html');
+INSERT INTO help VALUES (57,'standardSearch','standardSearchHelp.html');
+INSERT INTO help VALUES (58,'modifyDocumentTypeMetaData','modifyDocumentTypeMetaDataHelp.html');
+INSERT INTO help VALUES (59,'addDocField','addDocFieldHelp.html');
+INSERT INTO help VALUES (60,'editDocField','editDocFieldHelp.html');
+INSERT INTO help VALUES (61,'removeDocField','removeDocFieldHelp.html');
+INSERT INTO help VALUES (62,'addMetaData','addMetaDataHelp.html');
+INSERT INTO help VALUES (63,'editMetaData','editMetaDataHelp.html');
+INSERT INTO help VALUES (64,'removeMetaData','removeMetaDataHelp.html');
+INSERT INTO help VALUES (65,'addUser','addUserHelp.html');
+INSERT INTO help VALUES (66,'editUser','editUserHelp.html');
+INSERT INTO help VALUES (67,'removeUser','removeUserHelp.html');
+INSERT INTO help VALUES (68,'addUserToGroup','addUserToGroupHelp.html');
+INSERT INTO help VALUES (69,'removeUserFromGroup','removeUserFromGroupHelp.html');
+INSERT INTO help VALUES (70,'viewDiscussion','viewDiscussionThread.html');
+INSERT INTO help VALUES (71,'addComment','addDiscussionComment.html');
+INSERT INTO help VALUES (72,'listNews','listDashboardNewsHelp.html');
+INSERT INTO help VALUES (73,'editNews','editDashboardNewsHelp.html');
+INSERT INTO help VALUES (74,'previewNews','previewDashboardNewsHelp.html');
+INSERT INTO help VALUES (75,'addNews','addDashboardNewsHelp.html');
+INSERT INTO help VALUES (76,'modifyDocumentArchiveSettings','modifyDocumentArchiveSettingsHelp.html');
+INSERT INTO help VALUES (77,'addDocumentArchiveSettings','addDocumentArchiveSettingsHelp.html');
+INSERT INTO help VALUES (78,'listDocFields','listDocumentFieldsAdmin.html');
+INSERT INTO help VALUES (79,'editDocFieldLookups','editDocFieldLookups.html');
+INSERT INTO help VALUES (80,'addMetaDataForField','addMetaDataForField.html'); 
+INSERT INTO help VALUES (81,'editMetaDataForField','editMetaDataForField.html'); 
+INSERT INTO help VALUES (82,'removeMetaDataFromField','removeMetaDataFromField.html'); 
+INSERT INTO help VALUES (83,'listDocs','listDocumentsCheckoutHelp.html'); 
+INSERT INTO help VALUES (84,'editDocCheckout','editDocCheckoutHelp.html'); 
+INSERT INTO help VALUES (85,'listDocTypes','listDocTypesHelp.html'); 
+INSERT INTO help VALUES (86,'editDocTypeFields','editDocFieldHelp.html'); 
+INSERT INTO help VALUES (87,'addDocTypeFieldsLink','addDocTypeFieldHelp.html'); 
+INSERT INTO help VALUES (88,'listGroups','listGroupsHelp.html'); 
+INSERT INTO help VALUES (89,'editGroupUnit','editGroupUnitHelp.html'); 
+INSERT INTO help VALUES (90,'listOrg','listOrgHelp.html'); 
+INSERT INTO help VALUES (91,'listRole','listRolesHelp.html'); 
+INSERT INTO help VALUES (92,'listUnits','listUnitHelp.html'); 
+INSERT INTO help VALUES (93,'editUnitOrg','editUnitOrgHelp.html'); 
+INSERT INTO help VALUES (94,'removeUnitFromOrg','removeUnitFromOrgHelp.html'); 
+INSERT INTO help VALUES (95,'addUnitToOrg','addUnitToOrgHelp.html'); 
+INSERT INTO help VALUES (96,'listUsers','listUsersHelp.html'); 
+INSERT INTO help VALUES (97,'editUserGroups','editUserGroupsHelp.html'); 
+INSERT INTO help VALUES (98,'listWebsites','listWebsitesHelp.html'); 
