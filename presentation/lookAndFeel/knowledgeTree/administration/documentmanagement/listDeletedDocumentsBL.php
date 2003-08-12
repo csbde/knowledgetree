ed0b5953817720b497b008cc7726f3e0660448e2
Michael Joseph <michael@knowledgetree.com>
2003-08-12 08:58:31 +0000

    added copyright and gpl notice removed owl prefix from table aliases
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2580 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/documentmanagement/listDeletedDocumentsBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/documentmanagement/listDeletedDocumentsBL.php
@@ -9,13 +9,26 @@ require_once("$default->uiDirectory/documentmanagement/documentUI.inc");
 
 require_once("listDeletedDocumentsUI.inc");
 require_once("$default->fileSystemRoot/presentation/Html.inc");
-
 /**
  * $Id$
- *  
+ *
  * Business logic for listing deleted documents.
  *
- * Licensed under the GNU GPL. For full terms see the file DOCS/COPYING.
+ * Copyright (c) 2003 Jam Warehouse http://www.jamwarehouse.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * @version $Revision$
  * @author Michael Joseph <michael@jamwarehouse.com>, Jam Warehouse (Pty) Ltd, South Africa
