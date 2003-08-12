0e28431e110882929521fc853bf1d95e6999e25a
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-08-12 12:06:34 +0000

    added copyright and gpl notice removed owl prefix from table aliases
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2591 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/login.php
+++ presentation/login.php
@@ -2,23 +2,32 @@
 
 // main library routines and defaults
 require_once("../config/dmsDefaults.php");
-
 /**
  * $Id$
  *  
  * This page handles logging a user into the dms.
  * This page displays the login form, and performs the business logic login processing.
  *
- * Licensed under the GNU GPL. For full terms see the file COPYING.
+ * Copyright (c) 2003 Jam Warehouse http://www.jamwarehouse.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * @version $Revision$
  * @author Michael Joseph <michael@jamwarehouse.com>, Jam Warehouse (Pty) Ltd, South Africa
- * @package presentation
  */
 
-// -------------------------------
-// page start
-// -------------------------------
 global $default;
 
 if ($loginAction == "loginForm") {
