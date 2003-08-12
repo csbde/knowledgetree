2ad797a08bc6c2bf92b561652ac66b25fec91692
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-08-12 12:01:47 +0000

    added copyright and gpl notice removed owl prefix from table aliases
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2589 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/help.php
+++ presentation/lookAndFeel/knowledgeTree/help.php
@@ -1,4 +1,29 @@
 <?php
+/**
+ * $Id$
+ *
+ * Online context-sensitive help page.
+ *
+ * Copyright (c) 2003 Jam Warehouse http://www.jamwarehouse.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @version $Revision$
+ * @author Michael Joseph <michael@jamwarehouse.com>, Jam Warehouse (Pty) Ltd, South Africa
+ * @package administration.docfieldmanagement
+ */
 require_once("../../../config/dmsDefaults.php");
 require_once("$default->fileSystemRoot/presentation/Html.inc");
 global $default;
@@ -27,4 +52,3 @@ if ($sql->next_record()) {
 	echo "No help available for $fAction";
 }
 ?>
-
