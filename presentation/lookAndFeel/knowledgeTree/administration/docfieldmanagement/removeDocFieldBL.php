f01456003f07735ed924813ef27b9e5b7774cac1
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-08-12 08:39:05 +0000

    added copyright and gpl notice removed owl prefix from table aliases
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2579 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/removeDocFieldBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/removeDocFieldBL.php
@@ -1,19 +1,37 @@
 <?php
 /**
-* BL information for adding a DocField
-*
-* @author Mukhtar Dharsey
-* @date 5 February 2003
-* @package presentation.lookAndFeel.knowledgeTree.
-*
-*/
+ * $Id$
+ *
+ * Remove document field.
+ *
+ * Copyright (c) 2003 Jam Warehouse http://www.jamwarehouse.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @version $Revision$
+ * @author Mukhtar Dharsey, Jam Warehouse (Pty) Ltd, South Africa
+ * @package administration.docfieldmanagement
+ */
+
 require_once("../../../../../config/dmsDefaults.php");
 
 if (checkSession()) {
 	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternListBox.inc");
 	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternEditableListFromQuery.inc");
 	require_once("removeDocFieldUI.inc");
-	require_once("$default->fileSystemRoot/lib/security/permission.inc");
+	require_once("$default->fileSystemRoot/lib/security/Permission.inc");
 	require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentField.inc");
 	require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentType.inc");
 	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
