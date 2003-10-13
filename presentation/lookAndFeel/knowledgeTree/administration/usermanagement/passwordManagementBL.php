d40be5e334b0f1554f0661430b33d304b1c75939
rob <rob@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-10-13 13:45:00 +0000

    Update functionality so that admin can change user passwords when db authentication mode is used.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2738 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/administration/usermanagement/passwordManagementBL.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * $Id$
+ *
+ * Change a user's password
+ *
+ * Copyright (c) 2003 Jam Warehouse http://www.jamwarehouse.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @version $Revision$
+ * @author Rob Cherry, Jam Warehouse (Pty) Ltd, South Africa
+ * @package administration.usermanagement
+ */
+ 
+require_once("../../../../../config/dmsDefaults.php");
+
+if (checkSession()) {	
+	require_once("$default->fileSystemRoot/lib/security/Permission.inc");	
+	require_once("$default->fileSystemRoot/lib/users/User.inc");
+	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+	require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+	require_once("$default->fileSystemRoot/presentation/Html.inc");
+	require_once("passwordManagementUI.inc");	
+	
+	$oPatternCustom = & new PatternCustom();
+	
+	if (strcmp($default->authenticationClass,"DBAuthenticator") == 0) {
+		//only update passwords if we are in db authentication mode
+		 if (isset($fUserID)){
+		 	if (Permission::userIsSystemAdministrator()) {
+		 		$oUser = User::get($fUserID);
+		 		//only the administrator is allowed to change passwords here	 			
+			 	if (isset($fForUpdate)) {
+			 		//execute the update and return to the edit page??
+			 		if (strlen($fNewPassword) > 0 && strlen($fNewPasswordConfirm) > 0) {
+			 			//if passwords have been entered
+			 			if (strcmp($fNewPassword, $fNewPasswordConfirm) == 0) {
+			 				//if the password and its confirmation are the same		 				
+			 				$oUser->setPassword($fNewPassword);
+			 				if ($oUser->update()) {
+			 					//successful update		 				
+			 					$oPatternCustom->setHtml(getPasswordUpdateSuccessPage());
+			 				} else {
+			 					//update failed
+			 					$oPatternCustom->setHtml(getPage($oUser->getName()));
+			 					$main->setErrorMessage("An error occured while attempting to update the user's password");
+								$main->setFormAction($_SERVER["PHP_SELF"] . "?fForUpdate=1&fUserID=$fUserID");							
+			 				}
+			 			} else {
+			 				$oPatternCustom->setHtml(getPage($oUser->getName()));
+			 				$main->setErrorMessage("The password and its confirmation do not match.  Please try again.");
+							$main->setFormAction($_SERVER["PHP_SELF"] . "?fForUpdate=1&fUserID=$fUserID");		 						 				
+			 			} 
+			 		} else {
+			 			$oPatternCustom->setHtml(getPage($oUser->getName()));
+		 				$main->setErrorMessage("Blank passwords are not valid.  Please try again.");
+						$main->setFormAction($_SERVER["PHP_SELF"] . "?fForUpdate=1&fUserID=$fUserID");		 			
+			 		}	 		
+			 	} else {					
+			 		//show the page
+					$oPatternCustom->setHtml(getPage($oUser->getName()));
+					$main->setFormAction($_SERVER["PHP_SELF"] . "?fForUpdate=1&fUserID=$fUserID");			
+			 	}
+			} else {
+				$main->setErrorMessage("Only an administrator can update a user password from here");						
+			}
+		 }
+	} else {
+		$oPatternCustom->setHtml(getPage($oUser->getName()));
+		$main->setErrorMessage("Passwords can only be update in Knowledgew Tree when authentication is against the MySQL database, not against an LDAP server");
+		$main->setFormAction($_SERVER["PHP_SELF"]);
+	}	
+	//render the page
+	$main->setCentralPayload($oPatternCustom);
+	$main->render();	
+}
+?>
