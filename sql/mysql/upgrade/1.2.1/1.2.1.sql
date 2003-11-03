aedaf758f83ecbe33b2ba55f72ad6fccb554d56f
Michael Joseph <michael@knowledgetree.com>
2003-11-03 12:29:52 +0000

    added knowledgeTreeVersion update to upgrade script.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2752 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/1.2.1/1.2.1.sql
+++ sql/mysql/upgrade/1.2.1/1.2.1.sql
@@ -1,2 +1,3 @@
 ALTER TABLE folders ADD COLUMN inherit_parent_folder_permission BIT;
 UPDATE folders SET inherit_parent_folder_permission = 1;
+UPDATE system_settings SET value="1.2.1" WHERE name="knowledgeTreeVersion";
