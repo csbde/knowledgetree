56c1e550add3cfe9e121cc447d0f6e9430544b65
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-12-15 13:45:53 +0000

    #3497 changes to support listing comments by thread and indenting appropriately
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2801 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/discussions/viewDiscussionBL.php
+++ presentation/lookAndFeel/knowledgeTree/discussions/viewDiscussionBL.php
@@ -44,61 +44,48 @@ if (checkSession()) {
 	require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
     $oPatternCustom = & new PatternCustom();	
 	if (isset($fForDiscussion)) {		
-		if ($fDocumentID > 0) { 	
-			$iThreadID = DiscussionThread::getThreadIDforDoc($fDocumentID);						
-			if ($iThreadID) {// if thread ID does exist
-				$oThread = DiscussionThread::get($iThreadID);					
-				if($oThread) { //  if thread object exists
-					// Iterate through the number of comments
-					$sAllCommentID = $oThread->getAllCommentID();
-					$arrAllCommentID = explode(",", $sAllCommentID);										
-					$iNumMax = $oThread->getNumberOfReplies();						
+		if (isset($fDocumentID)) { 	
+			$aDocumentThreads = DiscussionThread::getList("document_id=$fDocumentID ORDER BY id");
+			if (count($aDocumentThreads) > 0) {
+				// call the ui function to display the comments
+				$oPatternCustom->setHtml(getPage($fDocumentID, $aDocumentThreads));				
 					
-					$sQuery = 	"SELECT 1 as ForView, subject, username, date, discussion_comments.id as com_id, discussion_threads.document_id as doc_id " .
-					 			"FROM (discussion_comments INNER JOIN users ON discussion_comments.user_id = users.id) " .
-					 			"INNER JOIN discussion_threads ON discussion_threads.id = discussion_comments.thread_id  " .
-					 			"WHERE discussion_threads.id = " . $iThreadID .
-					 			" ORDER BY discussion_comments.thread_id ASC";
-
-				    $aColumns = array("subject", "username", "date");
-				    $aColumnNames = array("<font color=white>Subject </font>", "<font color=white>User</font>", "<font color=white>Date</font>");
-				    $aColumnTypes = array(3,1,1);
-				    $aQueryStringVars = array("fViewComment", "iCommentID", "iDocumentID");
-				    $aQueryStringCols = array("ForView", "com_id", "doc_id");
-				    
-				    for ($i = 0; $i < $iNumMax; $i++) {
-						$aHyperLinkURL[$i] = $_SERVER['PHP_SELF'] ;		
-					}
-				    
-				    $oSearchResults = & new PatternTableSqlQuery ($sQuery, $aColumns, $aColumnTypes, $aColumnNames, "100%",  $aHyperLinkURL, $aQueryStringCols, $aQueryStringVars);
-				    $sToRender .= renderHeading("Document Discussion Thread");
-					$sToRender .= displayDocumentPath($oThread->getDocumentID());
-				    $oPatternCustom->addHtml($sToRender);
-				    $oPatternCustom->addHtml(getPageButtons($oThread));				    
-				    $oPatternCustom->addHtml($oSearchResults->render());    
-					
+//					$sQuery = 	"SELECT 1 as ForView, dc.subject AS subject, username, date, dc.id AS commentID, dt.document_id AS documentID " .
+//					 			"FROM discussion_comments AS dc " .
+//					 			"INNER JOIN users AS u ON dc.user_id = u.id " .
+//					 			"INNER JOIN discussion_threads AS dt ON dt.id = dc.thread_id  " .
+//					 			"WHERE dt.id in ( " . implode(",", $aDocumentThreads) . ") " .
+//					 			"ORDER BY dc.thread_id, dc.id, date ASC";
+//
+//				    $aColumns = array("subject", "username", "date");
+//				    $aColumnNames = array("<font color=white>Subject </font>", "<font color=white>User</font>", "<font color=white>Date</font>");
+//				    $aColumnTypes = array(3,1,1);
+//				    $aQueryStringVars = array("fViewComment", "iCommentID", "iDocumentID");
+//				    $aQueryStringCols = array("ForView", "commentID", "documentID");
+//				    
+//				    for ($i = 0; $i < $iNumMax; $i++) {
+//						$aHyperLinkURL[$i] = $_SERVER['PHP_SELF'] ;		
+//					}
+//				    
+//				    $oSearchResults = & new PatternTableSqlQuery ($sQuery, $aColumns, $aColumnTypes, $aColumnNames, "100%",  $aHyperLinkURL, $aQueryStringCols, $aQueryStringVars);
+//				    $sToRender .= renderHeading("Document Discussion Thread");
+//					$sToRender .= displayDocumentPath($fDocumentID);
+//				    $oPatternCustom->addHtml($sToRender);
+//				    $oPatternCustom->addHtml(getPageButtons($fDocumentID));				    
+//				    $oPatternCustom->addHtml($oSearchResults->render());    
+//					
 					// On opening, increment the number of views of current thread & update database
-					if($_SESSION['Discussion' . $fDocumentID][0]->bViews !=true ){
-						$oThread->setNumberOfViews();					
-						if($oThread->Update() == false) $oPatternCustom->addHtml("Failed to update. Please Contact Database Administrator in this regard") ;
-						$_SESSION['Discussion' . $fDocumentID][0]->bViews = true;
-					} 																
-				} else { 
-					$main->setErrorMessage("Error creating discussion thread object");
-				}						
+//					if($_SESSION['Discussion' . $fDocumentID][0]->bViews !=true ){
+//						$oThread->incrementNumberOfViews();					
+//						if($oThread->Update() == false) $oPatternCustom->addHtml("Failed to update. Please Contact Database Administrator in this regard") ;
+//						$_SESSION['Discussion' . $fDocumentID][0]->bViews = true;
+//					} 																
 			} else { // No current thread, option to create one					
 				$main->setErrorMessage("No discussion thread is currently available");
 				$oPatternCustom->addHtml(getNewThreadOption($fDocumentID));				
 			}	
 		} else { // Doument id  is negative 
-			$main->setErrorMessage("Invalid Document ID.  ID may not be negative.");
-		}
-	} else if (isset($fViewComment)){	// User wants to view a comment
-		if (isset($iCommentID)) {		// Check if a comment ID exists
-			$oComment = DiscussionComment::get($iCommentID);
-		  	$oUser = User::get($oComment->getUserID());		
-		  	$main->setFormAction("$default->rootUrl/presentation/lookAndFeel/knowledgeTree/discussions/addCommentBL.php?fDocumentID=$iDocumentID&fCommentID=$iCommentID&fReplyComment=1");		
-		  	$oPatternCustom->setHtml(getCommentBody($oComment,$iDocumentID,$oUser)) ;	  	
+			$main->setErrorMessage("You did not specify a document.");
 		}
 	} else { // If no discussion exists 
 		$main->setErrorMessage("Invalid function.  No such functionality exists for this page.");
