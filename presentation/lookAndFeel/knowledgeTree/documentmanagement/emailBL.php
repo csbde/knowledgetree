2c4a825c9cf702530809b861bafbeb6fa0b3e853
michael <michael@c91229c3-7414-0410-bfa2-8a42b809f60b>
2003-12-17 11:28:17 +0000

    #3500 added ability to send to groups and users.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2809 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/documentmanagement/emailBL.php
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/emailBL.php
@@ -26,88 +26,164 @@
  */
 
 require_once("../../../../config/dmsDefaults.php");
+require_once("$default->fileSystemRoot/lib/security/Permission.inc");
+require_once("$default->fileSystemRoot/lib/documentmanagement/Document.inc");
+require_once("$default->fileSystemRoot/lib/email/Email.inc");
+require_once("$default->fileSystemRoot/lib/users/User.inc");
+require_once("$default->fileSystemRoot/lib/groups/Group.inc");
+require_once("$default->fileSystemRoot/lib/documentmanagement/PhysicalDocumentManager.inc");
+require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentTransaction.inc");
+require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
+require_once("$default->fileSystemRoot/presentation/Html.inc");
+require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
+require_once("emailUI.inc");
+
+/**
+ * Sends emails to the selected groups
+ */
+function sendGroupEmails($aGroupIDs, $oDocument, $sComment = "") {
+	global $default;
+	
+    // loop through groups
+    for ($i=0; $i<count($aGroupIDs); $i++) {
+    	// validate the group id
+    	if ($aGroupIDs[$i] > 0) {
+		    $oDestGroup = Group::get($aGroupIDs[$i]);
+		    $default->log->info("sendingEmail to group " . $oDestGroup->getName());
+		    // for each group, retrieve all the users
+		    $aUsers = $oDestGroup->getUsers();
+		    // FIXME: this should send one email with multiple To: users
+		    for ($j=0; $j<count($aUsers); $j++) {
+	    		$default->log->info("sendingEmail to group-member " . $aUsers[$j]->getName() . " with email " . $aUsers[$j]->getEmail());	    	
+			    // the user has an email address and has email notification enabled
+				if (strlen($aUsers[$j]->getEmail())>0 && $aUsers[$j]->getEmailNotification()) {
+					//if the to address is valid, send the mail
+					if (validateEmailAddress($aUsers[$j]->getEmail())) {	    
+						sendEmail($aUsers[$j]->getEmail(), $aUsers[$j]->getName(), $oDocument->getID(), $oDocument->getName(), $sComment);
+					} else {
+						$default->log->error("email validation failed for " . $aUsers[$j]->getEmail());
+					}
+				} else {
+				$default->log->info("either " . $aUsers[$j]->getUserName() . " has no email address, or notification is not enabled");				
+				}
+		    }
+    	} else {
+    		$default->log->info("filtered group id=" . $aGroupIDs[$i]);
+    	}
+    }
+}
+
+/**
+ * Sends emails to the selected users
+ */
+function sendUserEmails($aUserIDs, $oDocument, $sComment = "") {
+	global $default;
+	
+    // loop through users
+    for ($i=0; $i<count($aUserIDs); $i++) {
+    	if ($aUserIDs[$i] > 0) {
+		    $oDestUser = User::get($aUserIDs[$i]);
+	    	$default->log->info("sendingEmail to user " . $oDestUser->getName() . " with email " . $oDestUser->getEmail());	    
+		    // the user has an email address and has email notification enabled
+			if (strlen($oDestUser->getEmail())>0 && $oDestUser->getEmailNotification()) {
+				//if the to address is valid, send the mail
+				if (validateEmailAddress($oDestUser->getEmail())) {	    
+					sendEmail($oDestUser->getEmail(), $oDestUser->getName(), $oDocument->getID(), $oDocument->getName(), $sComment);
+				}
+			} else {
+				$default->log->info("either " . $oDestUser->getUserName() . " has no email address, or notification is not enabled");
+			}
+    	} else {
+    		$default->log->info("filtered user id=" . $aUserIDs[$i]);
+    	}			
+    }  	
+}
+
+/**
+ * Constructs the email message text and sends the message
+ */
+function sendEmail($sDestEmailAddress, $sDestUserName, $fDocumentID, $sDocumentName, $sComment) {
+    global $default;
+    $oSendingUser = User::get($_SESSION["userID"]);
+    
+	$sMessage = "<font face=\"arial\" size=\"2\">";
+	$sMessage .= $sDestUserName . ",<br><br>";
+	$sMessage .= "Your colleague, " . $oSendingUser->getName() . ", wishes you to view the document entitled '" . $sDocumentName . "'.\n  ";
+	$sMessage .= "Click on the hyperlink below to view it.";
+	// add the link to the document to the mail
+	$sMessage .= "<br>" . generateControllerLink("viewDocument", "fDocumentID=$fDocumentID", $sDocumentName);
+	// add optional comment
+	if (strlen($sComment) > 0) {
+		$sMessage .= "<br><br>Comments:<br>$sComment";
+	}
+	$sMessage .= "</font>";
+	$sTitle = "Link: " . $sDocumentName . " from " . $sSendingUserName;
+	//email the hyperlink
+	$oEmail = new Email();
+	if ($oEmail->send($sDestEmailAddress, $sTitle, $sMessage)) {
+		$default->log->info("Send email ($sTitle) to $sDestEmailAddress");
+	} else {
+		$default->log->error("Error sending email ($sTitle) to $sDestEmailAddress");		
+	}
+	  
+	// emailed link transaction
+	$oDocumentTransaction = & new DocumentTransaction($fDocumentID, "Document link emailed to $sDestEmailAddress", EMAIL_LINK);
+	if ($oDocumentTransaction->create()) {
+		$default->log->debug("emailBL.php created email link document transaction for document ID=$fDocumentID");                                    	
+	} else {
+		$default->log->error("emailBL.php couldn't create email link document transaction for document ID=$fDocumentID");
+	}
+}
+
 if (checkSession()) {
     if (isset($fDocumentID)) {
-        require_once("$default->fileSystemRoot/lib/security/Permission.inc");
-        require_once("$default->fileSystemRoot/lib/documentmanagement/Document.inc");
-        require_once("$default->fileSystemRoot/lib/email/Email.inc");
-        require_once("$default->fileSystemRoot/lib/users/User.inc");
-        require_once("$default->fileSystemRoot/lib/documentmanagement/PhysicalDocumentManager.inc");
-        require_once("$default->fileSystemRoot/lib/documentmanagement/DocumentTransaction.inc");
-        require_once("$default->fileSystemRoot/lib/foldermanagement/Folder.inc");
-        require_once("$default->fileSystemRoot/presentation/Html.inc");
-        require_once("$default->fileSystemRoot/presentation/lookAndFeel/knowledgeTree/foldermanagement/folderUI.inc");
-        require_once("emailUI.inc");
-
         //get the document to send
         $oDocument = Document::get($fDocumentID);
 
         //if the user can view the document, they can email a link to it
         if (Permission::userHasDocumentReadPermission($fDocumentID)) {
             if (isset($fSendEmail)) {
-                //if we're going to send a mail, first make sure the to address is set
-                if (isset($fToEmail)) {
-                    if (validateEmailAddress($fToEmail)) {
-                        //if the to address is valid, send the mail
-                        global $default;
-                        $oUser = User::get($_SESSION["userID"]);
-                        $sMessage = "<font face=\"arial\" size=\"2\">";
-                        if (isset($fToName)) {
-                            $sMessage .= "$fToName,<br><br>Your colleague, " . $oUser->getName() . ", wishes you to view the document entitled '" . $oDocument->getName() . "'.\n  Click on the hyperlink below to view it.";
-                        } else {
-                            $sMessage .= "Your colleague, " . $oUser->getName() . ", wishes you to view the document entitled '" . $oDocument->getName() . "'.\n  Click on the hyperlink below to view it.";
-                        }
-                        // add the link to the document to the mail
-                        $sMessage .= "<br>" . generateControllerLink("viewDocument", "fDocumentID=$fDocumentID", $oDocument->getName());
-
-                        // add optional comment
-                        if (strlen($fComment) > 0) {
-                        	$sMessage .= "<br><br>Comments:<br>$fComment";
-                        }
-                        $sMessage .= "</font>";
-                        
-                        $sTitle = "Link: " . $oDocument->getName() . " from " . $oUser->getName();
-                        
-                        //email the hyperlink
-                        $oEmail = new Email();
-                        $oEmail->send($fToEmail, $sTitle, $sMessage);
-                        
-	                    // emailed link transaction
-	                    $oDocumentTransaction = & new DocumentTransaction($fDocumentID, "Document link emailed to $fToEmail", EMAIL_LINK);
-	                    if ($oDocumentTransaction->create()) {
-	                    	$default->log->debug("emailBL.php created email link document transaction for document ID=$fDocumentID");                                    	
-	                    } else {
-	                    	$default->log->error("emailBL.php couldn't create email link document transaction for document ID=$fDocumentID");
-	                    }
+	          	// explode group and user ids
+	          	$aGroupIDs = explode(",", $groupNewRight);
+	          	$aUserIDs = explode(",", $userNewRight);
+	          	$default->log->info("Sending email to groups=$groupNewRight; users=$userNewRight");
+	          	
+                //if we're going to send a mail, first make there is someone to send it to
+                if ((count($aGroupIDs) > 1) || (count($aUserIDs) > 1)) {
+	            	// send group emails
+	            	sendGroupEmails($aGroupIDs, $oDocument, $fComment);
+	            	// send user emails
+	            	sendUserEmails($aUserIDs, $oDocument, $fComment);
 	                    
-                        //go back to the document view page
-                        redirect("$default->rootUrl/control.php?action=viewDocument&fDocumentID=$fDocumentID");
-                    } else {
-                        //ask the user to enter a valid email address
-                        require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
-                        require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
-
-
-                        $oPatternCustom = & new PatternCustom();
-                        $oUserArray = User::getList();
-                        $oPatternCustom->setHtml(getDocumentEmailPage($oDocument,$oUserArray));
-                        $main->setErrorMessage("The email address you entered was invalid.  Please enter<br> " .
-                                               "an email address of the form someone@somewhere.some postfix");
-                        $main->setFormAction($_SERVER["PHP_SELF"] . "?fDocumentID=$fDocumentID&fSendEmail=1");
-                        $main->setCentralPayload($oPatternCustom);
-                        $main->render();
-                    }
-                }
+                    //go back to the document view page
+                    redirect("$default->rootUrl/control.php?action=viewDocument&fDocumentID=$fDocumentID");
+				} else {
+				    // ask the user to specify users or groups to send the mail to
+				    require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
+				    require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
+				    $main->setErrorMessage("You must select either a group or a user to send an email link to.");				    
+				    $oPatternCustom = & new PatternCustom();
+				    $oPatternCustom->setHtml(getDocumentEmailPage($oDocument));
+				    $main->setCentralPayload($oPatternCustom);
+	                $main->setOnLoadJavaScript("optGroup.init(document.forms[0]);optUser.init(document.forms[0]);");
+	                $main->setHasRequiredFields(true);
+	                $main->setAdditionalJavaScript(initialiseOptionTransferJavaScript());
+				    $main->setFormAction($_SERVER["PHP_SELF"] . "?fDocumentID=$fDocumentID&fSendEmail=1");	                
+	                $main->setDHTMLScrolling(false);
+				    $main->render();
+				}
             } else {
-                //ask for an email address
+                // display form
                 require_once("$default->fileSystemRoot/presentation/webpageTemplate.inc");
                 require_once("$default->fileSystemRoot/lib/visualpatterns/PatternCustom.inc");
-				
-				$oUserArray = User::getList();
                 $oPatternCustom = & new PatternCustom();
-                $oPatternCustom->setHtml(getDocumentEmailPage($oDocument,$oUserArray));
+                $oPatternCustom->setHtml(getDocumentEmailPage($oDocument));
                 $main->setCentralPayload($oPatternCustom);
+                $main->setOnLoadJavaScript("optGroup.init(document.forms[0]);optUser.init(document.forms[0]);");
+                $main->setHasRequiredFields(true);
+                $main->setAdditionalJavaScript(initialiseOptionTransferJavaScript());
                 $main->setFormAction($_SERVER["PHP_SELF"] . "?fDocumentID=$fDocumentID&fSendEmail=1");
+                $main->setDHTMLScrolling(false);
                 $main->render();
             }
         } else {
@@ -122,7 +198,29 @@ if (checkSession()) {
     }
 
 }
-
+function initialiseOptionTransferJavascript() {
+	return "<script LANGUAGE=\"JavaScript\">\n" .
+		"var optGroup = new OptionTransfer(\"groupSelect\",\"chosenGroups\");\n" .
+		"optGroup.setAutoSort(true);\n" .
+		"optGroup.setDelimiter(\",\");\n" .
+		"optGroup.saveNewLeftOptions(\"groupNewLeft\");\n" .
+		"optGroup.saveNewRightOptions(\"groupNewRight\");\n" .
+		"optGroup.saveRemovedLeftOptions(\"groupRemovedLeft\");\n" .
+		"optGroup.saveRemovedRightOptions(\"groupRemovedRight\");\n" .
+		"optGroup.saveAddedLeftOptions(\"groupAddedLeft\");\n" .
+		"optGroup.saveAddedRightOptions(\"groupAddedRight\");\n" .
+					
+		"var optUser = new OptionTransfer(\"userSelect\",\"chosenUsers\");\n" .
+		"optUser.setAutoSort(true);\n" .
+		"optUser.setDelimiter(\",\");\n" .
+		"optUser.saveNewLeftOptions(\"userNewLeft\");\n" .
+		"optUser.saveNewRightOptions(\"userNewRight\");\n" .
+		"optUser.saveRemovedLeftOptions(\"userRemovedLeft\");\n" .
+		"optUser.saveRemovedRightOptions(\"userRemovedRight\");\n" .
+		"optUser.saveAddedLeftOptions(\"userAddedLeft\");\n" .	
+		"optUser.saveAddedRightOptions(\"userAddedRight\");\n" .
+	"</SCRIPT>";		
+}
 /** use regex to validate the format of the email address */
 function validateEmailAddress($sEmailAddress) {
     $aEmailAddresses = array();
