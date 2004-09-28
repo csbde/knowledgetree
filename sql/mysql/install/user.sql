4fa6b8bf620df52a3930745d52908ea05c4c6446
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2004-09-28 11:24:17 +0000

    Don't hardcode that we're using the 'dms' database.  Use whatever database the installer sets on the command line.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2934 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/install/user.sql
+++ sql/mysql/install/user.sql
@@ -1,23 +1,23 @@
 -- default dms user
-GRANT SELECT, INSERT, UPDATE, DELETE ON dms.* TO dms@localhost IDENTIFIED BY 'djw9281js';
+GRANT SELECT, INSERT, UPDATE, DELETE ON * TO dms@localhost IDENTIFIED BY 'djw9281js';
 -- restricted indexer user
-GRANT SELECT ON dms.documents TO indexer@localhost IDENTIFIED BY 'idx158pqg';
-GRANT SELECT ON dms.folders TO indexer@localhost IDENTIFIED BY 'idx158pqg';
-GRANT SELECT ON dms.groups_folders_link TO indexer@localhost IDENTIFIED BY 'idx158pqg';
-GRANT SELECT ON dms.users_groups_link TO indexer@localhost IDENTIFIED BY 'idx158pqg';
-GRANT SELECT ON dms.folders_users_roles_link TO indexer@localhost IDENTIFIED BY 'idx158pqg';
-GRANT SELECT ON dms.users TO indexer@localhost IDENTIFIED BY 'idx158pqg';
-GRANT SELECT ON dms.groups_lookup TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT ON documents TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT ON folders TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT ON groups_folders_link TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT ON users_groups_link TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT ON folders_users_roles_link TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT ON users TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT ON groups_lookup TO indexer@localhost IDENTIFIED BY 'idx158pqg';
 
-GRANT SELECT, UPDATE ON dms.system_settings TO indexer@localhost IDENTIFIED BY 'idx158pqg';
-GRANT SELECT ON dms.document_transactions TO indexer@localhost IDENTIFIED BY 'idx158pqg';
-GRANT SELECT ON dms.documents TO indexer@localhost IDENTIFIED BY 'idx158pqg';
-GRANT SELECT ON dms.folders TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT, UPDATE ON system_settings TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT ON document_transactions TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT ON documents TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT SELECT ON folders TO indexer@localhost IDENTIFIED BY 'idx158pqg';
 
-GRANT INSERT, DELETE ON dms.document_text TO indexer@localhost IDENTIFIED BY 'idx158pqg';
-GRANT INSERT, DELETE ON dms.search_document_user_link TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT INSERT, DELETE ON document_text TO indexer@localhost IDENTIFIED BY 'idx158pqg';
+GRANT INSERT, DELETE ON search_document_user_link TO indexer@localhost IDENTIFIED BY 'idx158pqg';
 -- restricted archiver user
-GRANT SELECT, UPDATE ON dms.documents TO archiver@localhost IDENTIFIED BY 'ar(h923v3R';
+GRANT SELECT, UPDATE ON documents TO archiver@localhost IDENTIFIED BY 'ar(h923v3R';
 GRANT SELECT ON time_period TO archiver@localhost IDENTIFIED BY 'ar(h923v3R';
 GRANT SELECT ON time_unit_lookup TO archiver@localhost IDENTIFIED BY 'ar(h923v3R';
 GRANT SELECT ON document_transactions TO archiver@localhost IDENTIFIED BY 'ar(h923v3R';
