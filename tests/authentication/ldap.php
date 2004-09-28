3a52321c2200c5663ad8820f239be943e82ba2e4
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2004-09-28 11:56:04 +0000

    Make this at least compile...
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@2936 c91229c3-7414-0410-bfa2-8a42b809f60b

--- tests/authentication/ldap.php
+++ tests/authentication/ldap.php
@@ -1,27 +1,21 @@
 <?php
 echo "<pre>";
 $user="michael";
-s$password="michael";
+$password="michael";
 $ldap["domain"]="jamwarehouse.com";
-$ldap["dn"]=$user."@".$ldap["domain"]; //microsoft ldap wants the username@domai
-n for authentication
-//$ldap["dn"]="uid=$user, ou=The Jam Warehouse,ou=External to MRC, o=Medical Res
-earch Council";
+$ldap["dn"]=$user."@".$ldap["domain"]; //microsoft ldap wants the username@domain for authentication
+//$ldap["dn"]="uid=$user, ou=The Jam Warehouse,ou=External to MRC, o=Medical Research Council";
 $ldap["ds"]="192.168.1.8"; //ldap server
 if ($ldap["connection"]=ldap_connect($ldap["ds"])) {
     if ($ldap["connection"]) {
         echo "binding to " . $ldap["dn"];
         //if(@ldap_bind($ldap["connection"],$ldap["dn"],$password)) {
         if(@ldap_bind($ldap["connection"])) {
-            $result = ldap_search( $ldap["connection"], "CN=Users,DC=jamwarehous
-e,DC=com", "samaccountname=*mi*");
-            echo '$result = ldap_search( $ldap["connection"], "CN=Users,DC=jamwa
-rehouse,DC=com", "samaccountname=*mi*");';
-            //$result = ldap_search( $ldap["connection"], "o=Medical Research Co
-uncil", "uid=karen");
+            $result = ldap_search( $ldap["connection"], "CN=Users,DC=jamwarehouse,DC=com", "samaccountname=*mi*");
+            echo '$result = ldap_search( $ldap["connection"], "CN=Users,DC=jamwarehouse,DC=com", "samaccountname=*mi*");';
+            //$result = ldap_search( $ldap["connection"], "o=Medical Research Council", "uid=karen");
             $account="samaccountname=".$user;
-            //$result = ldap_list($ldap["connection"], "CN=Users,DC=jamwarehouse
-,DC=com", $account);
+            //$result = ldap_list($ldap["connection"], "CN=Users,DC=jamwarehouse,DC=com", $account);
             $entry = ldap_get_entries($ldap["connection"], $result);
             $fullname=$entry[0]["cn"][0];
             $email=$entry[0]["mail"][0];
@@ -34,8 +28,7 @@ uncil", "uid=karen");
 
             //print "<BR>Your name is ".$fullname." and your email is ".$email;
         } else {
-            print ("<BR><B>Incorrect password or user ".$user." not found.</B><P
->");
+            print ("<BR><B>Incorrect password or user ".$user." not found.</B><P>");
         }
     }
 }
