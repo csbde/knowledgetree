b2b295fe6207c4cfb3e4c5b50ed128cb10b6dcbc
Neil Blakey-Milner <nbm@knowledgetree.com>
2004-11-26 15:51:59 +0000

    Autodetect paths and server name, and componentise and modernise the initialisation process.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3000 c91229c3-7414-0410-bfa2-8a42b809f60b

--- config/dmsDefaults.php
+++ config/dmsDefaults.php
@@ -1,4 +1,6 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
 /**
  * $Id$
  *
@@ -21,47 +23,106 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */ 
 
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
+// If not defined, set KT_DIR based on my usual location in the tree
+if (!defined('KT_DIR')) {
+    define('KT_DIR', realpath(dirname(__FILE__) . '/..'));
+}
+
+if (!defined('KT_LIB_DIR')) {
+    define('KT_LIB_DIR', KT_DIR . '/lib');
+}
+
+// PATH_SEPARATOR added in PHP 4.3.0
+if (!defined('PATH_SEPARATOR')) {
+    if (substr(PHP_OS, 0, 3) == 'WIN') {
+        define('PATH_SEPARATOR', ';');
+    } else {
+        define('PATH_SEPARATOR', ':');
+    }
+}
+
+/// {{{ KTInit
+class KTInit {
+    // {{{ prependPath()
+    function prependPath ($path) {
+        $include_path = ini_get('include_path');
+        ini_set('include_path', $path . PATH_SEPARATOR . $include_path);
+    }
+    // }}}
+
+    // {{{ setupLogging()
+    function setupLogging () {
+        global $default;
+        require_once("$default->fileSystemRoot/lib/Log.inc");
+        $default->log = new Log($default->fileSystemRoot . "/log", INFO);
+        $default->timerLog = new Log($default->fileSystemRoot . "/log", INFO, "timer");
+    }
+    // }}}
+
+    // {{{ setupI18n()
+    /**
+     * setupI
+     *
+     */
+    function setupI18n () {
+        global $default;
+        if (in_array("gettext", get_loaded_extensions()) && function_exists('gettext') && function_exists('_')) {
+            require_once("$default->fileSystemRoot/lib/i18n/languageFunctions.inc");	
+            require_once("$default->fileSystemRoot/lib/i18n/accept-to-gettext.inc");	
+            if ($default->useAcceptLanguageHeader) {
+                $aInstalledLocales = getInstalledLocales();
+                $sLocale=al2gt($aInstalledLocales, 'text/html');
+                $default->defaultLanguage = $sLocale;
+            }		
+            putenv('LANG=' . $default->defaultLanguage); 
+            setlocale(LC_ALL, $default->defaultLanguage);
+            // Set the text domain
+            $sDomain = 'knowledgeTree';
+            bindtextdomain($sDomain, $default->fileSystemRoot . "/i18n"); 
+            textdomain($sDomain);
+        } else {
+            $default->log->info("Gettext not installed, i18n disabled.");
+            // define a dummy _ function so gettext is not -required-
+            function _($sString) {
+                return $sString;
+            }
+        }
+    }
+    // }}}
+}
+// }}}
+
+$default->fileSystemRoot = KT_DIR;
+$default->serverName = $_SERVER['HTTP_HOST'];
+
 // include the environment settings
 require_once("environment.php");
+
+
 // table mapping entries
 include("tableMappings.inc");
+
+KTInit::prependPath($default->pear_path);
+
 // instantiate log
-require_once("$default->fileSystemRoot/lib/Log.inc");
-$default->log = new Log($default->fileSystemRoot . "/log", INFO);
-$default->timerLog = new Log($default->fileSystemRoot . "/log", INFO, "timer");
-// setup i18n if gettext is installed
-if (in_array("gettext", get_loaded_extensions()) && function_exists('gettext') && function_exists('_')) {
-	require_once("$default->fileSystemRoot/lib/i18n/languageFunctions.inc");	
-	require_once("$default->fileSystemRoot/lib/i18n/accept-to-gettext.inc");	
-	if ($default->useAcceptLanguageHeader) {
-	    $aInstalledLocales = getInstalledLocales();
-	    $sLocale=al2gt($aInstalledLocales, 'text/html');
-	    $default->defaultLanguage = $sLocale;
-	}		
-	putenv('LANG=' . $default->defaultLanguage); 
-	setlocale(LC_ALL, $default->defaultLanguage);
-	// Set the text domain
-	$sDomain = 'knowledgeTree';
-	bindtextdomain($sDomain, $default->fileSystemRoot . "/i18n"); 
-	textdomain($sDomain);
-} else {
-	$default->log->info("Gettext not installed, i18n disabled.");
-	// define a dummy _ function so gettext is not -required-
-	function _($sString) {
-		return $sString;
-	}
-}
+KTInit::setupLogging();
+
+KTInit::setupI18n();
 
 // site map definition
 include("siteMap.inc");
 
-require_once("$default->fileSystemRoot/phpmailer/class.phpmailer.php");
-require_once("$default->fileSystemRoot/lib/session/Session.inc");
-require_once("$default->fileSystemRoot/lib/session/control.inc");
-require_once("$default->fileSystemRoot/presentation/Html.inc");
+require_once(KT_DIR . '/phpmailer/class.phpmailer.php');
+require_once(KT_LIB_DIR . '/session/Session.inc');
+require_once(KT_LIB_DIR . '/session/control.inc');
+require_once(KT_DIR . '/presentation/Html.inc');
 // browser settings
-require_once("$default->fileSystemRoot/phpSniff/phpSniff.class.php");
-require("browsers.inc");
+require_once(KT_DIR . '/phpSniff/phpSniff.class.php');
+require_once('browsers.inc');
 // import request variables and setup language
-require_once("$default->fileSystemRoot/lib/dms.inc");
+require_once(KT_LIB_DIR . '/dms.inc');
 ?>
