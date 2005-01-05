36b2cacf7642fc6dbb94953a64a0f2682ca94172
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-01-05 12:58:39 +0000

    Allow the admin to remove a group despite it having members, since all they're going to do is remove the users from that group, and remove a group despite it being a member of the unit, since all they're going to do is remove it from the unit.  So do it for them.  They still have to confirm that they want to delete that group.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3114 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/groupmanagement/removeGroupBL.php
+++ presentation/lookAndFeel/knowledgeTree/administration/groupmanagement/removeGroupBL.php
@@ -45,29 +45,21 @@ if (checkSession()) {
 
 	if (isset($fGroupID)) {
 		$oGroup = Group::get($fGroupID);
-		if (!$oGroup->hasUsers()) {
-			if (!$oGroup->hasUnit()) {
-				if (!$oGroup->hasRoutingSteps()) {
-					if (isset($fForDelete)) {
-						if ($oGroup->delete()) {
-							// FIXME: refactor getStatusPage in Html.inc
-							$oPatternCustom->setHtml(statusPage(_("Remove Group"), _("Group successfully removed"), "", "listGroups"));
-						} else {
-							$oPatternCustom->setHtml(statusPage(_("Remove Group"), _("Group deletion failed!"), _("There was an error deleting this group.  Please try again later."), "listGroups"));
-						}
-					} else {
-						$oPatternCustom->setHtml(getDeletePage($fGroupID));
-						$main->setFormAction($_SERVER["PHP_SELF"] . "?fForDelete=1");
-					}
-				} else {
-					$oPatternCustom->setHtml(statusPage(_("Remove Group"), _("This group is part of a document routing step!"), _("This group can not be deleted because it is involved in the document routing process."), "listGroups"));
-				}
-			} else {
-				$oPatternCustom->setHtml(statusPage(_("Remove Group"), _("This group is in a unit!"), _("This group can not be deleted because it belongs to a unit."), "listGroups"));
-			}					
-		} else {
-			$oPatternCustom->setHtml(statusPage(_("Remove Group"), _("This group has users!"), _("This group can not be deleted because there are still users in it."), "listGroups"));
-		}
+        if (!$oGroup->hasRoutingSteps()) {
+            if (isset($fForDelete)) {
+                if ($oGroup->delete()) {
+                    // FIXME: refactor getStatusPage in Html.inc
+                    $oPatternCustom->setHtml(statusPage(_("Remove Group"), _("Group successfully removed"), "", "listGroups"));
+                } else {
+                    $oPatternCustom->setHtml(statusPage(_("Remove Group"), _("Group deletion failed!"), _("There was an error deleting this group.  Please try again later."), "listGroups"));
+                }
+            } else {
+                $oPatternCustom->setHtml(getDeletePage($fGroupID));
+                $main->setFormAction($_SERVER["PHP_SELF"] . "?fForDelete=1");
+            }
+        } else {
+            $oPatternCustom->setHtml(statusPage(_("Remove Group"), _("This group is part of a document routing step!"), _("This group can not be deleted because it is involved in the document routing process."), "listGroups"));
+        }
 	} else {
 		$oPatternCustom->setHtml(statusPage(_("Remove Group"), _("No group was selected for deletion"), "", "listGroups"));
 	}
