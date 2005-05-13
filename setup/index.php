16d4e5017f1a8f1b1070cb2466b2f7a3782b0b1b
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-05-13 10:58:56 +0000

    Put a number of lines of comments to help people who don't even have PHP installed to realise that this is the problem.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3263 c91229c3-7414-0410-bfa2-8a42b809f60b

--- setup/index.php
+++ setup/index.php
@@ -1,3 +1,14 @@
 <?php
+# If you are reading this, then your web server is not serving PHP files
+# correctly.  Please read the instructions in doc/INSTALL.txt to solve
+# this problem.
+#
+#
+#
+#
+#
+#
+#
+#
 header("Location: precheckup.php");
 ?>
