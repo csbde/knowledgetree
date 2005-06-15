37827f4a3ef37d61562b76417efb58dea4b8f989
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-06-15 21:29:15 +0000

    Ensure the descriptors aren't shortened by the length of the descriptor field.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3380 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/2.0.6/create_upgrade_table.sql
+++ sql/mysql/upgrade/2.0.6/create_upgrade_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE upgrades (
   id int(10) unsigned NOT NULL default '0',
-  descriptor char(40) NOT NULL default '',
+  descriptor char(100) NOT NULL default '',
   description char(255) NOT NULL default '',
   date_performed datetime NOT NULL default '0000-00-00 00:00:00',
   result tinyint(4) NOT NULL default '0',
