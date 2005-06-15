a96e165d6b294bd7ee2826896f43b6e2ef6ea2c9
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-06-15 22:33:17 +0000

    Ensure parent can handle descriptors, and update the structure.sql for first installs.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3386 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/2.0.6/create_upgrade_table.sql
+++ sql/mysql/upgrade/2.0.6/create_upgrade_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE upgrades (
   description char(255) NOT NULL default '',
   date_performed datetime NOT NULL default '0000-00-00 00:00:00',
   result tinyint(4) NOT NULL default '0',
-  parent char(40) default NULL,
+  parent char(100) default NULL,
   PRIMARY KEY  (id),
   KEY descriptor (descriptor),
   KEY parent (parent)
