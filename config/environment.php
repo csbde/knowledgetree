d1d3386ba9f4faa9aa83f834540c2c44af18961e
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-07-20 16:41:19 +0000

    Explain how to specify paths in KnowledgeTree here too.
    Don't put a license on a configuration file.
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3403 c91229c3-7414-0410-bfa2-8a42b809f60b

--- config/environment.php
+++ config/environment.php
@@ -3,23 +3,16 @@
  * $Id$
  *
  * Defines KnowledgeTree application environment settings.
- *
- * Copyright (c) 2003 Jam Warehouse http://www.jamwarehouse.com
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */ 
+
+// ----------------------------------------------------------------
+// When entering paths in this file, use the Unix-style '/' path
+// separator, not the Windows-style '\' path separator.
+//
+// So, if you want to refer to C:\Web\KnowledgeTree\Documents,
+// use:
+//      C:/Web/KnowledgeTree/Documents
+// ----------------------------------------------------------------
 
 // ----------------------------------------------------------------
 // The options in this section should automatically be detected by
