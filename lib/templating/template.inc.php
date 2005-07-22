ede6708cb8c7aa489d247ad12d0859b9a8b281fe
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-07-22 13:32:56 +0000

    KTTemplate is an abstract class that shows the interface that template plugins need to implement.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3444 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ lib/templating/template.inc.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * $Id$
+ *
+ * Abstract template interface
+ *
+ * Copyright (c) 2005 Jam Warehouse http://www.jamwarehouse.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @version $Revision$
+ * @author Neil Blakey-Milner, Jam Warehouse (Pty) Ltd, South Africa
+ */
+
+class KTTemplate {
+    var $sPath;
+
+    function KTTemplate ($sPath) {
+        $this->sPath = $sPath;
+    }
+
+    function render($aDict) {
+        return PEAR::raiseError("Not implemented");
+    }
+}
+
+?>
