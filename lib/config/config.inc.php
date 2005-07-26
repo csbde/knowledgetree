06510c0218e33a74d6afcf2e5add6ad21b7f6c07
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-07-26 12:57:33 +0000

    Move over to .ini-style configuration for user configuration.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3464 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ lib/config/config.inc.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once("Config.php");
+
+define("KT_CONFIG_DEFAULT", true);
+
+class KTConfig {
+    var $conf = array();
+    var $aSectionFile;
+    var $aFileRoot;
+    var $flat = array();
+    var $flatns = array();
+
+    function loadFile($filename, $bDefault = false) {
+        $c = new Config;
+        $root =& $c->parseConfig($filename, "IniCommented");
+        $this->aFileRoot[$filename] =& $root;
+        $conf =& $root->toArray();
+        foreach ($conf["root"] as $seck => $secv) {
+            $aSectionFile[$seck] = $filename;
+            if (is_array($secv)) {
+                foreach ($secv as $k => $v) {
+                    $this->setns($seck, $k, $v);
+                }
+            } else {
+                $this->setns(null, $seck, $secv);
+            }
+        }
+        $this->conf = array_merge($this->conf, $conf["root"]);
+    }
+
+    function setns($seck, $k, $v, $bDefault = false) {
+        if ($v === "default") {
+            return;
+        } elseif ($v === "true") {
+            $v = true;
+        } elseif ($v === "false") {
+            $v = false;
+        }
+        $this->flat[$k] = $v;
+        if (!is_null($seck)) {
+            $this->flatns["$seck/$k"] = $v;
+        }
+        return;
+    }
+
+    function setdefaultns($seck, $k, $v) {
+        return $this->setns($seck, $k, $v, true);
+    }
+
+    var $expanded = array();
+    var $expanding = array();
+    function expand($val) {
+        if (strpos($val, '$') === false) {
+            return $val;
+        }
+        $v = $val;
+        while(($m = preg_match('/\$\{([^}]+)\}/', $v, $matches))) {
+            array_push($this->expanding, $matches[1]);
+            $r = $this->get($matches[1]);
+            if (PEAR::isError($r)) {
+                return $r;
+            }
+            $v = str_replace($matches[0], $r, $v);
+            $this->expanded[$matches[1]] = $r;
+        }
+        return $v;
+    }
+
+    function get($var) {
+        if (array_key_exists($var, $this->flatns)) {
+            return $this->expand($this->flatns[$var]);
+        }
+        if (array_key_exists($var, $this->flat)) {
+            return $this->expand($this->flat[$var]);
+        }
+        return null;
+    }
+
+    function &getSingleton() {
+        global $KTConfig;
+        return $KTConfig;
+    }
+}
+
+$KTConfig =& new KTConfig;
+
+?>
