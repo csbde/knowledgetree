1ff586f2c17f896a5ac5c4fa6890ddc9b5cfd901
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-07-26 12:32:37 +0000

    Quick tests for help functionality
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3462 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ tests/help/getHelpStringForSection.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once("../../config/dmsDefaults.php");
+require_once(KT_LIB_DIR . "/help/help.inc.php");
+
+error_reporting(E_ALL);
+
+var_dump(KTHelp::getHelpStringForSection('dashboard'));
+
+?>
