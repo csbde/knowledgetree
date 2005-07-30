0edba664f66536c98c118786d3a3db30c3491d5a
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-07-30 14:48:22 +0000

    Test for phpcompat's array_combine
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3492 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ tests/phpcompat/array_combine.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("../../config/dmsDefaults.php");
+
+$a = array('green', 'red', 'yellow');
+$b = array('avocado', 'apple', 'banana');
+$c = array_combine($a, $b);
+
+print_r($c);
+
+?>
