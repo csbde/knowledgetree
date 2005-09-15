37b738ca2a58383a9d53d61f50ecc8366206435f
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-09-15 09:14:27 +0000

    Add an abstracted import storage system, and implement importing from the filesystem.  This can be extended to import from various archive files (such as ZIP), from other document management tools, from CVS or SVN.  Includes the idea that multiple content versions exists.  Does not handle importing metadata at all.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3583 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ lib/import/bulkimport.inc.php
@@ -0,0 +1,90 @@
+<?php /* vim: set expandtab softtabstop=4 shiftwidth=4 foldmethod=marker: */
+/**
+ * $Id$
+ *
+ * Import all documents from an import storage location
+ *
+ * Copyright (c) 2005 Jam Warehouse http://www.jamwarehouse.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @version $Revision$
+ * @author Neil Blakey-Milner, Jam Warehouse (Pty) Ltd, South Africa
+ */
+
+require_once(KT_LIB_DIR . '/foldermanagement/folderutil.inc.php');
+require_once(KT_LIB_DIR . '/documentmanagement/documentutil.inc.php');
+require_once(KT_LIB_DIR . '/filelike/filelikeutil.inc.php');
+
+class KTBulkImportManager {
+    var $oStorage;
+
+    function KTBulkImportManager($oFolder, $oStorage, $oUser, $aOptions = null) {
+        $this->oFolder =& $oFolder;
+        $this->oStorage =& $oStorage;
+        $this->oUser =& $oUser;
+        $this->aOptions =& $aOptions;
+        if (is_null($aOptions)) {
+            $aOptions = array();
+        }
+        $this->aMetadata = KTUtil::arrayGet($aOptions, 'metadata', array());
+    }
+
+    function import() {
+        $res = $this->_importfolder($this->oFolder, "/");
+        if (PEAR::isError($res)) {
+            DBUtil::rollback();
+            return $res;
+        }
+        return;
+    }
+
+    function _importfolder($oFolder, $sPath) {
+        $aDocPaths = $this->oStorage->listDocuments($sPath);
+        foreach ($aDocPaths as $sDocumentPath) {
+            $res = $this->_importdocument($oFolder, $sDocumentPath);
+            if (PEAR::isError($res)) {
+                return $res;
+            }
+        }
+        $aFolderPaths = $this->oStorage->listFolders($sPath);
+        foreach ($aFolderPaths as $sFolderPath) {
+            $oThisFolder = KTFolderUtil::add($oFolder, basename($sFolderPath), $this->oUser);
+            if (PEAR::isError($oThisFolder)) {
+                return $oThisFolder;
+            }
+            $res = $this->_importfolder($oThisFolder, $sFolderPath);
+            if (PEAR::isError($res)) {
+                return $res;
+            }
+        }
+    }
+
+    function _importdocument($oFolder, $sPath) {
+        $aInfo = $this->oStorage->getDocumentInfo($sPath);
+        if (PEAR::isError($aInfo)) {
+            return $aInfo;
+        }
+        $aOptions = array(
+            // XXX: Multiversion Import
+            'contents' => $aInfo->aVersions[0],
+            'metadata' => $this->aMetadata,
+        );
+        $oDocument =& KTDocumentUtil::add($oFolder, basename($sPath), $this->oUser, $aOptions);
+        return $oDocument;
+    }
+}
+
+?>
