6b2da2d429514b7835a6b7a9dcef1bb7ed4ed818
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-09-15 09:47:42 +0000

    Simple script to rebuild a KT database from scratch for testing.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3584 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ sql/mysql/install/rebuild.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+db=kttest
+
+mysqladmin -u root -f drop $db
+mysqladmin -u root create $db
+mysql -u root $db < structure.sql
+mysql -u root $db < data.sql
+mysql -u root $db < user.sql
+
