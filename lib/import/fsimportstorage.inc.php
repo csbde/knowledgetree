bc525d96285ac2a82e129780173326bd4021fc22
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-09-15 09:14:27 +0000

    Add an abstracted import storage system, and implement importing from the filesystem.  This can be extended to import from various archive files (such as ZIP), from other document management tools, from CVS or SVN.  Includes the idea that multiple content versions exists.  Does not handle importing metadata at all.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3583 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ lib/import/fsimportstorage.inc.php
@@ -0,0 +1,102 @@
+<?php /* vim: set expandtab softtabstop=4 shiftwidth=4 foldmethod=marker: */
+/**
+ * $Id$
+ *
+ * Manages listing and contents for documents on a filesystem.
+ *
+ * Copyright (c) 2005 Jam Warehouse http://www.jamwarehouse.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @version $Revision$
+ * @author Neil Blakey-Milner, Jam Warehouse (Pty) Ltd, South Africa
+ */
+
+require_once(KT_LIB_DIR . '/import/importstorage.inc.php');
+require_once(KT_LIB_DIR . '/filelike/fsfilelike.inc.php');
+
+class KTFSImportStorage extends KTImportStorage {
+    function KTFSImportStorage($sBasePath) {
+        $this->sBasePath = $sBasePath;
+    }
+
+    function listDocuments($sFolderPath) {
+        $ret = array();
+        if (substr($sFolderPath, -1) === "/") {
+            $sFolderPath = substr($sFolderPath, 0, -1);
+        }
+        $sFullPath = sprintf("%s/%s", $this->sBasePath, $sFolderPath);
+        if (!is_dir($sFullPath)) {
+            return PEAR::raiseError('Path is not a folder');
+        }
+        $rDir = @opendir($sFullPath);
+        if ($rDir === false) {
+            return PEAR::raiseError('Failed to open folder');
+        }
+        while (($sFilename = readdir($rDir)) !== false) {
+            $sThisPath = sprintf("%s/%s", $sFullPath, $sFilename);
+            if (is_file($sThisPath)) {
+                if (empty($sFolderPath)) {
+                    $ret[] = $sFilename;
+                } else {
+                    $ret[] = sprintf("%s/%s", $sFolderPath, $sFilename);
+                }
+            }
+        }
+        closedir($rDir);
+        return $ret;
+    }
+
+    function listFolders($sFolderPath) {
+        $ret = array();
+        if (substr($sFolderPath, -1) === "/") {
+            $sFolderPath = substr($sFolderPath, 0, -1);
+        }
+        $sFullPath = sprintf("%s/%s", $this->sBasePath, $sFolderPath);
+        if (!is_dir($sFullPath)) {
+            return PEAR::raiseError('Path is not a folder');
+        }
+        $rDir = @opendir($sFullPath);
+        if ($rDir === false) {
+            return PEAR::raiseError('Failed to open folder');
+        }
+        while (($sFilename = readdir($rDir)) !== false) {
+            if (in_array($sFilename, array(".", ".."))) {
+                continue;
+            }
+            $sThisPath = sprintf("%s/%s", $sFullPath, $sFilename);
+            if (is_dir($sThisPath)) {
+                if (empty($sFolderPath)) {
+                    $ret[] = $sFilename;
+                } else {
+                    $ret[] = sprintf("%s/%s", $sFolderPath, $sFilename);
+                }
+            }
+        }
+        closedir($rDir);
+        return $ret;
+    }
+
+    function getDocumentInfo($sDocumentPath) {
+        return new KTImportStorageInfo(
+            basename($sDocumentPath),
+            array(
+                new KTFSFileLike(sprintf("%s/%s", $this->sBasePath, $sDocumentPath))
+            )
+        );
+    }
+}
+
+?>
---
+++ lib/import/importstorage.inc.php
@@ -0,0 +1,64 @@
+<?php /* vim: set expandtab softtabstop=4 shiftwidth=4 foldmethod=marker: */
+/**
+ * $Id$
+ *
+ * Interface for representing a method of listing and importing
+ * documents from storage.
+ *
+ * Copyright (c) 2005 Jam Warehouse http://www.jamwarehouse.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @version $Revision$
+ * @author Neil Blakey-Milner, Jam Warehouse (Pty) Ltd, South Africa
+ */
+
+class KTImportStorage {
+    function listDocuments($sFolderPath) {
+        return PEAR::raiseError('Not implemented');
+    }
+
+    function listFolders($sFolderPath) {
+        return PEAR::raiseError('Not implemented');
+    }
+
+    function getDocumentInfo($sDocumentPath) {
+        return PEAR::raiseError('Not implemented');
+    }
+}
+
+class KTImportStorageInfo {
+    /**
+     * File name to store in the repository.
+     */
+    var $sFilename;
+
+    /**
+     * Ordered array (oldest to newest) of KTFileLike objects that can
+     * get the contents for versions of the given file.
+     */
+    var $aVersions;
+
+    function KTImportStorageInfo ($sFilename, $aVersions) {
+        $this->sFilename = $sFilename;
+        $this->aVersions = $aVersions;
+    }
+
+    function getFilename() {
+        return $this->sFilename;
+    }
+}
+
+?>
