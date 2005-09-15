1fcb8cd54e45563bb0cc54055f4dc598b501616f
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-09-15 08:46:16 +0000

    The metadata version is stored in field metadata_version, not "metadata"
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3577 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/2.0.8/1-metadata_versions.sql
+++ sql/mysql/upgrade/2.0.8/1-metadata_versions.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `documents` ADD `live_document_id` INT;
 ALTER TABLE `documents` ADD INDEX ( `live_document_id` ) ;
-ALTER TABLE `documents` ADD `metadata` INT DEFAULT '0' NOT NULL ;
+ALTER TABLE `documents` ADD `metadata_version` INT DEFAULT '0' NOT NULL ;
