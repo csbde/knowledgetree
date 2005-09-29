d1407750a53ab8b85a3cef1bdc111748c3729687
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-09-29 11:27:01 +0000

    Simplified folder -> document type setting system (including the ability to say that the folder doesn't have any restrictions)
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3681 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/foldermanagement/folderDocumentTypes.php
@@ -0,0 +1,72 @@
+<?php
+
+require_once('../../../../config/dmsDefaults.php');
+require_once(KT_LIB_DIR . '/dispatcher.inc.php');
+require_once(KT_LIB_DIR . '/documentmanagement/DocumentType.inc');
+require_once(KT_LIB_DIR . '/foldermanagement/FolderDocTypeLink.inc');
+
+$sectionName = "Administration";
+require_once(KT_DIR . "/presentation/webpageTemplate.inc");
+
+class KTFolderDocumentTypeDispatcher extends KTStandardDispatcher {
+    function check () {
+        if (empty($_REQUEST['fFolderId'])) {
+            $this->permissionDenied();
+            exit(0);
+        }
+        $oPermission = KTPermission::getByName('ktcore.permissions.write');
+        $this->oFolder =& Folder::get($_REQUEST['fFolderId']);
+        $oUser =& User::get($_SESSION['userID']);
+        if (!KTPermissionUtil::userHasPermissionOnItem($oUser, $oPermission, $this->oFolder)) {
+            $this->permissionDenied();
+            exit(0);
+        }
+        return true;
+    }
+
+    function do_main() {
+        $oTemplating =& KTTemplating::getSingleton();
+        $oTemplate = $oTemplating->loadTemplate('ktcore/documenttypes/folderassign');
+
+        $sTable = KTUtil::getTableName('folder_doctypes');
+        $aQuery = array(
+            "SELECT document_type_id FROM $sTable WHERE folder_id = ?",
+            array($this->oFolder->getId()),
+        );
+        $aSelectedIds = DBUtil::getResultArrayKey($aQuery, 'document_type_id');
+
+        $oTemplate->setData(array(
+            'oFolder' => $this->oFolder,
+            'document_types' => DocumentType::getList(),
+            'selected_types' => $aSelectedIds,
+        ));
+        return $oTemplate;
+    }
+
+    function do_assign() {
+        if (empty($_REQUEST['restricted'])) {
+            $this->oFolder->setRestrictDocumentTypes(false);
+        } else {
+            $this->oFolder->setRestrictDocumentTypes(true);
+        }
+
+        $sTable = KTUtil::getTableName('folder_doctypes');
+        $res = DBUtil::runQuery(array(
+            "DELETE FROM $sTable WHERE folder_id = ?",
+            array($this->oFolder->getId()),
+        ));
+        foreach ($_REQUEST['document_types'] as $iDocumentTypeId) {
+            $oLink = new FolderDocTypeLink($this->oFolder->getId(), $iDocumentTypeId);
+            $oLink->create();
+        }
+        $this->oFolder->update();
+        $this->errorRedirectToMain('Changes made', 'fFolderId=' .
+                $this->oFolder->getId());
+        exit(0);
+    }
+}
+
+$d =& new KTFolderDocumentTypeDispatcher;
+$d->dispatch();
+
+?>
