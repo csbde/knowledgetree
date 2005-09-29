f462d938e5b1e9886b66be224543342ce6f4a8d0
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-09-29 11:36:42 +0000

    Use fieldsets instead of working on fields directly.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3684 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/documentmanagement/getTypeMetadataFields.php
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/getTypeMetadataFields.php
@@ -4,34 +4,23 @@ require_once('../../../../config/dmsDefaults.php');
 
 require_once(KT_LIB_DIR . '/dispatcher.inc.php');
 require_once(KT_LIB_DIR . '/templating/templating.inc.php');
-require_once(KT_LIB_DIR . '/documentmanagement/DocumentField.inc');
+require_once(KT_LIB_DIR . '/metadata/fieldset.inc.php');
 
 require_once(KT_LIB_DIR . '/visualpatterns/PatternMetaData.inc');
 
 class GetTypeMetadataFieldsDispatcher extends KTDispatcher {
     function do_main() {
-        return $this->getTypeMetadataFields($_REQUEST['fDocumentTypeID']);
+        return $this->getTypeMetadataFieldsets ($_REQUEST['fDocumentTypeID']);
     }
 
-    function getTypeMetadataFields ($iDocumentTypeID) {
-        /*ok*/ $sQuery = array("SELECT DF.id AS id " .
-          "FROM document_fields AS DF LEFT JOIN document_type_fields_link AS DTFL ON DTFL.field_id = DF.id " .
-          "WHERE DF.is_generic = ? " .
-          "AND DTFL.document_type_id = ?", array(false, $iDocumentTypeID));
-
-        $aIDs = DBUtil::getResultArray($sQuery);
-
-        $aFields = array();
-        foreach ($aIDs as $iID) {
-            $aFields[] =& call_user_func(array('DocumentField', 'get'), $iID);
-        }
+    function getTypeMetadataFieldsets ($iDocumentTypeID) {
         $aTemplateData = array(
             'caption' => _('Type specific meta data'),
             'empty_message' => _("No Type Specific Meta Data"),
-            'fields' => $aFields,
+            'fieldsets' => KTFieldSet::getForDocumentType($iDocumentTypeID),
         );
         $oTemplating = KTTemplating::getSingleton();
-        $oTemplate = $oTemplating->loadTemplate("ktcore/metadata_fields/editable_metadata_fields");
+        $oTemplate = $oTemplating->loadTemplate("ktcore/metadata_fields/editable_metadata_fieldsets");
         return $oTemplate->render($aTemplateData);
     }
 }
