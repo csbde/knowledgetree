35fb033a74f8d21f65f24881fa611d893cbaa99f
bshuttle <bshuttle@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-09-30 15:19:29 +0000

    first layer of the Boolean Search AJAX UI.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3697 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/search/ajaxBooleanSearch.php
@@ -0,0 +1,49 @@
+<?php
+require_once("../../../../config/dmsDefaults.php");
+require_once(KT_DIR . "/presentation/Html.inc");
+require_once(KT_LIB_DIR . "/templating/templating.inc.php");
+
+require_once(KT_LIB_DIR . "/database/dbutil.inc");
+require_once(KT_LIB_DIR . "/util/ktutil.inc");
+require_once(KT_LIB_DIR . "/dispatcher.inc.php");
+$sectionName = "General";
+require_once(KT_DIR . "/presentation/webpageTemplate.inc");
+
+require_once(KT_LIB_DIR . "/browse/Criteria.inc");
+
+/*
+ * example code - tests the frontend behaviour.  remember to check ajaxConditional.php 
+ * 
+ */
+
+class AjaxBooleanSearchDispatcher extends KTDispatcher {
+
+    function handle_output($data) {
+        return $data;
+    }
+    
+    function do_getNewCriteria() {
+        $criteriaType = KTUtil::arrayGet($_REQUEST, 'type');
+        if (empty($criteriaType)) {
+            return 'AJAX Error:  no criteria type specified.';
+        } 
+        $critObj = Criteria::getCriterionByNumber($criteriaType);
+        if (PEAR::isError($critObj)) {
+           return 'AJAX Error:  failed to initialise critiria of type "'.$type.'".';
+        }
+        // NBM:  there appears to be no reason to take $aRequest into searchWidget...
+        $noRequest = array();
+        return $critObj->searchWidget($noRequest);
+    }
+
+    function do_main() {
+        return "Ajax Error.  ajaxBooleanSearch::do_main should not be reachable."; 
+    }
+    
+    
+}
+
+$oDispatcher = new AjaxBooleanSearchDispatcher();
+$oDispatcher->dispatch();
+
+?>
