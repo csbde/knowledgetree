4581dfa7ada421d9b89b32e991259ebfd79a4531
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-10-05 12:22:25 +0000

    Add startWorkflowOnDocument, which assigns a workflow to a document, starting it off in the start state of the workflow.
    get/setControlledActionsForWorkflow assigns and gets the actions that
    the workflow modifies, either allowing or disallowing in each state.
    
    get/setEnabledActionsForState assigns and gets the actions that are
    allowed in a particular state.
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3731 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/workflow/workflowutil.inc.php
+++ lib/workflow/workflowutil.inc.php
@@ -44,5 +44,95 @@ class KTWorkflowUtil {
         }
         return $aRet;
     }
+
+    function startWorkflowOnDocument ($oWorkflow, $oDocument) {
+        $iDocumentId = KTUtil::getId($oDocument);
+        $iWorkflowId = KTUtil::getId($oWorkflow);
+        $oWorkflow =& KTWorkflow::get($iWorkflowId);
+        $iStartStateId = $oWorkflow->getStartStateId();
+        if (empty($iStartStateId)) {
+            return PEAR::raiseError('Cannot assign workflow with no starting state set');
+        }
+        $aOptions = array('noid' => true);
+        $aValues = array(
+            'document_id' => $iDocumentId,
+            'workflow_id' => $iWorkflowId,
+            'state_id' => $iStartStateId,
+        );
+        $sTable = KTUtil::getTableName('workflow_documents');
+        return DBUtil::autoInsert($sTable, $aValues, $aOptions);
+    }
+
+    function getControlledActionsForWorkflow($oWorkflow) {
+        $iWorkflowId = KTUtil::getId($oWorkflow);
+        $sTable = KTUtil::getTableName('workflow_actions');
+
+        $aQuery = array(
+            "SELECT action_name FROM $sTable WHERE workflow_id = ?",
+            array($iWorkflowId),
+        );
+        return DBUtil::getResultArrayKey($aQuery, 'action_name');
+    }
+
+    function setControlledActionsForWorkflow($oWorkflow, $aActions) {
+        $iWorkflowId = KTUtil::getId($oWorkflow);
+        $sTable = KTUtil::getTableName('workflow_actions');
+
+        $aQuery = array(
+            "DELETE FROM $sTable WHERE workflow_id = ?",
+            array($iWorkflowId),
+        );
+        $res = DBUtil::runQuery($aQuery);
+        if (PEAR::isError($res)) {
+            return $res;
+        }
+        $aOptions = array('noid' => true);
+        foreach ($aActions as $sAction) {
+            $res = DBUtil::autoInsert($sTable, array(
+                'workflow_id' => $iWorkflowId,
+                'action_name' => $sAction,
+            ), $aOptions);
+            if (PEAR::isError($res)) {
+                return $res;
+            }
+        }
+        return;
+    }
+
+    function setEnabledActionsForState($oState, $aActions) {
+        $iStateId = KTUtil::getId($oState);
+        $sTable = KTUtil::getTableName('workflow_state_actions');
+
+        $aQuery = array(
+            "DELETE FROM $sTable WHERE state_id = ?",
+            array($iStateId),
+        );
+        $res = DBUtil::runQuery($aQuery);
+        if (PEAR::isError($res)) {
+            return $res;
+        }
+        $aOptions = array('noid' => true);
+        foreach ($aActions as $sAction) {
+            $res = DBUtil::autoInsert($sTable, array(
+                'state_id' => $iStateId,
+                'action_name' => $sAction,
+            ), $aOptions);
+            if (PEAR::isError($res)) {
+                return $res;
+            }
+        }
+        return;
+    }
+
+    function getEnabledActionsForState($oState) {
+        $iStateId = KTUtil::getId($oState);
+        $sTable = KTUtil::getTableName('workflow_state_actions');
+
+        $aQuery = array(
+            "SELECT action_name FROM $sTable WHERE state_id = ?",
+            array($iStateId),
+        );
+        return DBUtil::getResultArrayKey($aQuery, 'action_name');
+    }
 }
 
