082ef09f2497c683be44997dd57066f3266fb32d
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-10-06 19:20:46 +0000

    Implement the near-backend parts of simple conditional metadata management.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3752 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/conditional/ajaxSimpleConditionals.php
+++ presentation/lookAndFeel/knowledgeTree/administration/docfieldmanagement/conditional/ajaxSimpleConditionals.php
@@ -1,5 +1,5 @@
 <?php
-require_once("../../../../../config/dmsDefaults.php");
+require_once("../../../../../../config/dmsDefaults.php");
 require_once(KT_DIR . "/presentation/Html.inc");
 require_once(KT_LIB_DIR . "/templating/templating.inc.php");
 require_once(KT_LIB_DIR . "/documentmanagement/DocumentField.inc");
@@ -8,9 +8,12 @@ require_once(KT_LIB_DIR . "/util/ktutil.inc");
 require_once(KT_LIB_DIR . "/dispatcher.inc.php");
 $sectionName = "Administration";
 require_once(KT_DIR . "/presentation/webpageTemplate.inc");
-require_once(KT_LIB_DIR . "/metadata/fieldset.inc.php");
 
-class AjaxConditionalAdminDispatcher extends KTDispatcher {
+require_once(KT_LIB_DIR . "/metadata/fieldset.inc.php");
+require_once(KT_LIB_DIR . '/metadata/fieldbehaviour.inc.php');
+
+class AjaxConditionalAdminDispatcher extends KTStandardDispatcher {
+    var $bAutomaticTransaction = true;
     function do_main() {
         return "Ajax Error.";
     }
@@ -23,6 +26,8 @@ class AjaxConditionalAdminDispatcher extends KTDispatcher {
 
     function do_storeRelationship() {
         // handle the store, and DON'T give a 500 ;)  does not act on the information.
+        global $default;
+        $default->log->error(http_build_query($_REQUEST));
         $parent_field = KTUtil::arrayGet($_REQUEST, 'parent_field');
         $parent_lookup = KTUtil::arrayGet($_REQUEST, 'parent_lookup');
         $child_lookups = KTUtil::arrayGet($_REQUEST, 'child_lookups');
@@ -33,31 +38,88 @@ class AjaxConditionalAdminDispatcher extends KTDispatcher {
         //     field_id:[lookup_id, lookup_id],
         //     field_id:[lookup_id, lookup_id],
         //  }
+
+        $oParentInstance = KTMetadataUtil::getOrCreateValueInstanceForLookup($parent_lookup);
+        $iBehaviourId = $oParentInstance->getBehaviourId();
+        $oParentMetadata =& MetaData::get($oParentInstance->getFieldValueId());
+        if (is_null($iBehaviourId)) {
+            $oBehaviour =& KTFieldBehaviour::createFromArray(array(
+                'name' => 'autoinstance' . $oParentInstance->getId(),
+                'humanname' => 'Auto instance' . $oParentMetadata->getName(),
+                'fieldid' => $oParentInstance->getFieldId(),
+            ));
+        } else {
+            $oBehaviour =& KTFieldBehaviour::get($iBehaviourId);
+        }
+        if (PEAR::isError($oBehaviour)) {
+            var_dump($oBehaviour);
+            return $oBehaviour;
+        }
+        $iBehaviourId = $oBehaviour->getId();
+        $oParentInstance->setBehaviourId($iBehaviourId);
+        $oParentInstance->update();
     
-    
-        print "not implemented.";
-        exit(1);
+        $sTable = KTUtil::getTableName('field_behaviour_options');
+        $aOptions = array('noid' => true);
+
+        foreach ($child_lookups as $iFieldId => $aLookups) {
+            foreach ($aLookups as $iLookupId) {
+                $oValueInstance =& KTMetadataUtil::getOrCreateValueInstanceForLookup($iLookupId);
+                if (PEAR::isError($oValueInstance)) {
+                    var_dump($oValueInstance);
+                    return $oValueInstance;
+                }
+                $res = DBUtil::autoInsert($sTable, array(
+                    'behaviour_id' => $iBehaviourId,
+                    'field_id' => $iFieldId,
+                    'instance_id' => $oValueInstance->getId(),
+                ), $aOptions);
+                if (PEAR::isError($res)) {
+                    var_dump($res);
+                    return $res;
+                }
+            }
+        }
     }
 
     // do you want the fieldset_id here?
     function do_updateActiveFields() {
+        global $default;
+        $default->log->error(http_build_query($_REQUEST));
         $active_field = KTUtil::arrayGet($_REQUEST, 'active_field'); // field which is "active".
         $fieldset_id = KTUtil::arrayGet($_REQUEST, 'fieldset_id');
         
         // REMEMBER TO SET CONTENT-TYPE application/xml
         $oTemplating =& KTTemplating::getSingleton();
         $oTemplate =& $oTemplating->loadTemplate('ktcore/metadata/conditional/ajax_simple_update_active_fields');
-        
+        $oTemplate->setData(array(
+            'aFieldIds' => KTMetadataUtil::getChildFieldIds($active_field),
+        ));
+        header("Content-Type: application/xml");
+        print $oTemplate->render();
+        exit(0);
     }
     
     // do you want the fieldset_id here?
     function do_updateActiveLookups() {
+        global $default;
+        $default->log->error(http_build_query($_REQUEST));
         $active_field = KTUtil::arrayGet($_REQUEST, 'active_field'); // field which is "active".
         $selected_lookup = KTUtil::arrayGet($_REQUEST, 'selected_lookup'); // selected value in said field.
         
         // REMEMBER TO SET CONTENT-TYPE application/xml
         $oTemplating =& KTTemplating::getSingleton();        
         $oTemplate =& $oTemplating->loadTemplate('ktcore/metadata/conditional/ajax_simple_update_active_lookups');
+
+        $aFieldLookups = KTMetadataUtil::getNextValuesForLookup($selected_lookup);
+
+        $oTemplate->setData(array(
+            'aFieldLookups' => $aFieldLookups,
+        ));
+
+        header("Content-Type: application/xml");
+        print $oTemplate->render();
+        exit(0);
     }
 
 }
