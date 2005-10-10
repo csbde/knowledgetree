ff8e75a67b1836b04e5d720dd929582d17d26e1c
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-10-10 09:06:38 +0000

    Add a "workflow" action to documents.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3786 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/documentmanagement/documentWorkflow.php
@@ -0,0 +1,135 @@
+<?php
+
+require_once("../../../../config/dmsDefaults.php");
+
+require_once(KT_DIR . "/presentation/Html.inc");
+
+require_once(KT_LIB_DIR . "/templating/templating.inc.php");
+require_once(KT_LIB_DIR . "/foldermanagement/Folder.inc");
+
+$sectionName = "Manage Documents";
+require_once(KT_DIR . "/presentation/webpageTemplate.inc");
+
+require_once(KT_LIB_DIR . "/dispatcher.inc.php");
+require_once(KT_LIB_DIR . "/validation/dispatchervalidation.inc.php");
+
+function displayFolderPathLink($aPathArray, $aPathNameArray, $sLinkPage = "") {
+    global $default;
+    if (strlen($sLinkPage) == 0) {
+        $sLinkPage = $_SERVER["PHP_SELF"];
+    }
+    $default->log->debug("displayFolderPathLink: slinkPage=$sLinkPage");
+    // display a separate link to each folder in the path
+    for ($i=0; $i<count($aPathArray); $i++) {
+        $iFolderID = $aPathArray[$i];
+        // retrieve the folder name for this folder
+        $sFolderName = $aPathNameArray[$i];
+        // generate a link back to this page setting fFolderID
+        $sLink = generateLink($sLinkPage,
+                              "fBrowseType=folder&fFolderID=$iFolderID",
+                              $sFolderName);
+        $sPathLinks = (strlen($sPathLinks) > 0) ? $sPathLinks . " > " . $sLink : $sLink;
+    }
+    return $sPathLinks;
+}
+
+
+class DocumentWorkflowDispatcher extends KTStandardDispatcher {
+    function do_main() {
+        $oTemplating = new KTTemplating;
+        $oTemplate = $oTemplating->loadTemplate("ktcore/document/document_workflow");
+        $oDocument =& $this->oValidator->validateDocument($_REQUEST['fDocumentID']);
+        $oPO = KTPermissionObject::get($oDocument->getPermissionObjectID());
+        $aPermissions = KTPermission::getList();
+        $aMapPermissionGroup = array();
+        foreach ($aPermissions as $oPermission) {
+            $oPA = KTPermissionAssignment::getByPermissionAndObject($oPermission, $oPO);
+            if (PEAR::isError($oPA)) {
+                continue;
+            }
+            $oDescriptor = KTPermissionDescriptor::get($oPA->getPermissionDescriptorID());
+            $iPermissionID = $oPermission->getID();
+            $aIDs = $oDescriptor->getGroups();
+            $aMapPermissionGroup[$iPermissionID] = array();
+            foreach ($aIDs as $iID) {
+                $aMapPermissionGroup[$iPermissionID][$iID] = true;
+            }
+        }
+        $aMapPermissionUser = array();
+        $aUsers = User::getList();
+        foreach ($aPermissions as $oPermission) {
+            $iPermissionID = $oPermission->getID();
+            foreach ($aUsers as $oUser) {
+                if (KTPermissionUtil::userHasPermissionOnItem($oUser, $oPermission, $oDocument)) {
+                    $aMapPermissionUser[$iPermissionID][$oUser->getID()] = true;
+                }
+            }
+        }
+
+        $oInherited = KTPermissionUtil::findRootObjectForPermissionObject($oPO);
+        if ($oInherited === $oDocument) {
+            $bEdit = true;
+        } else {
+            $iInheritedFolderID = $oInherited->getID();
+            $sInherited = displayFolderPathLink(Folder::getFolderPathAsArray($iInheritedFolderID),
+                        Folder::getFolderPathNamesAsArray($iInheritedFolderID),
+                        "$default->rootUrl/control.php?action=editFolderPermissions");
+            $bEdit = false;
+        }
+
+        $aTemplateData = array(
+            "permissions" => $aPermissions,
+            "groups" => Group::getList(),
+            "iDocumentID" => $_REQUEST['fDocumentID'],
+            "aMapPermissionGroup" => $aMapPermissionGroup,
+            "users" => $aUsers,
+            "aMapPermissionUser" => $aMapPermissionUser,
+            "edit" => $bEdit,
+            "inherited" => $sInherited,
+        );
+        return $oTemplate->render($aTemplateData);
+    }
+
+    function handleOutput($data) {
+        global $main;
+        $main->bFormDisabled = true;
+        $main->setCentralPayload($data);
+        $main->render();
+    }
+
+    function do_update() {
+        $oDocument = Document::get($_REQUEST['fDocumentID']);
+        $oPO = KTPermissionObject::get($oDocument->getPermissionObjectID());
+        $aFoo = $_REQUEST['foo'];
+        $aPermissions = KTPermission::getList();
+        foreach ($aPermissions as $oPermission) {
+            $iPermID = $oPermission->getID();
+            $aAllowed = KTUtil::arrayGet($aFoo, $iPermID, array());
+            KTPermissionUtil::setPermissionForID($oPermission, $oPO, $aAllowed);
+        }
+        KTPermissionUtil::updatePermissionLookupForPO($oPO);
+        return $this->errorRedirectToMain('Permissions updated',
+                array('fDocumentID' => $oDocument->getID()));
+    }
+
+    function do_copyPermissions() {
+        $oDocument = Document::get($_REQUEST['fDocumentID']);
+        KTPermissionUtil::copyPermissionObject($oDocument);
+        return $this->errorRedirectToMain('Permissions updated',
+                array('fDocumentID' => $oDocument->getID()));
+    }
+
+    function do_inheritPermissions() {
+        $oDocument = Document::get($_REQUEST['fDocumentID']);
+        KTPermissionUtil::inheritPermissionObject($oDocument);
+        return $this->errorRedirectToMain('Permissions updated',
+                array('fDocumentID' => $oDocument->getID()));
+    }
+
+    
+}
+
+$oDispatcher = new DocumentPermissionsDispatcher;
+$oDispatcher->dispatch();
+
+?>
