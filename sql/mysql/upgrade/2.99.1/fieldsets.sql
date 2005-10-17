6f53921ad6332112bfa891fb7e0352bb1655a118
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-10-17 09:21:38 +0000

    Final fieldsets changes for this release.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3834 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/2.99.1/fieldsets.sql
+++ sql/mysql/upgrade/2.99.1/fieldsets.sql
@@ -81,4 +81,10 @@ CREATE TABLE `zseq_fieldsets` (
   PRIMARY KEY  (`id`)
 ) TYPE=MyISAM;
 
+ALTER TABLE `document_fields` ADD INDEX `parent_fieldset` (`parent_fieldset`);
+ALTER TABLE `document_fields` ADD CONSTRAINT `document_fields_ibfk_1` FOREIGN KEY (`parent_fieldset`) REFERENCES `fieldsets` (`id`) ON DELETE CASCADE;
+ALTER TABLE `fieldsets` ADD COLUMN `is_complex` tinyint(1) NOT NULL default '0';
+ALTER TABLE `fieldsets` ADD COLUMN `is_complete` tinyint(1) NOT NULL default '1';
+ALTER TABLE `fieldsets` ADD INDEX `is_complete` (`is_complete`);
+
 SET FOREIGN_KEY_CHECKS=1;
