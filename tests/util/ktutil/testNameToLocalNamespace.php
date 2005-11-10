3fc01900fd3334c120cc6fad0f1420b9ceb68f21
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-11-10 15:52:42 +0000

    Test KTUtil::nameToLocalNamespace
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3953 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ tests/util/ktutil/testNameToLocalNamespace.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once("../../../config/dmsDefaults.php");
+require_once(KT_LIB_DIR . "/util/ktutil.inc");
+
+print KTUtil::nameToLocalNamespace("Saved Searches", "Neil's saved search");
+
+?>
