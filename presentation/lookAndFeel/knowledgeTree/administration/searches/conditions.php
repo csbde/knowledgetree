3763e85b9db4abd56d2006dadc64ed26880935c6
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-11-10 16:08:03 +0000

    Add system-wide saved searches and system condition management interfaces.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3956 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ presentation/lookAndFeel/knowledgeTree/administration/searches/conditions.php
@@ -0,0 +1,87 @@
+<?php
+
+// boilerplate includes
+require_once("../../../../../config/dmsDefaults.php");
+require_once(KT_DIR . "/presentation/Html.inc");
+require_once(KT_LIB_DIR . "/templating/templating.inc.php");
+require_once(KT_LIB_DIR . "/dispatcher.inc.php");
+require_once(KT_LIB_DIR . "/browse/Criteria.inc");
+require_once(KT_LIB_DIR . "/search/savedsearch.inc.php");
+
+// specific includes
+
+$sectionName = "General";
+require_once(KT_DIR . "/presentation/webpageTemplate.inc");
+
+class KTConditionDispatcher extends KTStandardDispatcher {
+    var $bAutomaticTransaction = true;
+
+    function do_main() {
+        $oTemplate =& $this->oValidator->validateTemplate('ktcore/search/administration/conditions');
+        $oTemplate->setData(array(
+            'conditions' => KTSavedSearch::getConditions(),
+        ));
+        return $oTemplate->render();
+    }
+
+    function do_new() {
+        $oTemplating = new KTTemplating;
+        $oTemplate = $oTemplating->loadTemplate("ktcore/boolean_search");
+        
+        $aCriteria = Criteria::getAllCriteria();
+        
+        $aTemplateData = array(
+            "title" => "Create a new condition",
+            "sNameTitle" => "Name of condition",
+            "aCriteria" => $aCriteria,
+            "searchButton" => "Save",
+        );
+        return $oTemplate->render($aTemplateData);
+    }
+
+    function do_view() {
+
+    }
+
+    function handleOutput($data) {
+        global $main;
+        $main->bFormDisabled = true;
+        $main->setCentralPayload($data);
+        $main->render();
+    }
+    
+    // XXX: Rename to do_save
+    function do_performSearch() {
+        $datavars = KTUtil::arrayGet($_REQUEST, 'boolean_search');
+        if (!is_array($datavars)) {
+            $datavars = unserialize($datavars);
+        }
+        
+        if (empty($datavars)) {
+            $this->errorRedirectToMain('You need to have at least 1 condition.');
+        }
+
+        $sName = $_REQUEST['name'];
+        $sNamespace = KTUtil::nameToLocalNamespace('Saved searches', $sName);
+
+        $oSearch = KTSavedSearch::createFromArray(array(
+            'name' => $sName,
+            'namespace' => $sNamespace,
+            'iscondition' => true,
+            'iscomplete' => true,
+            'userid' => null,
+            'search' => $datavars,
+        ));
+
+        $this->oValidator->notError($oSearch, array(
+            'redirect_to' => 'main',
+            'message' => 'Search not saved',
+        ));
+        $this->successRedirectToMain('Search saved');
+    }
+}
+
+$oDispatcher = new KTConditionDispatcher();
+$oDispatcher->dispatch();
+
+?>
