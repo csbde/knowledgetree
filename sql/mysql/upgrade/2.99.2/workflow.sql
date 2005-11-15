def076c86e84565d64dce99e8351a13278cc0a91
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-11-15 14:37:45 +0000

    Add guard_condition_id to workflow table
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@3992 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/2.99.2/workflow.sql
+++ sql/mysql/upgrade/2.99.2/workflow.sql
@@ -2,3 +2,6 @@ ALTER TABLE `workflow_states` ADD COLUMN `inform_descriptor_id` int(11) default 
 ALTER TABLE `workflow_transitions` CHANGE COLUMN `guard_permission_id` `guard_permission_id` int(11) default '0'; # was int(11) NOT NULL default '0'
 ALTER TABLE `workflow_transitions` ADD COLUMN `guard_group_id` int(11) default '0';
 ALTER TABLE `workflow_transitions` ADD COLUMN `guard_role_id` int(11) default '0';
+ALTER TABLE `workflow_transitions` ADD COLUMN `guard_condition_id` int(11) default NULL;
+ALTER TABLE `workflow_transitions` ADD INDEX `guard_condition` (`guard_condition_id`);
+
