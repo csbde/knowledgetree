fd381c4c094c8cf701f7833d5e00dfbd05034f29
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-11-16 14:50:38 +0000

    Add the is_stuck field, which controls whether automated synchronisation of this field will cause this lookup value to change enabled/disabled state.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4009 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/2.99.2/disabled_metadata.sql
+++ sql/mysql/upgrade/2.99.2/disabled_metadata.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `metadata_lookup` ADD COLUMN `disabled` tinyint(3) unsigned NOT NULL default '0';
 ALTER TABLE `metadata_lookup` ADD INDEX `disabled` (`disabled`);
-
+ALTER TABLE `metadata_lookup` ADD COLUMN `is_stuck` tinyint(1) NOT NULL default '0';
+ALTER TABLE `metadata_lookup` ADD INDEX `is_stuck` (`is_stuck`);
