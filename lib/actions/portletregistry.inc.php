04d6f681b0d57a77167ac61c25c369cd6910a409
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-11-18 18:24:07 +0000

    Add a registry that portlets can register themselves into.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4042 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ lib/actions/portletregistry.inc.php
@@ -0,0 +1,65 @@
+<?php
+
+class KTPortletRegistry {
+    var $actions = array();
+    // {{{ getSingleton
+    function &getSingleton () {
+        if (!KTUtil::arrayGet($GLOBALS, 'oKTPortletRegistry')) {
+            $GLOBALS['oKTPortletRegistry'] = new KTPortletRegistry;
+        }
+        return $GLOBALS['oKTPortletRegistry'];
+    }
+    // }}}
+
+    function registerPortlet($action, $name, $nsname, $path = "") {
+        if (!is_array($action)) {
+            $action = array($action);
+        }
+        foreach ($action as $slot) {
+            $this->portlets[$slot] = KTUtil::arrayGet($this->actions, $slot, array());
+            $this->actions[$slot][$nsname] = array($name, $path, $nsname);
+        }
+        $this->nsnames[$nsname] = array($name, $path, $nsname);
+    }
+
+    function getPortletsForPage($aBreadcrumbs) {
+        $aPortlets = array();
+        foreach ($aBreadcrumbs as $aBreadcrumb) {
+            $action = KTUtil::arrayGet($aBreadcrumb, 'action');
+            if (empty($action)) {
+                continue;
+            }
+            $aThisPortlets = $this->getPortlet($action);
+            if (empty($aThisPortlets)) {
+                continue;
+            }
+            foreach ($aThisPortlets as $aPortlet) {
+                $aPortlets[] = $aPortlet;
+            }
+        }
+
+        $aReturn = array();
+        $aDone = array();
+
+        foreach ($aPortlets as $aPortlet) {
+            if (in_array($aPortlet, $aDone)) {
+                continue;
+            }
+            $aDone[] = $aPortlet;
+
+            $sPortletClass = $aPortlet[0];
+            $aReturn[] = new $sPortletClass;
+        }
+        return $aReturn;
+    }
+
+    function getPortlet($slot) {
+        return $this->actions[$slot];
+    }
+
+    function getPortletByNsname($nsname) {
+        return $this->nsnames[$nsname];
+    }
+}
+
+?>
