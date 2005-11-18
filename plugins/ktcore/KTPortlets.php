85f1f4aab852b38beb6b5d8b8d69abe0955f6388
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-11-18 18:31:31 +0000

    Move the search portlet into the plugins architecture.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4048 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ plugins/ktcore/KTPortlets.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once(KT_LIB_DIR . '/actions/portletregistry.inc.php');
+require_once(KT_LIB_DIR . '/widgets/portlet.inc.php');
+
+$oPRegistry =& KTPortletRegistry::getSingleton();
+
+class KTSearchPortlet extends KTPortlet {
+
+    function KTSearchPortlet() {
+        parent::KTPortlet("Search");
+    }
+    function render() {
+        $oTemplating = new KTTemplating;
+        $oTemplate = $oTemplating->loadTemplate("kt3/portlets/search_portlet");
+        $aTemplateData = array(
+            "context" => $this,
+        );
+
+        return $oTemplate->render($aTemplateData);
+    }
+}
+
+$oPRegistry->registerPortlet('browse', 'KTSearchPortlet', 'ktcore.portlets.search', '/plugins/ktcore/KTPortlets.php');
+
