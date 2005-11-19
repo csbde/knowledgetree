86bc4048dbf96803ec24d6e6794000d93ba6f7c2
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-11-19 21:19:21 +0000

    Implement checkin and checkout using the actions system
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4062 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/KTDocumentActions.php
+++ plugins/ktcore/KTDocumentActions.php
@@ -2,12 +2,16 @@
 
 require_once(KT_LIB_DIR . '/actions/documentaction.inc.php');
 require_once(KT_LIB_DIR . '/subscriptions/Subscription.inc');
+require_once(KT_LIB_DIR . '/widgets/fieldWidgets.php');
+require_once(KT_LIB_DIR . '/browse/browseutil.inc.php');
+require_once(KT_LIB_DIR . '/documentmanagement/documentutil.inc.php');
 
 $oKTActionRegistry =& KTActionRegistry::getSingleton();
 
+// {{{ KTDocumentViewAction
 class KTDocumentViewAction extends KTDocumentAction {
     var $sBuiltInAction = 'downloadDocument';
-    var $sDisplayName = 'View';
+    var $sDisplayName = 'Download';
     var $sName = 'ktcore.actions.document.view';
 
     function customiseInfo($aInfo) {
@@ -24,30 +28,177 @@ class KTDocumentViewAction extends KTDocumentAction {
     }
 }
 $oKTActionRegistry->registerAction('documentaction', 'KTDocumentViewAction', 'ktcore.actions.document.view');
+// }}}
 
-class KTDocumentCheckInOutAction extends KTBuiltInDocumentAction {
-    var $sBuiltInAction = 'emailDocument';
-    var $sDisplayName = 'Checkin';
-    var $sName = 'ktcore.actions.document.checkinout';
+// {{{ KTDocumentCheckOutAction
+class KTDocumentCheckOutAction extends KTDocumentAction {
+    var $sBuiltInAction = 'checkOutDocument';
+    var $sDisplayName = 'Checkout';
+    var $sName = 'ktcore.actions.document.checkout';
 
     var $_sDisablePermission = "ktcore.permissions.write";
 
     function getInfo() {
         if ($this->oDocument->getIsCheckedOut()) {
-            $this->sBuiltInAction = 'checkInDocument';
-            $this->sDisplayName = 'Checkin';
-            if ($this->oDocument->getCheckedOutUserID() != $_SESSION["userID"]) {
-                $this->_bDisabled = true;
-                $this->_sDisabledText = sprintf(_("The document can only be checked back in by %s"), $oUser->getName());
-            }
-        } else {
-            $this->sBuiltInAction = 'checkOutDocument';
-            $this->sDisplayName = 'Checkout';
+            return null;
         }
         return parent::getInfo();
     }
+
+    function check() {
+        $res = parent::check();
+        if ($res !== true) {
+            return $res;
+        }
+        if ($this->oDocument->getIsCheckedOut()) {
+            $_SESSION['KTErrorMessage'][] = "This document is already checked out";
+            controllerRedirect('viewDocument', 'fDocumentId=' .  $this->oDocument->getId());
+            exit(0);
+        }
+        return true;
+    }
+
+    function do_main() {
+        $this->oPage->setBreadcrumbDetails("checkout");
+        $oTemplate =& $this->oValidator->validateTemplate('ktcore/action/checkout');
+        $checkout_fields = array();
+        $checkout_fields[] = new KTStringWidget('Reason', 'The reason for the checkout of this document for historical purposes, and to inform those who wish to check out this document.', 'reason', "", $this->oPage, true);
+
+        $oTemplate->setData(array(
+            'context' => &$this,
+            'checkout_fields' => $checkout_fields,
+        ));
+        return $oTemplate->render();
+    }
+
+    function do_checkout() {
+        $sReason = KTUtil::arrayGet($_REQUEST, 'reason');
+        $this->oValidator->notEmpty($sReason);
+        $oTemplate =& $this->oValidator->validateTemplate('ktcore/action/checkout_final');
+        $oTemplate->setData(array(
+            'context' => &$this,
+            'reason' => $sReason,
+        ));
+        return $oTemplate->render();
+    }
+
+    function do_checkout_final() {
+        $sReason = KTUtil::arrayGet($_REQUEST, 'reason');
+        $this->oValidator->notEmpty($sReason);
+
+        // flip the checkout status
+        $this->oDocument->setIsCheckedOut(true);
+        // set the user checking the document out
+        $this->oDocument->setCheckedOutUserID($_SESSION["userID"]);
+        // update it
+        if (!$this->oDocument->update()) {
+            $_SESSION['KTErrorMessage'][] = "There was a problem checking out the document.";
+            controllerRedirect('viewDocument', 'fDocumentId=' .  $this->oDocument->getId());
+        }
+
+        $oKTTriggerRegistry = KTTriggerRegistry::getSingleton();
+        $aTriggers = $oKTTriggerRegistry->getTriggers('checkout', 'postValidate');
+        foreach ($aTriggers as $aTrigger) {
+            $sTrigger = $aTrigger[0];
+            $oTrigger = new $sTrigger;
+            $aInfo = array(
+                "document" => $this->oDocument,
+            );
+            $oTrigger->setInfo($aInfo);
+            $ret = $oTrigger->postValidate();
+            if (PEAR::isError($ret)) {
+                $this->oDocument->delete();
+                return $ret;
+            }
+        }
+
+        $oDocumentTransaction = & new DocumentTransaction($this->oDocument->getID(), $sReason, CHECKOUT);
+        $oDocumentTransaction->create();
+        
+        $oStorage =& KTStorageManagerUtil::getSingleton();
+        $oStorage->download($this->oDocument);
+        exit(0);
+    }
 }
-$oKTActionRegistry->registerAction('documentaction', 'KTDocumentCheckInOutAction', 'ktcore.actions.document.checkinout');
+$oKTActionRegistry->registerAction('documentaction', 'KTDocumentCheckOutAction', 'ktcore.actions.document.checkout');
+// }}}
+
+class KTDocumentCheckInAction extends KTDocumentAction {
+    var $sBuiltInAction = 'checkInDocument';
+    var $sDisplayName = 'Checkin';
+    var $sName = 'ktcore.actions.document.checkin';
+
+    var $_sShowPermission = "ktcore.permissions.write";
+
+    function getInfo() {
+        if (!$this->oDocument->getIsCheckedOut()) {
+            return null;
+        }
+
+        if ($this->oDocument->getCheckedOutUserID() != $this->oUser->getId()) {
+            return null;
+        }
+        return parent::getInfo();
+    }
+
+    function check() {
+        $res = parent::check();
+        if ($res !== true) {
+            return $res;
+        }
+        if (!$this->oDocument->getIsCheckedOut()) {
+            $_SESSION['KTErrorMessage'][] = "This document is not checked out";
+            controllerRedirect('viewDocument', 'fDocumentId=' .  $this->oDocument->getId());
+            exit(0);
+        }
+        if ($this->oDocument->getCheckedOutUserID() != $this->oUser->getId()) {
+            $_SESSION['KTErrorMessage'][] = "This document is checked out, but not by you";
+            controllerRedirect('viewDocument', 'fDocumentId=' .  $this->oDocument->getId());
+            exit(0);
+        }
+        return true;
+    }
+
+    function do_main() {
+        $this->oPage->setBreadcrumbDetails("checkin");
+        $oTemplate =& $this->oValidator->validateTemplate('ktcore/action/checkin');
+        
+        $sReason = KTUtil::arrayGet($_REQUEST, 'reason', "");
+        $checkin_fields = array();
+        $checkin_fields[] = new KTFileUploadWidget('File', 'The updated document.', 'file', "", $this->oPage, true);
+        $checkin_fields[] = new KTStringWidget('Description', 'Describe the changes made to the document.', 'reason', $sReason, $this->oPage, true);
+
+        $oTemplate->setData(array(
+            'context' => &$this,
+            'checkin_fields' => $checkin_fields,
+        ));
+        return $oTemplate->render();
+    }
+
+    function do_checkin() {
+        $sReason = KTUtil::arrayGet($_REQUEST, 'reason');
+        $sReason = $this->oValidator->notEmpty($sReason);
+
+        // make sure the user actually selected a file first
+        if (strlen($_FILES['file']['name']) == 0) {
+            $this->errorRedirectToMain("No file was uploaded", 'fDocumentId=' . $this->oDocument->getId() . '&reason=' . $sReason);
+        }
+
+        // and that the filename matches
+        global $default;
+        $default->log->info("checkInDocumentBL.php uploaded filename=" . $_FILES['file']['name'] . "; current filename=" . $this->oDocument->getFileName());
+        if ($this->oDocument->getFileName() != $_FILES['file']['name']) {
+            $this->errorRedirectToMain("The file name of the uploaded file does not match the file name of the document in the system", 'fDocumentId=' . $this->oDocument->getId() . '&reason=' . $sReason);
+        }
+
+        $res = KTDocumentUtil::checkin($this->oDocument, $_FILES['file']['tmp_name'], $sReason, $this->oUser);
+        if (PEAR::isError($res)) {
+            $this->errorRedirectToMain("An error occurred while trying to check in the document", 'fDocumentId=' . $this->oDocument->getId() . '&reason=' . $sReason);
+        }
+        redirect("$default->rootUrl/control.php?action=viewDocument&fDocumentID=" . $this->oDocument->getID());
+    }
+}
+$oKTActionRegistry->registerAction('documentaction', 'KTDocumentCheckInAction', 'ktcore.actions.document.checkin');
 
 class KTDocumentDeleteAction extends KTBuiltInDocumentActionSingle {
     var $sBuiltInAction = 'deleteDocument';
