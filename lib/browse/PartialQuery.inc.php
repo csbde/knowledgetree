a72ca98d0a4be1348b14cc952e6f13dfe8a0156c
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-11-19 22:09:55 +0000

    BrowseQuery should only show live documents.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4068 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/browse/PartialQuery.inc.php
+++ lib/browse/PartialQuery.inc.php
@@ -65,7 +65,7 @@ class BrowseQuery extends PartialQuery{
     
     function getDocumentCount() { 
         // FIXME add permission checks here
-        $sQuery = "SELECT count(id) AS c FROM " . KTUtil::getTableName("documents") . " WHERE folder_id = ? ";
+        $sQuery = "SELECT count(id) AS c FROM " . KTUtil::getTableName("documents") . " WHERE folder_id = ? AND D.status_id = 1 ";
         $aParams = array($this->folder_id);
         
         return DBUtil::getOneResultKey(array($sQuery, $aParams), 'c'); // FIXME is this right? 
@@ -81,7 +81,9 @@ class BrowseQuery extends PartialQuery{
         
         if ($sJoinClause !== null) {
             $sQuery .= $sJoinClause;
-            foreach ($aJoinParams as $param) { $aParams[] = $param; } // FIXME use merge...
+            foreach ($aJoinParams as $param) {
+                $aParams[] = $param;
+            } // FIXME use merge...
         }        
     
         $sQuery .= " ORDER BY " . $sSortColumn . " " . $sSortOrder . " ";
@@ -102,12 +104,14 @@ class BrowseQuery extends PartialQuery{
         $aParams = array(); // main parameter array.
         $aJoinParams = array($aJoinParams);
         
-        $sQuery = "SELECT id FROM " . KTUtil::getTableName("documents") . " AS D WHERE folder_id = ? ";
+        $sQuery = "SELECT id FROM " . KTUtil::getTableName("documents") . " AS D WHERE folder_id = ? AND D.status_id = 1 ";
         $aParams = array($this->folder_id);
         
         if ($sJoinClause !== null) {
             $sQuery .= $sJoinClause;
-            foreach ($aJoinParams as $param) { $aParams[] = $param; } // FIXME use merge...
+            foreach ($aJoinParams as $param) {
+                $aParams[] = $param;
+            } // FIXME use merge...
         }        
 
         $sQuery .= " ORDER BY " . $sSortColumn . " " . $sSortOrder . " ";
