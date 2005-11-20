b0acd4d50ae7f47bbcd5bd8b10184e8c1fe5d163
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-11-20 22:38:11 +0000

    We're now using folder actions!
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4077 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/KTCorePlugin.php
+++ plugins/ktcore/KTCorePlugin.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('KTDocumentActions.php');
+require_once('KTFolderActions.php');
 require_once('KTPortlets.php');
 
 require_once(KT_LIB_DIR . '/storage/ondiskpathstoragemanager.inc.php');
