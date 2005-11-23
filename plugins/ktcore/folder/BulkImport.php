7b76c6cb8aa69e479147152deb2867a333959a54
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-11-23 22:15:14 +0000

    Implement bulk import as a folder action.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4146 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ plugins/ktcore/folder/BulkImport.php
@@ -0,0 +1,83 @@
+<?
+
+require_once(KT_LIB_DIR . "/actions/folderaction.inc.php");
+require_once(KT_LIB_DIR . "/import/fsimportstorage.inc.php");
+require_once(KT_LIB_DIR . "/import/bulkimport.inc.php");
+
+require_once(KT_LIB_DIR . "/validation/dispatchervalidation.inc.php");
+
+class KTBulkImportFolderAction extends KTFolderAction {
+    var $sDisplayName = 'Bulk import';
+    var $sName = 'ktcore.actions.folder.bulkImport';
+
+    var $_sShowPermission = "ktcore.permissions.write";
+    var $bAutomaticTransaction = true;
+
+    function do_main() {
+        $this->oPage->setBreadcrumbDetails("bulk import");
+        $oTemplate =& $this->oValidator->validateTemplate('ktcore/folder/bulkImport');
+        $add_fields = array();
+        $add_fields[] = new KTStringWidget('Path', 'The path containing the documents to be added to the document management system.', 'path', "", $this->oPage, true);
+
+        $aVocab = array();
+        foreach (DocumentType::getList() as $oDocumentType) {
+            $aVocab[$oDocumentType->getId()] = $oDocumentType->getName();
+        }
+        $fieldOptions = array("vocab" => $aVocab);
+        $add_fields[] = new KTLookupWidget('Document Type', 'FIXME', 'fDocumentTypeId', null, $this->oPage, true, "add-document-type", $fieldErrors, $fieldOptions);
+
+        $fieldsets = array();
+        $fieldsetDisplayReg =& KTFieldsetDisplayRegistry::getSingleton();
+        $activesets = KTFieldset::getGenericFieldsets();
+        foreach ($activesets as $oFieldset) {
+            $displayClass = $fieldsetDisplayReg->getHandler($oFieldset->getNamespace());
+            array_push($fieldsets, new $displayClass($oFieldset));
+        }
+
+        $oTemplate->setData(array(
+            'context' => &$this,
+            'add_fields' => $add_fields,
+            'generic_fieldsets' => $fieldsets,
+        ));
+        return $oTemplate->render();
+    }
+
+    function do_import() {
+        $aErrorOptions = array(
+            'redirect_to' => array('main', sprintf('fFolderId=%d', $this->oFolder->getId())),
+        );
+
+        $aErrorOptions['message'] = 'Invalid document type provided';
+        $oDocumentType = $this->oValidator->validateDocumentType($_REQUEST['fDocumentTypeId'], $aErrorOptions);
+
+        $aErrorOptions['message'] = 'Invalid path provided';
+        $sPath = $this->oValidator->validateString($_REQUEST['path'], $aErrorOptions);
+
+        $matches = array();
+        $aFields = array();
+        foreach ($_REQUEST as $k => $v) {
+            if (preg_match('/^metadata_(\d+)$/', $k, $matches)) {
+                $aFields[] = array(DocumentField::get($matches[1]), $v);
+            }
+        }
+
+        $aOptions = array(
+            'documenttype' => $oDocumentType,
+            'metadata' => $aFields,
+        );
+
+        $fs =& new KTFSImportStorage($sPath);
+        $bm =& new KTBulkImportManager($this->oFolder, $fs, $this->oUser, $aOptions);
+        DBUtil::startTransaction();
+        $res = $bm->import();
+        if (PEAR::isError($res)) {
+            DBUtil::rollback();
+            $_SESSION["KTErrorMessage"][] = _("Bulk import failed") . ": " . $res->getMessage();
+        } else {
+            DBUtil::commit();
+        }
+
+        controllerRedirect("browse", 'fFolderId=' . $this->oFolder->getID());
+        exit(0);
+    }
+}
