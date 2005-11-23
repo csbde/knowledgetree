d6099c4fb56e8173ab70a90a643a082496f57c6d
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-11-23 11:41:14 +0000

    The error viewer framework allows for pear errors to be displayed in the best possible manner for the error in question.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4121 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ lib/validation/errorviewer.inc.php
@@ -0,0 +1,95 @@
+<?php
+
+class KTErrorViewerRegistry {
+    var $aViewers = array();
+
+    function &getSingleton() {
+        $oRegistry =& KTUtil::arrayGet($GLOBALS, 'KTErrorViewerRegistry');
+        if ($oRegistry) {
+            return $oRegistry;
+        }
+        $GLOBALS['KTErrorViewerRegistry'] =& new KTErrorViewerRegistry();
+        return $GLOBALS['KTErrorViewerRegistry'];
+    }
+
+    function register($sViewerClassName, $sHandledClass) {
+        $this->aViewers[$sHandledClass] = $sViewerClassName;
+    }
+
+    function getViewer($oError) {
+        $sErrorClass = get_class($oError);
+
+        // Try for direct hit first
+        $sClass = $sErrorClass;
+        $sHandlerClass = KTUtil::arrayGet($this->aViewers, $sClass);
+        if ($sHandlerClass) {
+            return new $sHandlerClass($oError);
+        }
+
+        // PHP 4.0.5 added get_parent_class - this offers us the
+        // best/first match
+        if (function_exists('get_parent_class')) {
+            while ($sClass = get_parent_class($sClass)) {
+                $sHandlerClass = KTUtil::arrayGet($this->aViewers, $sClass);
+                if ($sHandlerClass) {
+                    return new $sHandlerClass($oError);
+                }
+            }
+        }
+
+        // Now try things the hard way - no best/first match (ick!)
+        // Reverse, since hopefully the best/first match will be added
+        // after the other matches.
+        foreach (array_reverse($this->aViewers) as $sHandlerClass => $sClass) {
+            if (is_a($oError, $sClass)) {
+                return new $sHandlerClass($oError);
+            }
+        }
+
+        // PEAR_Error should have caught things above, but just in case,
+        // check if getMessage is there, and use that:
+        if (method_exists($oError, 'getMessage')) {
+            return new KTStringErrorViewer($oError->getMessage());
+        }
+
+        // Check if we are a string
+        if (is_string($oError)) {
+            return new KTStringErrorViewer($oError);
+        }
+
+        // Give up.
+        return new KTStringErrorViewer("Unknown error");
+    }
+}
+
+$oEVRegistry =& KTErrorViewerRegistry::getSingleton();
+class KTErrorViewer {
+    function KTErrorViewer($oError) {
+        $this->oError = $oError;
+    }
+    function view() {
+        return $this->oError->getMessage();
+    }
+
+    function viewFull() {
+        return $this->oError->getString();
+    }
+}
+$oEVRegistry->register("KTErrorViewer", "PEAR_Error");
+
+class KTDBErrorViewer extends KTErrorViewer {
+    function view() {
+        return "Database error: " . $this->oError->errorMessage();
+    }
+}
+$oEVRegistry->register("KTDBErrorViewer", "DB_Error");
+
+class KTStringErrorViewer extends KTErrorViewer {
+    function view() {
+        return $this->oError;
+    }
+    
+    function viewFull() {
+        return $this->oError;
+    }
+}
