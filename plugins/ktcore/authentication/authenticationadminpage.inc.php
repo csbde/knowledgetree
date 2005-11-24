b3b0cfb6a7068bb05fde89d3eda5eed3b4d2949a
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-11-24 16:25:41 +0000

    Start the authentication framework - authentication providers provide authentication for users.  Users can be configured to use a particular authenticator, which is a specific instance given by the authentication provider configured for operation.  (So, basically, you can have two LDAP authenticators and the built-in authenticator, and different users can use different authenticators.)
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4158 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ plugins/ktcore/authentication/authenticationadminpage.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once(KT_LIB_DIR . '/dispatcher.inc.php');
+
+class KTAuthenticationAdminPage extends KTAdminDispatcher {
+    function do_main() {
+        return "Hello there";
+    }
+}
