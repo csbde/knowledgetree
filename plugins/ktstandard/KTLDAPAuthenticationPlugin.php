f97988885a38bf6591e40ceebe0e6c964b8dd33c
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-11-25 20:18:01 +0000

    Fix silly brainos
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4177 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktstandard/KTLDAPAuthenticationPlugin.php
+++ plugins/ktstandard/KTLDAPAuthenticationPlugin.php
@@ -1,12 +1,13 @@
 <?php
 
-class KTAuthenticationPlugin extends KTPlugin {
+class KTLDAPAuthenticationPlugin extends KTPlugin {
     var $sNamespace = "ktstandard.ldapauthentication.plugin";
 }
 
 $oPluginRegistry =& KTPluginRegistry::getSingleton();
 $oPluginRegistry->registerPlugin('KTLDAPAuthenticationPlugin', 'ktstandard.ldapauthentication.plugin', __FILE__);
-$oPlugin =& $oPluginRegistry->getPlugin('ktstandard.subscriptions.plugin');
+$oPlugin =& $oPluginRegistry->getPlugin('ktstandard.ldapauthentication.plugin');
 
 $oPlugin->registerAuthenticationProvider('LDAP Authentication', 'KTLDAPAuthenticationProvider', 'ktstandard.authentication.ldapprovider', 'ldap/ldapauthenticationprovider.inc.php');
 
+$oPlugin->register();
