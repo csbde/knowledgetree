21d87ce25f61e919efb4c78b449c03654b41f587
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-11-25 09:53:56 +0000

    Create a registered plugin for KTCore, and register admin pages and categories.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4164 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/KTCorePlugin.php
+++ plugins/ktcore/KTCorePlugin.php
@@ -1,10 +1,26 @@
 <?php
 
+require_once(KT_LIB_DIR . '/plugins/pluginregistry.inc.php');
+require_once(KT_LIB_DIR . '/plugins/plugin.inc.php');
+
+class KTCorePlugin extends KTPlugin {
+}
+
+$oRegistry =& KTPluginRegistry::getSingleton();
+$oRegistry->registerPlugin('KTCorePlugin', 'ktcore.plugin', __FILE__);
+
+$oPlugin =& $oRegistry->getPlugin('ktcore.plugin');
+
 require_once('KTDocumentActions.php');
 require_once('KTFolderActions.php');
 require_once('KTPortlets.php');
 require_once('KTPermissions.php');
+require_once('KTAdminPlugins.php');
+
+$oPlugin->registerAdminPage('authentication', 'KTAuthenticationAdminPage', 'principals', 'Authentication', 'FIXME: describe authentication', 'authentication/authenticationadminpage.inc.php');
+$oPlugin->registerAuthenticationProvider('LDAP Authentication', 'KTLDAPAuthenticationProvider', 'ktstandard.authentication.ldapprovider', 'authentication/ldapauthenticationprovider.inc.php');
+
+$oPlugin->register();
 
 require_once(KT_LIB_DIR . '/storage/ondiskpathstoragemanager.inc.php');
 
-?>
