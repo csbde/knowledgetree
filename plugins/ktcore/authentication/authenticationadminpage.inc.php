b91d7de5154807380cda3a72d5899ef862638562
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-11-25 10:39:21 +0000

    Front page for authentication sources.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4168 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/authentication/authenticationadminpage.inc.php
+++ plugins/ktcore/authentication/authenticationadminpage.inc.php
@@ -1,9 +1,30 @@
 <?php
 
 require_once(KT_LIB_DIR . '/dispatcher.inc.php');
+require_once(KT_LIB_DIR . '/authentication/authenticationproviderregistry.inc.php');
 
 class KTAuthenticationAdminPage extends KTAdminDispatcher {
     function do_main() {
-        return "Hello there";
+        $oTemplate =& $this->oValidator->validateTemplate('ktcore/authentication/manage');
+        $fields = array();
+
+        $fields[] = new KTStringWidget('Name', 'FIXME', 'name', "", $this->oPage, true);
+
+        $aVocab = array();
+        $oRegistry =& KTAuthenticationProviderRegistry::getSingleton();
+        $aProviders = $oRegistry->getAuthenticationProviders();
+        foreach ($aProviders as $aProvider) {
+            $aVocab[$aProvider[1]] = $aProvider[0];
+        }
+        $fieldOptions = array("vocab" => $aVocab);
+        $fields[] = new KTLookupWidget('Authentication provider', 'FIXME', 'authentication_provider', null, $this->oPage, true, null, $fieldErrors, $fieldOptions);
+
+        $oTemplate->setData(array(
+            'context' => &$this,
+            'fields' => $fields,
+            'providers' => $aProviders,
+        ));
+        return $oTemplate->render();
+
     }
 }
