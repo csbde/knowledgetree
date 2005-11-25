c4b1884a50b979a5bf74aa8ea97fe1387e69b700
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-11-25 18:30:19 +0000

    Authentication Providers are now dispatchers - they need to handle do_editSourceProvider and do_performEditSourceProvider.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4172 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/authentication/authenticationprovider.inc.php
+++ lib/authentication/authenticationprovider.inc.php
@@ -1,11 +1,12 @@
 <?php
 
-class KTAuthenticationProvider {
+class KTAuthenticationProvider extends KTStandardDispatcher {
     var $sName;
     var $sNamespace;
     var $bHasSource = false;
 
     function KTAuthenticationProvider() {
+        return parent::KTStandardDispatcher();
     }
 
     function configure($aInfo) {
@@ -21,5 +22,24 @@ class KTAuthenticationProvider {
             return null;
         }
         return $this;
+    }
+
+    function showSource() {
+        return null;
+    }
+
+    function getName() {
+        return $this->sName;
+    }
+    function getNamespace() {
+        return $this->sNamespace;
+    }
+
+    function do_editSourceProvider() {
+        return $this->errorRedirectTo('viewsource', "Provider does not support editing", 'source_id=' .  $_REQUEST['source_id']);
+    }
+
+    function do_performEditSourceProvider() {
+        return $this->errorRedirectTo('viewsource', "Provider does not support editing", 'source_id=' .  $_REQUEST['source_id']);
     }
 }
