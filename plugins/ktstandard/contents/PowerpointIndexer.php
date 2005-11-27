86233ae3a80b0b1083321105bdcc5ca403598a95
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-11-27 21:34:24 +0000

    Add a Microsoft Office (Powerpoint) presentation indexer
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4209 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ plugins/ktstandard/contents/PowerpointIndexer.php
@@ -0,0 +1,30 @@
+<?php
+
+class KTPowerpointIndexerTrigger {
+    function setDocument($oDocument) {
+        $this->oDocument = $oDocument;
+    }
+
+    function transform() {
+        $oStorage = KTStorageManagerUtil::getSingleton();
+        $sFile = $oStorage->temporaryFile($this->oDocument);
+
+        $cmdline = array("catppt", $sFile);
+        $myfilename = tempnam("/tmp", "kt.catppt");
+        $command = KTUtil::safeShellString($cmdline) . " >> " . $myfilename;
+        system($command);
+        $contents = file_get_contents($myfilename);
+        unlink($myfilename);
+        if (empty($contents)) {
+            return;
+        }
+        $aInsertValues = array(
+            'document_id' => $this->oDocument->getId(),
+            'document_text' => $contents,
+        );
+        $sTable = KTUtil::getTableName('document_text');
+        DBUtil::autoInsert($sTable, $aInsertValues, array('noid' => true));
+    }
+}
+
+?>
