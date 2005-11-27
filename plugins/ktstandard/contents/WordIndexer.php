c78a712bff564399097a90462975461b2060aceb
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-11-27 21:49:08 +0000

    Use the '-w' parameter to catdoc to remove needless line wrapping.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4212 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktstandard/contents/WordIndexer.php
+++ plugins/ktstandard/contents/WordIndexer.php
@@ -9,11 +9,15 @@ class KTWordIndexerTrigger {
         $oStorage = KTStorageManagerUtil::getSingleton();
         $sFile = $oStorage->temporaryFile($this->oDocument);
 
-        $cmdline = array("catdoc", $sFile);
+        $cmdline = array("catdoc", "-w", $sFile);
         $myfilename = tempnam("/tmp", "kt.catdoc");
         $command = KTUtil::safeShellString($cmdline) . " >> " . $myfilename;
         system($command);
         $contents = file_get_contents($myfilename);
+        unlink($myfilename);
+        if (empty($contents)) {
+            return;
+        }
         $aInsertValues = array(
             'document_id' => $this->oDocument->getId(),
             'document_text' => $contents,
