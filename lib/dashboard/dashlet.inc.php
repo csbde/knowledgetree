050426a4659563b8cd2386cd244940c91f2b292f
Brad Shuttleworth <brad@knowledgetree.com>
2005-11-29 10:21:56 +0000

    - add dashlets, which let plugins put items on the users dashboard. - made the dashboard not show plugins. - added plugin-helpers for dashlets. - fix typo in INSTALL.txt
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4240 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ lib/dashboard/dashlet.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+/* base class for dashlets. */
+require_once(KT_LIB_DIR . "/dashboard/dashletregistry.inc.php");
+
+class KTBaseDashlet {
+    var $oPlugin;
+    
+    function setPlugin($oPlugin) { $this->oPlugin =& $oPlugin; }
+    
+    // precondition check.
+    function is_active($oUser) { return true; }
+    function render() { return '<div class="ktError"><p>This Dashlet is incomplete.</p></div>'; }
+}
+
+//$oDashletRegistry =& KTDashletRegistry::getSingleton();
+//$oDashletRegistry->registerDashlet('KTBaseDashlet','ktcore.dashlets.abstractbase',__FILE__, null)
+
+?>
