4ebf668bb54575eb2fdcd6336e46f4803da14a93
bshuttle <bshuttle@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-11-29 14:33:20 +0000

    - add user assistance infrastructure. - incorporate basic information on the installation / howto side of things - highlight the beta state.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4258 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/KTDashletPlugins.php
+++ plugins/ktcore/KTDashletPlugins.php
@@ -5,6 +5,11 @@ require_once(KT_LIB_DIR . '/plugins/plugin.inc.php');
 require_once(KT_LIB_DIR . '/dashboard/dashlet.inc.php');
 require_once(KT_LIB_DIR . "/templating/templating.inc.php");
 require_once(KT_LIB_DIR . "/dashboard/Notification.inc.php");
+require_once(KT_LIB_DIR . "/security/Permission.inc");
+
+
+$oRegistry =& KTPluginRegistry::getSingleton();
+$oPlugin =& $oRegistry->getPlugin('ktcore.plugin');
 
 /* 
 * The registration hooks. 
@@ -12,6 +17,23 @@ require_once(KT_LIB_DIR . "/dashboard/Notification.inc.php");
 * Since this is too small to _actually_ need a full plugin object, we go:
 *
 */
+
+// ultra simple skeleton for the admin tutorial
+class KTBeta1InfoDashlet extends KTBaseDashlet {
+	function is_active($oUser) {
+		return true;
+	}
+	
+    function render() {
+		$oTemplating = new KTTemplating;
+		$oTemplate = $oTemplating->loadTemplate("ktcore/dashlets/beta1info");
+		$aTemplateData = array(
+		);
+		return $oTemplate->render($aTemplateData);
+    }
+}
+
+$oPlugin->registerDashlet('KTBeta1InfoDashlet', 'ktcore.dashlet.beta1info', __FILE__);
 
 class KTNotificationDashlet extends KTBaseDashlet {
 
@@ -36,10 +58,33 @@ class KTNotificationDashlet extends KTBaseDashlet {
     }
 }
 
-$oRegistry =& KTPluginRegistry::getSingleton();
-$oPlugin =& $oRegistry->getPlugin('ktcore.plugin');
 
-$oPlugin->registerDashlet('KTNotificationDashlet', 'ktcore.notifications.dashlet', __FILE__);
+$oPlugin->registerDashlet('KTNotificationDashlet', 'ktcore.dashlet.notifications', __FILE__);
+
+// replace the old checked-out docs.
+class KTCheckoutDashlet extends KTBaseDashlet {
+
+	var $oUser;
+	
+	function is_active($oUser) {
+		$this->oUser = $oUser;
+		return true;
+	}
+	
+    function render() {
+	    
+        $checked_out_documents = Document::getList(array("checked_out_user_id = ?", $this->oUser->getId()));
+        
+		$oTemplating = new KTTemplating;
+		$oTemplate = $oTemplating->loadTemplate("ktcore/dashlets/checkedout");
+		$aTemplateData = array(
+		    "documents" => $checked_out_documents,
+		);
+		return $oTemplate->render($aTemplateData);
+    }
+}
+
+$oPlugin->registerDashlet('KTCheckoutDashlet', 'ktcore.dashlet.checkout', __FILE__);
 
 
 ?>
