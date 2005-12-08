c62e39d5dcfffca5e8de9ecee9eb1dbd08289c89
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-12-08 11:43:23 +0000

    Update for new authentication system.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4368 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/logout.php
+++ presentation/logout.php
@@ -1,36 +1,19 @@
 <?php
-/**
- * $Id$
- *
- * This page logs the current user out of the system.
- *
- * Copyright (c) 2003 Jam Warehouse http://www.jamwarehouse.com
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * @version $Revision$
- * @author Michael Joseph <michael@jamwarehouse.com>, Jam Warehouse (Pty) Ltd, South Africa
- */
-
-// main library routines and defaults
 require_once("../config/dmsDefaults.php");
+require_once(KT_LIB_DIR . '/authentication/authenticationutil.inc.php');
 
-// logout
-$oAuth = new $default->authenticationClass;
-$oAuth->logout();
+class KTLogoutDispatcher extends KTStandardDispatcher {
+    function do_main() {
+        global $default;
 
-// redirect to root
-redirect((strlen($default->rootUrl) > 0 ? $default->rootUrl : "/"));
+        $oAuthenticator =& KTAuthenticationUtil::getAuthenticatorForUser($this->oUser);
+        $oAuthenticator->logout($this->oUser);
+        Session::destroy();
+
+        redirect((strlen($default->rootUrl) > 0 ? $default->rootUrl : "/"));
+        exit(0);
+    }
+}
+$d =& new KTLogoutDispatcher;
+$d->dispatch();
 ?>
