03e4bac2300024e7d46410ff7e4fa7248946a8c6
bshuttle <bshuttle@c91229c3-7414-0410-bfa2-8a42b809f60b>
2005-12-12 15:53:50 +0000

    - add very simple document_type browsing into the system - considering adding all lookup fields, though that might get a little too "busy". - does not incorporate the category version yet, since it will need to have an upgrade from   freetext to lookup.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4417 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/KTPortlets.php
+++ plugins/ktcore/KTPortlets.php
@@ -23,3 +23,37 @@ class KTSearchPortlet extends KTPortlet {
 
 $oPRegistry->registerPortlet(array('browse', 'dashboard'), 'KTSearchPortlet', 'ktcore.portlets.search', '/plugins/ktcore/KTPortlets.php');
 
+
+class KTBrowseModePortlet extends KTPortlet {
+
+    function KTBrowseModePortlet($sTitle = null) {
+        // match empty, false.
+        if ($sTitle == null) {
+            $sTitle = _('Browse Documents By');
+        }
+        parent::KTPortlet($sTitle);
+    }
+
+    function render() {    
+        // this is unfortunate, but such is life.
+        $current_action = KTUtil::arrayGet($_REQUEST, 'fBrowseMode', 'folder');
+        $modes = array(
+            'folder' => array('name' => _('Folder')),
+            
+            'document_type' => array('name' => _('Document Type'), 'target' => 'selectType'),
+        );        
+        
+        $oTemplating = new KTTemplating;        
+        $oTemplate = $oTemplating->loadTemplate("kt3/portlets/browsemodes_portlet");
+        $aTemplateData = array(
+            "context" => $this,
+            "current_action" => $current_action,
+            "modes" => $modes,
+        );
+
+        return $oTemplate->render($aTemplateData);        
+    }
+}
+$oPRegistry->registerPortlet(array('browse'), 'KTBrowseModePortlet', 'ktcore.portlets.browsemodes', '/plugins/ktcore/KTPortlets.php');
+
+
