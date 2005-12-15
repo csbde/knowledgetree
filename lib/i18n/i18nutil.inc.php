2183146a5178536dee58cdb7a9b53ffe9e244022
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-12-15 12:35:56 +0000

    Move getInstalledLocales from languageFunctions.inc, and also expand to include base languages.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4456 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/i18n/i18nutil.inc.php
+++ lib/i18n/i18nutil.inc.php
@@ -3,4 +3,32 @@
 class KTi18nUtil {
     function setup() {
     }
+
+    function getInstalledLocales() {
+        global $default;
+        // get a list of directories in ($default->fileSystemRoot . "/i18n")
+        $aLocales = array();
+        $aLocales["en"] = "en";
+        $aLocales["en-GB"] = "en";
+        $aLocales["en-UK"] = "en";
+        $aLocales["en-US"] = "en";
+        $aLocales["en-ZA"] = "en";
+        $dir = KT_DIR . '/i18n';
+        if ($handle = opendir($dir)) {
+           while (false !== ($file = readdir($handle))) {
+               if (in_array($file, array('.', '..', 'CVS'))) {
+                   continue;
+               }
+               if (is_dir($dir . '/' . $file)) {
+                   $i = strpos($file, '_');
+                   if ($i) {
+                       $aLocales[substr($file, 0, $i)] = $file;
+                   }
+                   $aLocales[strtr($file, '_', '-')] = $file;
+               }
+           }
+           closedir($handle);
+        }
+        return $aLocales;
+    }
 }
