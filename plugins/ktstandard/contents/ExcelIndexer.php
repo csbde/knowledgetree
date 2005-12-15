41cd794ccd06fe69d7c7e6c829347b9b89a93b79
Neil Blakey-Milner <nbm@knowledgetree.com>
2005-12-15 16:11:56 +0000

    Try to use xls2csv as a first shot.  If that fails, try to use PHPExcelReader instead.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4475 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktstandard/contents/ExcelIndexer.php
+++ plugins/ktstandard/contents/ExcelIndexer.php
@@ -7,12 +7,42 @@ class KTExcelIndexerTrigger extends KTBaseIndexerTrigger {
 
     var $mimetypes = array(
        'application/msword' => true,
+       'application/vnd.ms-excel' => true,
     );
     var $command = 'xls2csv';          // could be any application.
     var $args = array("-q", "0", "-c", " ");
     var $use_pipes = true;
     
     // see BaseIndexer for how the extraction works.
+    //
+    function extract_contents($sFilename, $sTempFilename) {
+        $res = parent::extract_contents($sFilename, $sTempFilename);
+        if (!empty($res)) {
+            return $res;
+        }
+
+        return $this->_fallbackExcelReader($sFilename, $sTempFilename);
+    }
+
+    function _fallbackExcelReader($sFilename, $sTempFilename) {
+        require_once(KT_DIR . '/thirdparty/excelreader/Excel/reader.php');
+        $reader = new Spreadsheet_Excel_Reader();
+        $reader->setOutputEncoding('UTF-8');
+        $reader->read($sFilename);
+
+        $t = fopen($sTempFilename, "w");
+        foreach ($reader->sheets as $aSheet) {
+            for ($i = 1; $i <= $aSheet['numRows']; $i++) {
+                for ($j = 1; $j <= $aSheet['numCols']; $j++) {
+                    fwrite($t, $aSheet['cells'][$i][$j] . " ");
+                }
+                fwrite($t, "\n");
+            }
+            fwrite($t, "\n\n\n");
+        }
+        fclose($t);
+        return file_get_contents($sTempFilename);
+    }
 }
 
 ?>
