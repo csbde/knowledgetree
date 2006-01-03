2108bdc13622bf71eddb10c9f906ecc8ca7daac1
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2006-01-03 19:44:54 +0000

    Add an admin page to manage plugins - currently can view and disable/enable plugins.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4572 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ plugins/ktcore/admin/plugins.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once(KT_LIB_DIR . '/dispatcher.inc.php');
+require_once(KT_LIB_DIR . '/validation/dispatchervalidation.inc.php');
+require_once(KT_LIB_DIR . '/templating/templating.inc.php');
+
+require_once(KT_LIB_DIR . '/plugins/plugin.inc.php');
+require_once(KT_LIB_DIR . '/plugins/pluginutil.inc.php');
+require_once(KT_LIB_DIR . '/plugins/pluginentity.inc.php');
+
+require_once(KT_LIB_DIR . "/templating/kt3template.inc.php");
+
+class KTPluginDispatcher extends KTAdminDispatcher {
+    var $bAutomaticTransaction = true;
+
+    function check() {
+        $this->aBreadcrumbs[] = array(
+            'url' => $_SERVER['PHP_SELF'],
+            'name' => _('Plugins'),
+        );
+        return parent::check();
+    }
+
+    function do_main() {
+        $aPlugins = KTPluginEntity::getList();
+        $aEnabledPluginIds = KTPluginEntity::getEnabledPlugins();
+
+        $oTemplating =& KTTemplating::getSingleton();
+        $oTemplate =& $oTemplating->loadTemplate('ktcore/plugins/list');
+        $oTemplate->setData(array(
+            'context' => $this,
+            'plugins' => $aPlugins,
+            'enabled_plugins' => $aEnabledPluginIds,
+        ));
+        return $oTemplate;
+    }
+
+    function do_update() {
+        $sTable = KTUtil::getTableName('plugins');
+        $aIds = KTUtil::arrayGet($_REQUEST, 'pluginids');
+        $sIds = DBUtil::paramArray($aIds);
+        $sQuery = sprintf('UPDATE %s SET disabled = true WHERE id NOT IN (%s)', $sTable, $sIds);
+        DBUtil::runQuery(array($sQuery, $aIds));
+        $sQuery = sprintf('UPDATE %s SET disabled = false WHERE id IN (%s)', $sTable, $sIds);
+        DBUtil::runQuery(array($sQuery, $aIds));
+        $this->successRedirectToMain('Plugins updated');
+    }
+
+    function do_reread() {
+        KTPluginUtil::registerPlugins();
+        $this->successRedirectToMain('Plugins read from the filesystem');
+    }
+}
+
+?>
