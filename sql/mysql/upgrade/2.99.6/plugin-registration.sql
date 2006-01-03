f2511f7899c50bd5f57c803b37e328265ed484fe
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2006-01-03 19:24:08 +0000

    Add the KTPluginEntity class and the plugin registration database table.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4566 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ sql/mysql/upgrade/2.99.6/plugin-registration.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `plugins` (
+  `id` int(11) NOT NULL default '0',
+  `namespace` varchar(255) NOT NULL default '',
+  `path` varchar(255) NOT NULL default '',
+  `version` int(11) NOT NULL default '0',
+  `disabled` tinyint(1) NOT NULL default '0',
+  `data` text,
+  PRIMARY KEY  (`id`),
+  KEY `name` (`namespace`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `zseq_plugins` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
