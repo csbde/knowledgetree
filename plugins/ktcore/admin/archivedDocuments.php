8dedfba32a664fd6bfd2cbd48166a1b9ea04119b
Brad Shuttleworth <brad@knowledgetree.com>
2006-01-16 09:37:11 +0000

    - archival copy mentioned deletion, which was wrong and confusing. - restoration of archived docs & deletion ops had unmet dependencies.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4596 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/admin/archivedDocuments.php
+++ plugins/ktcore/admin/archivedDocuments.php
@@ -9,6 +9,8 @@ require_once(KT_LIB_DIR . '/documentmanagement/DocumentTransaction.inc');
 require_once(KT_LIB_DIR . "/widgets/fieldWidgets.php");
 require_once(KT_LIB_DIR . "/templating/kt3template.inc.php");
 
+require_once(KT_LIB_DIR . "/documentmanagement/PhysicalDocumentManager.inc");
+
 // FIXME chain in a notification alert for un-archival requests.
 
 class ArchivedDocumentsDispatcher extends KTAdminDispatcher {
