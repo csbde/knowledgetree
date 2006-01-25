e870e8204e1a83e4027758b4eff6005261772c54
Brad Shuttleworth <brad@knowledgetree.com>
2006-01-25 16:11:16 +0000

    - fix for IE icons - archived documents now get browsed for restoration.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4689 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/admin/archivedDocuments.php
+++ plugins/ktcore/admin/archivedDocuments.php
@@ -9,27 +9,102 @@ require_once(KT_LIB_DIR . '/documentmanagement/DocumentTransaction.inc');
 require_once(KT_LIB_DIR . "/widgets/fieldWidgets.php");
 require_once(KT_LIB_DIR . "/templating/kt3template.inc.php");
 
+require_once(KT_LIB_DIR . "/browse/DocumentCollection.inc.php");
+require_once(KT_LIB_DIR . "/browse/BrowseColumns.inc.php");
+require_once(KT_LIB_DIR . "/browse/PartialQuery.inc.php");
+require_once(KT_LIB_DIR . "/browse/browseutil.inc.php");
+
 require_once(KT_LIB_DIR . "/documentmanagement/PhysicalDocumentManager.inc");
 
 // FIXME chain in a notification alert for un-archival requests.
+class KTArchiveTitle extends TitleColumn {
+    
+    function renderDocumentLink($aDataRow) {
+        $outStr .= $aDataRow["document"]->getName();
+        return $outStr;
+    }    
+       
+    function buildFolderLink($aDataRow) {
+        $baseurl = KTUtil::arrayGet($this->aOptions, "folderurl", "");
+        $kt_path_info = KTUtil::arrayGet($_REQUEST, 'kt_path_info');
+        if (empty($kt_path_info)) {
+            return sprintf('%s?fFolderId=%d', $baseurl, $aDataRow["folder"]->getId());
+        } else {
+            return sprintf('%s?kt_path_info=%s&fFolderId=%d', $baseurl, $kt_path_info, $aDataRow["folder"]->getId());
+        }
+    }
+}
 
 class ArchivedDocumentsDispatcher extends KTAdminDispatcher {
 
     function do_main () {
         $this->aBreadcrumbs[] = array('url' => $_SERVER['PHP_SELF'], 'name' => _('Archived Documents'));
         
-        $this->oPage->setBreadcrumbDetails(_('list'));
+        $this->oPage->setBreadcrumbDetails(_('browse'));
+            
+        $oFolder = Folder::get(KTUtil::arrayGet($_REQUEST, 'fFolderId', 1));
+        if (PEAR::isError($oFolder)) { 
+            $this->errorRedirectToMain(_('Invalid folder selected.'));
+            exit(0);
+        }
         
-        $aDocuments =& Document::getList("status_id=" . ARCHIVED);
+        // Setup the collection for move display.
         
+        $collection = new DocumentCollection();
         
-        $oTemplating =& KTTemplating::getSingleton();
-        $oTemplate = $oTemplating->loadTemplate('ktcore/document/admin/archivedlist');
-        $oTemplate->setData(array(
-            'context' => $this,
-            'documents' => $aDocuments,
-        ));
-        return $oTemplate;
+        $collection->addColumn(new SelectionColumn("Select","selected_docs[]", false, true));
+        $collection->addColumn(new KTArchiveTitle("Archive Documents","title"));        
+        
+        $qObj = new ArchivedBrowseQuery($oFolder->getId());
+        $collection->setQueryObject($qObj);
+
+        $batchPage = (int) KTUtil::arrayGet($_REQUEST, "page", 0);
+        $batchSize = 20;
+
+        $resultURL = sprintf("?fFolderId=%d&action=browse", $sMoveCode, $oFolder->getId());
+        $collection->setBatching($resultURL, $batchPage, $batchSize);
+
+        // ordering. (direction and column)
+        $displayOrder = KTUtil::arrayGet($_REQUEST, 'sort_order', "asc");
+        if ($displayOrder !== "asc") { $displayOrder = "desc"; }
+        $displayControl = KTUtil::arrayGet($_REQUEST, 'sort_on', "title");
+
+        $collection->setSorting($displayControl, $displayOrder);
+
+        $collection->getResults();    
+        
+        $aBreadcrumbs = array();
+        $folder_path_names = $oFolder->getPathArray();
+        $folder_path_ids = explode(',', $oFolder->getParentFolderIds());
+        if ($folder_path_ids[0] == 0) {
+            $folder_path_ids = array();
+        }
+        $folder_path_ids[] = $oFolder->getId();
+
+        foreach (range(0, count($folder_path_ids) - 1) as $index) {
+            $id = $folder_path_ids[$index];
+            $url = sprintf("?fFolderId=%d", $sMoveCode, $id);
+            $aBreadcrumbs[] = array("url" => $url, "name" => $folder_path_names[$index]);
+        }
+        
+        $oTemplating = new KTTemplating;
+        $oTemplate = $oTemplating->loadTemplate("ktcore/document/admin/archivebrowse");
+        $aTemplateData = array(
+              "context" => $this,
+              'folder' => $oFolder,
+              'breadcrumbs' => $aBreadcrumbs,
+              'collection' => $collection,
+              'collection_breadcrumbs' => $aBreadcrumbs,
+        );
+        
+        return $oTemplate->render($aTemplateData);                  
+    }
+    
+    /*
+     * Provide for "archived" browsing.
+     */
+    function do_browse() {
+    
     }
     
     function do_confirm_restore() {
