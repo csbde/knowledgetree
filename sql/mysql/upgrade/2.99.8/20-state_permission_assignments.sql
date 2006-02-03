aa78a5978a342145fdeac7db43f23a47d6f480f6
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2006-02-03 10:04:36 +0000

    Add Workflow State Permission Assignments, allowing permissions on documents to be overridden by virtue of their workflow state.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4795 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ sql/mysql/upgrade/2.99.8/20-state_permission_assignments.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `workflow_state_permission_assignments` (
+  `id` int(11) NOT NULL,
+  `workflow_state_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  `permission_descriptor_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `permission_id` (`permission_id`),
+  KEY `permission_descriptor_id` (`permission_descriptor_id`),
+  KEY `workflow_state_id` (`workflow_state_id`),
+  CONSTRAINT `workflow_state_permission_assignments_ibfk_7` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`),
+  CONSTRAINT `workflow_state_permission_assignments_ibfk_8` FOREIGN KEY (`permission_descriptor_id`) REFERENCES `permission_descriptors` (`id`)
+) TYPE=InnoDB;
