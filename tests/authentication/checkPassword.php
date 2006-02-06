53fef21ae5dfe38cb89e6a70fe06720681bf798c
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2006-02-06 10:12:02 +0000

    Make it easier to test with correct and incorrect passwords
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4843 c91229c3-7414-0410-bfa2-8a42b809f60b

--- tests/authentication/checkPassword.php
+++ tests/authentication/checkPassword.php
@@ -4,7 +4,12 @@ require_once("../../config/dmsDefaults.php");
 require_once(KT_LIB_DIR . '/authentication/authenticationutil.inc.php');
 
 $oUser =& User::getByUserName('nbm2');
-$foo = KTAuthenticationUtil::checkPassword($oUser, 'asdfa');
-var_dump($foo);
+if (0) {
+    $foo = KTAuthenticationUtil::checkPassword($oUser, 'asdf');
+    var_dump($foo);
+} else {
+    $foo = KTAuthenticationUtil::checkPassword($oUser, 'asdjasdjk');
+    var_dump($foo);
+}
 
 ?>
