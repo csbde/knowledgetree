0cda8c4a63beaf77803f5b84838c506b789fb0e2
Neil Blakey-Milner <nbm@knowledgetree.com>
2006-02-19 19:02:55 +0000

    Upgrading would fail with more than one unit because of not allowing null.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4969 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/2.99.8/10-units.sql
+++ sql/mysql/upgrade/2.99.8/10-units.sql
@@ -1,4 +1,5 @@
 ALTER TABLE `folders` DROP INDEX `fk_unit_id`; # was INDEX (`unit_id`)
 ALTER TABLE `folders` DROP COLUMN `unit_id`; # was int(11) default NULL
+ALTER TABLE `units_lookup` CHANGE COLUMN `folder_id` `folder_id` int(11) NOT NULL default '0';
 DROP TABLE `groups_units_link`;
 DROP TABLE `zseq_groups_units_link`;
