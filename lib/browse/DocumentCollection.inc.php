07e75aacca3ce14e45ec1cfa8fdafa8510fd596c
Neil Blakey-Milner <nbm@knowledgetree.com>
2006-02-19 10:41:50 +0000

    Fix typo in the name of _sDocumentJoinClause
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4953 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/browse/DocumentCollection.inc.php
+++ lib/browse/DocumentCollection.inc.php
@@ -133,11 +133,11 @@ class DocumentCollection {
 	  
 	  if ($no_folders) {
           $this->batchStart -= $this->folderCount;
-		 $documentSet = $this->_queryObj->getDocuments($documents_to_get, $this->batchStart, $this->_sDocumentSortField, $this->sort_order, $this->_sDocumentJoinQuery, $this->_aDocumentJoinParams);
+		 $documentSet = $this->_queryObj->getDocuments($documents_to_get, $this->batchStart, $this->_sDocumentSortField, $this->sort_order, $this->_sDocumentJoinClause, $this->_aDocumentJoinParams);
 	  } else {
 	     $folderSet = $this->_queryObj->getFolders($folders_to_get, $this->batchStart, $this->_sFolderSortField, $this->sort_order, $this->_sFolderJoinQuery, $this->_aFolderJoinParams);
 		 if ($documents_to_get > 0) {
-	        $documentSet = $this->_queryObj->getDocuments($documents_to_get, 0, $this->_sDocumentSortField, $this->sort_order, $this->_sDocumentJoinQuery, $this->_aDocumentJoinParams);
+	        $documentSet = $this->_queryObj->getDocuments($documents_to_get, 0, $this->_sDocumentSortField, $this->sort_order, $this->_sDocumentJoinClause, $this->_aDocumentJoinParams);
 		 }
 		 
 	  }
