2d339744c51aaf314f25e47eb6fedf0f62fad93b
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2006-02-20 10:47:59 +0000

    Hook Brad's folder renaming code up to folder actions.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4978 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ plugins/ktcore/folder/Rename.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once(KT_LIB_DIR . '/actions/folderaction.inc.php');
+
+require_once(KT_LIB_DIR . "/widgets/fieldsetDisplay.inc.php");
+require_once(KT_LIB_DIR . "/widgets/FieldsetDisplayRegistry.inc.php");
+require_once(KT_LIB_DIR . "/foldermanagement/folderutil.inc.php");
+require_once(KT_LIB_DIR . "/documentmanagement/observers.inc.php");
+
+require_once(KT_LIB_DIR . "/documentmanagement/documentutil.inc.php");
+
+class KTFolderRenameAction extends KTFolderAction {
+    var $sName = 'ktcore.actions.folder.rename';
+    var $_sShowPermission = "ktcore.permissions.write";
+
+    function getDisplayName() {
+        return _('Rename');
+    }
+
+    function do_main() {
+        $this->oPage->setBreadcrumbDetails(_("rename"));
+        $this->oPage->setTitle(_('Rename folder'));
+        $oTemplate =& $this->oValidator->validateTemplate('ktcore/folder/rename');
+
+        $fields = array();
+        $fields[] = new KTStringWidget(_('New folder name'), _('The name to which the current folder should be renamed.'), 'foldername', "", $this->oPage, true);
+        
+        $oTemplate->setData(array(
+            'context' => &$this,
+            'fields' => $fields,
+        ));
+        return $oTemplate->render();
+    }
+
+    function do_rename() {
+        $sName = KTUtil::arrayGet($_REQUEST, 'foldername');
+        $aOptions = array(
+            'redirect_to' => array('', sprintf('fFolderId=%d', $this->oFolder->getId())),
+            'message' => "No folder name given",
+        );
+        $this->oValidator->validateString($sName, $aOptions);
+
+        $res = KTFolderUtil::rename($this->oFolder, $sName, $this->oUser);
+        if (PEAR::isError($res)) {
+            $_SESSION['KTErrorMessage'][] = $res->getMessage();
+            redirect(KTBrowseUtil::getUrlForFolder($this->oFolder));
+            exit(0);
+        } else {
+            $_SESSION['KTInfoMessage'][] = sprintf(_('Folder "%s" renamed to "%s".'), $this->oFolder->getName(), $sName);
+        }
+
+        $this->commitTransaction();
+        redirect(KTBrowseUtil::getUrlForFolder($this->oFolder));
+        exit(0);
+    }
+
+}
+
+?>
