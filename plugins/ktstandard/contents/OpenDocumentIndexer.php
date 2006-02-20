323faae353ee0e97b68479379c15912ab94d23df
Neil Blakey-Milner <nbm@knowledgetree.com>
2006-02-20 14:33:42 +0000

    Add OpenDocument indexing support (requires unzip)
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4989 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ plugins/ktstandard/contents/OpenDocumentIndexer.php
@@ -0,0 +1,79 @@
+<?php
+
+require_once(KT_DIR . '/plugins/ktstandard/contents/BaseIndexer.php');
+
+class KTOpenDocumentIndexerTrigger extends KTBaseIndexerTrigger {
+    var $mimetypes = array(
+       'application/octet-stream' => true,
+       'application/zip' => true,
+       'application/x-zip' => true,
+       'application/vnd.oasis.opendocument.text' => true,
+       'application/vnd.oasis.opendocument.text-template' => true,
+       'application/vnd.oasis.opendocument.presentation' => true,
+       'application/vnd.oasis.opendocument.presentation-template' => true,
+       'application/vnd.oasis.opendocument.spreadsheet' => true,
+       'application/vnd.oasis.opendocument.spreadsheet-template' => true,
+    );
+
+    function transform() {
+        $iMimeTypeId = $this->oDocument->getMimeTypeId();
+        $sMimeType = KTMime::getMimeTypeName($iMimeTypeId);
+        $sFileName = $this->oDocument->getFileName();
+        if (in_array($sMimeType, array('application/octet-stream', 'application/zip', 'application/x-zip'))) {
+            $sExtension = KTMime::stripAllButExtension($sFileName);
+            $sTable = KTUtil::getTableName('mimetypes');
+            $sQuery = "SELECT id FROM $sTable WHERE LOWER(filetypes) = ?";
+            $aParams = array($sExtension);
+            $id = DBUtil::getOneResultKey(array($sQuery, $aParams), 'id');
+            if ($id) {
+                $this->oDocument->setMimeTypeId($id);
+                $this->oDocument->update();
+            }
+        }
+        parent::transform();
+    }
+
+    function extract_contents($sFilename, $sTmpFilename) {
+        $sUnzipCommand = KTUtil::findCommand("import/unzip", "unzip");
+        if (empty($sUnzipCommand)) {
+            return;
+        }
+        $this->sTmpPath = tempnam('/tmp', 'opendocumentextract');
+        if ($this->sTmpPath === false) {
+            return;
+        }
+        unlink($this->sTmpPath);
+        mkdir($this->sTmpPath, 0700);
+
+        $sCmd = array(
+            $sUnzipCommand,
+            "-q", "-n",
+            "-d", $this->sTmpPath,
+            $sFilename,
+        );
+        KTUtil::pexec($sCmd);
+
+        $sManifest = sprintf("%s/%s", $this->sTmpPath, "META-INF/manifest.xml");
+        if (!file_exists($sManifest)) {
+            $this->cleanup();
+            return;
+        }
+        $sContentFile = sprintf("%s/%s", $this->sTmpPath, "content.xml");
+        if (!file_exists($sContentFile)) {
+            $this->cleanup();
+            return;
+        }
+
+        $sContent = file_get_contents($sContentFile);
+        $sContent = preg_replace ("@(</?[^>]*>)+@", " ", $sContent);
+
+        $this->cleanup();
+        return $sContent;
+    }
+
+    function cleanup() {
+        KTUtil::deleteDirectory($this->sTmpPath);
+    }
+}
+
+?>
