270a6dac7fac5f99ea05ba5a3f397a71ca758f44
bshuttle <bshuttle@c91229c3-7414-0410-bfa2-8a42b809f60b>
2006-02-20 15:49:23 +0000

    hide nbm's search-title work.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@4991 c91229c3-7414-0410-bfa2-8a42b809f60b

--- search/booleanSearch.php
+++ search/booleanSearch.php
@@ -84,10 +84,11 @@ class BooleanSearchDispatcher extends KTStandardDispatcher {
         $collection = new DocumentCollection;
         $this->browseType = "Folder";
 
-        $collection->addColumn(new SelectionColumn("Browse Selection","selection"));
+        //$collection->addColumn(new SelectionColumn("Browse Selection","selection"));
         $t =& new TitleColumn("Test 1 (title)","title");
         $t->setOptions(array('documenturl' => $GLOBALS['KTRootUrl'] . '/view.php'));
         $collection->addColumn($t);
+        $collection->addColumn(new DownloadColumn('','download'));
         $collection->addColumn(new DateColumn(_("Created"),"created", "getCreatedDateTime"));
         $collection->addColumn(new DateColumn(_("Last Modified"),"modified", "getLastModifiedDate"));
         $collection->addColumn(new UserColumn(_('Creator'),'creator_id','getCreatorID'));
