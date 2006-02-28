421655b84c8c21c4cd88311ef9ee0bbe1692d7e3
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2006-02-28 16:53:52 +0000

    Add copyright notices to those files that didn't have them.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5027 c91229c3-7414-0410-bfa2-8a42b809f60b

--- edit.php
+++ edit.php
@@ -1,9 +1,34 @@
 <?php
-/*
- * Ruthlessly gutted from addDocument.php
+/**
+ * $Id$
+ *
+ * Copyright (c) 2006 Jam Warehouse http://www.jamwarehouse.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; using version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * -------------------------------------------------------------------------
+ *
+ * You can contact the copyright owner regarding licensing via the contact
+ * details that can be found on the KnowledgeTree web site:
+ *
+ *         http://www.ktdms.com/
+ *
+ * -------------------------------------------------------------------------
  *
  * @version $Revision$
  * @author Brad Shuttleworth, Jam Warehouse (Pty) Ltd, South Africa
+ *
  */
 
 require_once("config/dmsDefaults.php");
