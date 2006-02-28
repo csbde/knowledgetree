7a2e72aa422fe0e7c7fb88c3d625d5c7bb6742fe
bshuttle <bshuttle@c91229c3-7414-0410-bfa2-8a42b809f60b>
2006-02-28 15:18:42 +0000

    last.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5024 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktstandard/contents/WordIndexer.php
+++ plugins/ktstandard/contents/WordIndexer.php
@@ -19,6 +19,28 @@ class KTWordIndexerTrigger extends KTBaseIndexerTrigger {
         }
         return parent::extract_contents($sFilename, $sTempFilename);
     }
+    
+    function findLocalCommand() {
+        if (OS_WINDOWS) {
+            $this->command = 'c:\antiword\antiword.exe';
+            $this->commandconfig = 'indexer/antiword';
+            $this->args = array();
+        }
+        
+        $sCommand = KTUtil::findCommand($this->commandconfig, $this->command);
+        return $sCommand;
+    }
+    
+    function getDiagnostic() {
+        $sCommand = $this->findLocalCommand();
+        
+        // can't find the local command.
+        if (empty($sCommand)) {
+            return sprintf(_('Unable to find required command for indexing.  Please ensure that <strong>%s</strong> is installed and in the KnowledgeTree Path.  For more information on indexers and helper applications, please <a href="%s">visit the KTDMS site</a>.'), $this->command, $this->support_url);
+        }
+        
+        return null;
+    }
 }
 
 ?>
