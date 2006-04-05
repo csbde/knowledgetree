3e8703c1b2f6ce2a43ce9dc2076220342ee3aea7
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2006-04-05 15:32:10 +0000

    KTS-622: don't show a folder as orphaned if it is in the root folder.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5193 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/browse/browseutil.inc.php
+++ lib/browse/browseutil.inc.php
@@ -304,7 +304,6 @@ class KTBrowseUtil {
         $aPermissionDescriptors = KTPermissionUtil::getPermissionDescriptorsForUser($oUser);
         
         
-        
         if (empty($aPermissionDescriptors)) {
             return array();
         }
@@ -321,6 +320,7 @@ class KTBrowseUtil {
                 LEFT JOIN $sPLTable AS PL2 ON F2.permission_lookup_id = PL2.id LEFT JOIN $sPLATable AS PLA2 ON PLA2.permission_lookup_id = PL2.id AND PLA2.permission_id = ?
             WHERE
                 PLA.permission_descriptor_id IN ($sPermissionDescriptors)
+                AND F2.id != 1
                 AND NOT (PLA2.permission_descriptor_id IN ($sPermissionDescriptors))";
         $aParams = kt_array_merge(array($oPermission->getId(), $oPermission->getId()), $aPermissionDescriptors, $aPermissionDescriptors);
         $res = DBUtil::getResultArrayKey(array($sQuery, $aParams), 'id');
