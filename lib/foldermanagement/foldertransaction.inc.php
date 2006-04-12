5d2b98843e4de949df4e40dc25993f36de5be6ff
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2006-04-12 21:53:37 +0000

    KTFolderTransaction represents transactions on a folder (currently create, move, permissions change, and role allocations change).
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5233 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ lib/foldermanagement/foldertransaction.inc.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * $Id$
+ *
+ * Copyright (c) 2006 Jam Warehouse http://www.jamwarehouse.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; using version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * -------------------------------------------------------------------------
+ *
+ * You can contact the copyright owner regarding licensing via the contact
+ * details that can be found on the KnowledgeTree web site:
+ *
+ *         http://www.ktdms.com/
+ */
+
+require_once(KT_LIB_DIR . "/ktentity.inc");
+
+class KTFolderTransaction extends KTEntity {
+    var $_aFieldToSelect = array(
+        'iId' => 'id',
+        'iFolderId' => 'folder_id',
+        'iUserId' => 'user_id',
+        'dDateTime' => 'datetime',
+        'sIp' => 'ip',
+        'sComment' => 'comment',
+        'sTransactionNS' => 'transaction_namespace',
+    );
+
+    var $_bUsePearError = true;
+
+    function _table () {
+        return KTUtil::getTableName('folder_transactions');
+    }
+
+    // STATIC
+    function &get($iId) {
+        return KTEntityUtil::get('KTFolderTransaction', $iId);
+    }
+
+    // STATIC
+    function &createFromArray($aOptions) {
+        return KTEntityUtil::createFromArray('KTFolderTransaction', $aOptions);
+    }
+
+    // STATIC
+    function &getList($sWhereClause = null) {
+        return KTEntityUtil::getList2('KTFolderTransaction', $sWhereClause);
+    }
+
+    function &getByFolder($oFolder, $aOptions = null) {
+        $aOptions = KTUtil::meldOptions(array(
+            'multi' => true,
+        ), $aOptions);
+        $iFolderId = KTUtil::getId($oFolder);
+        return KTEntityUtil::getByDict('KTFolderTransaction', array(
+            'folder_id' => $iFolderId,
+        ), $aOptions);
+    }
+
+    function &getByUser($oFolder, $aOptions = null) {
+        $aOptions = KTUtil::meldOptions(array(
+            'multi' => true,
+        ), $aOptions);
+        $iFolderId = KTUtil::getId($oFolder);
+        return KTEntityUtil::getByDict('KTFolderTransaction', array(
+            'folder_id' => $iFolderId,
+        ), $aOptions);
+    }
+}
+
+?>
