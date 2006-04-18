05b526b12976848323f5c7b359e560dcbae57cea
nbm <nbm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2006-04-18 14:56:00 +0000

    Add database constraints to help prevent data integrity loss
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5273 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/3.0.1.6/workflow-into-metadata.sql
+++ sql/mysql/upgrade/3.0.1.6/workflow-into-metadata.sql
@@ -1,6 +1,11 @@
+SET FOREIGN_KEY_CHECKS=0;
 ALTER TABLE `document_metadata_version` ADD COLUMN `workflow_id` int(11) default NULL;
 ALTER TABLE `document_metadata_version` ADD COLUMN `workflow_state_id` int(11) default NULL;
 ALTER TABLE `document_metadata_version` ADD INDEX `workflow_id` (`workflow_id`);
 ALTER TABLE `document_metadata_version` ADD INDEX `workflow_state_id` (`workflow_state_id`);
 
+ALTER TABLE `document_metadata_version` ADD CONSTRAINT `document_metadata_version_ibfk_8` FOREIGN KEY (`workflow_id`) REFERENCES `workflows` (`id`);
+ALTER TABLE `document_metadata_version` ADD CONSTRAINT `document_metadata_version_ibfk_9` FOREIGN KEY (`workflow_state_id`) REFERENCES `workflow_states` (`id`);
+
 UPDATE document_metadata_version AS DC, workflow_documents AS WC, documents AS D SET DC.workflow_id = WC.workflow_id, DC.workflow_state_id = WC.state_id WHERE DC.document_id = WC.document_id AND DC.id = D.metadata_version_id;
+SET FOREIGN_KEY_CHECKS=1;
