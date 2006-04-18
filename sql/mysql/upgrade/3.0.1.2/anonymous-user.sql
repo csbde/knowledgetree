11f214660fe96c19ae5bc8726824fd1583cd610e
bshuttle <bshuttle@c91229c3-7414-0410-bfa2-8a42b809f60b>
2006-04-18 10:17:55 +0000

    add in information for the anonymous user.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5259 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ sql/mysql/upgrade/3.0.1.2/anonymous-user.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, username, name, password, max_sessions) VALUES (-2, 'anonymous','Anonymous','---------------', 30000);
+INSERT INTO `roles` VALUES (-4, 'Authenticated Users');
+INSERT INTO `roles` VALUES (-3, 'Everyone');
