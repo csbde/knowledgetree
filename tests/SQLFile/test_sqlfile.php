c07e818c1c7a803f1adf93aef7d79f91564a88f3
Neil Blakey-Milner <nbm@knowledgetree.com>
2006-05-15 13:32:31 +0000

    Convert to SimpleTest unit test.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5402 c91229c3-7414-0410-bfa2-8a42b809f60b

--- tests/SQLFile/test_sqlfile.php
+++ tests/SQLFile/test_sqlfile.php
@@ -1,8 +1,40 @@
 <?php
+require_once(dirname(__FILE__) . "/../test.php");
 
-require_once("../../config/dmsDefaults.php");
 require_once(KT_DIR . "/lib/database/sqlfile.inc.php");
 
-var_dump(SQLFile::sqlFromFile("test_sqlfile.sql"));
+class SQLFileTestCase extends KTUnitTestCase {
+    function testSQLFile() {
+        $aExpected = array(
+            "SELECT \"as;\";",
+            "SELECT \"as\\\";\";",
+            "SELECT \"as\\\\\";",
+            "SELECT \"as\\\\\";",
+            "SELECT \"as\\\\\";",
+            "SELECT \"as\\\\\";",
+            "SELECT 'as\\\\';",
+            "SELECT 'as\\\\';",
+            "SELECT 'as\\\\';",
+            "SELECT 'as\\\\';",
+            "SELECT 'as\\\\';",
+            "SELECT 'as\\';';",
+            "SELECT \"'as\\'\"';';",
+            "SELECT 'as\\\\';",
+            "SELECT 'as\\\\';",
+            "SELECT 'as\\\\';",
+            "SELECT 'as\\\\';",
+            "SELECT 'as\\\\';",
+            "SELECT 'as\\\\';",
+            "SELECT '
+asdf; \"\\\"  \\'
+
+asdf; ';",
+        );
+
+        $aReceived = SQLFile::sqlFromFile(dirname(__FILE__) . "/test_sqlfile.sql");
+
+        $this->assertExpectedResults($aExpected, $aReceived);
+    }
+}
 
 ?>
