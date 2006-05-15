5dbc534a5b8c8a6ebb3d627860ff4a261fb56246
Neil Blakey-Milner <nbm@knowledgetree.com>
2006-05-15 13:32:55 +0000

    Convert to SimpleTest unit tests
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5403 c91229c3-7414-0410-bfa2-8a42b809f60b

--- tests/config/testConfig.php
+++ tests/config/testConfig.php
@@ -1,28 +1,84 @@
 <?php
 
-error_reporting(E_ALL);
-
-require_once("../../config/dmsDefaults.php");
+require_once(dirname(__FILE__) . '/../test.php');
 require_once(KT_LIB_DIR . '/config/config.inc.php');
 
-$KTConfig = new KTConfig;
-$KTConfig->loadFile("foo.ini");
-$aExpectedRet = "asdf";
-$aRet = $KTConfig->get("asdf");
-if ($aRet === $aExpectedRet) {
-    print "Success!\n";
-} else {
-    print "Failure!\n";
-    print "Expected: " . print_r($aExpectedRet, true) . "\n";
-    print "Got: " . print_r($aRet, true) . "\n";
-}
-$aRet = $KTConfig->get("asdf/asdf");
-if ($aRet === $aExpectedRet) {
-    print "Success!\n";
-} else {
-    print "Failure!\n";
-    print "Expected: " . print_r($aExpectedRet, true) . "\n";
-    print "Got: " . print_r($aRet, true) . "\n";
+class ConfigTestCase extends KTUnitTestCase {
+    function testGetFlat() {
+        $KTConfig = new KTConfig;
+        $KTConfig->loadFile(dirname(__FILE__) . "/foo.ini");
+        $aExpectedRet = "asdf";
+        $aRet = $KTConfig->get("asdf");
+        $this->assertEqual($aExpectedRet, $aRet);
+    }
+
+    function testGetNS() {
+        $KTConfig = new KTConfig;
+        $KTConfig->loadFile(dirname(__FILE__) . "/foo.ini");
+        $aExpectedRet = "asdf";
+        $aRet = $KTConfig->get("asdf/asdf");
+        $this->assertEqual($aExpectedRet, $aRet);
+    }
+
+    function testExpandSimple() {
+        $KTConfig = new KTConfig;
+        $KTConfig->loadFile(dirname(__FILE__) . "/expand.ini");
+        $aExpectedRet = "bb";
+        $aRet = $KTConfig->get("expand/b");
+        $this->assertEqual($aExpectedRet, $aRet);
+    }
+
+    function testExpandEmail() {
+        $KTConfig = new KTConfig;
+        $KTConfig->loadFile(dirname(__FILE__) . "/expand.ini");
+        $aExpectedRet = "kt@mail.example.org";
+        $aRet = $KTConfig->get("mail/emailFrom");
+        $this->assertEqual($aExpectedRet, $aRet);
+    }
+
+    function testExpandMulti() {
+        $KTConfig = new KTConfig;
+        $KTConfig->loadFile(dirname(__FILE__) . "/expand.ini");
+        $aExpectedRet = "zxcvasdfzxcvrewqzxcvasdf";
+        $aRet = $KTConfig->get("multi/c");
+        $this->assertEqual($aExpectedRet, $aRet);
+    }
+
+    function testMulti1() {
+        $KTConfig = new KTConfig;
+        $KTConfig->loadFile(dirname(__FILE__) . "/foo.ini");
+        $KTConfig->loadFile(dirname(__FILE__) . "/bar.ini");
+        $aExpectedRet = "foo";
+        $aRet = $KTConfig->get("bar");
+        $this->assertEqual($aExpectedRet, $aRet);
+    }
+
+    function testMulti2() {
+        $KTConfig = new KTConfig;
+        $KTConfig->loadFile(dirname(__FILE__) . "/foo.ini");
+        $KTConfig->loadFile(dirname(__FILE__) . "/bar.ini");
+        $aExpectedRet = "foo";
+        $aRet = $KTConfig->get("bar/bar");
+        $this->assertEqual($aExpectedRet, $aRet);
+    }
+
+    function testMulti3() {
+        $KTConfig = new KTConfig;
+        $KTConfig->loadFile(dirname(__FILE__) . "/foo.ini");
+        $KTConfig->loadFile(dirname(__FILE__) . "/bar.ini");
+        $aExpectedRet = "foo";
+        $aRet = $KTConfig->get("bar/asdf");
+        $this->assertEqual($aExpectedRet, $aRet);
+    }
+
+    function testMulti4() {
+        $KTConfig = new KTConfig;
+        $KTConfig->loadFile(dirname(__FILE__) . "/foo.ini");
+        $KTConfig->loadFile(dirname(__FILE__) . "/bar.ini");
+        $aExpectedRet = "asdf";
+        $aRet = $KTConfig->get("asdf/asdf");
+        $this->assertEqual($aExpectedRet, $aRet);
+    }
 }
 
 ?>
