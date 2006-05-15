160b803a9e6556bb8a652aa329823da68ba5d9c3
Neil Blakey-Milner <nbm@knowledgetree.com>
2006-05-15 11:31:29 +0000

    Include file for all tests - turns KT into test mode (changes database, for example) on include.
    Add KTUnitTestCase, where we can add additional asserts and different
    behaviour if necessary.
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5398 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ tests/test.php
@@ -0,0 +1,34 @@
+<?php
+
+$GLOBALS['kt_test'] = true;
+require_once(dirname(__FILE__) . '/../config/dmsDefaults.php');
+require_once('simpletest/unit_tester.php');
+require_once('simpletest/mock_objects.php');
+require_once('simpletest/reporter.php');
+
+class KTUnitTestCase extends UnitTestCase {
+    function assertExpectedResults($aExpected, $aReceived) {
+        if ($aReceived == $aExpected) {
+            $this->pass('Expected results received');
+            return;
+        }
+
+        $iLen = count($aExpected);
+        for ($c = 0; $c < $iLen; $c++) {
+            if ($aReceived[$c] != $aExpected[$c]) {
+                $this->fail(sprintf("Failure.  Expected %s, but got %s\n", $aExpected[$c], $aReceived[$c]));
+            }
+        }
+    }
+
+    function assertEntity($oEntity, $sClass) {
+        if (is_a($oEntity, $sClass)) {
+            return $this->pass(sprintf('Object is a %s', $sClass));
+        }
+        return $this->fail(sprintf('Object is not a %s', $sClass));
+    }
+
+    function assertGroup($oGroup) {
+        return $this->assertEntity($oGroup, 'Group');
+    }
+}
