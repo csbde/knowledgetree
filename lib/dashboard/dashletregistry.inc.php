f93fb83a3eaa49157ae128af70cbab0fef45a4b5
Brad Shuttleworth <brad@knowledgetree.com>
2006-07-27 10:17:43 +0000

    move to new license.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5758 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/dashboard/dashletregistry.inc.php
+++ lib/dashboard/dashletregistry.inc.php
@@ -3,27 +3,25 @@
 /**
  * $Id$
  *
- * Copyright (c) 2006 Jam Warehouse http://www.jamwarehouse.com
+ * The contents of this file are subject to the KnowledgeTree Public
+ * License Version 1.1 ("License"); You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.ktdms.com/KPL
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ * 
+ * The Original Code is: KnowledgeTree Open Source
+ * 
+ * The Initial Developer of the Original Code is The Jam Warehouse Software
+ * (Pty) Ltd, trading as KnowledgeTree.
+ * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
+ * (C) 2006 The Jam Warehouse Software (Pty) Ltd;
+ * All Rights Reserved.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; using version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * -------------------------------------------------------------------------
- *
- * You can contact the copyright owner regarding licensing via the contact
- * details that can be found on the KnowledgeTree web site:
- *
- *         http://www.ktdms.com/
  */
 
 require_once(KT_LIB_DIR . "/plugins/pluginregistry.inc.php");
