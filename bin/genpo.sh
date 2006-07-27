f4b7fc8e557e2f90a9557b2b5b913ec948f3ba75
Brad Shuttleworth <brad@knowledgetree.com>
2006-07-27 12:45:17 +0000

    new PO file for 3.1 release.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5760 c91229c3-7414-0410-bfa2-8a42b809f60b

--- bin/genpo.sh
+++ bin/genpo.sh
@@ -8,6 +8,6 @@ pwd
 rm -f i18n/templates.c
 find resources -name "*.js" | sort | python ./bin/jsi18n.py > templates/ktcore/javascript_i18n.smarty
 php bin/smarty_to_gettext.php . > i18n/templates.c
-find . -type f -name "*.php" -o -name "*.inc" | sort | xgettext --no-wrap -d knowledgeTree -L PHP -s -f - --keyword=_kt -o i18n/knowledgeTree.po
-echo i18n/templates.c i18n/permissions.c | xargs -n 1 | sort | xgettext --no-wrap -d knowledgeTree -j -s -f - -o i18n/knowledgeTree.po
+find . -type f -name "*.php" -o -name "*.inc" | sort | xgettext --no-wrap -d knowledgeTree -L PHP -s -f - --keyword=_kt -o i18n/knowledgeTree.pot
+echo i18n/templates.c i18n/permissions.c | xargs -n 1 | sort | xgettext --no-wrap -d knowledgeTree -j -s -f - -o i18n/knowledgeTree.pot
 
