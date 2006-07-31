c54294e53ab8c49bfea60a210af4c4bd14e052d3
Neil Blakey-Milner <nbm@knowledgetree.com>
2006-07-31 08:44:17 +0000

    Ready for 3.1
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5772 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/install/data.sql
+++ sql/mysql/install/data.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 -- 
 -- Host: localhost
--- Generation Time: Jul 26, 2006 at 11:51 AM
+-- Generation Time: Jul 31, 2006 at 10:43 AM
 -- Server version: 5.0.22
 -- PHP Version: 4.4.2-1build1
 
@@ -732,7 +732,7 @@ INSERT INTO `status_lookup` VALUES (5, 'Incomplete');
 -- 
 
 INSERT INTO `system_settings` VALUES (1, 'lastIndexUpdate', '0');
-INSERT INTO `system_settings` VALUES (2, 'knowledgeTreeVersion', '3.0.3.7');
+INSERT INTO `system_settings` VALUES (2, 'knowledgeTreeVersion', '3.1');
 INSERT INTO `system_settings` VALUES (3, 'databaseVersion', '2.99.5');
 
 -- 
@@ -884,6 +884,7 @@ INSERT INTO `upgrades` VALUES (109, 'upgrade*3.0.3.5*99*upgrade3.0.3.5', 'Upgrad
 INSERT INTO `upgrades` VALUES (110, 'sql*3.0.3.6*0*3.0.3.6/document-restore.sql', 'Database upgrade to version 3.0.3.6: Document-restore', '2006-07-26 11:48:28', 1, 'upgrade*3.0.3.7*99*upgrade3.0.3.7');
 INSERT INTO `upgrades` VALUES (111, 'func*3.0.3.7*0*rebuildAllPermissions', 'Rebuild all permissions to ensure correct functioning of permission-definitions.', '2006-07-26 11:48:28', 1, 'upgrade*3.0.3.7*99*upgrade3.0.3.7');
 INSERT INTO `upgrades` VALUES (112, 'upgrade*3.0.3.7*99*upgrade3.0.3.7', 'Upgrade from version 3.0.3.5 to 3.0.3.7', '2006-07-26 11:48:28', 1, 'upgrade*3.0.3.7*99*upgrade3.0.3.7');
+INSERT INTO `upgrades` VALUES (113, 'upgrade*3.1*99*upgrade3.1', 'Upgrade from version 3.0.3.7 to 3.1', '2006-07-31 10:41:12', 1, 'upgrade*3.1*99*upgrade3.1');
 
 -- 
 -- Dumping data for table `user_history`
@@ -1334,7 +1335,7 @@ INSERT INTO `zseq_units_organisations_link` VALUES (1);
 -- Dumping data for table `zseq_upgrades`
 -- 
 
-INSERT INTO `zseq_upgrades` VALUES (112);
+INSERT INTO `zseq_upgrades` VALUES (113);
 
 -- 
 -- Dumping data for table `zseq_user_history`
