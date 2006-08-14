cd2d5c267014b7154db7257e1c226843a7484b1f
Bryn Divey <bryn@knowledgetree.com>
2006-08-14 09:27:14 +0000

    Fixed KTS-1270
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5798 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktstandard/ImmutableActionPlugin.php
+++ plugins/ktstandard/ImmutableActionPlugin.php
@@ -1,5 +1,31 @@
 <?php
 
+/**
+ * $Id: KTBulkExportPlugin.php 5758 2006-07-27 10:17:43Z bshuttle $
+ *
+ * The contents of this file are subject to the KnowledgeTree Public
+ * License Version 1.1 ("License"); You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.ktdms.com/KPL
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ * 
+ * The Original Code is: KnowledgeTree Open Source
+ * 
+ * The Initial Developer of the Original Code is The Jam Warehouse Software
+ * (Pty) Ltd, trading as KnowledgeTree.
+ * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
+ * (C) 2006 The Jam Warehouse Software (Pty) Ltd;
+ * All Rights Reserved.
+ *
+ */
+
+require_once(KT_LIB_DIR . '/plugins/plugin.inc.php'); 
+
 class KTImmutableActionPlugin extends KTPlugin {
     var $sNamespace = "ktstandard.immutableaction.plugin";
 
