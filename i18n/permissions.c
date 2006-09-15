828860d95907082fc48b032ed30f01beddc4cc3c
bshuttle <bshuttle@c91229c3-7414-0410-bfa2-8a42b809f60b>
2006-09-15 11:43:36 +0000

    Make permissions not use so much space for so little stuff.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@5971 c91229c3-7414-0410-bfa2-8a42b809f60b

--- i18n/permissions.c
+++ i18n/permissions.c
@@ -5,3 +5,11 @@ gettext("Core: Manage security");
 gettext("Core: Delete");
 gettext("Core: Manage workflow");
 gettext("Core: Folder details");
+
+gettext("Read");
+gettext("Write");
+gettext("Add Folder");
+gettext("Manage security");
+gettext("Delete");
+gettext("Manage workflow");
+gettext("Folder details");
---
+++ sql/mysql/upgrade/3.1.6.7/sane-names-for-stuff.sql
@@ -0,0 +1,7 @@
+UPDATE `permissions` SET human_name = 'Read' WHERE human_name = 'Core: Read';
+UPDATE `permissions` SET human_name = 'Write' WHERE human_name = 'Core: Write';
+UPDATE `permissions` SET human_name = 'Add Folder' WHERE human_name = 'Core: Add Folder';
+UPDATE `permissions` SET human_name = 'Manage security' WHERE human_name = 'Core: Manage security';
+UPDATE `permissions` SET human_name = 'Delete' WHERE human_name = 'Core: Delete';
+UPDATE `permissions` SET human_name = 'Manage workflow' WHERE human_name = 'Core: Manage workflow';
+UPDATE `permissions` SET human_name = 'Folder Details' WHERE human_name = 'Core: Folder Details';
