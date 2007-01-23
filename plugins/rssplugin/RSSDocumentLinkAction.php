b90ba449d387b432342268379c42a84f563d024b
Jalaloedien Abrahams <jalal@knowledgetree.com>
2007-01-23 14:30:12 +0000

    KTS-1624 "Allow user to subscribe to rss feed for documents and folders" Implemented.
    Reviewed By: Kevin
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6171 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ plugins/rssplugin/RSSDocumentLinkAction.php
@@ -0,0 +1,31 @@
+<?php
+require_once(KT_LIB_DIR . '/actions/folderaction.inc.php');
+require_once(KT_LIB_DIR . '/permissions/permission.inc.php');
+require_once(KT_LIB_DIR . '/permissions/permissionutil.inc.php');
+require_once(KT_LIB_DIR . '/browse/browseutil.inc.php');
+
+require_once(KT_LIB_DIR . '/plugins/plugin.inc.php');
+require_once(KT_LIB_DIR . '/plugins/pluginregistry.inc.php');
+
+require_once(KT_LIB_DIR . '/roles/Role.inc');
+
+class RSSDocumentLinkAction extends KTDocumentAction {
+    var $sName = 'ktcore.rss.plugin.document.link';
+    var $_sShowPermission = "ktcore.permissions.read";
+    var $sDisplayName = "RSS";
+    
+    function getDisplayName() {
+        // built server path
+        $hostPath = "http://".$_SERVER['HTTP_HOST']."/".$GLOBALS['KTRootUrl']."/";
+        
+        //get document object
+        $oDocument = $this->oDocument;
+        
+        // get document id
+        $iFId = $oDocument->getID();
+        
+        // return link...there MIGHT be a nicer way of doing this?
+        return "<a href='".KTBrowseUtil::buildBaseUrl('rss')."?folderId=".$iFId."' target='_blank'><img src='".$hostPath."resources/graphics/rss.gif' alt='RSS' border=0/></a>";
+    }
+}
+?>
