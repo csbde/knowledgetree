5c3e750eb64b45c7d7b696d95af2c055fe6bd36d
Jalaloedien Abrahams <jalal@knowledgetree.com>
2007-01-25 06:59:34 +0000

    KTS-1638 "RSS feed logo to be changed and action item to be moved" Implemented.
    Reviewed By: Kevin
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6173 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/rssplugin/RSSFolderLinkAction.php
+++ plugins/rssplugin/RSSFolderLinkAction.php
@@ -8,16 +8,14 @@ require_once(KT_LIB_DIR . '/plugins/plugin.inc.php');
 require_once(KT_LIB_DIR . '/plugins/pluginregistry.inc.php');
 
 require_once(KT_LIB_DIR . '/roles/Role.inc');
+require_once(KT_DIR. '/plugins/rssplugin/KTrss.inc.php');
 
 class RSSFolderLinkAction extends KTFolderAction {
     var $sName = 'ktcore.rss.plugin.folder.link';
     var $_sShowPermission = "ktcore.permissions.read";
-    var $sDisplayName = "RSS";
+    var $sDisplayName = 'RSS';
     
     function getDisplayName() {
-        // built server path
-        $hostPath = "http://".$_SERVER['HTTP_HOST']."/".$GLOBALS['KTRootUrl']."/";
-        
         //get folder object
         $oFolder = $this->oFolder;
         
@@ -25,7 +23,26 @@ class RSSFolderLinkAction extends KTFolderAction {
         $iFId = $oFolder->getID();
         
         // return link...there MIGHT be a nicer way of doing this?
-        return "<a href='".KTBrowseUtil::buildBaseUrl('rss')."?folderId=".$iFId."'><img src='".$hostPath."resources/graphics/rss.gif' alt='RSS' border=0/></a>";
+        return "RSS ".KTrss::getImageLink($iFId, 'folder');
+    }
+    
+    function do_main() {
+    	//get folder object
+        $oFolder = $this->oFolder;
+        
+        // get folder id
+        $iFId = $oFolder->getID();
+    	
+    	$oTemplating =& KTTemplating::getSingleton();
+       	$oTemplate = $oTemplating->loadTemplate('RSSPlugin/rssfolderaction');
+       	
+       	$aTemplateData = array(
+			'context' => $this,
+			'link' => KTrss::getRssLink($iFId, 'folder'),
+			'linkIcon' => KTrss::getImageLink($iFId, 'folder'),
+		);
+      
+        return $oTemplate->render($aTemplateData);
     }
 }
 ?>
