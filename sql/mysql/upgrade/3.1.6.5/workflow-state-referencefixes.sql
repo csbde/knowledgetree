621c25b7fcc6fbed5a53654e50feecf17138897b
conradverm <conradverm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-02-08 11:34:36 +0000

    KTS-1533 "Issues when trying to introduce db constraints during upgrades" Fixed. Deleting broken references prior to applying constraints. Reviewed by: Kevin Fourie
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6207 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/3.1.6.5/workflow-state-referencefixes.sql
+++ sql/mysql/upgrade/3.1.6.5/workflow-state-referencefixes.sql
@@ -2,6 +2,13 @@ ALTER TABLE `workflow_state_permission_assignments`
   DROP FOREIGN KEY `workflow_state_permission_assignments_ibfk_7`,
   DROP FOREIGN KEY `workflow_state_permission_assignments_ibfk_8`;
 
+-- CLEAROUT ANY BROKEN RECORDS PRIOR TO ASSIGNING CONSTRAINT
+
+DELETE FROM `workflow_state_permission_assignments` as wspa USING `workflow_state_permission_assignments` as wspa, permissions
+	WHERE not exists(select 1 from `permissions` as p where wspa.permission_id = p.id);        
+
+DELETE FROM `workflow_state_permission_assignments` as wspa USING `workflow_state_permission_assignments` as wspa, permission_descriptors
+	WHERE not exists(select 1 from `permission_descriptors` as pd where wspa.permission_descriptor_id = pd.id);        
 
 ALTER TABLE `workflow_state_permission_assignments`
   ADD CONSTRAINT `workflow_state_permission_assignments_ibfk_7` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE,
