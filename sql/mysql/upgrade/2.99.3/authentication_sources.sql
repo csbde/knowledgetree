6f20534f8ff4c36811b4b42017a2b18e75677949
conradverm <conradverm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-02-08 11:31:38 +0000

    KTS-1533 "Issues when trying to introduce db constraints during upgrades" Fixed. Deleting broken references prior to applying constraints. Reviewed by: Kevin Fourie
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6205 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/2.99.3/authentication_sources.sql
+++ sql/mysql/upgrade/2.99.3/authentication_sources.sql
@@ -4,6 +4,14 @@ ALTER TABLE `users` TYPE=InnoDB;
 ALTER TABLE `users` ADD COLUMN `authentication_details` varchar(255) default NULL;
 ALTER TABLE `users` ADD COLUMN `authentication_source_id` int(11) default NULL;
 ALTER TABLE `users` ADD INDEX `authentication_source` (`authentication_source_id`);
+
+-- CLEAROUT ANY BROKEN RECORDS PRIOR TO ASSIGNING CONSTRAINT
+
+UPDATE users
+    SET authentication_source_id = null 
+    WHERE not exists(select 1 from `authentication_sources` as ass where users.authentication_source_id =ass.id);
+
+
 ALTER TABLE `users` ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`authentication_source_id`) REFERENCES `authentication_sources` (`id`) ON DELETE SET NULL;
 
 CREATE TABLE `authentication_sources` (
