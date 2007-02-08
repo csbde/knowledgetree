ac43fe43f6d02aca99a8328a5ac78b9b64657ed5
Conrad Vermeulen <conrad@knowledgetree.com>
2007-02-08 11:32:31 +0000

    KTS-1533 "Issues when trying to introduce db constraints during upgrades" Fixed. Deleting broken references prior to applying constraints. Reviewed by: Kevin Fourie
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6206 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/2.99.3/document_fields_constraints.sql
+++ sql/mysql/upgrade/2.99.3/document_fields_constraints.sql
@@ -2,6 +2,19 @@ SET FOREIGN_KEY_CHECKS=0;
 ALTER TABLE `document_fields_link` TYPE=InnoDB;
 ALTER TABLE `document_fields_link` ADD INDEX `document_id` (`document_id`);
 ALTER TABLE `document_fields_link` ADD INDEX `document_field_id` (`document_field_id`);
+
+-- CLEAROUT ANY BROKEN RECORDS PRIOR TO ASSIGNING CONSTRAINT
+
+DELETE FROM `document_fields_link` as dfl USING `document_fields_link` as dfl, documents
+	WHERE not exists(select 1 from `documents` as d where dfl.document_id = d.id);        
+
 ALTER TABLE `document_fields_link` ADD CONSTRAINT `document_fields_link_ibfk_1` FOREIGN KEY (`document_id`) REFERENCES `documents` (`id`) ON DELETE CASCADE;
+
+-- CLEAROUT ANY BROKEN RECORDS PRIOR TO ASSIGNING CONSTRAINT
+
+DELETE FROM `document_fields_link` as dfl USING `document_fields_link` as dfl, document_fields
+	WHERE not exists(select 1 from `document_fields` as df where dfl.document_field_id = df.id);  
+        
 ALTER TABLE `document_fields_link` ADD CONSTRAINT `document_fields_link_ibfk_2` FOREIGN KEY (`document_field_id`) REFERENCES `document_fields` (`id`) ON DELETE CASCADE;
+
 SET FOREIGN_KEY_CHECKS=1;
