82f923ae61e57ae5b99239f89308456592c058fc
jalaloedien <jalaloedien@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-02-23 07:17:04 +0000

    KTS-1729 "Copyright comments missing from rss plugin files" Fixed.
    Reviewed By: Conrad
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6318 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/rssplugin/rss2array.inc.php
+++ plugins/rssplugin/rss2array.inc.php
@@ -1,18 +1,30 @@
 <?
 
-    #
-    # rss2array
-    #
-    # example usage:
-    #
-    #       require("inc.rss2array.php");
-    #       $rss_array = rss2array("http://news.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml");
-    #       print "<pre>";
-    #       print_r($rss_array);
-    #       print "</pre>";
-    #
-    # author: dan@freelancers.net
-    #
+/**
+ *
+ * Copyright (c) 2007 Jam Warehouse http://www.jamwarehouse.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; using version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * -------------------------------------------------------------------------
+ *
+ * You can contact the copyright owner regarding licensing via the contact
+ * details that can be found on the KnowledgeTree web site:
+ *
+ *         http://www.knowledgetree.com/
+ */
+    
 
     #
     # global vars
