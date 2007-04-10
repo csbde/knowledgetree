a4c4ee4b2fe108cfe0bcf916a95e79a37e2afab4
Conrad Vermeulen <conrad@knowledgetree.com>
2007-04-10 12:52:40 +0000

    KTS-1694 "SOAP Webservice Implementation" Implemented. These are samples for the WebService Object model.
    Reviewed By: Jalaloedien Abrahams
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6449 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ ktwsapi/examples/ktws_eg_create_folder.php
@@ -0,0 +1,52 @@
+<?
+
+/**
+ *
+ * Demonstrates creating a folder in an active session.
+ *
+ * The contents of this file are subject to the KnowledgeTree Public
+ * License Version 1.1 ("License"); You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.knowledgetree.com/KPL
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ * 
+ * The Original Code is: KnowledgeTree Open Source
+ * 
+ * The Initial Developer of the Original Code is The Jam Warehouse Software
+ * (Pty) Ltd, trading as KnowledgeTree.
+ * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
+ * (C) 2007 The Jam Warehouse Software (Pty) Ltd;
+ * All Rights Reserved.
+ *
+ */
+
+require_once('../ktwsapi.inc.php');
+
+$ktapi = new KTWSAPI(KTWebService_WSDL);
+
+// change session to something that is in table 'active_sessions'
+
+$response = $ktapi->active_session('qekolkpkk9mq2nlc31ghndi1l2');
+if (PEAR::isError($response))
+{
+	print $response->getMessage();
+	exit;
+}
+
+$root = $ktapi->get_root_folder();
+if (PEAR::isError($root))
+{
+	print $root->getMessage();
+	exit;
+}
+
+$folder = $root->add_folder('test folder');
+
+
+
+?>
