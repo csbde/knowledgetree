2348d6ac9c56ffee972d3448073463ab0eca4487
Conrad Vermeulen <conrad@knowledgetree.com>
2007-04-10 12:51:41 +0000

    KTS-1694 "SOAP Webservice Implementation" Implemented. This implements the Web Service Object Model API. Currently, it is in PHP5.
    Reviewed By: Jalaloedien Abrahams
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6448 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ ktwsapi/ktwsapi_cfg.inc.php
@@ -0,0 +1,40 @@
+<?
+
+/**
+ *
+ * This is the basic configuration for the KnowledgeTree WebService object model.
+ *
+ * The contents of this file are subject to the KnowledgeTree Public
+ * License Version 1.1 ("License"); You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.knowledgetree.com/KPL
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ * 
+ * The Original Code is: KnowledgeTree Open Source
+ * 
+ * The Initial Developer of the Original Code is The Jam Warehouse Software
+ * (Pty) Ltd, trading as KnowledgeTree.
+ * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
+ * (C) 2007 The Jam Warehouse Software (Pty) Ltd;
+ * All Rights Reserved.
+ *
+ */
+
+// Path to PEAR libraries
+
+define('KT_PEAR_DIR',					'c:/kt/kt.trunk/thirdparty/pear');
+
+// URL to KnowledgeTree installation
+// This must be configured by the administrator installing this component.
+
+define('KTWebServiceURL',				'http://ktdms.trunk');
+
+define('KTWebService_WSDL',				KTWebServiceURL . '/ktwebservice/?wsdl');
+define('KTUploadURL',					KTWebServiceURL . '/ktwebservice/upload.php');
+
+?>
