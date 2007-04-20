26265685f8970dabb286047b0dace9bc3fd035e3
conradverm <conradverm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-04-20 11:12:01 +0000

    KTS-1892 "Fatal Error occurs when in DMS Administration, Users and Groups, Authentication" Fixed. Bug popped in from the themes.
    Reviewed By: Jalaloedien Abrahams
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6505 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/KTCorePlugin.php
+++ plugins/ktcore/KTCorePlugin.php
@@ -93,7 +93,7 @@ class KTCorePlugin extends KTPlugin {
         $this->registerDashlet('KTIndexerStatusDashlet', 'ktcore.dashlet.indexer_status', 'KTDashlets.php');
         $this->registerDashlet('KTMailServerDashlet', 'ktcore.dashlet.mail_server', 'KTDashlets.php');
 
-        $this->registerAdminPage('authentication', 'KTAuthenticationAdminPage', 'principals', _kt('Authentication'), sprintf(_kt('By default, %s controls its own users and groups and stores all information about them inside the database. In many situations, an organisation will already have a list of users and groups, and needs to use that existing information to allow access to the DMS.   These <strong>Authentication Sources</strong> allow the system administrator to  specify additional sources of authentication data.'), 'authentication/authenticationadminpage.inc.php'), APP_NAME);
+        $this->registerAdminPage('authentication', 'KTAuthenticationAdminPage', 'principals', _kt('Authentication'), sprintf(_kt('By default, %s controls its own users and groups and stores all information about them inside the database. In many situations, an organisation will already have a list of users and groups, and needs to use that existing information to allow access to the DMS.   These <strong>Authentication Sources</strong> allow the system administrator to  specify additional sources of authentication data.'), APP_NAME), 'authentication/authenticationadminpage.inc.php');
 
         $this->registerPortlet(array('browse'),
                 'KTAdminModePortlet', 'ktcore.portlets.admin_mode',
