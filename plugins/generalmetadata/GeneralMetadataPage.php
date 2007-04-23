45e01e09ced579a025ff69a3cc63c35a6fe7d4a5
conradverm <conradverm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-04-23 14:52:00 +0000

    KTS-1725 "License version is incorrect" Corrected.
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6531 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/generalmetadata/GeneralMetadataPage.php
+++ plugins/generalmetadata/GeneralMetadataPage.php
@@ -1,4 +1,27 @@
 <?php
+
+/*
+ * The contents of this file are subject to the KnowledgeTree Public
+ * License Version 1.1 ("License"); You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.knowledgetree.com/KPL
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ * 
+ * The Original Code is: KnowledgeTree Open Source
+ * 
+ * The Initial Developer of the Original Code is The Jam Warehouse Software
+ * (Pty) Ltd, trading as KnowledgeTree.
+ * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
+ * (C) 2007 The Jam Warehouse Software (Pty) Ltd;
+ * All Rights Reserved.
+ *
+ */
+ 
 require_once(KT_LIB_DIR . '/plugins/plugin.inc.php');
 require_once(KT_LIB_DIR . '/plugins/pluginregistry.inc.php');
 require_once(KT_LIB_DIR . "/templating/templating.inc.php");
