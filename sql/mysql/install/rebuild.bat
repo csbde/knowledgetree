a6d2208747b9c0e602fb19b86712ddfc5921e00f
Jalaloedien Abrahams <jalal@knowledgetree.com>
2007-05-14 14:05:56 +0000

    KTS-1958 "Make \sql\mysql\install\rebuild.bat to take in parameters for database name. " Implemented.
    Reviewed By: Conrad
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6567 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/install/rebuild.bat
+++ sql/mysql/install/rebuild.bat
@@ -1,5 +1,21 @@
-mysqladmin -u root -p -f drop dms
-mysqladmin -u root -p create dms
-mysql -u root dms<structure.sql
-mysql -u root -p dms< data.sql
-mysql -u root -p dms<user.sql
+@ECHO OFF
+CLS
+ECHO.
+FOR /F "tokens=5* delims= " %%A IN ('VOL C: ^| FIND "drive C"') DO SET OLDLABEL=%%B
+ECHO Enter name of database (default is dms):
+FOR /F "TOKENS=*" %%? IN ('LABEL C: 2^>NUL') DO SET INPUT=%%?
+SET INPUT
+CLS
+LABEL C: %OLDLABEL%
+PAUSE
+CLS
+ECHO ---- Dropping database %INPUT% ----
+mysqladmin -u root -p -f drop %INPUT%
+ECHO ---- Creating database %INPUT% ----
+mysqladmin -u root -p create %INPUT%
+ECHO ---- Creating structure for database %INPUT% ----
+mysql -u root %INPUT%<structure.sql
+ECHO ---- Inserting data into database %INPUT% ----
+mysql -u root -p %INPUT%< data.sql
+ECHO ---- Creating user information for database %INPUT% ----
+mysql -u root -p %INPUT%<user.sql
