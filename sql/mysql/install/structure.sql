55c0fc24e8cc348880fb31b73e883b2a34f8d7c3
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-05-30 10:08:30 +0000

    KTC-95 "Update License Headers" Updated and added some missing headers.
    Committed By: Kevin
    Reviewed By: Conrad
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6606 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/install/structure.sql
+++ sql/mysql/install/structure.sql
@@ -1,11 +1,31 @@
--- phpMyAdmin SQL Dump
--- version 2.8.0.3-Debian-1
--- http://www.phpmyadmin.net
+--
+-- $Id$
+--    
+-- The contents of this file are subject to the KnowledgeTree Public
+-- License Version 1.1.2 ("License"); You may not use this file except in
+-- compliance with the License. You may obtain a copy of the License at
+-- http://www.knowledgetree.com/KPL
 -- 
--- Host: localhost
--- Generation Time: Oct 30, 2006 at 12:50 PM
--- Server version: 5.0.22
--- PHP Version: 4.4.2-1build1
+-- Software distributed under the License is distributed on an "AS IS"
+-- basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+-- See the License for the specific language governing rights and
+-- limitations under the License.
+--
+-- All copies of the Covered Code must include on each user interface screen:
+--    (i) the "Powered by KnowledgeTree" logo and
+--    (ii) the KnowledgeTree copyright notice
+-- in the same form as they appear in the distribution.  See the License for
+-- requirements.
+-- 
+-- The Original Code is: KnowledgeTree Open Source
+-- 
+-- The Initial Developer of the Original Code is The Jam Warehouse Software
+-- (Pty) Ltd, trading as KnowledgeTree.
+-- Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
+-- (C) 2007 The Jam Warehouse Software (Pty) Ltd;
+-- All Rights Reserved.
+-- Contributor( s): ______________________________________
+--
 
 SET FOREIGN_KEY_CHECKS=0;
 -- 
