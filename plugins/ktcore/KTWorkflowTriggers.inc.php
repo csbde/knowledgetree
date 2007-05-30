4edb8f135634d16a1f41a115a260bcf67a663c3c
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-05-30 11:18:21 +0000

    Merged in from DEV trunk...
    KTC-95
    "Update License Headers"
    Updated and added some missing headers.
    
    Committed By: Kevin
    Reviewed By: Conrad
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/STABLE/trunk@6607 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/KTWorkflowTriggers.inc.php
+++ plugins/ktcore/KTWorkflowTriggers.inc.php
@@ -4,15 +4,20 @@
  * $Id: KTWorkflowTriggers.php 5439 2006-05-25 13:20:15Z bryndivey $
  *
  * The contents of this file are subject to the KnowledgeTree Public
- * License Version 1.1 ("License"); You may not use this file except in
+ * License Version 1.1.2 ("License"); You may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.knowledgetree.com/KPL
  * 
  * Software distributed under the License is distributed on an "AS IS"
- * basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * See the License for the specific language governing rights and
+ * limitations under the License.
+ *
+ * All copies of the Covered Code must include on each user interface screen:
+ *    (i) the "Powered by KnowledgeTree" logo and
+ *    (ii) the KnowledgeTree copyright notice
+ * in the same form as they appear in the distribution.  See the License for
+ * requirements.
  * 
  * The Original Code is: KnowledgeTree Open Source
  * 
@@ -21,6 +26,7 @@
  * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
  * (C) 2007 The Jam Warehouse Software (Pty) Ltd;
  * All Rights Reserved.
+ * Contributor( s): ______________________________________
  *
  */
 
