c96d60a1560d3d0be4a1be9aeea01c0ce6769187
Kevin Fourie <kevin@knowledgetree.com>
2007-05-30 10:08:30 +0000

    KTC-95 "Update License Headers" Updated and added some missing headers.
    Committed By: Kevin
    Reviewed By: Conrad
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6606 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/admin/workflowsv2.php
+++ plugins/ktcore/admin/workflowsv2.php
@@ -1,4 +1,32 @@
 <?php
+/**
+ * $Id: view.php 6584 2007-05-23 13:43:15Z kevin_fourie $
+ *    
+ * The contents of this file are subject to the KnowledgeTree Public
+ * License Version 1.1.2 ("License"); You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.knowledgetree.com/KPL
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * See the License for the specific language governing rights and
+ * limitations under the License.
+ *
+ * All copies of the Covered Code must include on each user interface screen:
+ *    (i) the "Powered by KnowledgeTree" logo and
+ *    (ii) the KnowledgeTree copyright notice
+ * in the same form as they appear in the distribution.  See the License for
+ * requirements.
+ * 
+ * The Original Code is: KnowledgeTree Open Source
+ * 
+ * The Initial Developer of the Original Code is The Jam Warehouse Software
+ * (Pty) Ltd, trading as KnowledgeTree.
+ * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
+ * (C) 2007 The Jam Warehouse Software (Pty) Ltd;
+ * All Rights Reserved.
+ * Contributor( s): ______________________________________
+ */
 
 // core
 require_once(KT_LIB_DIR . "/dispatcher.inc.php");
