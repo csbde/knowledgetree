bcd6d1e3001eae0707073637c274895c2bed829e
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-06-05 16:05:54 +0000

    KTS-2015 "Change license of ktwsapi to BSD licence" Updated PHP code.
    Committed By: Kevin
    Reviewed By: Conrad
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6687 c91229c3-7414-0410-bfa2-8a42b809f60b

--- ktwsapi/php/examples/ktws_eg_create_folder.php
+++ ktwsapi/php/examples/ktws_eg_create_folder.php
@@ -1,34 +1,32 @@
 <?
-
 /**
+ * Copyright (c) 2007, The Jam Warehouse Software (Pty) Ltd.
  *
- * Demonstrates creating a folder in an active session.
+ * All rights reserved.
  *
- * The contents of this file are subject to the KnowledgeTree Public
- * License Version 1.1.2 ("License"); You may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.knowledgetree.com/KPL
- * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
- * See the License for the specific language governing rights and
- * limitations under the License.
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
  *
- * All copies of the Covered Code must include on each user interface screen:
- *    (i) the "Powered by KnowledgeTree" logo and
- *    (ii) the KnowledgeTree copyright notice
- * in the same form as they appear in the distribution.  See the License for
- * requirements.
- * 
- * The Original Code is: KnowledgeTree Open Source
- * 
- * The Initial Developer of the Original Code is The Jam Warehouse Software
- * (Pty) Ltd, trading as KnowledgeTree.
- * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
- * (C) 2007 The Jam Warehouse Software (Pty) Ltd;
- * All Rights Reserved.
- * Contributor( s): ______________________________________
+ *    i) Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *   ii) Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *  iii) Neither the name of the The Jam Warehouse Software (Pty) Ltd nor the 
+ *       names of its contributors may be used to endorse or promote products 
+ *       derived from this software without specific prior written permission.
  *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES ( INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT ( INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 require_once('../ktwsapi.inc.php');
