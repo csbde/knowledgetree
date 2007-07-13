543f5be338111e7f2017fd094ab3187458d1d443
Conrad Vermeulen <conrad@knowledgetree.com>
2007-07-13 14:46:50 +0000

    KTS-2178 "cross site scripting" Updated.
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@6914 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/browse/BrowseColumns.inc.php
+++ lib/browse/BrowseColumns.inc.php
@@ -6,7 +6,7 @@
  * License Version 1.1.2 ("License"); You may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.knowledgetree.com/KPL
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * See the License for the specific language governing rights and
@@ -17,9 +17,9 @@
  *    (ii) the KnowledgeTree copyright notice
  * in the same form as they appear in the distribution.  See the License for
  * requirements.
- * 
+ *
  * The Original Code is: KnowledgeTree Open Source
- * 
+ *
  * The Initial Developer of the Original Code is The Jam Warehouse Software
  * (Pty) Ltd, trading as KnowledgeTree.
  * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
@@ -30,17 +30,17 @@
  */
 
 /** BrowserColumns
- * 
+ *
  *  Presentation and render logic for the different columns.  Each has two
  *  major methods:
  *
  *     function renderHeader($sReturnURL)
  *     function renderData($aDataRow)
- *  
+ *
  *  renderHeader returns the _content_ of the header row.
  *  renderData returns the _content_ of the body row.
  */
- 
+
 require_once(KT_LIB_DIR . '/database/dbutil.inc');
 require_once(KT_LIB_DIR . '/users/User.inc');
 
@@ -53,36 +53,36 @@ class BrowseColumn {
     var $sort_on = false;
     var $sort_direction = 'asc';
     var $name = '-';
-    
-    function BrowseColumn($sLabel, $sName) { 
-       $this->label = $sLabel; 
-       $this->name = $sName; 
+
+    function BrowseColumn($sLabel, $sName) {
+       $this->label = $sLabel;
+       $this->name = $sName;
     }
     // FIXME is it _really_ worth using a template here?
-    function renderHeader($sReturnURL) { 
-        $text = _kt('Abstract') . ': ' . $this->label; 
+    function renderHeader($sReturnURL) {
+        $text = _kt('Abstract') . ': ' . $this->label;
         $href = $sReturnURL . '&sort_on=' . $this->name . '&sort_order=';
         if ($this->sort_on) {
             $href .= $this->sort_direction == 'asc' ? 'desc' : 'asc' ;
         } else {
             $href .= $this->sort_direction = 'asc';
         }
-        
-        return '<a href="' . $href . '">'.$text.'</a>';        
+
+        return '<a href="' . $href . '">'.$text.'</a>';
     }
-    
-    function renderData($aDataRow) { 
+
+    function renderData($aDataRow) {
        if ($aDataRow['type'] == 'folder') {
-           return $this->name . ': '. print_r($aDataRow['folder']->getName(), true);            
+           return $this->name . ': '. print_r($aDataRow['folder']->getName(), true);
         } else {
-           return $this->name . ': '. print_r($aDataRow['document']->getName(), true); 
+           return $this->name . ': '. print_r($aDataRow['document']->getName(), true);
         }
     }
     function setSortedOn($bIsSortedOn) { $this->sort_on = $bIsSortedOn; }
     function getSortedOn() { return $this->sort_on; }
     function setSortDirection($sSortDirection) { $this->sort_direction = $sSortDirection; }
     function getSortDirection() { return $this->sort_direction; }
-    
+
     function addToFolderQuery() { return array(null, null, null); }
     function addToDocumentQuery() { return array(null, null, null); }
 }
@@ -95,7 +95,7 @@ class TitleColumn extends BrowseColumn {
         $this->aOptions = $aOptions;
     }
     // unlike others, this DOESN'T give its name.
-    function renderHeader($sReturnURL) { 
+    function renderHeader($sReturnURL) {
         $text = _kt('Title');
         $href = $sReturnURL . '&sort_on=' . $this->name . '&sort_order=';
         if ($this->sort_on) {
@@ -103,9 +103,9 @@ class TitleColumn extends BrowseColumn {
         } else {
             $href .= $this->sort_direction = 'asc';
         }
-        
+
         return '<a href="' . $href . '">'.$text.'</a>';
-        
+
     }
 
     function renderFolderLink($aDataRow) {
@@ -116,7 +116,7 @@ class TitleColumn extends BrowseColumn {
     }
 
     function renderDocumentLink($aDataRow) {
-        $outStr = '<a href="' . $this->buildDocumentLink($aDataRow) . '" title="' . $aDataRow['document']->getFilename().'">';
+        $outStr = '<a href="' . $this->buildDocumentLink($aDataRow) . '" title="' . htmlentities($aDataRow['document']->getFilename(), ENT_NOQUOTES, 'UTF-8').'">';
         $outStr .= htmlentities($aDataRow['document']->getName(), ENT_NOQUOTES, 'UTF-8');
         $outStr .= '</a>';
         return $outStr;
@@ -133,14 +133,14 @@ class TitleColumn extends BrowseColumn {
             return KTBrowseUtil::getUrlForFolder($aDataRow['folder']);
         }
     }
-    
+
     // use inline, since its just too heavy to even _think_ about using smarty.
-    function renderData($aDataRow) { 
+    function renderData($aDataRow) {
        $outStr = '';
        if ($aDataRow['type'] == 'folder') {
            $outStr .= '<span class="contenttype folder">';
            $outStr .= $this->renderFolderLink($aDataRow);
-           $outStr .= '</span>';           
+           $outStr .= '</span>';
         } else {
            $outStr .= '<span class="contenttype '.$this->_mimeHelper($aDataRow['document']->getMimeTypeId()).'">';
            $outStr .= $this->renderDocumentLink($aDataRow);
@@ -149,11 +149,11 @@ class TitleColumn extends BrowseColumn {
         }
         return $outStr;
     }
-    
+
     function prettySize($size) {
         $finalSize = $size;
         $label = 'b';
-        
+
         if ($finalSize > 1000) { $label='Kb'; $finalSize = floor($finalSize/1000); }
         if ($finalSize > 1000) { $label='Mb'; $finalSize = floor($finalSize/1000); }
         return $finalSize . $label;
@@ -169,15 +169,15 @@ class TitleColumn extends BrowseColumn {
 
 class DateColumn extends BrowseColumn {
     var $field_function;
-    
+
     // $sDocumentFieldFunction is _called_ on the document.
     function DateColumn($sLabel, $sName, $sDocumentFieldFunction) {
         $this->field_function = $sDocumentFieldFunction;
         parent::BrowseColumn($sLabel, $sName);
-        
+
     }
-    
-    function renderHeader($sReturnURL) { 
+
+    function renderHeader($sReturnURL) {
         $text = $this->label;
         $href = $sReturnURL . '&sort_on=' . $this->name . '&sort_order=';
         if ($this->sort_on) {
@@ -185,31 +185,31 @@ class DateColumn extends BrowseColumn {
         } else {
             $href .= $this->sort_direction = 'asc';
         }
-        
+
         return '<a href="' . $href . '">'.$text.'</a>';
-        
+
     }
-    
+
     // use inline, since its just too heavy to even _think_ about using smarty.
-    function renderData($aDataRow) { 
+    function renderData($aDataRow) {
        $outStr = '';
        if ($aDataRow['type'] == 'folder') {
            $outStr = '&nbsp;';       // no-op on folders.
         } else {
            $fn = $this->field_function;
            $dColumnDate = strtotime($aDataRow['document']->$fn());
-           
+
            // now reformat this into something "pretty"
            $outStr = date('Y-m-d H:i', $dColumnDate);
         }
         return $outStr;
     }
-    
+
     function _mimeHelper($iMimeTypeId) {
         // FIXME lazy cache this.
         $sQuery = 'SELECT icon_path FROM mime_types WHERE id = ?';
         $res = DBUtil::getOneResult(array($sQuery, array($iMimeTypeId)));
-        
+
         if ($res[0] !== null) {
            return $res[0];
         } else {
@@ -228,15 +228,15 @@ class DateColumn extends BrowseColumn {
 
 class UserColumn extends BrowseColumn {
     var $field_function;
-    
+
     // $sDocumentFieldFunction is _called_ on the document.
     function UserColumn($sLabel, $sName, $sDocumentFieldFunction) {
         $this->field_function = $sDocumentFieldFunction;
         parent::BrowseColumn($sLabel, $sName);
-        
+
     }
-    
-    function renderHeader($sReturnURL) { 
+
+    function renderHeader($sReturnURL) {
         $text = $this->label;
         $href = $sReturnURL . '&sort_on=' . $this->name . '&sort_order=';
         if ($this->sort_on) {
@@ -244,13 +244,13 @@ class UserColumn extends BrowseColumn {
         } else {
             $href .= $this->sort_direction = 'asc';
         }
-        
+
         return '<a href="' . $href . '">'.$text.'</a>';
-        
+
     }
-    
+
     // use inline, since its just too heavy to even _think_ about using smarty.
-    function renderData($aDataRow) { 
+    function renderData($aDataRow) {
         $outStr = '';
         $fn = $this->field_function;
         $iUserId = null;
@@ -294,36 +294,36 @@ class SelectionColumn extends BrowseColumn {
         parent::BrowseColumn($sLabel, $sName);
     }
 
-    function renderHeader($sReturnURL) { 
+    function renderHeader($sReturnURL) {
         // FIXME clean up access to oPage.
         global $main;
         $main->requireJSResource('resources/js/toggleselect.js');
-        
+
         return '<input type="checkbox" title="toggle all" onclick="toggleSelectFor(this, \''.$this->name.'\')">';
-        
+
     }
-    
+
     // only include the _f or _d IF WE HAVE THE OTHER TYPE.
-    function renderData($aDataRow) { 
+    function renderData($aDataRow) {
         $localname = $this->name;
-        
-        if (($aDataRow['type'] === 'folder') && ($this->show_folders)) { 
+
+        if (($aDataRow['type'] === 'folder') && ($this->show_folders)) {
             if ($this->show_documents) {
-                $localname .= '_f[]'; 
+                $localname .= '_f[]';
             }
-            $v = $aDataRow['folderid']; 
-        } else if (($aDataRow['type'] === 'document') && $this->show_documents) { 
+            $v = $aDataRow['folderid'];
+        } else if (($aDataRow['type'] === 'document') && $this->show_documents) {
             if ($this->show_folders) {
-                $localname .= '_d[]'; 
+                $localname .= '_d[]';
             }
-            $v = $aDataRow['docid']; 
-        } else { 
-            return '&nbsp;'; 
+            $v = $aDataRow['docid'];
+        } else {
+            return '&nbsp;';
         }
-        
+
         return '<input type="checkbox" name="' . $localname . '" onclick="activateRow(this)" value="' . $v . '"/>';
     }
-    
+
 }
 
 
@@ -337,58 +337,58 @@ class SingleSelectionColumn extends SelectionColumn {
         parent::BrowseColumn($sLabel, $sName);
     }
 
-    function renderHeader($sReturnURL) { 
+    function renderHeader($sReturnURL) {
         global $main;
     }
-    
+
     // only include the _f or _d IF WE HAVE THE OTHER TYPE.
-    function renderData($aDataRow) { 
+    function renderData($aDataRow) {
         $localname = $this->name;
-        
-        if (($aDataRow['type'] === 'folder') && ($this->show_folders)) { 
+
+        if (($aDataRow['type'] === 'folder') && ($this->show_folders)) {
             if ($this->show_documents) {
-                $localname .= '_f'; 
+                $localname .= '_f';
             }
-            $v = $aDataRow['folderid']; 
-        } else if (($aDataRow['type'] === 'document') && $this->show_documents) { 
+            $v = $aDataRow['folderid'];
+        } else if (($aDataRow['type'] === 'document') && $this->show_documents) {
             if ($this->show_folders) {
-                $localname .= '_d'; 
+                $localname .= '_d';
             }
-            $v = $aDataRow['docid']; 
-        } else { 
-            return '&nbsp;'; 
+            $v = $aDataRow['docid'];
+        } else {
+            return '&nbsp;';
         }
-        
+
         return '<input type="radio" name="' . $localname . '" value="' . $v . '"/>';
     }
-    
+
 }
 
 
 class WorkflowColumn extends BrowseColumn {
 
-    function renderHeader($sReturnURL) {         
-        $text = $this->label; 
+    function renderHeader($sReturnURL) {
+        $text = $this->label;
         $href = $sReturnURL . '&sort_on=' . $this->name . '&sort_order=';
         if ($this->sort_on) {
             $href .= $this->sort_direction == 'asc' ? 'desc' : 'asc' ;
         } else {
             $href .= $this->sort_direction = 'asc';
         }
-        
+
         return '<a href="' . $href . '">'.$text.'</a>';
     }
-    
+
     // use inline, since its just too heavy to even _think_ about using smarty.
-    function renderData($aDataRow) { 
+    function renderData($aDataRow) {
         $localname = $this->name;
 
-        
+
         // only _ever_ show this folder documents.
-        if ($aDataRow['type'] === 'folder') { 
+        if ($aDataRow['type'] === 'folder') {
             return '&nbsp;';
         }
-        
+
         $oWorkflow = KTWorkflowUtil::getWorkflowForDocument($aDataRow['document']);
         $oState = KTWorkflowUtil::getWorkflowStateForDocument($aDataRow['document']);
         if (($oState == null) || ($oWorkflow == null)) {
@@ -400,23 +400,23 @@ class WorkflowColumn extends BrowseColumn {
 }
 
 class DownloadColumn extends BrowseColumn {
-    
-    function renderHeader($sReturnURL) {         
-        $text = '&nbsp;'; 
-        
+
+    function renderHeader($sReturnURL) {
+        $text = '&nbsp;';
+
         return $text;
     }
-    
 
-    function renderData($aDataRow) { 
+
+    function renderData($aDataRow) {
         $localname = $this->name;
 
-        
+
         // only _ever_ show this folder documents.
-        if ($aDataRow['type'] === 'folder') { 
+        if ($aDataRow['type'] === 'folder') {
             return '&nbsp;';
         }
-    
+
         // FIXME at some point we may want to hide this if the user doens't have the download action, but its OK for now.
         $link = KTUtil::ktLink('action.php','ktcore.actions.document.view', 'fDocumentId=' . $aDataRow['document']->getId());
         $outStr = sprintf('<a href="%s" class="ktAction ktDownload" title="%s">%s</a>', $link, _kt('Download Document'), _kt('Download Document'));
