c5e4f1175cb8aa2a3311c948c6f5cbf4ade72823
megan_w <megan_w@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-09-17 09:02:58 +0000

    KTS-1554 "Implement a Scheduler" Added the sql for creating the table containing the tasks
    Committed by: Megan Watson
    Reviewed by: Conrad Vermeulen
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7138 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/install/structure.sql
+++ sql/mysql/install/structure.sql
@@ -1167,6 +1167,26 @@ CREATE TABLE `saved_searches` (
 -- --------------------------------------------------------
 
 -- 
+-- Table structure for table `scheduler_tasks`
+-- 
+
+CREATE TABLE `scheduler_tasks` (
+  `id` int(11) NOT NULL default '0',
+  `task` varchar(50),
+  `script_url` varchar(255),
+  `script_params` varchar(255),
+  `on_completion` varchar(255),
+  `is_background` tinyint(4) NOT NULL default '0',
+  `is_complete` tinyint(4) NOT NULL default '0',
+  `frequency` varchar(25),
+  `run_time` int(11),
+  `previous_run_time` int(11),
+  `run_duration` float
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `search_document_user_link`
 -- 
 
@@ -2185,6 +2205,17 @@ CREATE TABLE `zseq_saved_searches` (
   `id` int(10) unsigned NOT NULL auto_increment,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `zseq_saved_searches`
+-- 
+
+CREATE TABLE `zseq_scheduler_tasks` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
