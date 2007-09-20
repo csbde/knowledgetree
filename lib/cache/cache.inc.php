4cbdd87e32d407f57b8942208b7014f0d4b8379b
Conrad Vermeulen <conrad@knowledgetree.com>
2007-09-20 14:32:30 +0000

    KTS-2358 "php5 migration" Updated. Removed & from &new. Added statics.
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7174 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/cache/cache.inc.php
+++ lib/cache/cache.inc.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * $Id$
- *    
+ *
  * The contents of this file are subject to the KnowledgeTree Public
  * License Version 1.1.2 ("License"); You may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.knowledgetree.com/KPL
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * See the License for the specific language governing rights and
@@ -17,9 +17,9 @@
  *    (ii) the KnowledgeTree copyright notice
  * in the same form as they appear in the distribution.  See the License for
  * requirements.
- * 
+ *
  * The Original Code is: KnowledgeTree Open Source
- * 
+ *
  * The Initial Developer of the Original Code is The Jam Warehouse Software
  * (Pty) Ltd, trading as KnowledgeTree.
  * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
@@ -32,11 +32,13 @@ class KTCache {
     var $aRollbackList = array();
 
     // {{{ getSingleton
-    function &getSingleton () {
-        if (!KTUtil::arrayGet($GLOBALS, 'oKTCache')) {
-            $GLOBALS['oKTCache'] = new KTCache;
-        }
-        return $GLOBALS['oKTCache'];
+    static function &getSingleton () {
+    	static $singleton = null;
+    	if (is_null($singleton))
+    	{
+    		$singleton = new KTCache();
+    	}
+    	return $singleton;
     }
     // }}}
 
@@ -73,7 +75,7 @@ class KTCache {
 
         $this->cacheDir = $aOptions['cacheDir'];
 
-        $this->oLite =& new Cache_Lite($aOptions);
+        $this->oLite = new Cache_Lite($aOptions);
     }
 
     function get($group, $id) {
