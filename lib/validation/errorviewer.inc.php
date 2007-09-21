ed9fbd5c6197cc33a858c0d4c77468462c9ed52a
Conrad Vermeulen <conrad@knowledgetree.com>
2007-09-21 07:34:08 +0000

    KTS-2358 "php5 migration" Updated. Removed & from &new. Added statics.
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7194 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/validation/errorviewer.inc.php
+++ lib/validation/errorviewer.inc.php
@@ -6,7 +6,7 @@
  * License Version 1.1.2 ("License"); You may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.knowledgetree.com/KPL
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * See the License for the specific language governing rights and
@@ -17,9 +17,9 @@
  *    (ii) the KnowledgeTree copyright notice
  * in the same form as they appear in the distribution.  See the License for
  * requirements.
- * 
+ *
  * The Original Code is: KnowledgeTree Open Source
- * 
+ *
  * The Initial Developer of the Original Code is The Jam Warehouse Software
  * (Pty) Ltd, trading as KnowledgeTree.
  * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
@@ -32,13 +32,13 @@
 class KTErrorViewerRegistry {
     var $aViewers = array();
 
-    function &getSingleton() {
-        $oRegistry =& KTUtil::arrayGet($GLOBALS, 'KTErrorViewerRegistry');
-        if ($oRegistry) {
-            return $oRegistry;
-        }
-        $GLOBALS['KTErrorViewerRegistry'] =& new KTErrorViewerRegistry();
-        return $GLOBALS['KTErrorViewerRegistry'];
+    static function &getSingleton() {
+    	static $singleton = null;
+    	if (is_null($singleton))
+    	{
+    		$singleton = new KTErrorViewerRegistry();
+    	}
+    	return $singleton;
     }
 
     function register($sViewerClassName, $sHandledClass) {
@@ -166,7 +166,7 @@ class KTStringErrorViewer extends KTErrorViewer {
     function view() {
         return $this->oError;
     }
-    
+
     function viewFull() {
         return $this->oError;
     }
