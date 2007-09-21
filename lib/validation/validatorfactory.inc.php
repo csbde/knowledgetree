f3ac8162cc3addf0519e0eecd2f9c2df32a9f48b
conradverm <conradverm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-09-21 07:34:08 +0000

    KTS-2358 "php5 migration" Updated. Removed & from &new. Added statics.
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7194 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/validation/validatorfactory.inc.php
+++ lib/validation/validatorfactory.inc.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * $Id$
- *    
+ *
  * The contents of this file are subject to the KnowledgeTree Public
  * License Version 1.1.2 ("License"); You may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.knowledgetree.com/KPL
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * See the License for the specific language governing rights and
@@ -17,9 +17,9 @@
  *    (ii) the KnowledgeTree copyright notice
  * in the same form as they appear in the distribution.  See the License for
  * requirements.
- * 
+ *
  * The Original Code is: KnowledgeTree Open Source
- * 
+ *
  * The Initial Developer of the Original Code is The Jam Warehouse Software
  * (Pty) Ltd, trading as KnowledgeTree.
  * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
@@ -37,13 +37,15 @@
 class KTValidatorFactory {
     var $validators = array();
 
-    function &getSingleton() {
-        if (!KTUtil::arrayGet($GLOBALS['_KT_PLUGIN'], 'oKTValidatorFactory')) {
-            $GLOBALS['_KT_PLUGIN']['oKTValidatorFactory'] = new KTValidatorFactory;
-        }
-        return $GLOBALS['_KT_PLUGIN']['oKTValidatorFactory'];    
+    static function &getSingleton () {
+		static $singleton=null;
+    	if (is_null($singleton))
+    	{
+    		$singleton = new KTValidatorFactory();
+    	}
+    	return $singleton;
     }
-    
+
     function registerValidator($sClassname, $sNamespace,  $sFilename = null) {
         $this->validators[$sNamespace] = array(
             'ns' => $sNamespace,
@@ -51,7 +53,7 @@ class KTValidatorFactory {
             'file' => $sFilename,
         );
     }
-    
+
     function &getValidatorByNamespace($sNamespace) {
         $aInfo = KTUtil::arrayGet($this->validators, $sNamespace);
         if (empty($aInfo)) {
@@ -60,30 +62,30 @@ class KTValidatorFactory {
         if (!empty($aInfo['file'])) {
             require_once($aInfo['file']);
         }
-        
+
         return new $aInfo['class'];
-    }    
-    
+    }
+
     // this is overridden to either take a namespace or an instantiated
     // class.  Doing it this way allows for a consistent approach to building
-    // forms including custom widgets. 
+    // forms including custom widgets.
     function &get($namespaceOrObject, $aConfig = null) {
         if (is_string($namespaceOrObject)) {
             $oValidator =& $this->getValidatorByNamespace($namespaceOrObject);
         } else {
             $oValidator = $namespaceOrObject;
         }
-        
+
         if (PEAR::isError($oValidator)) {
             return $oValidator;
         }
-        
+
         $aConfig = (array) $aConfig; // always an array
         $res = $oValidator->configure($aConfig);
         if (PEAR::isError($res)) {
             return $res;
         }
-        
+
         return $oValidator;
     }
 }
