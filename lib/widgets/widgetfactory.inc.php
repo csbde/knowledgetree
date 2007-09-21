e78f5e6197e802bdc3e3643756625829aa70fd00
conradverm <conradverm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-09-21 07:36:26 +0000

    KTS-2358 "php5 migration" Updated. Removed & from &new. Added statics.
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7195 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/widgets/widgetfactory.inc.php
+++ lib/widgets/widgetfactory.inc.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * $Id$
- *    
+ *
  * The contents of this file are subject to the KnowledgeTree Public
  * License Version 1.1.2 ("License"); You may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.knowledgetree.com/KPL
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * See the License for the specific language governing rights and
@@ -17,9 +17,9 @@
  *    (ii) the KnowledgeTree copyright notice
  * in the same form as they appear in the distribution.  See the License for
  * requirements.
- * 
+ *
  * The Original Code is: KnowledgeTree Open Source
- * 
+ *
  * The Initial Developer of the Original Code is The Jam Warehouse Software
  * (Pty) Ltd, trading as KnowledgeTree.
  * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
@@ -38,13 +38,15 @@
 class KTWidgetFactory {
     var $widgets = array();
 
-    function &getSingleton() {
-        if (!KTUtil::arrayGet($GLOBALS['_KT_PLUGIN'], 'oKTWidgetFactory')) {
-            $GLOBALS['_KT_PLUGIN']['oKTWidgetFactory'] = new KTWidgetFactory;
-        }
-        return $GLOBALS['_KT_PLUGIN']['oKTWidgetFactory'];    
+    static function &getSingleton () {
+		static $singleton=null;
+    	if (is_null($singleton))
+    	{
+    		$singleton = new KTWidgetFactory();
+    	}
+    	return $singleton;
     }
-    
+
     function registerWidget($sClassname, $sNamespace,  $sFilename = null) {
         $this->widgets[$sNamespace] = array(
             'ns' => $sNamespace,
@@ -52,7 +54,7 @@ class KTWidgetFactory {
             'file' => $sFilename,
         );
     }
-    
+
     function &getWidgetByNamespace($sNamespace) {
         $aInfo = KTUtil::arrayGet($this->widgets, $sNamespace);
         if (empty($aInfo)) {
@@ -61,30 +63,30 @@ class KTWidgetFactory {
         if (!empty($aInfo['file'])) {
             require_once($aInfo['file']);
         }
-        
+
         return new $aInfo['class'];
-    }    
-    
+    }
+
     // this is overridden to either take a namespace or an instantiated
     // class.  Doing it this way allows for a consistent approach to building
-    // forms including custom widgets. 
+    // forms including custom widgets.
     function &get($namespaceOrObject, $aConfig = null) {
         if (is_string($namespaceOrObject)) {
             $oWidget =& $this->getWidgetByNamespace($namespaceOrObject);
         } else {
             $oWidget = $namespaceOrObject;
         }
-        
+
         if (PEAR::isError($oWidget)) {
             return $oWidget;
         }
-        
+
         $aConfig = (array) $aConfig; // always an array
         $res = $oWidget->configure($aConfig);
         if (PEAR::isError($res)) {
             return $res;
         }
-        
+
         return $oWidget;
     }
 }
