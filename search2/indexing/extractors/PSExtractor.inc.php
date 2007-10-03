444cef1597a94f7fb376c2daac3d53a71176579e
conradverm <conradverm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-10-03 20:12:06 +0000

    KTS-673 "The search algorithm needs some work" Updated. Internationalisation
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7288 c91229c3-7414-0410-bfa2-8a42b809f60b

--- search2/indexing/extractors/PSExtractor.inc.php
+++ search2/indexing/extractors/PSExtractor.inc.php
@@ -4,7 +4,7 @@ class PSExtractor extends ApplicationExtractor
 {
 	public function __construct()
 	{
-		parent::__construct('extractors','pstotext','pstotext','PostScript Text Extractor','-nopgbrk -enc UTF-8 {source} {target}');
+		parent::__construct('extractors','pstotext','pstotext',_kt('PostScript Text Extractor'),'-nopgbrk -enc UTF-8 {source} {target}');
 	}
 
 	public function getSupportedMimeTypes()
