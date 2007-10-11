26ac7b56d1938ed0218501a09f6cd72b929bb93c
Conrad Vermeulen <conrad@knowledgetree.com>
2007-10-11 17:15:09 +0000

    KTS-2442 "Some database tables use unsigned int, but relations are mostly to signed int fields. This means foreign key constraints cannot be added easily." Fixed. Meant to update folders table, not documents.
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7331 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/3.5.0/relation_friendly.sql
+++ sql/mysql/upgrade/3.5.0/relation_friendly.sql
@@ -3,8 +3,19 @@ alter table `folders` change `owner_id`  `owner_id`  int  NULL;
 alter table `index_files` change `document_id`  `document_id`  int NOT NULL;
 alter table `index_files` change `user_id`  `user_id`  int NOT NULL;
 alter table `type_workflow_map` change `workflow_id`  `workflow_id` int  NULL;
+
 alter table document_content_version change mime_id mime_id int null default 9;
+
 alter table documents change owner_id owner_id int null;
+alter table documents change creator_id creator_id int null;
+alter table documents change modified_user_id modified_user_id int null;
+
+alter table document_transactions change document_id document_id int null;
+alter table document_transactions change user_id user_id int null;
+
+alter table folder_transactions change folder_id folder_id int null;
+alter table folder_transactions change user_id user_id int null;
+
 alter table folders change parent_id parent_id int null;
 update documents set owner_id=null where owner_id=0;
 update folders set parent_id=null where parent_id=0;
