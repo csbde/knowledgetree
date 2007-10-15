ca914c3cc4870ed4b2ddd7a5223f6bb05c0e1e9f
Conrad Vermeulen <conrad@knowledgetree.com>
2007-10-15 13:23:45 +0000

    KTS-673 "The search algorithm needs some work" Updated. using externalBinary
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7372 c91229c3-7414-0410-bfa2-8a42b809f60b

--- search2/indexing/extractors/PSExtractor.inc.php
+++ search2/indexing/extractors/PSExtractor.inc.php
@@ -4,7 +4,7 @@ class PSExtractor extends ApplicationExtractor
 {
 	public function __construct()
 	{
-		parent::__construct('extractors','pstotext','pstotext',_kt('PostScript Text Extractor'),'-nopgbrk -enc UTF-8 {source} {target}');
+		parent::__construct('externalBinary','pstotext','pstotext',_kt('PostScript Text Extractor'),'-nopgbrk -enc UTF-8 {source} {target}');
 	}
 
 	public function getSupportedMimeTypes()
