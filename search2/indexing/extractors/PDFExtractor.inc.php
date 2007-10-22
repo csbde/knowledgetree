a73a26212d620ca654e69f5794b1424aec792e94
conradverm <conradverm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-10-22 21:47:14 +0000

    KTS-673 "The search algorithm needs some work" Updated.
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7470 c91229c3-7414-0410-bfa2-8a42b809f60b

--- search2/indexing/extractors/PDFExtractor.inc.php
+++ search2/indexing/extractors/PDFExtractor.inc.php
@@ -4,7 +4,7 @@ class PDFExtractor extends ApplicationExtractor
 {
 	public function __construct()
 	{
-		parent::__construct('externalBinary','pdftotext','pdftotext',_kt('PDF Text Extractor'),'-nopgbrk -enc UTF-8 {source} {target}');
+		parent::__construct('externalBinary','pdftotext','pdftotext',_kt('PDF Text Extractor'),'-nopgbrk -enc UTF-8 \'{source}\' \'{target}\'');
 	}
 
 	public function getSupportedMimeTypes()
