b9dff734489e7608f6caf543997316180c054e2e
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-10-22 15:33:32 +0000

    KTS-2525 "Create windows service to wrap around scheduler" Updated. Added taskrunner.bat and modified service scripts.
    Committed By: Kevin Fourie
    Reviewed By: Conrad Vermeulen
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7456 c91229c3-7414-0410-bfa2-8a42b809f60b

--- bin/win32/installScheduler.php
+++ bin/win32/installScheduler.php
@@ -1,19 +1,11 @@
 <?php
 
-$scriptPath = realpath(dirname(__FILE__) . '/schedulerService.php');
+$scriptPath = realpath(dirname(__FILE__) . '/taskrunner.bat');
     
-// Setup php binary path
-$phpPath = realpath('../../php/php.exe');
-if (!is_file($phpPath))
-{
-    die('Cannot find php.exe');
-}
-
 win32_create_service(array( 
             'service' => 'ktscheduler',
             'display' => 'ktdmsScheduler',
-            'params' => $scriptPath,
-            'path' => $phpPath
+            'path' => $scriptPath
             ));
 
 ?>
---
+++ bin/win32/taskrunner.bat
@@ -0,0 +1,3 @@
+@echo off
+"@@BITROCK_INSTALLDIR@@\php\php.exe" "@@BITROCK_INSTALLDIR@@\knowledgeTree\bin\win32\schedulerService.php"
+
