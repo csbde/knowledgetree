fdee9d820b635e60664304a438db5c5ed0c3e067
conradverm <conradverm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-10-23 21:39:25 +0000

    KTS-1753 " Implement Disk Usage Plugin" Updated. Added cross platform calling of df
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7517 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/housekeeper/DiskUsageDashlet.inc.php
+++ plugins/housekeeper/DiskUsageDashlet.inc.php
@@ -6,32 +6,32 @@
  * KnowledgeTree Open Source Edition
  * Document Management Made Simple
  * Copyright (C) 2004 - 2007 The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * You can contact The Jam Warehouse Software (Pty) Limited, Unit 1, Tramber Place,
  * Blake Street, Observatory, 7925 South Africa. or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
- * copyright notice. 
+ * must display the words "Powered by KnowledgeTree" and retain the original
+ * copyright notice.
  * Contributor( s): ______________________________________
  */
 
@@ -83,10 +83,21 @@ class DiskUsageDashlet extends KTBaseDashlet
 		// we will only check if the result is not cached, or after 5 minutes
 		if ($check)
 		{
-			$result = ktutil::pexec($this->dfCmd);
+			$cmd = $this->dfCmd;
 
-			$result = $result['out'];
-			unset($result[0]);
+			if (OS_WINDOWS)
+			{
+				$cmd = str_replace( '/','\\',$cmd);
+				$result = `"$cmd" 2>&1`;
+			}
+			else
+			{
+				$result = shell_exec($cmd." 2>&1");
+			}
+
+			$result = explode("\n", $result);
+
+			unset($result[0]); // gets rid of headings
 
 			$usage=array();
 			foreach($result as $line)
