792dac862e31b6b3a8da3a4f860eed3db76b5896
Conrad Vermeulen <conrad@knowledgetree.com>
2007-10-23 13:18:34 +0000

    KTS-2440 "Adding a folder descendant lookup table for search" Added.
    Reviewed By: Kevin Fourie
    Committed By: Conrad Vermeulen
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7492 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/3.5.0/folder_descendants.sql
+++ sql/mysql/upgrade/3.5.0/folder_descendants.sql
@@ -4,3 +4,6 @@ CREATE TABLE `folder_descendants`
     `folder_id` int(11) NOT NULL,
     primary key (parent_id,folder_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+alter table folder_descendants add foreign key(parent_id) references folders(id);
+alter table folder_descendants add foreign key(folder_id) references folders(id);
