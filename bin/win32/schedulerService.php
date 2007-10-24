dc6c2b86c00e1a5fb02484351f37700f53f80c95
Conrad Vermeulen <conrad@knowledgetree.com>
2007-10-24 02:29:22 +0000

    KTS-2525 "Create windows service to wrap around scheduler" Updated. scheduler service must be run from correct relative location.
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7542 c91229c3-7414-0410-bfa2-8a42b809f60b

--- bin/win32/schedulerService.php
+++ bin/win32/schedulerService.php
@@ -6,6 +6,7 @@ $myservicename = 'ktscheduler';
 if (!win32_start_service_ctrl_dispatcher($myservicename)) die('Could not connect to service :'.$myservicename);
 win32_set_service_status(WIN32_SERVICE_RUNNING);
 
+chdir(dirname(__FILE__)); // need to be here to include dmsDefaults
 require_once('../../config/dmsDefaults.php');
 
 global $default;
@@ -15,35 +16,38 @@ $schedulerInterval = $config->get('KnowledgeTree/schedulerInterval',10); // inte
 //$phpPath = $config->get('externalBinary/php','php'); // TODO - fix me
 
 // Change to knowledgeTree/bin folder
-
 $dir = realpath(dirname(__FILE__) . '/..');
 chdir($dir);
 
 // Setup php binary path
 $phpPath = realpath('../../php/php.exe');
-
 if (!is_file($phpPath))
 {
 	$default->log->error("Scheduler: php not found: $phpPath");
 	exit;
 }
 
+$default->log->error("Scheduler Service: starting main loop");
+
+$loop = true;
 // Main Scheduler Service Loop
-while (1) {
+while ($loop)
+{
+    switch (win32_get_last_control_message())
+    {
 
-    switch (win32_get_last_control_message()) {
-
-        case WIN32_SERVICE_CONTROL_CONTINUE: break; // Continue server routine
-        case WIN32_SERVICE_CONTROL_INTERROGATE: win32_set_service_status(WIN32_NO_ERROR); break; // Respond with status
-        case WIN32_SERVICE_CONTROL_STOP: win32_set_service_status(WIN32_SERVICE_STOPPED); exit; // Terminate script
-        default: win32_set_service_status(WIN32_ERROR_CALL_NOT_IMPLEMENTED); // Add more cases to handle other service calls
+        case WIN32_SERVICE_CONTROL_CONTINUE:
+        	break; // Continue server routine
+        case WIN32_SERVICE_CONTROL_INTERROGATE:
+        	win32_set_service_status(WIN32_NO_ERROR);
+        	break; // Respond with status
+        case WIN32_SERVICE_CONTROL_STOP: win32_set_service_status(WIN32_SERVICE_STOPPED);
+        	$loop = false; // Terminate script
+        	continue;
+        default:
+        	win32_set_service_status(WIN32_ERROR_CALL_NOT_IMPLEMENTED); // Add more cases to handle other service calls
     }
-
-
-
-
     // Run the scheduler script
-
     $cmd = "\"$phpPath\" scheduler.php";
 
 	$cmd = str_replace( '/','\\',$cmd);
@@ -57,5 +61,7 @@ while (1) {
 
 }
 win32_set_service_status(WIN32_SERVICE_STOPPED);
+
+$default->log->error("Scheduler Service: exiting main loop");
 
 ?>
