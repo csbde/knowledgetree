15c30f87a1cf11d6e11ff8dfbc1b5e77092aab8a
Conrad Vermeulen <conrad@knowledgetree.com>
2007-10-25 09:41:40 +0000

    KTS-2588 "ktdmsScheduler not starting automatically after install" Fixed. An explicit call is made to start the scheduler
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7567 c91229c3-7414-0410-bfa2-8a42b809f60b

--- bin/win32/installScheduler.php
+++ bin/win32/installScheduler.php
@@ -1,11 +1,13 @@
 <?php
 
 $scriptPath = realpath(dirname(__FILE__) . '/taskrunner.bat');
-    
-win32_create_service(array( 
+
+win32_create_service(array(
             'service' => 'ktscheduler',
             'display' => 'ktdmsScheduler',
             'path' => $scriptPath
             ));
+
+win32_start_service('ktscheduler');
 
 ?>
