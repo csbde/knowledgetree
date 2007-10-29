7c6f04910ed64edc3c94670a530ef85597c93dd5
Megan Watson <megan@knowledgetree.com>
2007-10-29 13:54:42 +0000

    KTS-2622 "Copying a folder containing Checkout documents results in the copied documents also being checked out like the originals." Fixed. Added code to cancel the checkout.
    Committed by: Megan Watson
    Reviewed by: Jonathan Byrne
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7598 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/documentmanagement/documentutil.inc.php
+++ lib/documentmanagement/documentutil.inc.php
@@ -10,32 +10,32 @@
  * KnowledgeTree Open Source Edition
  * Document Management Made Simple
  * Copyright (C) 2004 - 2007 The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * You can contact The Jam Warehouse Software (Pty) Limited, Unit 1, Tramber Place,
  * Blake Street, Observatory, 7925 South Africa. or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
- * copyright notice. 
+ * must display the words "Powered by KnowledgeTree" and retain the original
+ * copyright notice.
  * Contributor( s): ______________________________________
  */
 
@@ -191,17 +191,17 @@ class KTDocumentUtil {
 
         return true;
     }
-    
+
     function archive($oDocument, $sReason) {
-        
+
         $this->startTransaction();
         $oDocument->setStatusID(ARCHIVED);
         $res = $oDocument->update();
-        
+
         if (PEAR::isError($res) || ($res === false)) {
             return PEAR::raiseError(_kt('There was a database error while trying to archive this file'));
         }
-        
+
         $oDocumentTransaction = & new DocumentTransaction($oDocument, sprintf(_kt('Document archived: %s'), $sReason), 'ktcore.transactions.update');
         $oDocumentTransaction->create();
 
@@ -222,12 +222,12 @@ class KTDocumentUtil {
                 return $ret;
             }
         }
-        
+
         // fire subscription alerts for the archived document
         $oSubscriptionEvent = new SubscriptionEvent();
         $oFolder = Folder::get($oDocument->getFolderID());
         $oSubscriptionEvent->ArchivedDocument($oDocument, $oFolder);
-        
+
         return true;
     }
 
@@ -904,6 +904,10 @@ class KTDocumentUtil {
         $res = KTDocumentUtil::copyMetadata($oNewDocument, $oDocument->getMetadataVersionId());
         if (PEAR::isError($res)) { return $res; }
 
+        $oNewDocument->setIsCheckedOut(false);
+        $oNewDocument->setCheckedOutUserID(-1);
+
+
         // finally, copy the actual file.
         $oStorage =& KTStorageManagerUtil::getSingleton();
         $res = $oStorage->copy($oDocument, $oNewDocument);
@@ -967,7 +971,7 @@ class KTDocumentUtil {
                 return $ret;
             }
         }
-        
+
         // fire subscription alerts for the copied document
         $oSubscriptionEvent = new SubscriptionEvent();
         $oFolder = Folder::get($oDocument->getFolderID());
@@ -1077,7 +1081,7 @@ class KTDocumentUtil {
                 return $ret;
             }
         }
-        
+
         // fire subscription alerts for the moved document
         $oSubscriptionEvent = new SubscriptionEvent();
         $oSubscriptionEvent->MoveDocument($oDocument, $oFolder, $oOriginalFolder);
