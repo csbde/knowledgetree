2756c2973815dcc0a96c606bc40b71d746db591d
Jalaloedien Abrahams <jalal@knowledgetree.com>
2007-11-01 12:49:23 +0000

    KTS-2652 "Would be nice to have a dedicated rss feed dashlet for KnowledgeTree blog, etc" Fixed. Extended rss plugin
    Committed By: Jalaloedien Abrahams
    Reviewed By: Conrad Vermeulen
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7625 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/rssplugin/js/update.js
+++ plugins/rssplugin/js/update.js
@@ -37,3 +37,42 @@ function GetXmlHttpObject(){
 	}
 	return objXMLHttp;
 }
+
+function loadDedicatedFeed(user){
+	xmlHttpD=GetXmlHttpObject();
+	if (xmlHttpD===null){
+		alert ("Browser does not support HTTP Request");
+		return;
+	}
+	var feed = FEED_URL;
+	// First check if there is a feed - in the event the 'Select feed' option was selected
+	if(feed !== 'null'){
+		var url=currloc+"plugins/rssplugin/loadDedicatedFeed.inc.php";
+		url=url+"?feed="+feed;
+		url=url+"&user="+user;
+		url=url+"&sid="+Math.random();
+		xmlHttpD.onreadystatechange=stateChangedD;
+		xmlHttpD.open("GET",url,true);
+		xmlHttpD.send(null);
+	}
+}
+
+function stateChangedD(){ 
+	if (xmlHttpD.readyState==4 || xmlHttpD.readyState=="complete"){ 
+		document.getElementById("RSSDedicatedDashlet").style.display = "block";
+		document.getElementById("rssDedicatedBlock").innerHTML=xmlHttpD.responseText;
+	}else{
+		document.getElementById("RSSDedicatedDashlet").style.display = "block";
+	    document.getElementById("rssDedicatedBlock").innerHTML="Loading feed...";
+	}
+} 
+
+function GetXmlHttpObjectD(){ 
+	var objXMLHttpD=null;
+	if (window.XMLHttpRequest){
+		objXMLHttpD=new XMLHttpRequest();
+	}else if (window.ActiveXObject){
+		objXMLHttpD=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	return objXMLHttpD;
+}
---
+++ plugins/rssplugin/loadDedicatedFeed.inc.php
@@ -0,0 +1,93 @@
+<?php
+/*
+ * $Id: loadFeed.inc.php 7481 2007-10-23 09:47:11Z kevin_fourie $
+ *
+ * KnowledgeTree Open Source Edition
+ * Document Management Made Simple
+ * Copyright (C) 2004 - 2007 The Jam Warehouse Software (Pty) Limited
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 3 as published by the
+ * Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * You can contact The Jam Warehouse Software (Pty) Limited, Unit 1, Tramber Place,
+ * Blake Street, Observatory, 7925 South Africa. or email info@knowledgetree.com.
+ * 
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ * 
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
+ * must display the words "Powered by KnowledgeTree" and retain the original 
+ * copyright notice. 
+ * Contributor( s): ______________________________________
+ *
+ */
+ require_once('../../config/dmsDefaults.php');
+ require_once(KT_DIR. '/plugins/rssplugin/rss2array.inc.php');
+ require_once(KT_DIR. '/plugins/rssplugin/KTrss.inc.php');
+ 
+ $feed = $_GET["feed"];
+ $user = $_GET["user"];
+ session_start();
+ 
+ $sCache = _checkCache($feed);
+ 
+ if(!is_null($sCache)) {
+ 	$aRSSArray =  $sCache;
+ }else{
+ 	$aRSSArray = rss2array($feed);
+	$_SESSION['kt_dedicated_rss'][$feed]['lastcheck'] = time();
+    $_SESSION['kt_dedicated_rss'][$feed] = $aRSSArray;
+ }
+ // Prepare response data to be passed back to page
+ $reposonse = "<h3>".$aRSSArray[channel][title]."</h3>" .
+ 		"<div class='outerContainer' id='outerContainer'>" .
+ 		"<table width='90%'>";
+		for($i=0;$i<count($aRSSArray[items]);$i++){
+			 $reposonse .= "<tr>
+				<td colspan='2'><strong><a href='".$aRSSArray[items][$i][link]."' target='_blank'>".$aRSSArray[items][$i][title]."</a><strong></td>
+			</tr>
+			<tr>
+				<td>".$aRSSArray[items][$i][description]."</td>
+			</tr>
+			<tr><td colspan='2'><br></td></tr>";
+		}
+ $reposonse .= "</table></div><br>";
+	 
+ echo  $reposonse;
+ 
+ function _checkCache($feed) {
+	session_start();
+    $iLastCheck = $_SESSION['kt_dedicated_rss'][$feed]['lastcheck'];
+
+    if (empty($iLastCheck)) {
+        return;
+    }
+    $sStoredFeed = $_SESSION['kt_dedicated_rss'][$feed];
+    if (empty($sStoredFeed)) {
+        $now = time();
+        $diff = $now - $iLastCheck;
+        if ($diff > (5*60)) {
+            return;
+        }
+    }
+    $now = time();
+    $diff = $now - $iLastCheck;
+    if ($diff > (5*60)) {
+        return;
+    }
+    return $sStoredFeed;
+}
+?>
