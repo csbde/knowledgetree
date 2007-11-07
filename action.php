0eb5b35f668ad3b80a136d7aed53dc44df34a0e7
Megan Watson <megan@knowledgetree.com>
2007-11-07 07:44:44 +0000

    KTS-2664 "When no document is selected when performing a bulk action in a folder the error message is not displayed as per Knowledge Tree standard." Fixed. Added a new redirect method to dispatcher "redirectToBrowse()". The action now redirects to the browse.php instead of action.php.
    Committed by: Megan Watson
    Reviewed by: Conrad Vermeulen
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7644 c91229c3-7414-0410-bfa2-8a42b809f60b

--- action.php
+++ action.php
@@ -5,32 +5,32 @@
  * KnowledgeTree Open Source Edition
  * Document Management Made Simple
  * Copyright (C) 2004 - 2007 The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * You can contact The Jam Warehouse Software (Pty) Limited, Unit 1, Tramber Place,
  * Blake Street, Observatory, 7925 South Africa. or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
- * copyright notice. 
+ * must display the words "Powered by KnowledgeTree" and retain the original
+ * copyright notice.
  * Contributor( s): ______________________________________
  *
  */
@@ -86,35 +86,35 @@ class KTActionDispatcher extends KTStandardDispatcher {
 
     function do_bulkaction() {
         $act = (array) KTUtil::arrayGet($_REQUEST, 'submit',null);
-        
+
         $targets = array_keys($act);
         if (!empty($targets)) {
             $target = $targets[0];
         } else {
-            $this->errorRedirectToMain(_kt('No action selected.'));
+            $this->errorRedirectToBrowse(_kt('No action selected.'));
             exit(0);
         }
 
         $aFolderSelection = KTUtil::arrayGet($_REQUEST, 'selection_f' , array());
-        $aDocumentSelection = KTUtil::arrayGet($_REQUEST, 'selection_d' , array());        
+        $aDocumentSelection = KTUtil::arrayGet($_REQUEST, 'selection_d' , array());
 
         $oFolder = Folder::get(KTUtil::arrayGet($_REQUEST, 'fFolderId', 1));
-        if (PEAR::isError($oFolder)) { 
-            $this->errorRedirectToMain(_kt('Invalid folder selected.'));
+        if (PEAR::isError($oFolder)) {
+            $this->errorRedirectToBrowse(_kt('Invalid folder selected.'));
             exit(0);
         }
 
         if (empty($aFolderSelection) && empty($aDocumentSelection)) {
-            $this->errorRedirectToMain(_kt('Please select documents or folders first.'), sprintf('fFolderId=%d', $oFolder->getId()));
+            $this->errorRedirectToBrowse(_kt('Please select documents or folders first.'), sprintf('fFolderId=%d', $oFolder->getId()));
             exit(0);
-        }        
-        
+        }
+
         // prepare for passing to bulk actions
         $oActionRegistry =& KTActionRegistry::getSingleton();
         $oAction =& $oActionRegistry->initializeAction($target, $this->oUser);
-        
+
         if(!$oAction || PEAR::isError($oAction)) {
-            $this->errorRedirectToMain(_kt('No such action.'));
+            $this->errorRedirectToBrowse(_kt('No such action.'));
             exit(0);
         }
 
@@ -126,7 +126,7 @@ class KTActionDispatcher extends KTStandardDispatcher {
 
         //        exit(0);
     }
-        
+
 
     /**
      * Handle output from this dispatcher.
