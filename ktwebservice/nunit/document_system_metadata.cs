7e71bedda18d55b3918a490c2095f66f8ff2114a
conradverm <conradverm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-11-09 10:25:26 +0000

    WSA-57 "Checkout functions should return document detail" Updated.
    WSA-56
    "rename document_detail.updated fields to be document_detail.modified to be consistent with normal interface"
    Updated.
    
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7657 c91229c3-7414-0410-bfa2-8a42b809f60b

--- ktwebservice/nunit/document_system_metadata.cs
+++ ktwebservice/nunit/document_system_metadata.cs
@@ -64,13 +64,16 @@ namespace MonoTests.KnowledgeTree
 			fs[0].fields[2].name = "Media Type";
 			fs[0].fields[2].value = "Text";
 
-			kt_sysdata_item[] sysdata = new kt_sysdata_item[2];
+			kt_sysdata_item[] sysdata = new kt_sysdata_item[3];
 			sysdata[0] = new kt_sysdata_item();
 			sysdata[0].name = "created_by";
 			sysdata[0].value = "Anonymous";
 			sysdata[1] = new kt_sysdata_item();
 			sysdata[1].name = "created_date";
 			sysdata[1].value = "2007-01-17";
+			sysdata[2] = new kt_sysdata_item();
+			sysdata[2].name = "modified_by";
+			sysdata[2].value = "admin";
 
 
 			kt_document_detail update_resp = this._kt.update_document_metadata(this._session, this._doc1.docId, fs, sysdata);
@@ -87,6 +90,8 @@ namespace MonoTests.KnowledgeTree
 			Assert.AreEqual("Text", update_resp.metadata[1].fields[2].value);
 
 			Assert.AreEqual("Anonymous", update_resp.created_by);
+			Assert.AreEqual("2007-01-17 00:00:00", update_resp.created_date);
+			Assert.AreEqual("Administrator", update_resp.modified_by);
 			Assert.AreEqual("2007-01-17 00:00:00", update_resp.created_date);
 	    	}
 
@@ -159,7 +164,7 @@ namespace MonoTests.KnowledgeTree
 			sysdata[1].name = "created_date";
 			sysdata[1].value = "2007-01-17";
 
-			kt_response  resp = this._kt.checkout_base64_document(this._session, this._doc1.docId, "test checkin", false);
+			kt_document_detail  resp = this._kt.checkout_base64_document(this._session, this._doc1.docId, "test checkin", false);
 			Assert.AreEqual(0, resp.status_code);
 
 
