1992040ce7f4d0e7755715ca54ef53d251880290
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-11-22 19:12:34 +0000

    KTC-262 "Action Restriction section of a work flow there are extra two extra columns as well as one that says "no name" after you select "Edit Action""
    Fixed. Plugins now return their names correctly.
    
    Committed By: Kevin Fourie
    Reviewed By: Jonathan Byrne
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7724 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktstandard/PDFGeneratorAction.php
+++ plugins/ktstandard/PDFGeneratorAction.php
@@ -2,35 +2,29 @@
 /**
  * $Id$
  *
- * KnowledgeTree Open Source Edition
- * Document Management Made Simple
- * Copyright (C) 2004 - 2007 The Jam Warehouse Software (Pty) Limited
+ * The contents of this file are subject to the KnowledgeTree Public
+ * License Version 1.1.2 ("License"); You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.knowledgetree.com/KPL
  * 
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License version 3 as published by the
- * Free Software Foundation.
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * See the License for the specific language governing rights and
+ * limitations under the License.
+ *
+ * All copies of the Covered Code must include on each user interface screen:
+ *    (i) the "Powered by KnowledgeTree" logo and
+ *    (ii) the KnowledgeTree copyright notice
+ * in the same form as they appear in the distribution.  See the License for
+ * requirements.
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details.
+ * The Original Code is: KnowledgeTree Open Source
  * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact The Jam Warehouse Software (Pty) Limited, Unit 1, Tramber Place,
- * Blake Street, Observatory, 7925 South Africa. or email info@knowledgetree.com.
- * 
- * The interactive user interfaces in modified source and object code versions
- * of this program must display Appropriate Legal Notices, as required under
- * Section 5 of the GNU General Public License version 3.
- * 
- * In accordance with Section 7(b) of the GNU General Public License version 3,
- * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
- * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
- * copyright notice. 
+ * The Initial Developer of the Original Code is The Jam Warehouse Software
+ * (Pty) Ltd, trading as KnowledgeTree.
+ * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
+ * (C) 2007 The Jam Warehouse Software (Pty) Ltd;
+ * All Rights Reserved.
  * Contributor( s): ______________________________________
  *
  */
@@ -51,8 +45,8 @@ class PDFGeneratorAction extends KTDocumentAction {
     var $sDisplayName = 'Generate PDF';
     // Note: 'asc' below seems to be a catchall for plain text docs.
     //       'htm' and 'html' should work but are not so have been removed for now.
-    var $aAcceptedMimeTypes = array('doc', 'ods', 'odt', 'ott', 'txt', 'rtf', 'sxw', 'stw',
-            //                                    'html', 'htm',
+    var $aAcceptedMimeTypes = array('doc', 'ods', 'odt', 'ott', 'txt', 'rtf', 'sxw', 'stw', 
+            //                                    'html', 'htm', 
             'xml' , 'pdb', 'psw', 'ods', 'ots', 'sxc',
             'stc', 'dif', 'dbf', 'xls', 'xlt', 'slk', 'csv', 'pxl',
             'odp', 'otp', 'sxi', 'sti', 'ppt', 'pot', 'sxd', 'odg',
@@ -62,7 +56,7 @@ class PDFGeneratorAction extends KTDocumentAction {
         // We need to handle Windows differently - as usual ;)
         if (substr( PHP_OS, 0, 3) == 'WIN') {
             $cmdpath = KT_DIR . "/../openoffice/openoffice/program/python.bat";
-            $cmdpath = str_replace( '/','\\',$cmdpath);
+            $cmdpath = str_replace( '/','\\',$cmdpath);   
         } else {
             $cmdpath = "../openoffice/program/python";
         }
@@ -75,9 +69,8 @@ class PDFGeneratorAction extends KTDocumentAction {
                     return _kt('Generate PDF') . "&nbsp;<a href=\"" . KTUtil::ktLink( 'action.php', 'ktstandard.pdf.generate', array( "fDocumentId" => $this->oDocument->getId(), "action" => "pdfdownload") ) . "\" <img src='resources/mimetypes/pdf.png' alt='PDF' border=0/></a>";
                 }
             }
-            return _kt('Generate PDF');
         }
-        return '';
+        return 'PDF Generator';
     }
 
     function form_main() {
@@ -123,7 +116,7 @@ class PDFGeneratorAction extends KTDocumentAction {
                 $this->do_pdfdownload();
         }
         redirect(KTUtil::ktLink( 'action.php', 'ktstandard.pdf.generate', array( "fDocumentId" => $this->oDocument->getId() ) ) );
-        exit(0);
+        exit(0);  
     }
 
     function do_main() {
@@ -168,7 +161,7 @@ class PDFGeneratorAction extends KTDocumentAction {
     /**
      * Method for downloading the document as a pdf.
      *
-     * @return true on success else false
+     * @return true on success else false 
      */
     function do_pdfdownload() {
 
@@ -188,7 +181,7 @@ class PDFGeneratorAction extends KTDocumentAction {
             if (substr( PHP_OS, 0, 3) == 'WIN') {
 
                 $cmd = "\"" . KT_DIR . "/../openoffice/openoffice/program/python.bat\" \"". KT_DIR . "/bin/openoffice/pdfgen.py\" \"" . $sPath . "\" \"" . $sTempFilename . "\"";
-                $cmd = str_replace( '/','\\',$cmd);
+                $cmd = str_replace( '/','\\',$cmd);   
 
                 // TODO: Check for more errors here
                 // SECURTIY: Ensure $sPath and $sTempFilename are safe or they could be used to excecute arbitrary commands!
@@ -211,7 +204,7 @@ class PDFGeneratorAction extends KTDocumentAction {
 
             }
 
-            // Check the tempfile exists and the python script did not return anything (which would indicate an error)
+            // Check the tempfile exists and the python script did not return anything (which would indicate an error) 
             if (file_exists($sTempFilename) && $res == '') {
 
                 $sUrlEncodedFileName = substr($oDocument->getFileName(), 0, strrpos($oDocument->getFileName(), '.') );
@@ -231,7 +224,7 @@ class PDFGeneratorAction extends KTDocumentAction {
 
                 // HTTP/1.0
                 // header("Pragma: no-cache"); // Don't send this header! It breaks IE.
-
+                
                 // Get a filelike object and send it to the browser
                 $oFile = new KTFSFileLike($sTempFilename);
                 KTFileLikeUtil::send_contents($oFile);
@@ -239,23 +232,23 @@ class PDFGeneratorAction extends KTDocumentAction {
                 unlink($sTempFilename);
 
                 // Create the document transaction
-                $oDocumentTransaction = new DocumentTransaction($oDocument, 'Document downloaded as PDF', 'ktcore.transactions.download', $aOptions);
+                $oDocumentTransaction = & new DocumentTransaction($oDocument, 'Document downloaded as PDF', 'ktcore.transactions.download', $aOptions);
                 $oDocumentTransaction->create();
                 // Just stop here - the content has already been sent.
-                exit(0);
+                exit(0);  
 
             } else {
                 // Set the error messsage and redirect to view document
-                $this->addErrorMessage(_kt('An error occurred generating the PDF - please contact the system administrator. ' . $res));
+                $this->addErrorMessage(_kt('An error occurred generating the PDF - please contact the system administrator.<br>' . $res));
                 redirect(generateControllerLink('viewDocument',sprintf(_kt('fDocumentId=%d'),$oDocument->getId())));
-                exit(0);
+                exit(0);  
             }
 
         } else {
             // Set the error messsage and redirect to view document
-            $this->addErrorMessage(_kt('An error occurred generating the PDF - please contact the system administrator. The path to the document did not exist.'));
+            $this->addErrorMessage(_kt('An error occurred generating the PDF - please contact the system administrator.<br>The path to the document did not exist.'));
             redirect(generateControllerLink('viewDocument',sprintf(_kt('fDocumentId=%d'),$oDocument->getId())));
-            exit(0);
+            exit(0);  
         }
 
 
