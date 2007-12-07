6e118cae0321c916255e1206059a0ac59ffa9554
Megan Watson <megan@knowledgetree.com>
2007-12-07 14:12:54 +0000

    KTC-292 "Minor version does not increase when the Document Title is changed." Fixed. Added a new content version and updated the version number.
    Committed by: Megan Watson
    Reviewed by: Jonathan Byrne
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7795 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/document/edit.php
+++ plugins/ktcore/document/edit.php
@@ -4,32 +4,32 @@
  * KnowledgeTree Open Source Edition
  * Document Management Made Simple
  * Copyright (C) 2004 - 2007 The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * You can contact The Jam Warehouse Software (Pty) Limited, Unit 1, Tramber Place,
  * Blake Street, Observatory, 7925 South Africa. or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
- * copyright notice. 
+ * must display the words "Powered by KnowledgeTree" and retain the original
+ * copyright notice.
  * Contributor( s): ______________________________________
  *
  */
@@ -198,6 +198,10 @@ class KTDocumentEditAction extends KTDocumentAction {
             $this->oDocument->setDocumentTypeId($doctypeid);
         }
         $this->oDocument->setName(($data['document_title']));
+        $this->oDocument->startNewContentVersion($this->oUser);
+        $this->oDocument->setMinorVersionNumber($this->oDocument->getMinorVersionNumber()+1);
+        $this->oDocument->setLastModifiedDate(getCurrentDateTime());
+        $this->oDocument->setModifiedUserId($this->oUser->getId());
 
         $res = $this->oDocument->update();
         if (PEAR::isError($res)) {
