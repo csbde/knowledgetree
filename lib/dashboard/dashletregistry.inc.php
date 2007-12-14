fe0cfcbf57d1fd256bd0d8c9ffe6bdc72235f7fa
megan_w <megan_w@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-12-14 06:43:02 +0000

    KTC-286 "Welcome to knowledgeTree dashlet should always be on the top left corner of the dashlet container" Fixed. Refactored the way the users state is refreshed. It now uses the plugin_helper table to get any new dashlets.
    Committed by: Megan Watson
    Reviewed by: Conrad Vermeulen
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7816 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/dashboard/dashletregistry.inc.php
+++ lib/dashboard/dashletregistry.inc.php
@@ -5,32 +5,32 @@
  * KnowledgeTree Open Source Edition
  * Document Management Made Simple
  * Copyright (C) 2004 - 2007 The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * You can contact The Jam Warehouse Software (Pty) Limited, Unit 1, Tramber Place,
  * Blake Street, Observatory, 7925 South Africa. or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
- * copyright notice. 
+ * must display the words "Powered by KnowledgeTree" and retain the original
+ * copyright notice.
  * Contributor( s): ______________________________________
  *
  */
@@ -51,6 +51,55 @@ class KTDashletRegistry {
     function registerDashlet($name, $nsname, $filename = "", $sPlugin = "") {
 
         $this->nsnames[$nsname] = array($name, $filename, $nsname, $sPlugin);
+    }
+
+    /**
+     * Get any dashlets added since the user's last login
+     *
+     * @param object $oUser
+     */
+    function getNewDashlets($oUser, $sCurrent) {
+        $new = array();
+        $inactive = array();
+
+        static $sInactive = '';
+        $sIgnore = (!empty($sInactive)) ? $sCurrent.','.$sInactive : $sCurrent;
+
+        // Get all dashlets that haven't already been displayed to the user and are active for the user
+        $query = "SELECT * FROM plugin_helper h
+            INNER JOIN plugins p ON (h.plugin = p.namespace)
+            WHERE p.disabled = 0 AND classtype = 'dashlet'
+            AND h.classname NOT IN ($sIgnore)";
+
+        $res = DBUtil::getResultArray($query);
+
+        // If the query is not empty, get the dashlets and return the new active ones
+        // Add the inactive ones the list.
+        if(!PEAR::isError($res) && !empty($res)){
+            $oRegistry =& KTPluginRegistry::getSingleton();
+            foreach ($res as $item){
+                $name = $item['classname'];
+                $filename = $item['pathname'];
+                $sPluginName = $item['plugin'];
+
+                require_once($filename);
+                $oPlugin =& $oRegistry->getPlugin($sPluginName);
+
+                $oDashlet = new $name;
+                $oDashlet->setPlugin($oPlugin);
+                if ($oDashlet->is_active($oUser)) {
+                    $new[] = $name;
+                }else{
+                    $inactive[] = "'$name'";
+                }
+            }
+            // Add new inactive dashlets
+            $sNewInactive = implode(',', $inactive);
+            $sInactive = (!empty($sInactive)) ? $sInactive.','.$sNewInactive : $sNewInactive;
+
+            return $new;
+        }
+        return '';
     }
 
     // FIXME we might want to do the pruning now, but I'm unsure how to handle the preconditions.
