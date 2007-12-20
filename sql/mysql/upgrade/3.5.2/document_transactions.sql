b83823a127928b5837666db76eb47d1be89178cc
conradverm <conradverm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2007-12-20 15:48:08 +0000

    KNP-33 "Top Download Dashlet takes a long time to load" Fixed.
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7850 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/3.5.2/document_transactions.sql
+++ sql/mysql/upgrade/3.5.2/document_transactions.sql
@@ -3,3 +3,4 @@ alter table document_transactions change ip ip varchar(15);
 alter table document_transactions change filename filename mediumtext;
 alter table document_transactions change comment comment mediumtext;
 alter table document_transactions change transaction_namespace transaction_namespace varchar(255);
+alter table document_transactions add index (`datetime`,`transaction_namespace`);
