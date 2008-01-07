3e48f102ea808c9703f0fd182c0b30d6dd1637e3
Conrad Vermeulen <conrad@knowledgetree.com>
2008-01-07 11:00:28 +0000

    WSA-91 "error with move_document: Cannot assign object of type Dowco.KTWrapper.atlantic_kt.kt_response to an object of type Dowco.KTWrapper.atlantic_kt.kt_document_detail." Fixed.
    Committed By: Conrad Vermeulen
    Reviewed By: Megan Watson
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7861 c91229c3-7414-0410-bfa2-8a42b809f60b

--- ktwebservice/nunit/document_copy.cs
+++ ktwebservice/nunit/document_copy.cs
@@ -8,29 +8,29 @@ namespace MonoTests.KnowledgeTree
 
 	[TestFixture]
 	public class DocumentCopyTest : KTTest
-    	{ 
-		private int 			_folderId; 
-		private Document		_doc1; 
+    	{
+		private int 			_folderId;
+		private Document		_doc1;
 
 
 		[SetUp]
 		public void SetUp()
-		{ 
-			this._folderId = 1; 
+		{
+			this._folderId = 1;
 
 			this._doc1 = new Document(1, this._session, this._kt, this._verbose, false);
-			this._doc1.createFile(this._folderId);  
-			
-			
-			
+			this._doc1.createFile(this._folderId);
+
+
+
 		}
 
 		[TearDown]
 		public void TearDown()
 		{
-			this._doc1.deleteFile(); 
+			this._doc1.deleteFile();
 		}
-			
+
 		[Test]
 		public void FindDocumentBeforeCopy()
 		{
@@ -47,35 +47,38 @@ namespace MonoTests.KnowledgeTree
 			{
 				System.Console.WriteLine("document not found. that is ok!");
 			}
-		} 		
-		
+		}
+
 
 		[Test]
 		public void CopyTest()
 		{
-			kt_document_detail linkresp = this._kt.copy_document(this._session, this._doc1.docId, 1, "copy", "test123", "test123.txt");
-			Assert.AreEqual(0, linkresp.status_code); 
-			Assert.AreEqual("test123.txt", linkresp.filename); 
-			Assert.AreEqual("test123", linkresp.title); 
+		kt_folder_detail response2 = this._kt.create_folder(this._session, 1, "kt_unit_test_move");
+		       	Assert.AreEqual(0,response2.status_code);
+		        int folderId =    response2.id;
 
-			 
+
+			kt_document_detail linkresp = this._kt.copy_document(this._session, this._doc1.docId, folderId, "copy", "");
+			Assert.AreEqual(0, linkresp.status_code);
+			Assert.AreEqual("kt_unit_test1.txt", linkresp.filename);
+			Assert.AreEqual("kt unit test1", linkresp.title);
+
+
 
 	    	}
-		
+
 		[Test]
 		public void FindDocumentAfterCopy()
 		{
-			String filename = "Root Folder/test123";
+			String filename = "Root Folder/kt unit test1";
 			if (this._verbose) System.Console.WriteLine("Finding document before add: " + filename);
 			kt_document_detail documentDetail = this._kt.get_document_detail_by_title(this._session, 1, filename, "");
-			Assert.AreEqual(0, documentDetail.status_code); 
-			 
-				if (this._verbose) System.Console.WriteLine("Found document - deleting");
-				kt_response response = this._kt.delete_document(this._session, documentDetail.document_id, "Delete - cleaning up before add");
-				Assert.AreEqual(0, response.status_code);
-			 
-		} 	
-		
+			Assert.AreEqual(0, documentDetail.status_code);
+
+
+
+		}
+
 
 
 	}
