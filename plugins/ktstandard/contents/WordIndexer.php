16bcd54da3f5003d2a32a7487ae5a82bdabff5cc
Megan Watson <megan@knowledgetree.com>
2008-01-17 13:21:12 +0000

    KTS-2811 "KnowledgeTree Indexer plugin not indexing in 3.4.5 on a windows machine" Fixed. Added a new util function for exec on windows, that iterates through the execute functions till one works.
    Committed by: Megan Watson
    Reviewed by: Kevin Fourie
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7926 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktstandard/contents/WordIndexer.php
+++ plugins/ktstandard/contents/WordIndexer.php
@@ -5,32 +5,32 @@
  * KnowledgeTree Open Source Edition
  * Document Management Made Simple
  * Copyright (C) 2004 - 2007 The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * You can contact The Jam Warehouse Software (Pty) Limited, Unit 1, Tramber Place,
  * Blake Street, Observatory, 7925 South Africa. or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
- * copyright notice. 
+ * must display the words "Powered by KnowledgeTree" and retain the original
+ * copyright notice.
  * Contributor( s): ______________________________________
  *
  */
@@ -45,62 +45,64 @@ class KTWordIndexerTrigger extends KTBaseIndexerTrigger {
     var $commandconfig = 'indexer/catdoc';          // could be any application.
     var $args = array("-w", "-d", "UTF-8");
     var $use_pipes = true;
-    
+
     function extract_contents($sFilename, $sTempFilename) {
-        if (OS_WINDOWS) {	    
+        if (OS_WINDOWS) {
             $this->command = 'c:\antiword\antiword.exe';
             $this->commandconfig = 'indexer/antiword';
             $this->args = array();
         }
 	  putenv('LANG=en_US.UTF-8');
-	
+
 	  $sCommand = KTUtil::findCommand($this->commandconfig, $this->command);
         if (empty($sCommand)) {
             return false;
         }
-        
-        if (OS_WINDOWS) {	
-            $sDir = dirname(dirname($sCommand));
-	          putenv('HOME=' . $sDir);
 
+        if (OS_WINDOWS) {
+            $sDir = dirname(dirname($sCommand));
+	        putenv('HOME=' . $sDir);
+
+	        /*
             $cmdline = array($sCommand);
             $cmdline = kt_array_merge($cmdline, $this->args);
             $cmdline[] = $sFilename;
-            
+
             $sCmd = KTUtil::safeShellString($cmdline);
         	$sCmd .= " >> " . escapeshellarg($sTempFilename);
-        	
+
         	$sCmd = str_replace( '/','\\',$sCmd);
-        	
+
             $sCmd = "start /b \"kt\" " . $sCmd;
-            
+
             pclose(popen($sCmd, 'r'));
-        	
+
             $this->aCommandOutput = 1;
             $contents = file_get_contents($sTempFilename);
             return $contents;
+            */
         }
         return parent::extract_contents($sFilename, $sTempFilename);
     }
-    
+
     function findLocalCommand() {
         if (OS_WINDOWS) {
             $this->command = 'c:\antiword\antiword.exe';
             $this->commandconfig = 'indexer/antiword';
             $this->args = array();
-        }        
+        }
         $sCommand = KTUtil::findCommand($this->commandconfig, $this->command);
         return $sCommand;
     }
-    
+
     function getDiagnostic() {
         $sCommand = $this->findLocalCommand();
-        
+
         // can't find the local command.
         if (empty($sCommand)) {
             return sprintf(_kt('Unable to find required command for indexing.  Please ensure that <strong>%s</strong> is installed and in the %s Path.  For more information on indexers and helper applications, please <a href="%s">visit the %s site</a>.'), $this->command, APP_NAME, $this->support_url, APP_NAME);
         }
-        
+
         return null;
     }
 }
