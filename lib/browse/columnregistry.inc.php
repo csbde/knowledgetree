2676819e98b29c8aa716ea28aca4dc8be826f667
conradverm <conradverm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-01-28 15:35:34 +0000

    KTS-2893 "Uninstalled columns may cause browsing the repository to break" Fixed.
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@7973 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/browse/columnregistry.inc.php
+++ lib/browse/columnregistry.inc.php
@@ -6,32 +6,32 @@
  * KnowledgeTree Open Source Edition
  * Document Management Made Simple
  * Copyright (C) 2004 - 2008 The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * You can contact The Jam Warehouse Software (Pty) Limited, Unit 1, Tramber Place,
  * Blake Street, Observatory, 7925 South Africa. or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
- * copyright notice. 
+ * must display the words "Powered by KnowledgeTree" and retain the original
+ * copyright notice.
  * Contributor( s): ______________________________________
  *
  */
@@ -93,8 +93,13 @@ class KTColumnRegistry {
         $view_columns = array();
         foreach ($view_column_entries as $oEntry) {
             $res = $this->getColumn($oEntry->getColumnNamespace());
-            if (PEAR::isError($res)) { return $res; }
-
+            if (PEAR::isError($res))
+            {
+            	// this was returning before, but the function calling this is just doing
+            	// an array merge, so the error propogation is not great.
+            	// if this is an unexpected column, lets just skip it for now.
+            	continue;
+            }
             $aOptions = $oEntry->getConfigArray();
             $aOptions['column_id'] = $oEntry->getId();
             $aOptions['required_in_view'] = $oEntry->getRequired();
