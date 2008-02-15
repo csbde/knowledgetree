67d9f73c92b5bb6dc239935792bf1ab0c6b23474
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-02-15 15:58:43 +0000

    Merged in from DEV trunk...
    KTS-3043
    "On reread of plugins the quicklinks plugin is set to disabled"
    Fixed. Moved the checks on the end version after an upgrade into the if statement containing the upgrade.
    
    Committed By: Megan Watson
    Reviewed By: Conrad Vermeulen
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/STABLE/trunk@8076 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/metadata/metadatautil.inc.php
+++ lib/metadata/metadatautil.inc.php
@@ -2,39 +2,37 @@
 /**
  * $Id$
  *
- * Utility functions for retrieving information from and managing
- * fieldsets with conditions.
- *
  * KnowledgeTree Open Source Edition
  * Document Management Made Simple
  * Copyright (C) 2004 - 2008 The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * You can contact The Jam Warehouse Software (Pty) Limited, Unit 1, Tramber Place,
  * Blake Street, Observatory, 7925 South Africa. or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
- * copyright notice. 
+ * must display the words "Powered by KnowledgeTree" and retain the original
+ * copyright notice.
  * Contributor( s): ______________________________________
+ *
  */
 
 require_once(KT_LIB_DIR . "/ktentity.inc");
@@ -76,6 +74,9 @@ class KTMetadataUtil {
          */
         
         $oBehaviour =& KTUtil::getObject('KTFieldBehaviour', $oBehaviour);
+        if(PEAR::isError($oBehaviour)) {
+	    return array();
+        }
         
         $GLOBALS['default']->log->debug('KTMetadataUtil::_getNextForBehaviour, behaviour is ' . $oBehaviour->getId());
 
@@ -189,6 +190,7 @@ class KTMetadataUtil {
         //var_dump($lookup); exit(0);
         $oValueInstance = KTValueInstance::getByLookupSingle($lookup);
         if (PEAR::isError($oValueInstance) || is_null($oValueInstance) || ($oValueInstance === false)) { return true; } // throw an error
+
         $aValues = KTMetadataUtil::_getNextForBehaviour($oValueInstance->getBehaviourId(), $aCurrentSelections);
         $GLOBALS['default']->log->debug('KTMetadataUtil::getNext, values are ' . print_r($aValues, true));
         $aReturn = array();
