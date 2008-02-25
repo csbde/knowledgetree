cbb79ea7d7eaf7a7f9a7c702d59232d32312906e
Megan Watson <megan@knowledgetree.com>
2008-02-25 13:00:59 +0000

    KTS-3056 "Create a script to check if Open Office is running (SUP-614)" Fixed. Added the script to the scheduled tasks.
    Committed by: Megan Watson
    Reviewed by: Conrad Vermeulen
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@8136 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/install/data.sql
+++ sql/mysql/install/data.sql
@@ -808,7 +808,8 @@ INSERT INTO `scheduler_tasks` VALUES
 (2,'Index Migration','search2/indexing/bin/cronMigration.php','',0,'5mins','2007-10-01',NULL,0,'system'),
 (3,'Index Optimisation','search2/indexing/bin/optimise.php','',0,'weekly','2007-10-01',NULL,0,'system'),
 (4,'Periodic Document Expunge','bin/expungeall.php','',0,'weekly','2007-10-01',NULL,0,'disabled'),
-(5,'Database Maintenance','bin/dbmaint.php','optimize',0,'monthly','2007-10-01',NULL,0,'disabled');
+(5,'Database Maintenance','bin/dbmaint.php','optimize',0,'monthly','2007-10-01',NULL,0,'disabled'),
+(6,'Open Office test','bin/checkopenoffice.php','',0,'1min','2007-10-01',NULL,0,'enabled');
 /*!40000 ALTER TABLE `scheduler_tasks` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -1720,7 +1721,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `zseq_scheduler_tasks` WRITE;
 /*!40000 ALTER TABLE `zseq_scheduler_tasks` DISABLE KEYS */;
-INSERT INTO `zseq_scheduler_tasks` VALUES (5);
+INSERT INTO `zseq_scheduler_tasks` VALUES (6);
 /*!40000 ALTER TABLE `zseq_scheduler_tasks` ENABLE KEYS */;
 UNLOCK TABLES;
 
