3404150a3caede6e04bc8be8eaa1abde546aa3b7
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-02-29 15:05:19 +0000

    Merged in from DEV trunk...
    KTS-3108
    "In bulk import gzip should be removed from the list of supported formats"
    Fixed. Removed the text.
    
    Committed by: Megan Watson
    Reviewed by: Conrad Vermeulen
    
    KTS-3112
    "Fatal error on new installation when directed to login.php line 168"
    Fixed. This just hides a symptom, but not the reall cause.
    
    Committed By: Conrad Vermeulen
    Reviewed By: Megan Watson
    
    KTS-3112
    "Fatal error on new installation when directed to login.php line 168"
    Fixed. Deleting plugin helper usually fixes this.
    
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/STABLE/trunk@8210 c91229c3-7414-0410-bfa2-8a42b809f60b

--- login.php
+++ login.php
@@ -165,8 +165,20 @@ class LoginPageDispatcher extends KTDispatcher {
         $aRegisteredLangs = $oReg->geti18nLanguages('knowledgeTree');
         $aLanguageNames = $oReg->getLanguages('knowledgeTree');
         $aRegisteredLanguageNames = array();
-        foreach (array_keys($aRegisteredLangs) as $sLang) {
-            $aRegisteredLanguageNames[$sLang] = $aLanguageNames[$sLang];
+
+        if (empty($aRegisteredLangs))
+        {
+   	    	$query = "DELETE FROM plugin_helper";
+    	    DBUtil::runQuery($query);
+        	redirect($_SERVER['REQUEST_URI']);
+        	exit;
+        }
+        else
+        {
+        	foreach (array_keys($aRegisteredLangs) as $sLang)
+        	{
+        		$aRegisteredLanguageNames[$sLang] = $aLanguageNames[$sLang];
+        	}
         }
         $sLanguageSelect = $default->defaultLanguage;
 
