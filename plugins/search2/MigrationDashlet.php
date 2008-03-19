2fa9ed35b6034cd491a6441c4aff95a8b44fced9
Megan Watson <megan@knowledgetree.com>
2008-03-19 10:17:34 +0000

    KTS-3181 "Scheduler: Migration process fails" Fixed. Added check if migrated documents = 0 then make them 1 for the division.
    Committed by: Megan Watson
    Reviewed by: Conrad Vermeulen
    
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@8284 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/search2/MigrationDashlet.php
+++ plugins/search2/MigrationDashlet.php
@@ -6,32 +6,32 @@
  * KnowledgeTree Open Source Edition
  * Document Management Made Simple
  * Copyright (C) 2004 - 2008 The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * You can contact The Jam Warehouse Software (Pty) Limited, Unit 1, Tramber Place,
  * Blake Street, Observatory, 7925 South Africa. or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
- * copyright notice. 
+ * must display the words "Powered by KnowledgeTree" and retain the original
+ * copyright notice.
  * Contributor( s): ______________________________________
  *
  */
@@ -45,6 +45,7 @@ class LuceneMigrationDashlet extends KTBaseDashlet
 
 	function is_active($oUser)
 	{
+	    return true;
 	    if (!Permission::userIsSystemAdministrator($oUser))
 	    {
 	    	return false;
@@ -86,7 +87,7 @@ class LuceneMigrationDashlet extends KTBaseDashlet
 			$migrationStartString = date('Y-m-d H:i:s', $migrationStart);
 			$migrationTime = KTUtil::getSystemSetting('migrationTime',0);
 			$migrationPeriod = KTUtil::computePeriod($migrationTime, '');
-			$timePerDocument = $migrationTime / $migratedDocuments;
+			$timePerDocument = $migrationTime / ($migratedDocuments > 0) ? $migratedDocuments : 1;
 			$estimatedPeriod = $timePerDocument * $migratingDocuments;
 			$estimatedTime = date('Y-m-d H:i:s', $migrationStart + $estimatedPeriod);
 			$estimatedPeriod = KTUtil::computePeriod($estimatedPeriod, '');
