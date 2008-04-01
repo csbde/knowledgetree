b4a5ea42e451ee3376b061ea1ae7dcf24d0093f9
Megan Watson <megan@knowledgetree.com>
2008-04-01 10:46:21 +0000

    KTS-3162 "Allow the user to change the ordering of the fields in a fieldset" Fixed. Added columns and logic for ordering the fields
    Committed by: Megan Watson
    Reviewed by: Jonathan Byrne
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@8335 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/install/data.sql
+++ sql/mysql/install/data.sql
@@ -175,7 +175,10 @@ UNLOCK TABLES;
 
 LOCK TABLES `document_fields` WRITE;
 /*!40000 ALTER TABLE `document_fields` DISABLE KEYS */;
-INSERT INTO `document_fields` VALUES (2,'Tag','STRING',0,0,0,2,0,'Tag Words'),(3,'Document Author','STRING',0,0,0,3,0,'Please add a document author'),(4,'Category','STRING',0,1,0,3,0,'Please select a category'),(5,'Media Type','STRING',0,1,0,3,0,'Please select a media type');
+INSERT INTO `document_fields` VALUES (2,'Tag','STRING',0,0,0,2,0,'Tag Words',0),
+(3,'Document Author','STRING',0,0,0,3,0,'Please add a document author',0),
+(4,'Category','STRING',0,1,0,3,0,'Please select a category',1),
+(5,'Media Type','STRING',0,1,0,3,0,'Please select a media type',2);
 /*!40000 ALTER TABLE `document_fields` ENABLE KEYS */;
 UNLOCK TABLES;
 
