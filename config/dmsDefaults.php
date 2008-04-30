de63b2cacd4e1b0d9bc41d00b4f37ab50ed9b31b
Conrad Vermeulen <conrad@knowledgetree.com>
2008-04-30 15:10:47 +0000

    KTS-3326 "Bug when loading cached config results in an error with array_key_exists." Fixed.
    Committed By: Conrad Vermeulen
    Reviewed By: Philip Arkoll
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@8413 c91229c3-7414-0410-bfa2-8a42b809f60b

--- config/dmsDefaults.php
+++ config/dmsDefaults.php
@@ -8,31 +8,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): Guenter Roeck______________________________________
  *
@@ -392,25 +392,25 @@ class KTInit {
 
 function catchFatalErrors()
 {
-	
+
 	$CustomErrorPage = KTCustomErrorViewer::getCustomErrorRedirectPage();
 	if($CustomErrorPage != '0')
 	{
 		ini_set('display_errors','On');
 	    $phperror='><div id="phperror" style="display:none">';
 		ini_set('error_prepend_string',$phperror);
-	
+
 		$sUrl = KTInit::guessRootUrl();
 		global $default;
 		$sRootUrl = ($default->sslEnabled ? 'https' : 'http') .'://'.$_SERVER['HTTP_HOST'].$sUrl;
-		
+
 		$CustomErrorPage = basename($CustomErrorPage);
-		
+
 		$phperror='</div>><form name="catcher" action="'.$sRootUrl.'/'.$CustomErrorPage.'" method="post" ><input type="hidden" name="fatal" value=""></form>
 		<script> document.catcher.fatal.value = document.getElementById("phperror").innerHTML; document.catcher.submit();</script>';
 		ini_set('error_append_string',$phperror);
 	}
-	
+
 }
 
 
@@ -481,10 +481,18 @@ function catchFatalErrors()
             $oKTConfig =& KTConfig::getSingleton();
             $oKTConfig->loadCache($cache_file);
 
-            foreach ($oKTConfig->flat as $k => $v) {
-                $default->$k = $oKTConfig->get($k);
+            // checking flatns as it should be more complete
+            if (empty($oKTConfig->flatns)) {
+                $use_cache = false;
             }
-        } else {
+            else  {
+                foreach ($oKTConfig->flat as $k => $v) {
+                    $default->$k = $oKTConfig->get($k);
+                }
+            }
+        }
+
+        if (!$use_cache) {
             $oKTConfig =& KTConfig::getSingleton();
 
 			$oKTConfig->setdefaultns('ui', 'appName', 'KnowledgeTree');
