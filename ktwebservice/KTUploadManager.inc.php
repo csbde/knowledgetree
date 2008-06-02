040531d2f44adfdfad5c6180a1e0a408d24c45f1
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-06-02 15:32:25 +0000

    Merged in from DEV trunk...
    WSA-127
    "Upload fails because of issue with the comparison of upload paths (SUP-787)"
    Fixed.
    
    Committed By: Conrad Vermeulen
    Reviewed By: Philip Arkoll
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/branches/3.5.3-Branch@8563 c91229c3-7414-0410-bfa2-8a42b809f60b

--- ktwebservice/KTUploadManager.inc.php
+++ ktwebservice/KTUploadManager.inc.php
@@ -10,31 +10,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  *
@@ -54,7 +54,7 @@ class KTUploadManager
 
 		$this->age = $config->get('webservice/uploadExpiry',60);
 		$this->temp_dir = $config->get('webservice/uploadDirectory');
-		$this->temp_dir = str_replace('\\','/', $this->temp_dir);
+		$this->temp_dir = strtolower(str_replace('\\','/', $this->temp_dir));
 	}
 
 	/**
@@ -79,7 +79,7 @@ class KTUploadManager
 	function is_valid_temporary_file($tempfilename)
 	{
 		$tempdir = substr($tempfilename,0,strlen($this->temp_dir));
-		$tempdir = str_replace('\\','/', $tempdir);
+		$tempdir = strtolower(str_replace('\\','/', $tempdir));
 		return ($tempdir == $this->temp_dir);
 	}
 
