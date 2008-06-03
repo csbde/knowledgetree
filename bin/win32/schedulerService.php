0fe6427abf3de5452308878d83ae788c3d5da125
megan_w <megan_w@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-06-03 08:49:52 +0000

    KTS-3406 "CLONE -KTScheduler Service does not start Automaticaly (SUP-835)" Fixed. Added a 2 minute sleep to ensure mysql is running before starting the service.
    Committed by: Megan Watson
    Reviewed by: Conrad Vermeulen
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@8566 c91229c3-7414-0410-bfa2-8a42b809f60b

--- bin/win32/schedulerService.php
+++ bin/win32/schedulerService.php
@@ -7,31 +7,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  *
@@ -42,6 +42,10 @@ $myservicename = 'ktscheduler';
 // Connect to service dispatcher and notify that startup was successful
 if (!win32_start_service_ctrl_dispatcher($myservicename)) die('Could not connect to service :'.$myservicename);
 win32_set_service_status(WIN32_SERVICE_RUNNING);
+
+// Scheduler is dependent on the mysql server being up,
+// so we sleep for a minute to ensure the server is running before we start the scheduler
+sleep(120);
 
 chdir(dirname(__FILE__)); // need to be here to include dmsDefaults
 require_once('../../config/dmsDefaults.php');
