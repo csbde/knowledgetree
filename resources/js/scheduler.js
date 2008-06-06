04a199b65186e0b99bac76c421dc6cb12f733a8a
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-06-06 12:28:33 +0000

    Merged in from DEV trunk...
    KTC-541
    "On Task Scheduler Management Page in web interface - 'Frequency' drop-down and save button combo jumps"
    Fixed. The drop-down form is now displayed in the table cell and not as a floating div.
    
    KTC-542
    "Task Scheduler Management Page - remove 'Clean-up old tasks' link at the bottom of the page"
    Fixed. Removed the link.
    
    KTS-3417
    "Scheduler - replace YUI with ExtJS"
    Fixed. Removed the yui ajax, used an extjs request.
    
    Committed by: Megan Watson
    Reviewed by: Conrad Vermeulen
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/branches/3.5.3-Branch@8580 c91229c3-7414-0410-bfa2-8a42b809f60b

--- resources/js/scheduler.js
+++ resources/js/scheduler.js
@@ -1,31 +1,34 @@
-
+// Clean up tasks marked as completed
 var clearTasks = function(sUrl) {
 
-    var callback = {
-        success: function(o) {
-            alert('Tasks have been successfully cleaned up');
+    Ext.Ajax.request({
+        url: sUrl,
+        success: function(response) {
+            alert('The tasks have been successfully cleaned up');
         },
-        failure: function(o) {
-            alert('Clean up failed!');
+        failure: function(response) {
+            alert('Error. The clean up failed.');
         }
-    }
-
-    var transaction = YAHOO.util.Connect.asyncRequest('GET', sUrl, callback);
+    });
 }
 
-//<!-- Change the run time to now -->
+//<!-- Reschedule the task to run the next time the scheduler runs -->
 var runOnNext = function(fId, sUrl) {
     var runDiv = document.getElementById('runDiv'+fId);
 
     //<!-- Display the new runtime -->
     var displayDate = formatDate('','no');
 
-    var callback = {
-        success: function(o) {
+    Ext.Ajax.request({
+        url: sUrl,
+        success: function(response) {
             runDiv.innerHTML = displayDate;
-        }
-    }
-    var transaction = YAHOO.util.Connect.asyncRequest('POST', sUrl, callback, "fId="+fId);
+        },
+        failure: function(response) {
+            alert('Error. The update failed, please refresh the page and try again.');
+        },
+        params: { fId: fId }
+    });
 }
 
 //<!-- Enable / disable the task -->
@@ -34,52 +37,67 @@ var toggleStatus = function(fId, sUrl, sDisableText, sEnableText) {
     var statusLink = document.getElementById('statusLink'+fId);
     var freqLink = document.getElementById('freqLink'+fId);
     var runnowLink = document.getElementById('runnowLink'+fId);
-    var freqDrop = document.getElementById('freqDrop'+fId);
     var fontClass = document.getElementById('font'+fId);
     var freqDiv = document.getElementById('div'+fId);
+    var runDiv = document.getElementById('runDiv'+fId);
+    var freq = document.getElementById('freq_'+fId).value;
 
-    var callback = {
-        success: function(o) {
-            if(statusLink.innerHTML == sDisableText){
-                statusLink.innerHTML = sEnableText;
+    var date = new Date();
+    var msNow = date.getTime();
+    var now = parseInt(msNow / 1000);
+    var runTime = calculateFreq(freq, now);
+    var displayDate = formatDate(runTime);
+
+    Ext.Ajax.request({
+        url: sUrl,
+        success: function(response) {
+            if(statusLink.value == sDisableText){
+                statusLink.value = sEnableText;
                 freqLink.style.visibility = "hidden";
                 runnowLink.style.visibility = "hidden";
-                freqDrop.style.visibility = "hidden";
+                runDiv.style.visibility = 'hidden';
                 fontClass.className = 'descriptiveText';
                 freqDiv.style.display = "none";
             }else{
-                statusLink.innerHTML = sDisableText;
+                statusLink.value = sDisableText;
                 freqLink.style.visibility = "visible";
                 runnowLink.style.visibility = "visible";
-                freqDrop.style.visibility = "visible";
                 freqDiv.style.display = "block";
+                freqDiv.style.visibility = "visible";
                 fontClass.className = '';
+
+                runDiv.style.visibility = 'visible';
+                runDiv.innerHTML = displayDate;
             }
-        }
-    }
-    var transaction = YAHOO.util.Connect.asyncRequest('POST', sUrl, callback, "fId="+fId);
+        },
+        failure: function(response) {
+            alert('Error. The status update failed, please refresh the page and try again.');
+        },
+        params: { fId: fId }
+    });
 }
 
+// Display the form to changed the frequency at which the task runs
 var showFrequencyDiv = function(fId) {
     var formDiv = document.getElementById('formDiv');
-    var fInput = document.getElementById('fId');
-    var tblCol = document.getElementById('tblCol');
-    var freqDiv = document.getElementById('div'+fId);
-    var posFreq = YAHOO.util.Dom.getXY('div'+fId);
 
-    //<!-- Hide the frequency and display the form in place -->
-    formDiv.style.display = "block";
-    freqDiv.style.display = "none";
-    formDiv.style.visibility = "visible";
-    freqDiv.style.visibility = "hidden";
+    if(formDiv.innerHTML == ''){
+        return;
+    }
+
+    var fInput = document.getElementById('fId');
+    var tblCol = document.getElementById('col_'+fId);
+
     fInput.value = fId;
-    tblCol.width = "20%";
-    YAHOO.util.Dom.setXY('formDiv', posFreq);
+    tblCol.innerHTML = formDiv.innerHTML;
+    tblCol.style.display = 'block';
+    formDiv.innerHTML = '';
 }
 
+// Save the new frequency
 var saveFreq = function(sUrl) {
     var fId = document.getElementById('fId').value;
-    var tblCol = document.getElementById('tblCol');
+    var tblCol = document.getElementById('col_'+fId);
     var formDiv = document.getElementById('formDiv');
     var freqDiv = document.getElementById('div'+fId);
     var runDiv = document.getElementById('runDiv'+fId);
@@ -89,32 +107,35 @@ var saveFreq = function(sUrl) {
     var freq = fSelect.options[fSelect.selectedIndex].value;
     var freqLabel = fSelect.options[fSelect.selectedIndex].label;
 
+    // Move the form content back to the form div
+    formDiv.innerHTML = tblCol.innerHTML;
+    tblCol.innerHTML = '';
+    tblCol.style.display = 'none';
+
     //<!-- Work out new runtime using the frequency -->
     var prevNum = new Number(prevInput);
     var nextTime = calculateFreq(freq, prevNum);
     var displayDate = formatDate(nextTime);
 
-    var callback = {
-        success: function(o) {
+    Ext.Ajax.request({
+        url: sUrl,
+        success: function(response) {
             freqDiv.innerHTML = freqLabel;
             runDiv.innerHTML = displayDate;
+            document.getElementById('freq_'+fId).value = freq;
+        },
+        failure: function(response) {
+            alert('Error. The frequency update failed, please refresh the page and try again.');
+        },
+        params: {
+            frequency: freq,
+            fId: fId
         }
-    }
-
-    YAHOO.util.Connect.setForm(freqForm);
-    var transaction = YAHOO.util.Connect.asyncRequest('POST', sUrl, callback);
-
-    tblCol.width = "";
-    formDiv.style.display = "none";
-    freqDiv.style.display = "block";
-    formDiv.style.visibility = "hidden";
-    freqDiv.style.visibility = "visible";
+    });
 }
 
-//<!-- Calculate the next run time based on the previous runtime -->
+//<!-- Calculate the next run time based on the given runtime -->
 var calculateFreq = function(freq, prev) {
-
-    var curDate = new Date();
     var iDiff = 0;
 
     switch(freq) {
@@ -155,16 +176,23 @@ var calculateFreq = function(freq, prev) {
         }
 
         var iNextTime = prev + iDiff;
+
         return iNextTime;
 }
 
 //<!-- Return a formatted date given a unix timestamp -->
+// If the date if prior to the current date then return the current date
 var formatDate = function(unixTime, useUnix) {
     if(useUnix == 'no') {
         var newDate = new Date();
     }else{
         var milliTime = unixTime * 1000;
         var newDate = new Date(milliTime);
+
+        var curDate = new Date();
+        if(newDate < curDate){
+            newDate = curDate;
+        }
     }
 
     var year = newDate.getFullYear();
