bd7a6e28ed9912cbd6e4c2ac13322952b6a2883b
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-06-18 14:39:13 +0000

    Merged in from DEV trunk...
    KTS-3440
    "Restart open office periodically"
    Fixed. A count is incremented on indexing a document (successfully or not), after 50 documents open office is restarted.
    
    Committed by: Megan Watson
    Reviewed by: Conrad Vermuelen
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/branches/3.5.3-Branch@8636 c91229c3-7414-0410-bfa2-8a42b809f60b

--- search2/indexing/indexerCore.inc.php
+++ search2/indexing/indexerCore.inc.php
@@ -7,31 +7,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  *
@@ -503,6 +503,26 @@ abstract class Indexer
         DBUtil::runQuery($sql);
 
         $default->log->debug("index: Queuing indexing of $document_id");
+
+    }
+
+    private static function incrementCount()
+    {
+        // Get count from system settings
+        $count = Indexer::getIndexedDocumentCount();
+        $count = (int)$count + 1;
+        Indexer::updateIndexedDocumentCount($count);
+    }
+
+    public static function getIndexedDocumentCount()
+    {
+        $count = KTUtil::getSystemSetting('indexedDocumentCount', 0);
+        return (int) $count;
+    }
+
+    public static function updateIndexedDocumentCount($cnt = 0)
+    {
+        KTUtil::setSystemSetting('indexedDocumentCount', $cnt);
     }
 
 	public static function reindexQueue()
@@ -965,6 +985,9 @@ abstract class Indexer
 
         foreach($result as $docinfo)
         {
+        // increment indexed documents count
+        Indexer::incrementCount();
+
         	$docId=$docinfo['document_id'];
         	$extension=$docinfo['filetypes'];
         	$mimeType=$docinfo['mimetypes'];
