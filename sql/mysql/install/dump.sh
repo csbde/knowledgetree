49e7b632177126997e0f9f5d70e25c481af71cf2
conradverm <conradverm@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-06-26 13:52:33 +0000

    KTS-3424 "Remove reliance on zseq_tables" Fixed. Now can now deal with the auto inc on the tables. dbutil is bypassing peardb for the id, but will be rectified in 3.6.
    Committed By: Conrad Vermeulen
    Reviewed By: Megan Watson
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@8704 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/install/dump.sh
+++ sql/mysql/install/dump.sh
@@ -14,5 +14,13 @@ DATE=`date +%Y-%m-%d-%H-%M-%S`
 # Create the Structure Dump
 mysqldump -u root -p $DB --no-data --skip-add-drop-table > structure-$DATE.sql
 
+cat structure-$DATE.sql | sed 's/ AUTO_INCREMENT=[0-9]*//g' > structure.tmp
+mv structure.tmp structure-$DATE.sql
+
 # Create the Data Dump
 mysqldump -u root -p $DB --no-create-info > data-$DATE.sql
+
+
+sed "s/[)],[(]/),\n(/g"  data-$DATE.sql > data.tmp
+sed "s/VALUES [(]/VALUES\n(/g"  data.tmp >  data-$DATE.sql
+rm data.tmp
