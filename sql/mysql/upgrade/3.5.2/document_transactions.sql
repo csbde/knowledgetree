25225fcf3e561b8ab83830eda3ef2a10d24ebea1
Conrad Vermeulen <conrad@knowledgetree.com>
2008-06-27 08:50:20 +0000

    KTS-3130 "Failed Upgrade 3.4.6 to 3.5.2 Stable (database problem)" Fixed. text and blobs cannot have default values.
    Committed By: Conrad Vermeulen
    Reviewed By: Megan Waston
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@8707 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/3.5.2/document_transactions.sql
+++ sql/mysql/upgrade/3.5.2/document_transactions.sql
@@ -1,6 +1,6 @@
 alter table document_transactions change version version float default NULL;
 alter table document_transactions change ip ip varchar(15);
-alter table document_transactions change filename filename mediumtext NOT NULL default '';
-alter table document_transactions change comment comment mediumtext NOT NULL default '';
+alter table document_transactions change filename filename mediumtext NOT NULL;
+alter table document_transactions change comment comment mediumtext NOT NULL;
 alter table document_transactions change transaction_namespace transaction_namespace varchar(255);
 alter table document_transactions add index (`datetime`,`transaction_namespace`);
