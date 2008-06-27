e3e7d2971e3b7f2b7e2d1d528f64cb326760de5d
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-06-27 10:49:19 +0000

    KTS-3424 "Remove reliance on zseq_tables" Updated. Fixed dump script typo.
    Committed By: Kevin Fourie
    Reviewed By: Conrad Vermeulen
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@8714 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/install/dump.sh
+++ sql/mysql/install/dump.sh
@@ -55,13 +55,13 @@ echo $HEADER > data-$DATE.sql
 # Create the Structure Dump
 mysqldump -u root -p $DB --no-data --skip-add-drop-table >> structure-$DATE.sql
 
-cat structure-$DATE.sql | sed 's/ AUTO_INCREMENT=[0-9]*//g' >> structure.tmp
+cat structure-$DATE.sql | sed 's/ AUTO_INCREMENT=[0-9]*//g' > structure.tmp
 mv structure.tmp structure-$DATE.sql
 
 # Create the Data Dump
 mysqldump -u root -p $DB --no-create-info >> data-$DATE.sql
 
 
-sed "s/[)],[(]/),\n(/g"  data-$DATE.sql >> data.tmp
-sed "s/VALUES [(]/VALUES\n(/g"  data.tmp >>  data-$DATE.sql
+sed "s/[)],[(]/),\n(/g"  data-$DATE.sql > data.tmp
+sed "s/VALUES [(]/VALUES\n(/g"  data.tmp >  data-$DATE.sql
 rm data.tmp
