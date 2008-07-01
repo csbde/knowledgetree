38ef212fbea15abf83771897601dbff848012606
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-07-01 14:04:54 +0000

    Merged in from DEV trunk...
    Some ws updates.
    
    KTC-565
    "Disabling the automatic workflow associated with a document type does not stop it from being assigned to a new document."
    Fixed. Added a check to only get enabled workflows.
    
    Committed by: Megan Watson
    Reviewed by: Jonathan Byrne
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/branches/3.5.3-Branch@8743 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktstandard/workflow/TypeAssociator.php
+++ plugins/ktstandard/workflow/TypeAssociator.php
@@ -6,31 +6,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  *
@@ -97,8 +97,11 @@ class DocumentTypeWorkflowAssociator extends KTWorkflowAssociationHandler {
     function getWorkflowForType($iDocTypeId, $oDocument) {
         if (is_null($iDocTypeId)) { return null; }
 
+        // Link to the workflows table to ensure disabled workflows aren't associated
         $sQuery = 'SELECT `workflow_id` FROM ' . KTUtil::getTableName('type_workflow_map');
-        $sQuery .= ' WHERE `document_type_id` = ?';
+        $sQuery .= ' LEFT JOIN workflows w ON w.id = m.workflow_id
+            WHERE document_type_id = ? AND w.enabled = 1';
+
         $aParams = array($iDocTypeId);
         $res = DBUtil::getOneResultKey(array($sQuery, $aParams), 'workflow_id');
 
