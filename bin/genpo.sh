95edaf4b6eb74ce8bf0fc808b3c36fab27344e8f
Kevin Fourie <kevin@knowledgetree.com>
2008-07-10 10:59:22 +0000

    OEM-190 "Remove reference to KnowledgeTree in OEM advanced search" Fixed. Reference removed. Updated i18n files too.
    Committed by: Kevin Fourie
    Reviewed by: Jonathan Byrne
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@8773 c91229c3-7414-0410-bfa2-8a42b809f60b

--- bin/genpo.sh
+++ bin/genpo.sh
@@ -5,9 +5,26 @@ cd $DIR
 cd ..
 pwd
 
+#pull in comm stuff
+cp -a ../Commercial-Plugins-DEV-trunk/conditional-metadata plugins/
+cp -a ../Commercial-Plugins-DEV-trunk/custom-numbering plugins/
+cp -a ../Commercial-Plugins-DEV-trunk/documentcomparison plugins/
+cp -a ../Commercial-Plugins-DEV-trunk/documentpreview plugins/
+cp -a ../Commercial-Plugins-DEV-trunk/network plugins/
+cp -a ../Commercial-Plugins-DEV-trunk/professional-reporting plugins/
+cp -a ../Commercial-Plugins-DEV-trunk/wintools plugins/
+
 rm -f i18n/templates.c
 find resources -name "*.js" | sort | python ./bin/jsi18n.py > templates/ktcore/javascript_i18n.smarty
 php bin/smarty_to_gettext.php . > i18n/templates.c
 find . -type f -name "*.php" -o -name "*.inc" | sort | xgettext --no-wrap -d knowledgeTree -L PHP -s -f - --keyword=_kt -o i18n/knowledgeTree.pot
 echo i18n/templates.c i18n/transactions.c i18n/permissions.c | xargs -n 1 | sort | xgettext --no-wrap -d knowledgeTree -j -s -f - -o i18n/knowledgeTree.pot
 
+#remove comm stuff again
+rm -rf plugins/conditional-metadata
+rm -rf plugins/custom-numbering
+rm -rf plugins/documentcomparison
+rm -rf plugins/documentpreview
+rm -rf plugins/network
+rm -rf plugins/professional-reporting
+rm -rf plugins/wintools
