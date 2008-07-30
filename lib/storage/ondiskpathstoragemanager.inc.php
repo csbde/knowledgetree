8dc4a506f8cbd171f3cb68854a222f46166be7a8
Megan Watson <megan@knowledgetree.com>
2008-07-30 13:33:48 +0000

    KTS-3103 "addDocument error when using KTOnDiskPathStorageManager: uploadTmpFile function does not exist" Fixed. Added the uploadTmpFile function.
    Committed by: Megan Watson
    Reviewed by: Conrad Vermeulen
    
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@8882 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/storage/ondiskpathstoragemanager.inc.php
+++ lib/storage/ondiskpathstoragemanager.inc.php
@@ -16,31 +16,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  */
@@ -81,6 +81,29 @@ class KTOnDiskPathStorageManager extends KTStorageManager {
         } else {
             return false;
         }
+    }
+
+    /**
+     * Upload a temporary file
+     *
+     * @param unknown_type $sUploadedFile
+     * @param unknown_type $sTmpFilePath
+     * @return unknown
+     */
+    function uploadTmpFile($sUploadedFile, $sTmpFilePath) {
+
+        //copy the file accross
+        if (OS_WINDOWS) {
+            $sTmpFilePath = str_replace('\\','/',$sTmpFilePath);
+        }
+        if ($this->writeToFile($sUploadedFile, $sTmpFilePath)) {
+            if (file_exists($sTmpFilePath)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
     }
 
     function getPath(&$oDocument) {
@@ -406,7 +429,7 @@ class KTOnDiskPathStorageManager extends KTStorageManager {
         }
         return true;
 	}
-	
+
 	/**
 	 * Completely remove a document version
 	 *
@@ -417,7 +440,7 @@ class KTOnDiskPathStorageManager extends KTStorageManager {
 	    $sDocumentRoot = $oConfig->get('urls/documentRoot');
 	    $iContentId = $oVersion->getContentVersionId();
         $oContentVersion = KTDocumentContentVersion::get($iContentId);
-        
+
 	    $sPath = $oContentVersion->getStoragePath();
 	    $sFullPath = sprintf("%s/%s", $sDocumentRoot, $sPath);
 	    if (file_exists($sFullPath)) {
