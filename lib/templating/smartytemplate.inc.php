d9062df715c08779aa24af81f6238b0bb79e3b7f
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-08-01 10:14:33 +0000

    Merged in from STABLE trunk...
    KTS-3376
    "The "recent search list" that shows up in the search portlet is sharing everyone's recent searches"
    Fixed.
    
    Committed By: Conrad Vermeulen
    Reviewed By: Megan Watson
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/STABLE/branches/3.5.3-Release-Branch@8925 c91229c3-7414-0410-bfa2-8a42b809f60b

--- lib/templating/smartytemplate.inc.php
+++ lib/templating/smartytemplate.inc.php
@@ -8,31 +8,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  */
@@ -104,6 +104,8 @@ class KTSmartyTemplate extends KTTemplate {
 			$_SESSION['search2_quickQuery'] = '';
         }
 
+        $smarty->assign('search2_anonymous', !array_key_exists('userID', $_SESSION) || ($_SESSION['userID'] == -2));
+        $smarty->assign('search2_user', $_SESSION['userId']);
         $smarty->assign('search2_quick', $search2_quick);
         $smarty->assign('search2_general', $search2_general);
         $smarty->assign('search2_quickQuery', $search2_quickQuery);
