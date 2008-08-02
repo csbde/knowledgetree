6f8781f002b22b064e1c690bcefbcbc733b98204
Kevin Fourie <kevin@knowledgetree.com>
2008-08-02 14:57:55 +0000

    KTS-3415 "Add work spaces plugin to on-premise" Updated. Corrected license header.
    Committed By: Kevin Fourie
    Reviewed By: Conrad Vermeulen
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@8936 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/MyDropDocumentsPlugin/MyDropDocumentsPage.php
+++ plugins/MyDropDocumentsPlugin/MyDropDocumentsPage.php
@@ -1,28 +1,37 @@
 <?php
-/*
- * The contents of this file are subject to the KnowledgeTree Public
- * License Version 1.1.2 ("License"); You may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.knowledgetree.com/KPL
- * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
- * See the License for the specific language governing rights and
- * limitations under the License.
+/**
+ * $Id: $
  *
- * All copies of the Covered Code must include on each user interface screen:
- *    (i) the "Powered by KnowledgeTree" logo and
- *    (ii) the KnowledgeTree copyright notice
- * in the same form as they appear in the distribution.  See the License for
- * requirements.
+ * KnowledgeTree Community Edition
+ * Document Management Made Simple
+ * Copyright (C) 2008 KnowledgeTree Inc.
+ * Portions copyright The Jam Warehouse Software (Pty) Limited
  * 
- * The Original Code is: KnowledgeTree Open Source
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 3 as published by the
+ * Free Software Foundation.
  * 
- * The Initial Developer of the Original Code is The Jam Warehouse Software
- * (Pty) Ltd, trading as KnowledgeTree.
- * Portions created by The Jam Warehouse Software (Pty) Ltd are Copyright
- * (C) 2007 The Jam Warehouse Software (Pty) Ltd;
- * All Rights Reserved.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ * California 94120-7775, or email info@knowledgetree.com.
+ * 
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ * 
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
+ * must display the words "Powered by KnowledgeTree" and retain the original 
+ * copyright notice.
  * Contributor( s): ______________________________________
  *
  */
