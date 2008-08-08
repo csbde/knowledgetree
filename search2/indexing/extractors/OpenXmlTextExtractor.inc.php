1fb9c5e2f1eaae0dc0663cd65c01a2bdb31c5681
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-08-08 16:02:04 +0000

    Merged in from STABLE trunk...
    KTS-3456
    "Allow indexing to not be dependant on open office"
    Fixed.  Unzip requires a file to have an extension in wondows. Renamed tmp directories to be cleaned up by cleanup script.
    
    Committed By: Conrad Vermeulen
    Reviewed By: Kevin Fourie
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/STABLE/branches/3.5.3-Release-Branch@9049 c91229c3-7414-0410-bfa2-8a42b809f60b

--- search2/indexing/extractors/OpenXmlTextExtractor.inc.php
+++ search2/indexing/extractors/OpenXmlTextExtractor.inc.php
@@ -7,31 +7,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  *
@@ -58,6 +58,11 @@ class OpenXmlTextExtractor extends ExternalDocumentExtractor
 	public function getDisplayName()
 	{
 		return _kt('Open Xml Text Extractor');
+	}
+
+	public function needsIntermediateSourceFile()
+	{
+		return true;
 	}
 
 	/**
@@ -147,7 +152,7 @@ class OpenXmlTextExtractor extends ExternalDocumentExtractor
 		$temp_dir = $config->get('urls/tmpDirectory');
 
 		$docid = $this->document->getId();
-		$time = 'openxml_'. time() . '-' . $docid;
+		$time = 'ktindexer_openxml_'. time() . '-' . $docid;
 		$this->openxml_dir = $temp_dir . '/' . $time;
 
 		$this->sourcefile = str_replace('\\','/',$this->sourcefile);
