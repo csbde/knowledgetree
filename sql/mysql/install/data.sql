e325b9cbf4c441abd08806c9b03f30f58cd62f6f
Kevin Fourie <kevin@knowledgetree.com>
2008-08-11 21:44:39 +0000

    KTS-3596 "Remove old search plugins when upgrading from 3.4.6 to 3.5.3" Fixed. Added removal during upgrade.
    Committed by: Kevin Fourie
    Reviewed by: Megan Watson
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@9091 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/install/data.sql
+++ sql/mysql/install/data.sql
@@ -1707,9 +1707,11 @@ INSERT INTO `upgrades` VALUES
 (192,'sql*3.5.3*0*3.5.3/db_optimizations.sql','Database optimizations.','2008-07-30 00:00:00',1,'upgrade*3.5.3*99*upgrade3.5.3'),
 (193,'sql*3.5.3*0*3.5.3/del_adminversion_plugin.sql','Remove the old Admin Version Notifier.','2008-07-30 00:00:00',1,'upgrade*3.5.3*99*upgrade3.5.3'),
 (194,'func*3.5.3*0*removeAdminVersionNotifier','Remove the old Admin Version Notifier files','2008-07-30 00:00:00',1,'upgrade*3.5.3*99*upgrade3.5.3'),
-(195,'sql*3.5.3*0*3.5.3/add_autoinc.sql','Add auto increment to tables SQL.','2008-07-30 00:00:00',1,'upgrade*3.5.3*99*upgrade3.5.3'),
-(196,'func*3.5.3*0*addAutoIncrementToTables','Add auto increment.','2008-07-30 00:00:00',1,'upgrade*3.5.3*99*upgrade3.5.3'),
-(197,'upgrade*3.5.3*99*upgrade3.5.3','Upgrade from version 3.5.2 to 3.5.3','2008-03-30 00:00:00',1,'upgrade*3.5.3*99*upgrade3.5.3');
+(195,'sql*3.5.3*0*3.5.3/del_oldsearch_plugins.sql','Remove the old Search Plugins SQL.','2008-07-30 00:00:00',1,'upgrade*3.5.3*99*upgrade3.5.3'),
+(196,'func*3.5.3*0*removeOldSearchPlugins','Remove the old Search Plugins files','2008-07-30 00:00:00',1,'upgrade*3.5.3*99*upgrade3.5.3'),
+(197,'sql*3.5.3*0*3.5.3/add_autoinc.sql','Add auto increment to tables SQL.','2008-07-30 00:00:00',1,'upgrade*3.5.3*99*upgrade3.5.3'),
+(198,'func*3.5.3*0*addAutoIncrementToTables','Add auto increment.','2008-07-30 00:00:00',1,'upgrade*3.5.3*99*upgrade3.5.3'),
+(199,'upgrade*3.5.3*99*upgrade3.5.3','Upgrade from version 3.5.2 to 3.5.3','2008-03-30 00:00:00',1,'upgrade*3.5.3*99*upgrade3.5.3');
 /*!40000 ALTER TABLE `upgrades` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -2640,7 +2642,7 @@ UNLOCK TABLES;
 LOCK TABLES `zseq_upgrades` WRITE;
 /*!40000 ALTER TABLE `zseq_upgrades` DISABLE KEYS */;
 INSERT INTO `zseq_upgrades` VALUES
-(197);
+(199);
 /*!40000 ALTER TABLE `zseq_upgrades` ENABLE KEYS */;
 UNLOCK TABLES;
 
