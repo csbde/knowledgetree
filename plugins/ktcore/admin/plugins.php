d88615c74c35416bb90fbb69b68450747108ff1b
Megan Watson <megan@knowledgetree.com>
2008-08-15 10:29:01 +0000

    KTS-3625 "Fatal PEAR_error when clicking the 'Update' for the plugins after 346OSS to 353 OSS upgrade." Fixed. Added check on tagcloud plugin before calling getDisabled()
    Committed by: Megan Watson
    Reviewed by: Conrad Vermeulen
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@9177 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/admin/plugins.php
+++ plugins/ktcore/admin/plugins.php
@@ -6,31 +6,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  *
@@ -121,31 +121,31 @@ class KTPluginDispatcher extends KTAdminDispatcher {
         }
         KTPluginEntity::clearAllCaches();
 
+        // FIXME!!! Plugin manager needs to be updated to deal with this situation. This code should be in the plugin.
         //enabling or disabling Tag fieldset depending on whether tag cloud plugin is enabled or disabled.
         //Get tag cloud object
         $oTagClouPlugin = KTPluginEntity::getByNamespace('ktcore.tagcloud.plugin');
-        if($oTagClouPlugin->getDisabled() == '1')
-        {
-        	//disable tag fieldset
-        	$aFV = array(
-                'disabled' => true,
-            );
-            $aWFV = array(
-                'namespace' => 'tagcloud'
-            );
-        	$res = DBUtil::whereUpdate('fieldsets', $aFV, $aWFV);
-        }
-        //if Tag Cloud plugin disabled
-        if($oTagClouPlugin->getDisabled() == '0')
-        {
-        	//enable tag fieldset
-        	$aFV = array(
-                'disabled' => false,
-            );
-            $aWFV = array(
-                'namespace' => 'tagcloud'
-            );
-        	$res = DBUtil::whereUpdate('fieldsets', $aFV, $aWFV);
+        if(!PEAR::isError($oTagClouPlugin) && !is_a($oTagClouPlugin, 'KTEntityNoObjects') && !is_null($oTagClouPlugin)){
+            if($oTagClouPlugin->getDisabled() == '1')
+            {
+            	//disable tag fieldset
+            	$aFV = array(
+                    'disabled' => true,
+                );
+                $aWFV = array(
+                    'namespace' => 'tagcloud'
+                );
+            	$res = DBUtil::whereUpdate('fieldsets', $aFV, $aWFV);
+            } else {
+            	//enable tag fieldset
+            	$aFV = array(
+                    'disabled' => false,
+                );
+                $aWFV = array(
+                    'namespace' => 'tagcloud'
+                );
+            	$res = DBUtil::whereUpdate('fieldsets', $aFV, $aWFV);
+            }
         }
 
         $this->successRedirectToMain(_kt('Plugins updated'));
