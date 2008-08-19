b4e5aa4779e5f9067f878d342eec39930dfe4516
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-08-19 14:35:00 +0000

    Merged in from STABLE trunk...
    KTC-586
    "Improve the mime icons for shortcuts"
    Fixed. Added new mime types.
    
    Committed by: Megan Watson
    Reviewed by: Kevin Fourie
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/STABLE/branches/3.5.3a-Release-Branch@9194 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/KTColumns.inc.php
+++ plugins/ktcore/KTColumns.inc.php
@@ -6,31 +6,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  */
@@ -117,7 +117,7 @@ class AdvancedTitleColumn extends AdvancedColumn {
     	}else{
     		$iDocId = $aDataRow["document"]->getId();
     	}
-		
+
         $url = KTBrowseUtil::getUrlForDocument($iDocId);
         if($aDataRow['document']->isSymbolicLink()){
         	$aDataRow['document']->switchToRealCore();
@@ -171,10 +171,10 @@ class AdvancedTitleColumn extends AdvancedColumn {
         } else {
         	$contenttype = $this->_mimeHelper($aDataRow["document"]->getMimeTypeId());
            	$link = $this->renderDocumentLink($aDataRow);
-           	
+
            	//Render an image instead of the size in case of a shortcut
-           if($aDataRow['document']->isSymbolicLink()){           	
-	           	return sprintf('<span class="contenttype %s">%s <img src="resources/tango-icons/shortcut.png" /></span>', $contenttype, $link);
+           if($aDataRow['document']->isSymbolicLink()){
+               return sprintf('<span class="contenttype %s shortcut">%s</span>', $contenttype, $link);
            }else{
           		 $size = $this->prettySize($aDataRow["document"]->getSize());
            		return sprintf('<span class="contenttype %s">%s (%s)</span>', $contenttype, $link, $size);
@@ -416,7 +416,7 @@ class AdvancedSingleSelectionColumn extends AdvancedSelectionColumn {
             return '&nbsp;';
         }
 
-        $return =  '<input type="radio" name="' . $localname . '" value="' . $v . '" ';  
+        $return =  '<input type="radio" name="' . $localname . '" value="' . $v . '" ';
         if($this->show_folders && $this->show_documents){
         	$return .= 'onClick="forceSingleSelect(this)" ';
         }
