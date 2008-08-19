4c59373a90167c81e38816cfabf351af716674fa
Megan Watson <megan@knowledgetree.com>
2008-08-19 13:35:58 +0000

    KTS-3626 "CLONE -Can't authenticate RSS feed(SUP-979)" Fixed. The authentication was calling the database directly without checking for authentication sources.
    Committed by: Megan Watson
    Reviewed by: Kevin Fourie
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@9191 c91229c3-7414-0410-bfa2-8a42b809f60b

--- rss.php
+++ rss.php
@@ -6,48 +6,39 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  */
 
 require_once('config/dmsDefaults.php');
 require_once(KT_LIB_DIR .'/authentication/DBAuthenticator.inc');
+require_once(KT_LIB_DIR .'/authentication/authenticationutil.inc.php');
 require_once(KT_DIR. '/plugins/rssplugin/KTrss.inc.php');
-require_once(KT_LIB_DIR . '/browse/browseutil.inc.php');
-
-// widget includes.
-require_once(KT_LIB_DIR . "/widgets/portlet.inc.php");
-require_once(KT_LIB_DIR . "/widgets/fieldsetDisplay.inc.php");
-require_once(KT_LIB_DIR . "/widgets/FieldsetDisplayRegistry.inc.php");
-require_once(KT_LIB_DIR . "/actions/documentaction.inc.php");
-require_once(KT_LIB_DIR . "/browse/browseutil.inc.php");
-
-require_once(KT_LIB_DIR . '/mime.inc.php');
 
 // workaround to get http authentication working in cgi mode
 $altinfo = KTUtil::arrayGet( $_SERVER, 'kt_auth', KTUtil::arrayGet( $_SERVER, 'REDIRECT_kt_auth'));
@@ -146,6 +137,12 @@ if (!validateUser($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) {
 
 // Validate user credentials
 function validateUser($username, $password){
-    return DBAuthenticator::checkPassword($username, $password);
+    //return DBAuthenticator::checkPassword($username, $password);
+    $oUser =& User::getByUsername($username);
+    if(PEAR::isError($oUser) || is_a($oUser, 'KTEntityNoObjects')){
+        return false;
+    }
+
+    return KTAuthenticationUtil::checkPassword($oUser, $password);
 }
 ?>
