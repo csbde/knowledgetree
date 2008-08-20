e41eadff8700f42a115b8f9e5256af20922bf805
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-08-20 08:22:53 +0000

    Merged in from STABLE trunk...
    KTS-3631
    "Custom error page throws an error: Undefined index: HTTPS"
    Fixed. Now checks if the $_SERVER['HTTPS'] variable is set.
    
    Committed by: Megan Watson
    Reviewed by: Kevin Fourie
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/STABLE/branches/3.5.3a-Release-Branch@9197 c91229c3-7414-0410-bfa2-8a42b809f60b

--- customerrorpage.php
+++ customerrorpage.php
@@ -6,31 +6,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  */
@@ -38,13 +38,13 @@
 if (array_key_exists('fatal', $_POST))
 {
 	$posted = $_POST['fatal'];
-	
+
 }
 
 if (array_key_exists('Error_MessageOne', $_POST) && array_key_exists('Error_MessageTwo', $_POST))
 {
 	$sErrorMessage = $_POST['Error_MessageOne'].''.$_POST['Error_MessageTwo'];
-	
+
 }
 
 session_start();
@@ -62,7 +62,7 @@ $sRoot = $sHost.$sScriptName;
 $sLastChar = substr($sScriptName, -1, 1);
 $sScriptName = ($sLastChar == '\\' || $sLastChar == '/') ? substr($sScriptName, 0, -1) : $sScriptName;
 $bSSLEnabled = false;
-if ($_SERVER['HTTPS'] === 'on')
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
 {
 	$bSSLEnabled = true;
 }
@@ -74,34 +74,34 @@ $sRootUrl = ($bSSLEnabled ? 'https://' : 'http://').$sRoot;
 	<head>
 		<title>Knowledgetree - Desklet</title>
 		<link rel="stylesheet" type="text/css" href="<?php echo $sRootUrl ?>/resources/css/errors.css" />
-	
-	<script type="text/javascript"> 
-		
+
+	<script type="text/javascript">
+
 function Click()
 {
 	var open = document.getElementById('exp');
 	open.style.display = 'block';
 	var border = document.getElementById('error-container');
-	border.style.height = '220px'; 
+	border.style.height = '220px';
 }
 
-</script>		
-		
+</script>
+
 	</head>
 	<body>
-	
+
 		<div id="error-container">
-			
+
 			<div id="acc-error">
-	
-						
+
+
 				<h1>An Has Error Occurred</h1>
 				<p>You have encountered a problem with your document management system.</p>
 				<p>Please contact your systems administrator.</p>
 				<p>For more information on the error click here: <img src="<?php echo $sRootUrl ?>/resources/graphics/info.gif" style="cursor: pointer;" onclick="Click()" /><div id ="exp" style="display: none; "> <?php if(isset($sErrorMessage)){ echo $sErrorMessage;  }else  if(isset($posted)){ echo $posted; } else if($phperror){ echo $phperror; } ?></div></p>
-				
-			</div>		
-		</div>		
-				
+
+			</div>
+		</div>
+
 	</body>
 </html>
