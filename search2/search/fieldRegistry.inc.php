4140651402b2dbbe628f65a10ff37b4d2a9f65ba
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-09-09 19:26:30 +0000

    Merged in from STABLE trunk...
    KTS-3652
    "Created a Field set with an '&' performed an advanced search and received an error"
    Fixed.
    
    Committed By: Conrad Vermeulen
    Reviewed By: Megan Watson
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/STABLE/branches/3.5.3a-Release-Branch@9215 c91229c3-7414-0410-bfa2-8a42b809f60b

--- search2/search/fieldRegistry.inc.php
+++ search2/search/fieldRegistry.inc.php
@@ -6,31 +6,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  *
@@ -163,6 +163,7 @@ class ExprFieldRegistry
     	{
     		$fieldset = $fieldset->getValue();
     	}
+    	$fieldset = html_entity_decode($fieldset);
     	if (!array_key_exists($fieldset,$this->metadata))
     	{
     		throw new ResolutionException("Metadata class for fieldset '$fieldset' and field '$field' not found.");
@@ -171,6 +172,7 @@ class ExprFieldRegistry
     	{
     		$field = $field->getValue();
     	}
+    	$field = html_entity_decode($field);
     	if (!array_key_exists($field,$this->metadata[$fieldset]))
     	{
     		throw new ResolutionException("Metadata class for fieldset '$fieldset' and field '$field' not found.");
