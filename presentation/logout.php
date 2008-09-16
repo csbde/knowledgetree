56f76a74b595a49345a14cb32269fc81da95c3e5
Megan Watson <megan@knowledgetree.com>
2008-09-16 07:49:33 +0000

    KTS-3692 "Add triggers to the logout process" Fixed. Triggers on logout are now checked.
    Committed by: Megan Watson
    Reviewed by: Conrad Vermeulen
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@9303 c91229c3-7414-0410-bfa2-8a42b809f60b

--- presentation/logout.php
+++ presentation/logout.php
@@ -6,48 +6,58 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  *
  */
 
-require_once("../config/dmsDefaults.php");
-require_once(KT_LIB_DIR . '/authentication/authenticationutil.inc.php');
+require_once('../config/dmsDefaults.php');
+require_once(KT_LIB_DIR . '/triggers/triggerregistry.inc.php');
+//require_once(KT_LIB_DIR . '/authentication/authenticationutil.inc.php');
 
 class KTLogoutDispatcher extends KTDispatcher {
     function do_main() {
         global $default;
 
+        // Check for any triggers on logout
+        $oKTTriggerRegistry = KTTriggerRegistry::getSingleton();
+        $aTriggers = $oKTTriggerRegistry->getTriggers('logout', 'postValidate');
+        foreach ($aTriggers as $aTrigger) {
+            $sTrigger = $aTrigger[0];
+            $oTrigger = new $sTrigger;
+            $ret = $oTrigger->postValidate();
+        }
+
         /*$oAuthenticator =& KTAuthenticationUtil::getAuthenticatorForUser($this->oUser);
         $oAuthenticator->logout($this->oUser);*/
         Session::destroy();
 
-        redirect((strlen($default->rootUrl) > 0 ? $default->rootUrl : "/"));
+        redirect((!empty($default->rootUrl) ? $default->rootUrl : '/'));
         exit(0);
     }
 }
