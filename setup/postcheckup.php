8a44f8e062626e88a2a8a3dd094514399d669503
Megan Watson <megan@knowledgetree.com>
2008-09-23 13:20:27 +0000

    KTS-3642 "Problem with dropbox Beta Assigning the return value of new by reference is deprecated" Fixed. Added a check on the uploads directory, if it doesn't exist it will be created.
    Committed by: Megan Watson
    Reviewed by: Conrad Vermeulen
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@9383 c91229c3-7414-0410-bfa2-8a42b809f60b

--- setup/postcheckup.php
+++ setup/postcheckup.php
@@ -6,31 +6,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  *
@@ -42,6 +42,16 @@ require_once('../config/dmsDefaults.php');
 
 function writablePath($name, $path) {
     $ret = sprintf('<tr><td>%s (%s)</td><td>', $name, $path);
+
+    // Ensure the path is a full/absolute path
+    $path = KTUtil::isAbsolutePath($path) ? $path : KT_DIR . $path;
+
+    // Check if the directory exists and create it if it doesn't
+    if(!file_exists($path)){
+        mkdir($path, 0755);
+    }
+
+    // Check if directory is writable
     if (is_writable($path)) {
         $ret .= sprintf('<font color="green"><b>Writeable</b></font>');
     } else {
@@ -71,8 +81,11 @@ that things are still set up correctly.</p>
 
 <table width="50%">
   <tbody>
-  <?php echo writablePath('Log directory', $default->logDirectory)?>
-  <?php echo writablePath('Document directory', $default->documentRoot)?>
+  <?php
+      echo writablePath('Log directory', $default->logDirectory);
+      echo writablePath('Document directory', $default->documentRoot);
+      echo writablePath('Webservice uploads directory', $default->uploadDirectory);
+  ?>
   </tbody>
 </table>
 
