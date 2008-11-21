5ca42071f68dc3f30eec80d1efc886064286d3bd
Megan Watson <megan@knowledgetree.com>
2008-11-21 09:31:33 +0000

    KTS-3937 - Added the document id in the where clause of the sql. "CLONE -"Last Comments" for "document transition reason" showing up for every document (SUP-1219)"
    Committed by: Megan Watson
    Reviewed by: Donald Jackson
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@9605 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/ktcore/KTDocumentViewlets.php
+++ plugins/ktcore/KTDocumentViewlets.php
@@ -6,31 +6,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  *
@@ -39,7 +39,7 @@
 require_once(KT_LIB_DIR . "/actions/documentviewlet.inc.php");
 require_once(KT_LIB_DIR . "/workflow/workflowutil.inc.php");
 
-// {{{ KTDocumentDetailsAction 
+// {{{ KTDocumentDetailsAction
 class KTWorkflowViewlet extends KTDocumentViewlet {
     var $sName = 'ktcore.viewlets.document.workflow';
 
@@ -47,20 +47,21 @@ class KTWorkflowViewlet extends KTDocumentViewlet {
         $oKTTemplating =& KTTemplating::getSingleton();
         $oTemplate =& $oKTTemplating->loadTemplate("ktcore/document/viewlets/workflow");
         if (is_null($oTemplate)) { return ""; }
-        
+
         $oWorkflowState = KTWorkflowState::get($this->oDocument->getWorkflowStateId());
         if (PEAR::isError($oWorkflowState)) {
             return "";
         }
-        
+
         $aDisplayTransitions = array();
         $aTransitions = KTWorkflowUtil::getTransitionsForDocumentUser($this->oDocument, $this->oUser);
         if (empty($aTransitions)) {
             return "";
         }
-        
+
         // Check if the document has been checked out
         $bIsCheckedOut = $this->oDocument->getIsCheckedOut();
+        $iId = $this->oDocument->getId();
         if($bIsCheckedOut){
             // If document is checked out, don't link into the workflow.
             $aDisplayTransitions = array();
@@ -69,43 +70,47 @@ class KTWorkflowViewlet extends KTDocumentViewlet {
             	if(is_null($oTransition) || PEAR::isError($oTransition)){
                 	continue;
                 }
-                
+
                 $aDisplayTransitions[] = array(
-                    'url' => KTUtil::ktLink('action.php', 'ktcore.actions.document.workflow', array("fDocumentId" => $this->oDocument->getId(), "action" => "quicktransition", "fTransitionId" => $oTransition->getId())),
+                    'url' => KTUtil::ktLink('action.php', 'ktcore.actions.document.workflow', array("fDocumentId" => $iId, "action" => "quicktransition", "fTransitionId" => $oTransition->getId())),
                     'name' => $oTransition->getName(),
                 );
             }
         }
-        
+
 		//Retreive the comment for the previous transition
 		$aCommentQuery = array(
-        "SELECT comment FROM document_transactions where transaction_namespace='ktcore.transactions.workflow_state_transition' ORDER BY id DESC LIMIT 1;"
+            "SELECT comment FROM document_transactions
+            where transaction_namespace='ktcore.transactions.workflow_state_transition'
+            AND document_id = ?
+            ORDER BY id DESC LIMIT 1;"
 		);
+		$aCommentQuery[] = array($iId);
 		$aTransitionComments = DBUtil::getResultArray($aCommentQuery);
-		$oLatestTransitionComment = null; 
+		$oLatestTransitionComment = null;
 		if(!empty($aTransitionComments))
 		{
 			$aRow = $aTransitionComments[0];
 			$oLatestTransitionComment = $aRow['comment'];
 			$iCommentPosition = strpos($oLatestTransitionComment,':'); //comment found after first colon in string
-			
+
 			if($iCommentPosition>0) //if comment found
 			{
-				$oLatestTransitionComment = substr($oLatestTransitionComment, $iCommentPosition+2, (strlen($oLatestTransitionComment)-$iCommentPosition));	
+				$oLatestTransitionComment = substr($oLatestTransitionComment, $iCommentPosition+2, (strlen($oLatestTransitionComment)-$iCommentPosition));
 			}
-			else //if no comment found - i.e. first state in workflow		
+			else //if no comment found - i.e. first state in workflow
 			{
 				$oLatestTransitionComment = null;
 			}
 		}
-		
+
         $oTemplate->setData(array(
             'context' => $this,
             'bIsCheckedOut' => $bIsCheckedOut,
             'transitions' => $aDisplayTransitions,
             'state_name' => $oWorkflowState->getName(),
 			'comment' => $oLatestTransitionComment,
-        ));        
+        ));
         return $oTemplate->render();
     }
 }
