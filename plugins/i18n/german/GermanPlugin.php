8233666fd9d6b3e279f091832a0488edc323da2e
kevin_fourie <kevin_fourie@c91229c3-7414-0410-bfa2-8a42b809f60b>
2008-11-24 14:56:19 +0000

    Updated language plugin license headers.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/branches/RB_3.5.4a@9616 c91229c3-7414-0410-bfa2-8a42b809f60b

--- plugins/i18n/german/GermanPlugin.php
+++ plugins/i18n/german/GermanPlugin.php
@@ -1,23 +1,39 @@
 <?php
-/*
- * $Id: $
+
+/**
+ * $Id$
  *
- * The contents of this file are subject to the KnowledgeTree
- * Commercial Editions On-Premise License ("License");
- * You may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.knowledgetree.com/about/legal/
- * The terms of this license may change from time to time and the latest
- * license will be published from time to time at the above Internet address.
- *
- * This edition of the KnowledgeTree software
- * is NOT licensed to you under Open Source terms.
- * You may not redistribute this source code.
- * For more information please see the License above.
- *
- * (c) 2008 KnowledgeTree Inc.
- * Portions copyright The Jam Warehouse Software (Pty) Ltd;
- * All Rights Reserved.
+ * KnowledgeTree Community Edition
+ * Document Management Made Simple
+ * Copyright (C) 2008 KnowledgeTree Inc.
+ * Portions copyright The Jam Warehouse Software (Pty) Limited
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 3 as published by the
+ * Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ * California 94120-7775, or email info@knowledgetree.com.
+ * 
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ * 
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
+ * must display the words "Powered by KnowledgeTree" and retain the original 
+ * copyright notice.
+ * Contributor( s): ______________________________________
  *
  */
 
