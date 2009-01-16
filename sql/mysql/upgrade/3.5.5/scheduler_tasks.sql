be57b5646d68725576fe5d70253d325fa781132e
Megan Watson <megan@knowledgetree.com>
2009-01-16 07:36:17 +0000

    Created a document processor script which uses curl to run the document processor.
    Committed by: Megan Watson
    Reviewed by: Donald Jackson
    
    
    
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@9719 c91229c3-7414-0410-bfa2-8a42b809f60b

--- sql/mysql/upgrade/3.5.5/scheduler_tasks.sql
+++ sql/mysql/upgrade/3.5.5/scheduler_tasks.sql
@@ -1,5 +1,5 @@
 select @id:=ifnull(max(id),0)+1 from scheduler_tasks;
-INSERT INTO `scheduler_tasks` VALUES (@id,'Document Processor','search2/documentProcessor/bin/documentProcessor.php','',0,'1min','2007-10-01',NULL,0,'system');
+INSERT INTO `scheduler_tasks` VALUES (@id,'Document Processor','search2/bin/cronDocumentProcessor.php','',0,'1min','2007-10-01',NULL,0,'system');
 
 DELETE FROM scheduler_tasks WHERE task = 'Indexing';
 DELETE FROM scheduler_tasks WHERE task = 'Refresh Index Statistics';
