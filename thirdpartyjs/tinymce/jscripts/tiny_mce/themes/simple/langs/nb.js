70a3582834cef1c4fd4cb5ba116ffea4418ca450
Kevin Fourie <kevin@knowledgetree.com>
2009-01-26 13:20:49 +0000

    Adding missing items back to thirdpartyjs.
    git-svn-id: https://kt-dms.svn.sourceforge.net/svnroot/kt-dms/trunk@9751 c91229c3-7414-0410-bfa2-8a42b809f60b

---
+++ thirdpartyjs/tinymce/jscripts/tiny_mce/themes/simple/langs/nb.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('nb.simple',{
+bold_desc:"Fet (Ctrl+B)",
+italic_desc:"Kursiv (Ctrl+I)",
+underline_desc:"Understreket (Ctrl+U)",
+striketrough_desc:"Gjennomstreket",
+bullist_desc:"Punktliste",
+numlist_desc:"Nummerliste",
+undo_desc:"Angre (Ctrl+Z)",
+redo_desc:"Gj\u00F8r om (Ctrl+Y)",
+cleanup_desc:"Rens uryddig kode"
+});
