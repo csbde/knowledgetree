b2ded08ae832c79d988f65c5f521d1c6c1e99d70
Megan <megan@knowledgetree.com>
2009-03-11 09:48:11 +0200

    KTS-4098 - Mysql doesn't see accented characters as different from the unaccented character. Added a check for this into the php. "CLONE -I can't rename a field "Applicabilité" to "Applicabilite" (SUP-1590)" Fixed.
    Committed by: Megan Watson
    Reviewed by: Kevin Cyster

--- plugins/ktcore/admin/fieldsets/basic.inc.php
+++ plugins/ktcore/admin/fieldsets/basic.inc.php
@@ -7,31 +7,31 @@
  * Document Management Made Simple
  * Copyright (C) 2008, 2009 KnowledgeTree Inc.
  * Portions copyright The Jam Warehouse Software (Pty) Limited
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  *
@@ -287,7 +287,8 @@ class BasicFieldsetManagementDispatcher extends KTAdminDispatcher {
         // check that the field name either hasn't changed, or doesn't exist.
         if ($data['name'] != $this->oField->getName()) {
             $oOldField = DocumentField::getByFieldsetAndName($this->oFieldset, $data['name']);
-            if (!PEAR::isError($oOldField)) {
+            // If the field exists throw an error. Mysql doesn't distinguish between Ž and e so check the names are different in php.
+            if (!PEAR::isError($oOldField) && $oOldField->getName() == $data['name']) {
                 $extra_errors['name'] = _kt("That name is already in use in this fieldset.  Please specify a unique name.");
             }
         }
