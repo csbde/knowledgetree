cb720337952bc300f7174af7d4c0adcd4d398487
Jarrett Jordaan <jarrett@knowledgetree.com>
2009-07-27 14:13:09 +0200

    StoryId778896:Cleanup Installer Framework
    Committed by: Jarrett Jordaan
    
    Reviewed by: Megan Watson

--- setup/wizard/services/indexingTask.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd /home/jarrett/ktdms/knowledgeTree/search2/indexing/bin
-/home/jarrett/ktdms/php/bin/php -Cq cronIndexer.php
--- setup/wizard/services/schedulerTask.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /home/jarrett/ktdms/knowledgeTree/bin
-while true; do
-/home/jarrett/ktdms/php/bin/php -Cq scheduler.php
-  sleep 30
-done
