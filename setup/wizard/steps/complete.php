00b2a929bc78b6d0482c06c3795db86a01ad392d
Paul Barrett <paul@knowledgetree.com>
2009-08-20 14:35:01 +0200

    Partial Post Installation Checkup: checks file system locations for writeable status, checks document location in or out of web root, checks database access and transaction support.
    Story ID:779236. In order to install KnowledgeTree successfully, as a user, I would like a post-installation checkup step
    
    In Progress
    
    Committed by: Paul Barrett
    
    Reviewed by: Jarrett Jordaan

--- setup/wizard/steps/complete.php
+++ setup/wizard/steps/complete.php
@@ -40,15 +40,361 @@
 * @version Version 0.1
 */
 
-class complete extends Step
-{
+class complete extends Step {
+
+    /**
+	* Reference to Database object
+	*
+	* @author KnowledgeTree Team
+	* @access private
+	* @var object
+	*/	
+    private $_dbhandler = null;
+    
+    /**
+     * List of services to check
+     * 
+     * @access private
+     * @var array
+     */
+    private $_services = array('Lucene', 'Scheduler');
+    
+    public function __construct() {
+        $this->_dbhandler = new dbUtil();
+    }
 
     function configure() {
         $this->temp_variables = array("step_name"=>"complete");
     }
 
     function doStep() {
+        $this->doRun();
     	return 'landing';
     }
+    
+    function doRun() {
+        // check filesystem (including location of document directory and logging)
+        $this->checkFileSystem();        
+        // check database
+        $this->checkDb();
+        // check services
+        $this->checkServices();
+    }
+    
+    private function checkFileSystem()
+    {
+        // defaults
+        $this->temp_variables['varDirectory'] = '';
+        $this->temp_variables['documentRoot'] = '';
+        $this->temp_variables['logDirectory'] = '';
+        $this->temp_variables['tmpDirectory'] = '';
+        $this->temp_variables['uploadDirectory'] = '';
+        $this->temp_variables['config'] = '';
+        $this->temp_variables['docLocation'] = '';
+        
+        $docRoot = '';
+
+        // retrieve path information from session
+        $config = $this->getDataFromSession("configuration");
+        $paths = $config['paths'];
+
+        $html = '<td><div class="%s"></div></td>'
+              . '<td>%s</td>'
+              . '<td class="%s">%s</td>';
+
+        // check paths are writeable
+        foreach ($paths as $path)
+        {
+            $output = '';
+            
+            $result = $this->checkPermission($path['path']);
+            $output = sprintf($html, $result['class'], 
+                                     $path['path'], 
+                                     (($result['class'] == 'tick') ? '' : 'error' ), 
+                                     (($result['class'] == 'tick') ? 'Writeable' : 'Not Writeable' ));
+            
+            $this->temp_variables[($path['setting'] != '') ? $path['setting'] : 'config'] = $output;
+            
+            // for document location check
+            if ($path['setting'] == 'documentRoot') {
+                $docRoot = $path['path']; 
+            }
+        }
+        
+        // check document path internal/external to web root
+        // compare SYSTEM_DIR to root path of documentRoot
+        $sysDir = preg_replace('/\\\\+|\/+/', '\/', SYSTEM_DIR);
+        $docRoot = preg_replace('/\\\\+|\/+/', '\/', $docRoot);
+        if (($pos = strpos($docRoot, $sysDir)) !== false) {
+            $this->temp_variables['docLocation'] = '<td><div class="cross_orange"></div></td>'
+                                                 . '<td class="warning" colspan="2">Your document directory is set to the default, which is inside the web root. '
+                                                 . 'This may present a security problem if your documents can be accessed from the web, '
+                                                 . 'working around the permission system in KnowledgeTree.</td>';
+        }
+        else {
+            $this->temp_variables['docLocation'] = '<td><div class="tick"></div></td>'
+                                                 . '<td>Your document directory is outside the web root.</td>';
+        }
+    }
+    
+    private function checkDb()
+    {
+        // defaults
+        $this->temp_variables['dbConnectAdmin'] = '';
+        $this->temp_variables['dbConnectUser'] = '';
+        $this->temp_variables['dbPrivileges'] = '';
+        $this->temp_variables['dbTransaction'] = '';
+        
+//        $html = '<td><div class="%s"></div></td>'
+//              . '<td>%s</td>'
+//              . '<td class="%s">%s</td>';
+        
+        // retrieve database information from session
+        $dbconf = $this->getDataFromSession("database");
+        //print_r($dbconf);
+        // make db connection - admin
+        $loaded = $this->_dbhandler->load($dbconf['dhost'], $dbconf['dmsname'], $dbconf['dmspassword'], $dbconf['dname']);
+        if (!$loaded) {
+            $this->temp_variables['dbConnectAdmin'] .= /*sprintf($html, 'cross', 
+                                     $path['path'], 
+                                     (($result['class'] == 'tick') ? '' : 'error' ), 
+                                     (($result['class'] == 'tick') ? 'Writeable' : 'Not Writeable' ));*/
+                                     '<td><div class="cross"></div></td>'
+                                               .  '<td class="error">Unable to connect to database (user: ' . $dbconf['dmsname'] . ')</td>';
+        }
+        else
+        {
+            $this->temp_variables['dbConnectAdmin'] .= '<td><div class="tick" style="float:left;"></div></td>'
+                                               .  '<td>Database connectivity successful (user: ' . $dbconf['dmsname'] . ')</td>';
+        }
+        
+        // make db connection - user
+        $loaded = $this->_dbhandler->load($dbconf['dhost'], $dbconf['dmsusername'], $dbconf['dmsuserpassword'], $dbconf['dname']);
+        // if we can log in to the database, check access
+        // TODO check write access?
+        if ($loaded)
+        {
+            $this->temp_variables['dbConnectUser'] .= '<td><div class="tick" style="float:left;"></div></td>'
+                                               .  '<td>Database connectivity successful (user: ' . $dbconf['dmsusername'] . ')</td>';
+
+            $qresult = $this->_dbhandler->query('SELECT COUNT(id) FROM documents');
+            if (!$qresult)
+            {
+                $this->temp_variables['dbPrivileges'] .= '<td><div class="cross" style="float:left;"></div></td>'
+                                                      .  '<td class="error">'
+                                                      .  'Unable to do a basic database query<br/>Error: ' . $this->_dbhandler->getLastError()
+                                                      .  '</td>';
+            }
+            else
+            {
+                $this->temp_variables['dbPrivileges'] .= '<td><div class="tick" style="float:left;"></div></td><td>Basic database query successful</td>';
+            }
+            
+            // check transaction support
+            $sTable = 'system_settings';
+            $this->_dbhandler->startTransaction();
+            $this->_dbhandler->query('INSERT INTO ' . $sTable . ' (name, value) VALUES ("transactionTest", "1")');
+            $this->_dbhandler->rollback();
+            $res = $this->_dbhandler->query("SELECT id FROM $sTable WHERE name = 'transactionTest' LIMIT 1");
+            if (!$res) {
+                $this->temp_variables['dbTransaction'] = '<td><div class="cross_orange" style="float:left;"></div></td>'
+                                                       . '<span class="error">Transaction support not available in database</span></td>';
+            } else {
+                $this->temp_variables['dbTransaction'] = '<td><div class="tick" style="float:left;"></div></td><td>Database has transaction support</td>';
+            }
+            $this->_dbhandler->query('DELETE FROM ' . $sTable . ' WHERE name = "transactionTest"');
+        }
+        else
+        {
+            $this->temp_variables['dbConnectUser'] .= '<td><div class="cross" style="float:left;"></div></td>'
+                                               .  '<span class="error">Unable to connect to database (user: ' . $dbconf['dmsusername'] . ')</span></td>';
+        }
+    }
+    
+    private function checkServices()
+    {
+        // defaults
+        $this->temp_variables['luceneServiceStatus'] = '';
+        $this->temp_variables['schedulerServiceStatus'] = '';
+        
+        return null;
+        
+        $processOrder = array();
+        if (strtolower(OS) == 'windows')
+        {
+            $processOrder[] = 'Start';
+            $processOrder[] = 'Stop';
+        }
+        else if (strtolower(OS) == 'unix')
+        {
+            $processOrder[] = 'Stop';
+            $processOrder[] = 'Start';   
+        }
+            
+        // loop through services and attempt to stop and then start them (in the case of Linux,) or start and stop them (in the case of Windows)
+        // (Linux service is started after install, Windows is not)
+        foreach ($this->_services as $serviceName)
+        {
+            // check installed
+            $statusCheck = OS."ServiceInstalled";
+            $className = OS.$serviceName;
+			$service = new $className();
+    		$installed = $this->$statusCheck($service);
+            if ($installed) {
+                
+            }
+            else {
+                
+            }
+            
+            // check start/stop - different orders dependant on system
+            foreach($processOrder as $operation)
+            {
+//                $opExec = 'service' . $operation;
+//                $opSuccess = $this->$opExec();
+//                if ($opSuccess) {
+//                    
+//                }
+//                else {
+//                    
+//                }
+            }
+        }
+    }
+    
+    // these next function are dupes of ones in steps/service.php and steps/configuration.php - abstract these to another class (parent or helper)
+    // and remove from here and original classes
+    
+    /**
+     * Check whether a given directory / file path exists and is writable
+     *
+	 * @author KnowledgeTree Team
+     * @access private
+     * @param string $dir The directory / file to check
+     * @param boolean $create Whether to create the directory if it doesn't exist
+     * @return array The message and css class to use
+     */
+    private function checkPermission($dir, $create=false)
+    {
+        $exist = 'Directory does not exist';
+        $write = 'Directory is not writable';
+        $ret = array('class' => 'cross');
+
+        if(!file_exists($dir)){
+            if($create === false){
+                $this->done = false;
+                $ret['msg'] = $exist;
+                return $ret;
+            }
+            $par_dir = dirname($dir);
+            if(!file_exists($par_dir)){
+                $this->done = false;
+                $ret['msg'] = $exist;
+                return $ret;
+            }
+            if(!is_writable($par_dir)){
+                $this->done = false;
+                $ret['msg'] = $exist;
+                return $ret;
+            }
+            mkdir($dir, '0755');
+        }
+
+        if(is_writable($dir)){
+            $ret['class'] = 'tick';
+            return $ret;
+        }
+
+        $this->done = false;
+        $ret['msg'] = $write;
+        return $ret;
+    }
+    
+    /**
+	* Check if windows service installed
+	*
+	* @author KnowledgeTree Team
+	* @param object
+	* @access public
+	* @return boolean
+	*/
+	public function windowsServiceInstalled($service) {
+//	    print_r($service, true)."<BR>";
+		$status = $service->status(); // Check if service has been installed
+		echo "STAT: ".$status."<BR>";
+		if($status != 'STOPPED') { // Check service status
+			$this->error[] = $service->getName()." Could not be added as a WINDOWS Service";
+			return false;
+		}
+		return true;
+	}
+	
+   	/**
+	* Check if unix service installed
+	*
+	* @author KnowledgeTree Team
+	* @param object
+	* @access public
+	* @return boolean
+	*/
+	public function unixServiceInstalled($service) {
+		$status = $service->status(); // Check if service has been installed
+		if($status != 'STARTED') { // Check service status
+			$this->error[] = $service->getName()." Could not be added as a UNIX Service";
+			return false;
+		}
+		return true;
+	}
+	
+   	/**
+	* Starts all services
+	*
+	* @author KnowledgeTree Team
+	* @param object
+	* @access public
+	* @return mixed
+	*/
+	public function installStep() {
+		foreach ($this->services as $serviceName) {
+			$className = OS.$serviceName;
+			$service = new $className();
+			$status = $this->serviceStart($service);
+			
+		}
+		
+		return true;
+	}
+	
+   	/**
+	* Starts service
+	*
+	* @author KnowledgeTree Team
+	* @param object
+	* @access private
+	* @return string
+	*/
+	private function serviceStart($service) {
+		if(OS == 'windows') {
+			$service->load(); // Load Defaults
+			$service->start(); // Start Service
+			return $service->status(); // Get service status
+		}
+	}
+   	/**
+	* Stops service
+	*
+	* @author KnowledgeTree Team
+	* @param object
+	* @access private
+	* @return string
+	*/
+	private function serviceStop($service) {
+		if(OS == 'windows') {
+			$service->load(); // Load Defaults
+			$service->stop(); // Stop Service
+			return $service->status(); // Get service status
+		}
+	}
+    
 }
 ?>
