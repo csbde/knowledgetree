8d60de5d4ecc7f6afdc755e5ba86bed01c2daece
Paul Barrett <paul@knowledgetree.com>
2009-11-06 17:45:21 +0200

    Added thumbnail seetings to data.sql and upgrade sql
    Committed by: Paul Barrett
    
    Reviewed by: Megan Watson

--- sql/mysql/upgrade/3.7.0/config_settings.sql
+++ sql/mysql/upgrade/3.7.0/config_settings.sql
@@ -1,2 +1,5 @@
 INSERT INTO `config_settings` (group_name, display_name, description, item, value, default_value, type, options, can_edit)
 VALUES ('urls', 'Internal Var Directory', 'The path to the internal var directory that must sit within the web root', 'internalVarDirectory', 'default', '${fileSystemRoot}/var', 'string', NULL, 0);
+
+INSERT INTO `config_settings` (group_name, display_name, description, item, value, default_value, type, options, can_edit)
+VALUES ('urls', 'PDF Directoy', 'The path for storing the generated PDF Documents', 'pdfDirectory', 'default', '${varDirectory}/Pdf', 'string', '', 1);
