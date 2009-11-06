5b81ca308359a03ee61dc3f380482b1f0b47aa53
Paul Barrett <paul@knowledgetree.com>
2009-11-06 12:05:38 +0200

    Removed incorrect externalBinary references for zip and unzip and updated correctly in database step of installer as export and import types respectively
    Committed by: Paul Barrett
    
    Reviewed by: Megan Watson

--- sql/mysql/upgrade/3.7.0/config_settings.sql
+++ sql/mysql/upgrade/3.7.0/config_settings.sql
@@ -1,8 +1,2 @@
 INSERT INTO `config_settings` (group_name, display_name, description, item, value, default_value, type, options, can_edit)
 VALUES ('urls', 'Internal Var Directory', 'The path to the internal var directory that must sit within the web root', 'internalVarDirectory', 'default', '${fileSystemRoot}/var', 'string', NULL, 0);
-
-INSERT INTO `config_settings` (group_name, display_name, description, item, value, default_value, type, options, can_edit)
-VALUES ('externalBinary', 'zip', 'The path to the zip binary', 'zipPath', 'default', 'zip', 'string', NULL, 0);
-
-INSERT INTO `config_settings` (group_name, display_name, description, item, value, default_value, type, options, can_edit)
-VALUES ('externalBinary', 'unzip', 'The path to the unzip binary', 'unzipPath', 'default', 'unzip', 'string', NULL, 0);
