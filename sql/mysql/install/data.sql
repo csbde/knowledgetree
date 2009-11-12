01dd9bd5a2f6c15235069a04599053d8e07ef133
Charl Joseph Mert <charl@knowledgetree.com>
2009-11-12 12:09:06 +0200

    Reverted the install/data.sql change back to the older /bin/ajaxtasks/downloadTask.php as this wasn't required for the fix.
    Committed by: Charl Joseph Mert
    Reviewed by: Megan Watson

--- sql/mysql/install/data.sql
+++ sql/mysql/install/data.sql
@@ -1377,7 +1377,7 @@ INSERT INTO `scheduler_tasks` VALUES
 (8,'Disk Usage and Folder Utilisation Statistics','plugins/housekeeper/bin/UpdateStats.php','',0,'5mins','2007-10-01 00:00:00',NULL,0,'enabled'),
 (9,'Refresh Index Statistics','search2/bin/cronIndexStats.php','',0,'1min','2007-10-01',NULL,0,'enabled'),
 (10,'Refresh Resource Dependancies','search2/bin/cronResources.php','',0,'1min','2007-10-01',NULL,0,'enabled'),
-(11,'Bulk Download Queue','presentation/lookAndFeel/knowledgeTree/bulkdownload/downloadTask.php','',0,'1min','2007-10-01',NULL,0,'system'),
+(11,'Bulk Download Queue','bin/ajaxtasks/downloadTask.php','',0,'1min','2007-10-01',NULL,0,'system'),
 (12,'Call Home','bin/system_info.php','',0,'daily','2009-10-01',NULL,0,'system');
 
 /*!40000 ALTER TABLE `scheduler_tasks` ENABLE KEYS */;
