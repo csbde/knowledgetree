68c609c2640292d5e4f6a700c46bb4044f9da75f
Paul Barrett <paul@knowledgetree.com>
2009-11-23 16:04:16 +0200

    Attempted fix of issue with imagemagick path not set correctly on a migration/upgrade from 3.6.1 - setting did not exist and so could not be updated, setup wizard should now create if not present
    KTS-4526. Upgrade: Imagemagick path is not set correctly, user cannot view thumbnails.
    
    Fixed
    
    Committed by: Paul Barrett
    
    Reviewed by: Megan Watson

--- plugins/thumbnails/thumbnailsPlugin.php
+++ plugins/thumbnails/thumbnailsPlugin.php
@@ -49,6 +49,15 @@ class thumbnailsPlugin extends KTPlugin {
         require_once(KT_LIB_DIR . '/templating/templating.inc.php');
         $oTemplating =& KTTemplating::getSingleton();
         $oTemplating->addLocation('thumbnails', $plugin_dir.'templates', 'thumbnails.generator.processor.plugin');
+        
+	    // check for existing config settings entry and only add if not already present
+        $sql = 'SELECT id FROM `config_settings` WHERE group_name = "externalBinary" AND item = "convertPath"';
+        $result = DBUtil::getOneResult($sql);
+	    if(PEAR::isError($result) || empty($result)) {
+	    	DBUtil::runQuery('INSERT INTO `config_settings` (group_name, display_name, description, item, value, default_value, type, options, can_edit) '
+	    					. 'VALUES ("externalBinary", "convert", "The path to the ImageMagick \"convert\" binary", "convertPath", "default", "convert", '
+	    					. '"string", NULL, 1);');
+		}
     }
 }
 
