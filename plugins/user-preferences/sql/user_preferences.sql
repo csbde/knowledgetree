992bb1ca3b9df575d93c3b60a4a69b716a2a9673
Jarrett Jordaan <jarrett@knowledgetree.com>
2010-03-12 13:53:21 +0200

    PT-2378610 : Updated user preference plugin.
    Committed by: Jarrett Jordaan
    
    Reviewed by: Megan Watson

--- plugins/user-preferences/sql/user_preferences.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `user_preferences` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) unsigned NOT NULL,
-  `prefkey` varchar(255) NOT NULL,
-  `prefvalue` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
