0158ec8d3cb2479be882940da43ac2f06aee02d6
Mark Holtzhausen <mark@knowledgetree.com>
2010-05-05 11:38:26 +0200

    delete ktlive plugins directory

--- plugins/ktlive/liveEnable.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if(!defined('KT_LIVE_DIR'))define('KT_LIVE_DIR',KT_PLUGIN_DIR.'ktlive/');
-
-require_once(KT_LIVE_DIR.'liveAccountRouting.helper.php');
-require_once(KT_LIVE_DIR.'liveAccounts.helper.php');
-require_once(KT_LIVE_DIR.'liveRenderError.helper.php');
-?>
