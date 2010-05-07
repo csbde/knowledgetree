336bc92f9e9b0ce0b66035ce730bab86c4a5e137
Megan Watson <megan@knowledgetree.com>
2010-05-07 15:51:48 +0200

    Created the restricted environment config setting for turning off certain administrative areas. PT: 3319944
    Committed by: Megan Watson

---
+++ sql/mysql/upgrade/3.7.0.5/config_setting_restricted_env.sql
@@ -0,0 +1,2 @@
+INSERT INTO config_settings(`group_name`, `display_name`, `description`, `item`, `value`, `default_value`, `type`, `options`, `can_edit`)
+VALUES('ui', 'Restricted Environment', 'Removes certain administrative features from the interface to prevent users from accessing the functionality', 'restrictedEnv', 'false', 'false', 'boolean', '', 0);
