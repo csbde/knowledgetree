742721eece756fbdf1d08cb9f22c12cc3b1e7acf
Megan Watson <megan@knowledgetree.com>
2010-06-23 08:41:35 +0200

    Sanitized the comment before displaying it via the ajax.
    Committed by: Megan Watson

--- plugins/comments/ajaxComments.php
+++ plugins/comments/ajaxComments.php
@@ -5,32 +5,32 @@
  * KnowledgeTree Community Edition
  * Document Management Made Simple
  * Copyright (C) 2008, 2009, 2010 KnowledgeTree Inc.
- * 
- * 
+ *
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 3 as published by the
  * Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco, 
+ *
+ * You can contact KnowledgeTree Inc., PO Box 7775 #87847, San Francisco,
  * California 94120-7775, or email info@knowledgetree.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * KnowledgeTree" logo and retain the original copyright notice. If the display of the 
+ * KnowledgeTree" logo and retain the original copyright notice. If the display of the
  * logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
- * must display the words "Powered by KnowledgeTree" and retain the original 
+ * must display the words "Powered by KnowledgeTree" and retain the original
  * copyright notice.
  * Contributor( s): ______________________________________
  *
@@ -49,27 +49,27 @@ class AjaxCommentsDispatcher extends KTStandardDispatcher {
         return "Ajax Error.";
     }
 
-    // a lot simpler than the standard dispatcher, this DOESN'T include a large amount of "other" stuff ... we are _just_ called to handle 
+    // a lot simpler than the standard dispatcher, this DOESN'T include a large amount of "other" stuff ... we are _just_ called to handle
     // input/output of simple HTML components.
     function handleOutput($data) {
         print $data;
     }
 
-
-
     // do you want the fieldset_id here?
     function do_postComment() {
         global $default;
         $default->log->error(http_build_query($_REQUEST));
-        
+
         $comment = KTUtil::arrayGet($_REQUEST, 'comment');
         $documentId = KTUtil::arrayGet($_REQUEST, 'documentId');
-        
-        
+
         Comments::add_comment($documentId, $comment);
-        
+
         sleep(5);
-        
+
+        // Ensure comment is sanitised to prevent xss attacks
+        $comment = htmlspecialchars($comment, ENT_QUOTES, 'UTF-8', FALSE);
+
         echo '<div class="item newcomment">
 	            <div class="icon"><img src="http://www.gravatar.com/avatar/'.md5($this->getEmailForId($_SESSION['userID'])).'?s=48" alt="'.$this->getUserForId($_SESSION['userID']).'" title="'.$this->getUserForId($_SESSION['userID']).'" /></div>
 	            <div class="activityfeed_content">
@@ -80,24 +80,21 @@ class AjaxCommentsDispatcher extends KTStandardDispatcher {
 	            </div>
 	        </div>';
     }
-    
+
     function getUserForId($iUserId) {
         $u = User::get($iUserId);
         if (PEAR::isError($u) || ($u == false)) { return _kt('User no longer exists'); }
         return $u->getName();
     }
-	
+
 	function getEmailForId($iUserId) {
         $u = User::get($iUserId);
-		
+
         if (PEAR::isError($u) || ($u == false)) { return _kt('User no longer exists'); }
         return $u->getEmail();
     }
-    
-
 }
 
 $oDispatcher = new AjaxCommentsDispatcher();
 $oDispatcher->dispatch();
-
 ?>
