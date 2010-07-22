64e7e552ad4fe91e0622a74dc8d13f98844051ec
Mark Holtzhausen <mark@knowledgetree.com>
2010-07-22 11:43:34 +0200

    Fixed footer positioning on pages where elements appear or disappear on user interaction.
    by Mark Holtzhausen
    reviewed by: Delany Middleton

--- lib/templating/kt3template.inc.php
+++ lib/templating/kt3template.inc.php
@@ -142,6 +142,7 @@ class KTPage {
         $aJS[] = 'thirdpartyjs/jquery/jquery_noconflict.js';
         $aJS[] = 'thirdpartyjs/jquery/plugins/urlparser/jquery.url.js';
         $aJS[] = 'resources/js/search2widget.js';
+        $aJS[] = 'resources/js/newui/kt.containers.js';
         $aJS[] = 'resources/js/newui/newUIFunctionality.js';
         $aJS[] = 'resources/js/newui/jquery.helper.js';
         $aJS[] = 'resources/js/newui/buttontabs.jquery.js';
--- resources/js/newui/kt.containers.js
+++ resources/js/newui/kt.containers.js
@@ -11,11 +11,16 @@ kt=new function(){
 	this.init=function(){
 //		$priv.call('kt.pages.all.onload');
 //		$priv.call('kt.pages.browseView.onload');
+		jQuery(document).click(function(){setTimeout('kt.lib.setFooter();',500);});
+		setTimeout('kt.lib.setFooter();',500);
 	}
 };
 
-kt={}
 kt.lib={}; /* General functions */
 kt.events={}; /* KT's own symantic event engine library */
 kt.ktjapi={}; /* AJAX library to connect with KTAPI in the background */
 kt.pages={}; /* Specific to a certain page */
+
+(function($){
+	$(document).ready(function(){kt.init();});
+})(jQuery);
