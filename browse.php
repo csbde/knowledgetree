5e0e45903a6225089338ca68c87789773278ff20
Mark Holtzhausen <mark@knowledgetree.com>
2010-08-02 12:11:17 +0200

    Updated Mime Types

--- browse.php
+++ browse.php
@@ -382,6 +382,7 @@ class BrowseDispatcher extends KTStandardDispatcher {
 		
 		$iconFile='resources/mimetypes/newui/'.KTMime::getIconPath($item['mimetypeid']).'.png';
 		$item['icon_exists']=file_exists($iconFile);
+		$item['icon_file']=$iconFile;
 		
 		if($item['icon_exists']){		
 			$item['mimeicon']=str_replace('\\','/',$GLOBALS['default']->rootUrl.'/'.$iconFile);
@@ -428,7 +429,8 @@ class BrowseDispatcher extends KTStandardDispatcher {
 		
 
 		// Check if the thumbnail exists
-		$dev_no_thumbs=isset($_GET['noThumbs'])?true:false;
+		$dev_no_thumbs=(isset($_GET['noThumbs']) || $_SESSION['browse_no_thumbs'])?true:false;
+		$_SESSION['browse_no_thumbs']=$dev_no_thumbs;
 		if(!$dev_no_thumbs){
 			$oStorage=KTStorageManagerUtil::getSingleton();
 	        
@@ -493,7 +495,10 @@ class BrowseDispatcher extends KTStandardDispatcher {
 							</ul>
 							<div class="title"><a class="clearLink" href="view.php?fDocumentId=[id]" style="">[filename]</a></div>
 							
-							<div class="detail"><span class="item">Owner: <span class="user">[owned_by]</span></span><span class="item">Created: <span class="date">[created_date]</span> by <span class="user">[created_by]</span></span><span class="item">Updated: <span class="date">[modified_date]</span> by <span class="user">[modified_by]</span></span></div>
+							<div class="detail"><span class="item">
+								Owner: <span class="user">[owned_by]</span></span><span class="item">Created: <span class="date">[created_date]</span> by <span class="user">[created_by]</span></span><span class="item">Updated: <span class="date">[modified_date]</span> by <span class="user">[modified_by]</span></span>
+								<code>[icon_file]</code>
+							</div>
 						</td>
 					</tr>
 					<tr>
