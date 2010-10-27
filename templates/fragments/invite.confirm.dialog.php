9ebabfceddef92b3e2b1bd8624c518f26a97f0b4
Megan Watson <megan@knowledgetree.com>
2010-10-27 13:02:19 +0200

    Fixed small bugs in invite users. Updated UI text.
    Committed by: Megan Watson

---
+++ templates/fragments/invite.confirm.dialog.php
@@ -0,0 +1,39 @@
+<?php
+include_once('../../ktapi/ktapi.inc.php');
+
+$url = KTUtil::kt_url();
+
+$users_url = $url. '/admin.php?kt_path_info=userSetup/users';
+$groups_url = $url. '/admin.php?kt_path_info=userSetup/groups';
+$contact_url = 'http://www.knowledgetree.com/contact-us';
+?>
+
+<table border="0" cellspacing="5" cellpadding="5px" width="100%">
+
+    <tr>
+        <td colspan="2" valign="top">
+            Invitations to join KnowledgeTree were sent to: <a href="<?= $users_url; ?>"><span id="invitedUsers"></span> users</a>
+        </td>
+    </tr>
+
+    <tr>
+        <td colspan="2" id="showInvitedGroup">
+            As part of the: <a href="<?= $groups_url; ?>"><span id="invitedGroup"></span> group</a>
+        </td>
+    </tr>
+
+    <tr id="inviteLicenses" style="display: none;">
+        <td width="15%" valign="top">Warning: </td>
+        <td width="85%" valign="top">
+            The number of users invited now exceeds your available KnowledgeTree licenses.<br />
+            Please <a href="<?= $contact_url; ?>">contact us</a> to purchase additional licenses.
+        </td>
+    </tr>
+
+    <tr>
+        <td colspan="2" align="right">
+            <input id="ul_actions_upload_btn" class="ul_actions_btns" type="button" value="Close" onClick="kt.app.inviteusers.closeConfirmWindow();" />
+        </td>
+    </tr>
+
+</table>
