fbd02c51c478a235b71c08f39c583df7c8ded2d9
Jarrett Jordaan <jarrett@knowledgetree.com>
2010-11-10 14:03:32 +0200

    share_with_unlicensed_users_PT_5247790: Added column to store parent object id. Updated sharedContent entity. Code Cleanup. Removed unused functions.
    Committed by: Jarrett Jordaan
    
    Reviewed by:

--- sql/mysql/upgrade/3.7.0.8/shared_content.sql
+++ sql/mysql/upgrade/3.7.0.8/shared_content.sql
@@ -1,1 +1,8 @@
-CREATE TABLE `shared_content` (`user_id` INT NOT NULL, `object_id` INT NOT NULL, `type` enum('folder', 'document') DEFAULT 'document', `permissions` int(1) NOT NULL DEFAULT 0, INDEX (`user_id`), CONSTRAINT `shared_content_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS `shared_content` (
+  `user_id` int(11) NOT NULL,
+  `object_id` int(11) NOT NULL,
+  `type` enum('folder','document') NOT NULL DEFAULT 'document',
+  `permissions` int(1) NOT NULL DEFAULT '0',
+  `parent_id` int(11) DEFAULT NULL,
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
