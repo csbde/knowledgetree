141e5d76b8ef2db5610ad3bd8ec2a94a9d7f9f81
Paul Barrett <paul@knowledgetree.com>
2010-12-03 11:42:28 +0200

    Text changes for confirmation dialog.
    Story/Branch: share_with_unlicensed_users_PT_5247790
    
    Committed by: Paul Barrett

--- templates/fragments/users/invite.shared.confirm.dialog.php
+++ templates/fragments/users/invite.shared.confirm.dialog.php
@@ -1,4 +1,5 @@
 <?php
+
 include_once('../../ktapi/ktapi.inc.php');
 
 $url = KTUtil::kt_url();
@@ -7,14 +8,13 @@ $users_url = $url. '/admin.php?kt_path_info=userSetup/users&show_all=1';
 $groups_url = $url. '/admin.php?kt_path_info=userSetup/groups&show_all=1';
 $contact_url = 'http://www.knowledgetree.com/contact-us';
 
-
 ?>
 
 <table class="uploadTable" style="height:120%;" border="0">
 
     <tr>
         <td colspan="2" valign="top">
-            Invitations to join KnowledgeTree were sent to: <a href="<?= $users_url; ?>"><span id="invitedUsers"></span> users </a>
+            Content was shared with: <a href="<?= $users_url; ?>"><span id="invitedUsers"></span> users </a>
         </td>
     </tr>
 
@@ -31,24 +31,10 @@ $contact_url = 'http://www.knowledgetree.com/contact-us';
             <ul id="failedUsers" style="padding-left: 30px;"></ul>
         </td>
     </tr>
-
-    <tr id="showInvitedGroup" style="display: none;">
-        <td colspan="2" style="padding-top: 10px;">
-            As part of the: <a href="<?= $groups_url; ?>"><span id="invitedGroup"></span> group</a>
-        </td>
-    </tr>
-
-    <tr id="inviteLicenses" style="display: none;">
-        <td width="15%" valign="top" style="padding-top: 10px;">Warning: </td>
-        <td width="85%" valign="top" style="padding-top: 10px;">
-            The number of users invited now exceeds your available KnowledgeTree licenses.<br />
-            Please <a href="<?= $contact_url; ?>">contact us</a> to purchase additional licenses.
-        </td>
-    </tr>
-
+    
     <tr id="showNoPerms" style="display: none;">
         <td colspan="2" valign="top" style="padding-top: 10px;">
-            There are users that do not have permissions:
+            There are users that do not have permissions to see the content you have shared:
             <ul id="noPerms" style="padding-left: 30px;"></ul>
         </td>
     </tr>
