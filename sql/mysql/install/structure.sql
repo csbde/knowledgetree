344a5bff3aca2b72a7344cc98801539b61c2c688
Megan Watson <megan@knowledgetree.com>
2011-01-10 10:07:21 +0200

    Permissions Caching: Added permissions cache table to sql install files.
    Committed by: Megan Watson

--- sql/mysql/install/structure.sql
+++ sql/mysql/install/structure.sql
@@ -1225,6 +1225,16 @@ CREATE TABLE `permission_dynamic_conditions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Table structure for table `permission_fast_cache`
+--
+
+create table `permission_fast_cache` (
+    `user_id` INT(11) NOT NULL,
+    `descriptor_id` INT(11) NOT NULL,
+    KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
 -- Table structure for table `permission_lookup_assignments`
 --
 
