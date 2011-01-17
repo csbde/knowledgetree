8ddfaeb354b9fa6bb73841c2ca15c8d698101079
Jarrett Jordaan <jarrett@knowledgetree.com>
2011-01-17 12:13:24 +0200

    Timezones_PT6370357: Removed rounding off of offset in date. Updated serialized object in timezone config setting.
    Committed by: Jarrett Jordaan
    
    Reviewed by:

--- sql/mysql/upgrade/3.7.1/timezone_settings.sql
+++ sql/mysql/upgrade/3.7.1/timezone_settings.sql
@@ -6,7 +6,7 @@ UPDATE config_groups SET category="System Settings" WHERE category="General Sett
 UPDATE config_groups SET category="General Settings" WHERE name="tweaks";
 UPDATE config_groups SET category="General Settings" WHERE name="session";
 
-ALTER TABLE  `config_settings` CHANGE  `type`  `type` ENUM(  'boolean',  'string',  'numeric_string',  'numeric',  'radio',  'dropdown',  'class' ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT  'string';
+ALTER TABLE  `config_settings` CHANGE  `type`  `type` ENUM(  'boolean',  'string',  'numeric_string',  'numeric',  'radio',  'dropdown',  'class' ) DEFAULT 'string';
 
 INSERT INTO `config_settings` (`group_name`, `display_name`, `description`, `item`, `value`, `default_value`, `type`, `options`, `can_edit`) VALUES
-('tweaks', 'Set Timezone', 'Defines your timezone you are in.', 'setTimezone', 'default', 'UTC', 'class', 'a:2:{s:4:"file";s:29:"lib/datetime/datetimeutil.inc.php";s:5:"class";s:12:"datetimeutil";}', 1);
+('tweaks', 'Set Timezone', 'Defines your timezone you are in.', 'setTimezone', 'default', 'UTC', 'class', 'a:2:{s:4:"file";s:33:"lib/datetime/datetimeutil.inc.php";s:5:"class";s:12:"datetimeutil";}', 1);
