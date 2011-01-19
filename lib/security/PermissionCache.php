550816ce5ddf1b1ea39ee90aeffb1ec10b9f91a3
Jarrett Jordaan <jarrett@knowledgetree.com>
2011-01-19 16:32:00 +0200

    Changes needed to get commercial staging up and running.
    Committed by: Jarrett Jordaan
    
    Reviewed by:

--- lib/security/PermissionCache.php
+++ lib/security/PermissionCache.php
@@ -54,7 +54,7 @@ class PermissionCache
      * @access private
      * @var PermissionMemCache
      */
-    private $memcache;
+    private $memcache = false;
 
     /**
      * The name of the cache table
@@ -741,9 +741,13 @@ class PermissionMemCache
      */
     private function initMemcache()
     {
-        if(MemCacheUtil::$enabled){
-            return true;
-        }
+    	if(class_exists('MemCacheUtil'))
+    	{
+	        if(MemCacheUtil::$enabled)
+	        {
+	            return true;
+	        }
+    	}
 
         $oConfig = KTConfig::getSingleton();
         $enabled = $oConfig->setMemcache();
