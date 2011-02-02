2d642482bdf90c2c1443bc7cd921d5f67ae2800e
Megan Watson <megan@knowledgetree.com>
2011-02-02 11:00:03 +0200

    Updated the version to 3.7.1.0
    Committed by: Megan Watson
    Reviewed by: Martin Kirsten

--- sql/mysql/upgrade/3.7.1.0/folder_changes.sql
+++ sql/mysql/upgrade/3.7.1.0/folder_changes.sql
@@ -1,3 +1,3 @@
-ALTER TABLE document_transactions ADD COLUMN parent_id INTEGER;
+ALTER TABLE document_transactions ADD COLUMN parent_id INT(11);
 
-ALTER TABLE folder_transactions ADD COLUMN parent_id INTEGER;
+ALTER TABLE folder_transactions ADD COLUMN parent_id INT(11);
