8f092a280a25e3f894de5fb26486f640e42b9842
Paul Barrett <paul@knowledgetree.com>
2011-02-11 16:16:53 +0200

    Fix duplicated function issue: quick and dirty fix using function_exists.
    Story/Branch/Issue: webcomms_refactor
    
    Committed by: Paul Barrett

--- templates/fragments/upload/upload.doctypes.php
+++ templates/fragments/upload/upload.doctypes.php
@@ -1,22 +1,27 @@
 <?php
+
 include_once('../../ktapi/ktapi.inc.php');
 
 $KT = new KTAPI(3);
 //$KT->get(3);// Set it to Use Web Version 3
 
 //Pick up the session
-$session=KTAPI_UserSession::getCurrentBrowserSession($KT);
+$session = KTAPI_UserSession::getCurrentBrowserSession($KT);
 $KT->start_system_session($session->user->getUserName());
 
-function getDocTypes(){
-	$types=DocumentType::getList();
-	$ret=array();
-	foreach($types as $type){
-		$ret[$type->aFieldArr['id']]=$type->aFieldArr;
-	}
-	return $ret;
-}
+if (!function_exists('getDocTypes')) {
+    function getDocTypes()
+    {
+        $types = DocumentType::getList();
 
+        $ret = array();
+        foreach ($types as $type) {
+            $ret[$type->aFieldArr['id']] = $type->aFieldArr;
+        }
+
+        return $ret;
+    }
+}
 
 ?>
     <div id="uploaddialog">
@@ -28,8 +33,8 @@ function getDocTypes(){
         			<select class="ul_doctype" onChange="kt.app.upload.changeGlobalDoctype(this.options[this.selectedIndex].value);">
         				<?php
         				  $docTypes=getDocTypes();
-        				  foreach($docTypes as $docTypeId => $docType):
-        				   if(!$docType['disabled']):
+        				  foreach ($docTypes as $docTypeId => $docType) :
+        				   if (!$docType['disabled']) :
         				?>
         				<option value="<?php echo $docTypeId; ?>" ><?php echo $docType['name']; ?></option>
 
--- templates/fragments/upload/upload.metadata.dialog.php
+++ templates/fragments/upload/upload.metadata.dialog.php
@@ -1,33 +1,37 @@
 <?php
+
 include_once('../../ktapi/ktapi.inc.php');
 
 $KT = new KTAPI(3);
 //$KT->get(3);// Set it to Use Web Version 3
 
 //Pick up the session
-$session=KTAPI_UserSession::getCurrentBrowserSession($KT);
+$session = KTAPI_UserSession::getCurrentBrowserSession($KT);
 $KT->start_system_session($session->user->getUserName());
 
-function getDocTypes(){
-	$types=DocumentType::getList();
-	$ret=array();
-	foreach($types as $type){
-		$ret[$type->aFieldArr['id']]=$type->aFieldArr;
-	}
-	return $ret;
+if (!function_exists('getDocTypes')) {
+    function getDocTypes()
+    {
+        $types = DocumentType::getList();
+
+        $ret = array();
+        foreach ($types as $type) {
+            $ret[$type->aFieldArr['id']] = $type->aFieldArr;
+        }
+
+        return $ret;
+    }
 }
 
-
 ?>
-
     <div>
        <table class="metadataTable" border="0" cellspacing="0" cellpadding="0">
        	<tr><td class="ul_meta_selectDocType">Select Document Type<span class="ul_meta_docTypeOptions">
 	        <select class="ul_doctype" onChange="kt.app.upload.getMetaItem(this).changeDocType(this.options[this.selectedIndex].value);">
 	        	<?php
-	        	  $docTypes=getDocTypes();
-	        	  foreach($docTypes as $docTypeId => $docType):
-	        	   if(!$docType['disabled']):
+	        	  $docTypes = getDocTypes();
+	        	  foreach ($docTypes as $docTypeId => $docType) :
+	        	      if (!$docType['disabled']) :
 	        	?>
 	        	<option value="<?php echo $docTypeId; ?>" ><?php echo $docType['name']; ?></option>
 
