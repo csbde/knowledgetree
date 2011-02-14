04e5c9ccf5db974a539bb8953cf4974c42eaf38e
Jarrett Jordaan <jarrett@knowledgetree.com>
2011-02-14 13:10:34 +0200

    KT-377: Zoho edits failing for shared user. Updated commercial, ktlive reference.
    Committed by: Jarrett Jordaan
    
    Reviewed by:

--- ktapi/ktapi.inc.php
+++ ktapi/ktapi.inc.php
@@ -66,6 +66,9 @@ require_once(KTAPI_DIR .'/KTAPIBulkActions.inc.php');
 require_once(KTAPI_DIR .'/KTAPITrigger.inc.php');
 require_once(KTAPI_DIR .'/KTAPIConditionalMetadata.inc.php');
 
+require_once(KT_LIB_DIR . '/users/shareduserutil.inc.php');
+require_once(KT_LIB_DIR . '/render_helpers/sharedContent.inc');
+
 /**
 * This class defines functions that MUST exist in the inheriting class
 *
@@ -612,7 +615,27 @@ class KTAPI
 			$error = $user;
 			return $error;
 		}
-
+		
+    	if(SharedUserUtil::isSharedUser())
+    	{
+    		if($object instanceof DocumentProxy || $object instanceof Document)
+    		{
+    			if(!SharedContent::canAccessDocument($user->getID(), $object->getID()))
+    			{
+    				return new PEAR_Error(KTAPI_ERROR_INSUFFICIENT_PERMISSIONS);
+    			}
+    		}
+    		elseif ($object instanceof FolderProxy || $object instanceof Folder)
+    		{
+    			if(!SharedContent::canAccessFolder($user->getID(), $object->getID()))
+    			{
+    				return new PEAR_Error(KTAPI_ERROR_INSUFFICIENT_PERMISSIONS);
+    			}
+    		}
+    		
+    		return $user;
+    	}
+    	
 		if (!KTPermissionUtil::userHasPermissionOnItem($user, $permission, $object))
 		{
 			$error = new PEAR_Error(KTAPI_ERROR_INSUFFICIENT_PERMISSIONS);
