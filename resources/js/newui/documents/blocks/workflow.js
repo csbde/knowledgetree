53a890d18e059d0e602d3be8cedb4399a179d89e
Jarrett Jordaan <jarrett@knowledgetree.com>
2011-03-29 12:20:38 +0200

    newui_Subscriptions_PT_984040: Added tooltip. JS cleanup.
    Committed by: Jarrett Jordaan
    
    Reviewed by:

--- resources/js/newui/documents/blocks/workflow.js
+++ resources/js/newui/documents/blocks/workflow.js
@@ -3,12 +3,12 @@ function workflowActions(){}
 /* Submit an ajax request */
 workflowActions.prototype.submitForm = function() {
 	var address = jQuery('[name|="start_workflow_form"]').attr('action');
-	address = address + '&method=ajax'
+	address = address + '&method=ajax';
 	jQuery.ajax({ url: address,	dataType: "html", type: "POST", cache: false, success: 
 					function(data) {
 						return data;
 					}
 	});
-}
+};
 
-var workflow = new workflowActions();
+workflow = new workflowActions();
