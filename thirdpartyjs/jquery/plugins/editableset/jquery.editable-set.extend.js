4b6e97bc787bc69da9795820cadd301b0b4187cd
Paul Barrett <paul@knowledgetree.com>
2011-04-11 09:58:46 +0200

    Fix loading of existing tags in new inline edit mode.
    Ref: Inline_Editing_PT_9382905
    
    Committed by: Paul Barrett

--- thirdpartyjs/jquery/plugins/editableset/jquery.editable-set.extend.js
+++ thirdpartyjs/jquery/plugins/editableset/jquery.editable-set.extend.js
@@ -1,15 +1,15 @@
 jQuery.editableSet.addInputType('multiselect', {
 	/* create input element */
-	element : function(object, attrs) {		
+	element : function(object, attrs) {
 		var val = '';
-		
+
 		if (attrs['data-value-id'] != null)
 		{
 			val = jQuery('#'+attrs['data-value-id']).text();
 			//hide the 'value' span
 			jQuery('#'+attrs['data-value-id']).hide();
 		}
-				
+
 		var dataOptions = attrs['data-options'];
 		//need to check whether we need to chop off trailing ','
 		var lastIndexOfComma = attrs['data-options'].lastIndexOf(',');
@@ -17,47 +17,47 @@ jQuery.editableSet.addInputType('multiselect', {
 		{
 			dataOptions = attrs['data-options'].slice(0, lastIndexOfComma)+']';
 		}
-		
+
 		var options = JSON.parse(dataOptions);
-		
+
 		//strip all whitespace!
 		var selectedValue = val;	//jQuery.trim(attrs.value);
-		
+
 		// Clean up the attributes
 		delete attrs['data-type'];
 		delete attrs.value;
 		delete attrs['data-options'];
-		
+
 		// Pull into its own object so that we can add +option+s
 		var newObject = jQuery.fn.editableSet.attributor( jQuery('<select multiple/>'), attrs );
-				
+
 		// Wrap in closure to manage scope
 		(function() {
 		var option;
 		for( option in options ) {
 			// Extract the values and texts appropriately
 			var selectTextAndValue = jQuery.fn.editableSet.extractTextAndValue( options, option );
-			
+
 			if(selectTextAndValue.value != 'undefined' || selectTextAndValue.text != 'undefined') {
 				jQuery('<option />', {
 				value : selectTextAndValue.value,
 				text  : selectTextAndValue.text
 				}).appendTo( newObject );
 			}
-		}            
+		}
 		})();
-		
+
 		//now select the selected (jQuery NOT working!)
 	 /* for (var idx = 0; idx < newObject[0].options.length; idx++) {
 		if (newObject[0].options[idx].text == selectedValue) {
 			newObject[0].selectedIndex = idx;
 		}
 		}*/
-		
+
 		jQuery(object).replaceWith( newObject );
-		
+
 		var selectedValues = selectedValue.split(',');
-		
+
 		// Apply the +selected+ attribute;
 		 newObject.val(selectedValues);
 	}
@@ -65,8 +65,8 @@ jQuery.editableSet.addInputType('multiselect', {
 
 jQuery.editableSet.addInputType('tree', {
 	/* create tree with radio input elements */
-	element : function(object, attrs) {		
-		var val = '';	
+	element : function(object, attrs) {
+		var val = '';
 		if (attrs['data-value-id'] != null)
 		{
 			val = jQuery('#'+attrs['data-value-id']).text();
@@ -77,46 +77,46 @@ jQuery.editableSet.addInputType('tree', {
 		{
 			val = jQuery.trim($('span#'+attrs['data-name']).text());
 		}
-				
+
 		var options = JSON.parse(attrs['data-options']);
-		
+
 		var html = buildTree(attrs['data-name'], options, '');
-		
+
 		html = '<ul class="kt_treenodes">'+html+'</ul>';
-		
+
 		var newObject = jQuery(html);
-		
+
 		jQuery(object).replaceWith(newObject);
-		
+
 		//select the appropriate radio button!
 		jQuery('input:radio[name="'+attrs['data-name']+'"]').filter('[value="'+val+'"]').attr('checked', true);
 	}
-	
+
 });
 
 function buildTree(fieldid, data, html)
-{	
+{
 	if(data.type == 'tree')
-	{		
+	{
 		if (data.treename.toLowerCase() != 'root')
 		{
 			html += '<li class="treenode inactive"><a onclick="toggleElementClass(\'active\', this.parentNode);toggleElementClass(\'inactive\', this.parentNode);" class="pathnode">'+data.treename+'</a>';	//'</ul><ul>'+html;	//+'</ul>';
 		}
-		
+
 		html += '<ul>';
-		
+
 		jQuery.each(data.fields, function(index, value)
 		{
 			html = buildTree(fieldid, value, html);
 		});
-		
+
 		html += '</ul>';
 	}
 	else if (data.type == 'field')
 	{
 		html += '<li class="leafnode"><input type="radio" value="'+data.name+'" name="'+fieldid+'"/>'+data.name;	//span class="descriptiveText" data-name="'+fieldid+'" data-value-id="value-'+fieldid+'" data-options=\'['+value+']\'/></li>';
 	}
-	
+
 	return html;
 };
 
@@ -124,7 +124,7 @@ jQuery.editableSet.addInputType('datepicker', {
 	 /* create input element */
 	element : function(object, attrs, self) {
 		//console.dir(opts);
-		var val = '';	
+		var val = '';
 		if (attrs['data-value-id'] != null)
 		{
 			val = jQuery('#'+attrs['data-value-id']).text();
@@ -135,7 +135,7 @@ jQuery.editableSet.addInputType('datepicker', {
 		{
 			val = jQuery.trim($('span#'+attrs['data-name']).text());
 		}
-		
+
 		var datePicker = new Ext.form.DateField({
 	    	format: 'Y-m-d', //YYYY-MMM-DD
 	        width: 100,
@@ -159,17 +159,17 @@ jQuery.editableSet.addInputType('datepicker', {
 				},
 				/*'valid': function(dateField) {
 					console.log('valid date is '+dateField.getValue());
-					if (!dateField.getValue()) 
+					if (!dateField.getValue())
 					{
 						console.log('false valid date');
-					} 
-					else 
+					}
+					else
 					{
 					}
 				},*/
 				'invalid': function(dateField) {
 					//console.log('invalid date is '+dateField.getValue());
-					
+
 					// beforeLoad callback
 					//jQuery.isFunction(opts.onInvalid) && opts.onInvalid.call(self);
 					//self.invalid.push('datePicker');
@@ -182,27 +182,27 @@ jQuery.editableSet.addInputType('datepicker', {
 				}/*,
 				'change': function(dateField, date) {
 					console.log('change date is '+dateField.getValue());
-					if (!dateField.getValue()) 
+					if (!dateField.getValue())
 					{
 						console.log('false change date');
-					} 
-					else 
+					}
+					else
 					{
 					}
 				}*/
 	    	}
    		});
-   	
+
    		jQuery(object).replaceWith(jQuery('<span id="ph_'+attrs['data-name']+'"/>'));
-   	
+
    		datePicker.render('ph_'+attrs['data-name']);
 	}
 });
 
 jQuery.editableSet.addInputType('htmleditor', {
 	 /* create input element */
-	element : function(object, attrs) {	
-		var val = '';	
+	element : function(object, attrs) {
+		var val = '';
 		if (attrs['data-value-id'] != null)
 		{
 			val = jQuery('#'+attrs['data-value-id']).text();
@@ -213,10 +213,10 @@ jQuery.editableSet.addInputType('htmleditor', {
 		{
 			val = jQuery.trim($('span#'+attrs['data-name']).text());
 		}
-		
+
 		var htmlEd = new Ext.form.HtmlEditor({
 	        width: 210,
-	        height: 160, 
+	        height: 160,
 	        id: attrs['data-name'],
 	        //cls: 'ul_meta_fullField ul_meta_field_[id]',
 	        autoscroll: true,
@@ -241,7 +241,7 @@ jQuery.editableSet.addInputType('htmleditor', {
 				},*/
 	            /*'sync': function(editor, text){
 	            	console.log('sync '+text);
-	            	
+
 	            	var trimmed = text.replace(/(<br>)|&nbsp;/g, '').trim();
 	            	console.log('sync '+trimmed);
 	            	var maxSize = parseInt(attrs['data-maxsize']);
@@ -250,15 +250,15 @@ jQuery.editableSet.addInputType('htmleditor', {
 	            		console.log('max reached');
 	            		return false;
 	            	}
-	            	
-	            	
+
+
 			    	//kt.app.upload.getMetaItem(jQuery('#ul_meta_field_htmlEditor_[id]')).setMetaData('[id]', text);
-	
+
 					//ensure that not blank text
 	    			if([is_mandatory] == '1') {
 						//remove <br> and &nbsp;
 	    				var trimmed = text.replace(/(<br>)|&nbsp;/g, '').trim();
-	
+
 						if(requiredDone && trimmed.length == 0) {
 							requiredDone = false;
 							kt.app.upload.getMetaItem(jQuery('#ul_meta_field_htmlEditor_[id]')).registerRequiredFieldNotDone('ul_meta_field_[id]');
@@ -270,11 +270,11 @@ jQuery.editableSet.addInputType('htmleditor', {
 				}*/
 	    	}
 	    });
-	   	
+
 	   	jQuery(object).replaceWith(jQuery('<span id="ph_'+attrs['data-name']+'"/>'));
-	   	
+
 	   	htmlEd.render('ph_'+attrs['data-name']);
-	   	
+
 	   	/*if (attrs['data-maxsize'] != null)
 		{
 			var maxSize = '';
@@ -282,13 +282,13 @@ jQuery.editableSet.addInputType('htmleditor', {
 			{
 				console.log('htmleditor trying for max size');
 				maxSize = parseInt(attrs['data-maxsize']);
-				
+
 				console.log('maxSize '+maxSize);
-				
+
 				newObject.data['maxsize'] = parseInt(maxSize); //max character limit
-				
+
 				console.dir(newObject);
-				
+
 				newObject.unbind('keypress.restrict').bind('keypress.restrict', function(e){
 					restrict(newObject, e);
 				});
@@ -303,12 +303,12 @@ jQuery.editableSet.addInputType('tokeninput', {
 	/* create input element */
 	element : function(object, attrs) {
 		var newObject = jQuery.fn.editableSet.attributor( jQuery('<input />'), attrs );
-		
+
 		jQuery(object).replaceWith( newObject );
-		
-		var tags = attrs['data-tags'];
+
+		var tags = JSON.parse(attrs['data-tags']);
 		var tagScript = attrs['data-tag-script'];
-		
+
 		jQuery(newObject).tokenInput(tagScript, {
 	        // Alter the minChars value to determine how much the user must type before a search is initiated
 	        minChars: 2,
