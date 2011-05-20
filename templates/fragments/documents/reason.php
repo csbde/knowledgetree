5fa053250448000c5cd87baba35030f689ab57a5
Megan Watson <megan@knowledgetree.com>
2011-05-20 06:40:18 +0200

    Merging Electronic Signature changes into Checkout Modal branch
    Copy / Move Modal
    
    Updated electronic signatures, adjusted behaviour to be more generic.
    
    Committed by: Megan Watson
    Conflicts:
    
    	resources/js/newui/kt.api.js

--- templates/fragments/documents/reason.php
+++ templates/fragments/documents/reason.php
@@ -4,32 +4,41 @@
 <table class="uploadTable" style="height:100px;">
 	<tbody>
 		<tr id="error" style='display:none;'>
-			<td colspan=2> <p class="errorMessage"></p> </td>
+			<td> <p class="errorMessage"></p> </td>
 		</tr>
 		<tr>
 			<td>
-		        <span for="reason" name="reason_label"></span>
+				<p id="esign-info" style='display: none'>
+					An electronic signature is required in order to continue, please enter your login details below.
+				</p>
+				
+				<p id="reason-info">
+					A reason is required in order to continue, please enter one below.
+				</p>
 			</td>
 		</tr>
-		<tr id="user"  style='display:none;'>
-			<td> <label for="username">Email Address</label> </td>
-	    	<td align="right"> <input name="sign_username"> </td>
+		<tr id="user" style='display:none;'>
+			<td> 
+				<label for="username">Email Address</label>  <br />
+				<input name="sign-username" size="30"> 
+			</td>
 		</tr>
-		<tr id="pass"  style='display:none;'>
-			<td> <label for="password">Password</label> </td>
-	    	<td align="right"> <input name="sign_password"> </td>
+		<tr id="pass" style='display:none;'>
+			<td> 
+				<label for="password">Password</label> <br />
+				<input type="password" name="sign-password" size="30"> 
+			</td>
 		</tr>
 		<tr id=reason>
 			<td>
-				<br/>
-				<span for="reason" name="reason_field"></span>
+				<label for="reason" name="reason-field" id="reason-label">Note</label> <br />
 				<textarea cols="50" rows="5" name="reason"></textarea>
 		    </td>
 		</tr>
 		<tr>
 	    	<td align="right" valign="top" class="ul_actions">
 				<a href="#" onclick="vActions.closeDisplay('reason'); return false;" class="ul_actions_cancel_link">Cancel</a>
-				<input name="reason_submit" type="button" onclick="kt.app.document_actions.submit_reason();" value="Submit" class="ul_actions_btns" id="ul_actions_upload_btn">
+				<input name="reason_submit" type="button" onclick="kt.api.esignatures.saveESignatures();" value="Submit" class="ul_actions_btns" id="ul_actions_upload_btn">
 	    	</td>
 		</tr>
 	</tbody>
