ed3d5ac8f9f711baee2d7c26c36eac70128b9f5c
Tohir Solomons <tohir@knowledgetree.com>
2011-06-13 16:39:49 +0200

    First stab at getting menu to reposition on document details page
    Committed by: Tohir Solomons

--- resources/js/newui/documents/kt.app.buttons.js
+++ resources/js/newui/documents/kt.app.buttons.js
@@ -1,4 +1,52 @@
 jQuery(function() {
+	
+	jQuery(window.parent.document).scroll(function(){
+		jQuery('ul.splitmenu:visible').each(function(index) {
+			updateSplitButtonMenu(this);
+		});
+	});
+	
+	function updateSplitButtonMenu(element)
+	{
+		// Chrome/Safari uses body, IE/Firefox uses HTML for scrolling offset
+		if (jQuery("body").scrollTop() > jQuery("html").scrollTop()) {
+			scrollElement = "body";
+		} else {
+			scrollElement = "html";
+		}
+		
+		jQuery(element).css({'top': (jQuery(element).parent().prev().height())+'px', 'margin-left':0, 'margin-top':0, 'position':'absolute', 'margin-top':0});
+		
+		heightOffset = jQuery(element).parent().offset().top +  jQuery(element).height() + jQuery(element).prev().height();
+		
+		windowAndScrollOffset = jQuery(window).height()+jQuery(scrollElement).scrollTop();
+		
+		/*
+		console.log('Height+Offset: '+windowAndScrollOffset);
+		console.log('Item: '+(jQuery(this).parent().next().height()+5));
+		
+		console.log('scroll: '+jQuery(scrollElement).scrollTop());
+		console.dir(jQuery(this).parent().offset());
+		console.dir(jQuery(this).parent().position());
+		
+		console.log('heightOffset: '+heightOffset);
+		console.log('windowAndScrollOffset: '+windowAndScrollOffset);
+		*/
+		
+		if (heightOffset > windowAndScrollOffset) {
+			//console.log('need to move up');
+			diff = heightOffset - windowAndScrollOffset;
+			
+			//console.log('Moving Up: -'+(diff+jQuery(element).prev().height()));
+			
+			// Move item up by difference + 20px
+			jQuery(element).css('margin-top', '-'+(diff+jQuery(element).prev().height())+'px').css('margin-left', jQuery(element).prev().width()+'px');
+		}
+		
+		//jQuery('body').append("<div style='position:absolute; width: 40px; top: 0; left: 0; border: 1px solid red; height: "+heightOffset+"px'></div>");
+	}
+
+	
 	jQuery(".action").hover(
 			function () {
 			jQuery(this).children().addClass('hover');
@@ -19,6 +67,8 @@ jQuery(function() {
 			jQuery(".splitmenu").hide();
 			jQuery(".split").removeClass('selected');
 			jQuery(this).addClass('selected').parent().next().show();
+			
+			updateSplitButtonMenu(jQuery(this).addClass('selected').parent().next());
 		}
 	});
 
