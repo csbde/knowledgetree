fd486c7888410dfec352c9ce94cf2b280025d507
Paul Barrett <paul@knowledgetree.com>
2011-06-15 10:42:21 +0200

    Update auth page and php-saml library for vulnerability patch.
    Ref: Onelogin
    
    Committed by: Paul Barrett

--- thirdparty/php-saml/lib/onelogin/saml/xmlsec.php
+++ thirdparty/php-saml/lib/onelogin/saml/xmlsec.php
@@ -1,18 +1,70 @@
 <?php
+
   require(dirname(__FILE__) . '/../../xmlseclibs/xmlseclibs.php');
 
-  class XmlSec {
-    public $x509certificate;
-    private $doc;
-    
-    function __construct($val) {
-      $this->doc = $val;
+  /**
+   * Determine if the SAML response is valid using a provided x509 certificate.
+   */
+  class SamlXmlSec {
+    /**
+     * A SamlResponse class provided to the constructor.
+     */
+    private $settings;
+
+    /**
+     * The documentument to be tested.
+     */
+    private $document;
+
+    /**
+     * Construct the SamlXmlSec object.
+     *
+     * @param SamlResponse $settings
+     *   A SamlResponse settings object containing the necessary
+     *   x509 certicate to test the document.
+     * @param string $document
+     *   The document to test.
+     */
+    function __construct($settings, $document) {
+      $this->settings = $settings;
+      $this->document = $document;
     }
+
+
+    /**
+     * Determine if the document passes the security test.
+     *
+     * @return
+     *   TRUE if the document passes. This could throw a generic Exception
+     *   if the document or key cannot be found.
+     */
     
+    function validateNumAssertions(){
+      $rootNode = $this->document; //->documentElement->ownerDocument;
+      $assertionNodes = $rootNode->getElementsByTagName('Assertion');
+      return ($assertionNodes->length == 1);
+    }
+
+    function validateTimestamps(){
+      $rootNode = $this->document;
+      $timestampNodes = $rootNode->getElementsByTagName('Conditions');
+      for($i=0;$i<$timestampNodes->length;$i++){
+        $nbAttribute = $timestampNodes->item($i)->attributes->getNamedItem("NotBefore");
+        $naAttribute = $timestampNodes->item($i)->attributes->getNamedItem("NotOnOrAfter");
+        if($nbAttribute && strtotime($nbAttribute->textContent) > time()){
+            return false;
+        }
+        if($naAttribute && strtotime($naAttribute->textContent) <= time()){
+            return false;
+        }
+      }
+      return true;
+    }
+ 
     function is_valid() {
     	$objXMLSecDSig = new XMLSecurityDSig();
 
-    	$objDSig = $objXMLSecDSig->locateSignature($this->doc);
+    	$objDSig = $objXMLSecDSig->locateSignature($this->document);
     	if (! $objDSig) {
     		throw new Exception("Cannot locate Signature Node");
     	}
@@ -20,7 +72,6 @@
     	$objXMLSecDSig->idKeys = array('ID');
 
     	$retVal = $objXMLSecDSig->validateReference();
-
     	if (! $retVal) {
     		throw new Exception("Reference Validation Failed");
     	}
@@ -31,13 +82,24 @@
     	}
     	$key = NULL;
 
+    	$singleAssertion = $this->validateNumAssertions();
+      if (!$singleAssertion){
+        throw new Exception("Only one SAMLAssertion allowed");
+      }
+
+      $validTimestamps = $this->validateTimestamps();
+      if (!$validTimestamps){
+        throw new Exception("SAMLAssertion conditions not met");
+      }
+
     	$objKeyInfo = XMLSecEnc::staticLocateKeyInfo($objKey, $objDSig);
 
-      $objKey->loadKey($this->x509certificate, FALSE, true);
-      
+      $objKey->loadKey($this->settings->x509certificate, FALSE, true);
+
     	$result = $objXMLSecDSig->verify($objKey);
     	return $result;
     }
-  }
+
+ }
 
 ?>
