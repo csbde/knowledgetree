04cf1c39c4be068cca732ae8c82eead1db5c1cee
Jarrett Jordaan <jarrett@knowledgetree.com>
2011-07-25 17:11:21 +0200

    queueActions_PT_14877635: Add new skeleton browse views.
    Committed by: Jarrett Jordaan
    
    Reviewed by:

---
+++ lib/render_helpers/BulkActionBrowse.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(KT_LIB_DIR . '/render_helpers/BrowseView.inc.php');
+
+/**
+ * Default user browse view class.
+ * Does not differ from regular browse view, so nothing here.  Kind of silly but does allow a nicer name.
+ */
+class BulkActionBrowse extends BrowseView {
+
+}
+
+?>
